1c71ecd89caa6360a5a546625bd03388fa8fb98c
Luke Batchelor <lbatchelor@atlassian.com>
2016-05-16 14:12:57 +1000

    Added less loading

--- packages/sean-button/index.js
+++ packages/sean-button/index.js
@@ -1,5 +1,7 @@
-import './src/css/style.css'; //import our css using es6 imports
+import './src/less/style.less'; //import our css using es6 imports
 import seanButton from './src/js/sean-button.js';
 
 var body = document.querySelector('body');
 body.textContent = 'I can call es6 code: ' + seanButton();
+
+body.innerHTML += '<br><br><span>This should be styled from less!</span>'

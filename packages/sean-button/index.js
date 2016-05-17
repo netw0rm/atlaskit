11e1a680d663e6f7278c89676040059c68dfabb8
Luke Batchelor <lbatchelor@atlassian.com>
2016-05-17 10:29:39 +1000

    More code for sean-button component to use other helper functions

--- packages/sean-button/index.js
+++ packages/sean-button/index.js
@@ -1,7 +1,11 @@
 import './src/less/style.less'; //import our css using es6 imports
-import seanButton from './src/js/sean-button.js';
+import seanButton, {subtract} from './src/js/sean-button.js';
 
-var body = document.querySelector('body');
-body.textContent = 'I can call es6 code: ' + seanButton();
+export default function(){
+	var body = document.querySelector('body');
 
-body.innerHTML += '<br><br><span>This should be styled from less!</span>'
+	var button = seanButton();
+	body.appendChild(button);
+
+	body.innerHTML += ' <--This is a Sean-Button<br><br><span class="blue">This should be styled from less!</span>' + subtract(2, 1);
+}

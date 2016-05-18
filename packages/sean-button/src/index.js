0f5953f1c5b801eccf4d32a9b1ba8c46bba9bded
Luke Batchelor <lbatchelor@atlassian.com>
2016-05-18 14:24:21 +1000

    Added some functionality to sean-button

--- packages/sean-button/src/index.js
+++ packages/sean-button/src/index.js
@@ -2,11 +2,11 @@ import './style.less'; // import our css using es6 imports
 import add from './add';
 
 // create a 'sean-button'
-export default () => {
+export default (a, b) => {
   const theButton = document.createElement('span');
 
   theButton.classList.add('sean-button');
-  theButton.innerHTML = `1 + 4 = ${add(1, 4)}`;
+  theButton.innerHTML = `${a} + ${b} = ${add(a, b)}`;
 
   return theButton;
 };

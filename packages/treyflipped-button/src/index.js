793f8813648051bb789685b22f3ca1617396dd9d
Trey Shugart <treshugart@gmail.com>
2016-05-23 17:32:32 +1000

    chore: Get working with native / non-native shadow dom styling.

--- packages/treyflipped-button/src/index.js
+++ packages/treyflipped-button/src/index.js
@@ -1,11 +1,26 @@
-import styles from './style.less'; // eslint-disable-line no-unused-vars
-import {skate, vdom} from 'skatejs';
+import styles, { locals } from './style.less'; // eslint-disable-line no-unused-vars
+import { skate, vdom } from 'skatejs';
+
+const div = document.createElement('div');
+const hasShadowDom = div.attachShadow || div.createShadowRoot;
+
+function style (styles) {
+  if (hasShadowDom) {
+    vdom.style(styles.toString());
+  } else if (!styles.isInHead) {
+    const style = document.createElement('style');
+    style.textContent = styles.toString();
+    document.head.appendChild(style);
+    styles.isInHead = true;
+  }
+}
 
 export default skate('x-hello', {
   properties: {
     name: { attribute: true }
   },
-  render (elem) {
-    vdom.text(`BYE!!, ${elem.name}`);
+  render () {
+    style(styles);
+    vdom.div({ class: locals.testing }, 'testing');
   }
 });

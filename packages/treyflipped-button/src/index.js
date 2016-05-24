16f13f88c67e91ee17a8db83839a39028207b46e
Trey Shugart <treshugart@gmail.com>
2016-05-24 15:45:01 +1000

    chore: Start spking how we might use jss on a per-componnet basis.

--- packages/treyflipped-button/src/index.js
+++ packages/treyflipped-button/src/index.js
@@ -1,26 +1,94 @@
-import styles, { locals } from './style.less'; // eslint-disable-line no-unused-vars
-import { skate, vdom } from 'skatejs';
+// import styles from './index.less';
+import { skate, state, prop, vdom } from 'skatejs';
+import jss from 'jss';
+import jssNested from 'jss-nested';
 
-const div = document.createElement('div');
-const hasShadowDom = div.attachShadow || div.createShadowRoot;
+jss.use(jssNested());
 
-function style (styles) {
-  if (hasShadowDom) {
-    vdom.style(styles.toString());
-  } else if (!styles.isInHead) {
-    const style = document.createElement('style');
-    style.textContent = styles.toString();
-    document.head.appendChild(style);
-    styles.isInHead = true;
-  }
+function styles (fn) {
+  return function (props) {
+    const idom = vdom.IncrementalDOM;
+    let currentElement;
+
+    vdom.span(function () {
+      currentElement = idom.currentElement();
+      const userStyle = fn(props);
+      const cache = currentElement.__cache || (currentElement.__cache = currentElement.__cache = jss.createStyleSheet(userStyle, { link: true }));
+      const userStyleKeys = Object.keys(userStyle);
+
+      // patch rules
+      userStyleKeys.forEach(function (userStyleKey) {
+        const existingRule = cache.getRule(userStyleKey);
+        const newRule = userStyle[userStyleKey];
+        const newRuleKeys = Object.keys(newRule);
+
+        if (existingRule) {
+          const existingRuleKeys = Object.keys(existingRule.style);
+
+          // Remove all existing properties that aren't in the new properties.
+          existingRuleKeys.forEach(function (existingRuleKey) {
+            if (newRuleKeys.indexOf(existingRuleKey) === -1) {
+              existingRule.prop(existingRuleKey, null);
+            }
+          });
+
+          // Set all new properties.
+          newRuleKeys.forEach(function (newRuleKey) {
+            existingRule.prop(newRuleKey, newRule[newRuleKey]);
+          });
+        } else {
+          cache.addRule(userStyleKey, newRule);
+        }
+      });
+
+      if (!currentElement.hasChildNodes()) {
+        cache.renderer.head = currentElement;
+        cache.renderer.attach();
+      }
+      cache.deploy();
+      idom.skip();
+    });
+
+    return currentElement.__cache.classes;
+  };
 }
+
+const css = styles(function (props) {
+  return {
+    testing: {
+      'transition': `transform ${3 * props.speed}ms`,
+      'transform-origin': 'left',
+      '&:hover': {
+        'transform': `scale(${props.amount || '1'})`
+      }
+    }
+  };
+});
+
+skate('motion-pulse', {
+  properties: {
+    amount: prop.number(),
+    speed: prop.number()
+  },
+  render (elem) {
+    const classes = css(state(elem));
+    vdom.div({ class: classes.testing }, function () {
+      vdom('slot');
+    });
+  }
+});
 
 export default skate('x-hello', {
   properties: {
-    name: { attribute: true }
+    name: { attribute: true },
+    speed: prop.number()
   },
   render () {
-    style(styles);
-    vdom.div({ class: locals.testing }, 'testing');
+    vdom('motion-pulse', { amount: 1.3, speed: 100 }, function () {
+      vdom.div('testing');
+    });
+    vdom('motion-pulse', { amount: 1.2, speed: 200 }, function () {
+      vdom.div('testing');
+    });
   }
 });

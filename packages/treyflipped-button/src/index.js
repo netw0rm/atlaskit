618caddcb79f0b34d7ce84a0d09bf9889afa1f6e
Luke Batchelor <lbatchelor@atlassian.com>
2016-05-26 10:48:09 +1000

    feat: Spiked bounce component animation

--- packages/treyflipped-button/src/index.js
+++ packages/treyflipped-button/src/index.js
@@ -1,31 +1,44 @@
 import css from './index.less';
+import dynamics from 'dynamics.js';
 import { skate, prop, vdom } from 'skatejs';
+import objectAssign from 'object-assign';
 
-skate('motion-scale', {
+
+skate('motion-bounce', {
   properties: {
     amount: prop.number(),
-    hovering: prop.boolean(),
-    speed: prop.number()
+    duration: prop.number(),
+    styles: {
+      default() {
+        return {
+          cursor: 'pointer',
+          'transform': 'translateY(0px)',
+          zIndex: 0
+        };
+      },
+      render() {
+        return true;
+      }
+    }
   },
   render (elem) {
-    const style = {
-      cursor: 'pointer',
-      transition: `transform ${3 * elem.speed}ms`,
-      transformOrigin: 'left',
-      zIndex: 0
-    };
-
-    if (elem.hovering) {
-      style.transform = `scale(${elem.amount || '1'})`;
-      style.zIndex = 1;
-    }
-
     vdom.style(css.toString());
     vdom.div({
       class: css.locals.testing,
-      onmouseover: () => elem.hovering = true,
-      onmouseout: () => elem.hovering = false,
-      style
+      onclick: () => {
+        dynamics.animate(elem.styles, {
+          'transform': `translateY(-${elem.amount}px)`
+        }, 
+          {
+            change: (changeData) => {
+              //use objectAssign to make sure we get a new reference each time
+              elem.styles = objectAssign({}, changeData);
+            },
+            duration: elem.duration,
+            type: dynamics.bounce
+          });
+      },
+      style: elem.styles
     }, function () {
       vdom('slot');
     });
@@ -38,10 +51,10 @@ export default skate('x-hello', {
     speed: prop.number()
   },
   render () {
-    vdom('motion-scale', { amount: 2, speed: 100 }, function () {
+    vdom('motion-bounce', { amount: 100, duration: 1000 }, function () {
       vdom.div('Test 1');
     });
-    vdom('motion-scale', { amount: 1.2, speed: 200 }, function () {
+    vdom('motion-bounce', { amount: 50, duration: 2000 }, function () {
       vdom.div('Test 2');
     });
   }

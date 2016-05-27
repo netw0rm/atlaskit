c3f8479df3359c8ce4d34e5a34693fbce7f41ec4
Joscha Feth <jfeth@atlassian.com>
2016-05-27 15:56:25 +1000

    chore(saucelabs): use one tunnel for all

--- karma.conf.js
+++ karma.conf.js
@@ -67,6 +67,7 @@ module.exports = function (config) {
     config = Object.assign(config, {
       sauceLabs: {
         testName: 'AtlasKit',
+        startConnect: !process.env.SAUCELABS_HAS_TUNNEL,
         recordScreenshots: false,
         connectOptions: {
           verbose: true

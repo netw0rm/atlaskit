d0fe610e9bfd2168f500186aa0736ba9c53128d9
Luke Batchelor <lbatchelor@atlassian.com>
2016-05-27 13:36:01 +1000

    feat: Added atlas-animations (bounce, grow, bounce-grow, pulse) to test the apis

---
+++ packages/atlas-animation/demo/index.js
@@ -0,0 +1,4 @@
+import demoHTML from './index.html'; // eslint-disable-line no-unused-vars
+import component from '../src/index.js'; // eslint-disable-line no-unused-vars
+
+export default component;
---
+++ packages/atlas-animation/test/index.js
@@ -0,0 +1,11 @@
+import treyflippedButton from '../src/index.js';
+
+describe('treyflipped-button', () => {
+  describe('passing test', () => {
+    it('should pass', () => {
+      expect(true).to.be.true;
+    });
+
+    it('should use treyflippedButton', () => expect(treyflippedButton).to.not.be.null);
+  });
+});

653f208a01733e6427fd2eed2d1af915b8b9b69c
Luke Batchelor <lbatchelor@atlassian.com>
2016-06-06 10:36:18 +1000

    fix: spike webanimations-js polyfill and react integration
    affects: atlas-animation3

--- packages/atlas-animation3/demo/index.js
+++ packages/atlas-animation3/demo/index.js
@@ -1,8 +1,24 @@
 import demoHTML from './index.html'; // eslint-disable-line no-unused-vars
 import webanimation from 'web-animations-js/web-animations-next.min'; // eslint-disable-line no-unused-vars, max-len
+import React, { Component } from 'react';
+import { Animation } from 'react-web-animation';
+import ReactDOM from 'react-dom';
 
 import { skate, prop, vdom, state } from 'skatejs';
 import css from './index.less';
+
+/* I didn't bother spiking the actal consumption of the animations, because it should be almost
+   identical to the others. We've discussed how we consume without exposing implementation detail
+   but have decided that exposing it would not be a problem in this case as it will be native soon.
+
+   Animations will consist of two main bits, the keyframes and the timings. I think we should have
+   them behind a function that takes the parameters to the animation and either:
+   a) return the animation player object (and start playing if they havent set it not to)
+   b) return a function that returns the player object above (configure an animation once, call
+      it as you need it)
+   c) just return the keyframes and timing objects
+   d) return an object that contains a+b or a+c (to allow both consumption methods
+   e) have separate functions for React vs non-react consumption                                  */
 
 
 function animateRegularDomBox() {
@@ -77,3 +93,51 @@ export default skate('x-hello', {
     });
   },
 });
+
+class Basic extends Component {
+
+  getKeyFrames() {
+    return [
+        { transform: 'scale(1)', opacity: 1, offset: 0 },
+        { transform: 'scale(.5)', opacity: 0.5, offset: 0.3 },
+        { transform: 'scale(.667)', opacity: 0.667, offset: 0.7875 },
+        { transform: 'scale(.6)', opacity: 0.6, offset: 1 },
+    ];
+  }
+
+  getTiming(duration) {
+    return {
+      duration,
+      easing: 'ease-in-out',
+      delay: 0,
+      iterations: 3,
+      direction: 'alternate',
+      fill: 'none',
+    };
+  }
+
+  render() {
+    return React.createElement(Animation, {
+      keyframes: this.getKeyFrames(),
+      timing: this.getTiming(2500),
+    },
+          React.createElement('div', { style: {
+            color: 'red',
+          } },
+            'Web Animations API Rocks')
+    );
+  }
+}
+
+/* Terrible hack to make sure the dom is loaded before trying to append to it. Only for POC */
+
+setTimeout(() => {
+  ReactDOM.render(
+    React.createElement(Basic, { name: 'World' }),
+    document.getElementById('reactContainer')
+  );
+}, 10);
+
+if (typeof window !== 'undefined') {
+  window.React = React;
+}

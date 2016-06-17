bcd9b9207ccfdb8987af36816842cff5527bfaa6
Luke Batchelor <lbatchelor@atlassian.com>
2016-06-17 15:48:09 +1000

    feat: Rest of the bare functionality for ak-avatar
    affects: ak-avatar, ak-shared-styles

--- packages/ak-avatar/src/index.js
+++ packages/ak-avatar/src/index.js
@@ -1,4 +1,5 @@
-import styles from './index.less';
+import headStyles from 'style!./host.less'; // eslint-disable-line no-unused-vars, import/no-unresolved, max-len
+import shadowStyles from './shadow.less';
 
 import { enumeration } from 'ak-common';
 import { skate, vdom } from 'ak-base-component';
@@ -9,26 +10,19 @@ const SIZE_ATTRIBUTE_ENUM = {
   missingDefault: 'medium',
   invalidDefault: 'medium',
 };
-const AUI_AVATAR_DEFAULT_TYPE = 'user';
 
 export default skate('ak-avatar', {
   render(elem) {
-    vdom.style(styles.toString());
-    vdom('ak-avatar-inner', () => {
-      vdom.img({
-        alt: elem.alt,
-        src: elem.src,
-      });
+    vdom.style(shadowStyles.toString());
+    vdom.img({
+      alt: elem.alt,
+      src: elem.src,
     });
   },
   properties: {
     size: enumeration(SIZE_ATTRIBUTE_ENUM)({
       attribute: true,
     }),
-    type: {
-      attribute: true,
-      default: AUI_AVATAR_DEFAULT_TYPE,
-    },
     src: {
       attribute: true,
     },
@@ -37,4 +31,3 @@ export default skate('ak-avatar', {
     },
   },
 });
-

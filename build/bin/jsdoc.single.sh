5921e686b55113d3a3c4d34a0c5ceb8fd8c4cf7b
Ben Wong <benwong@atlassian.com>
2016-06-23 16:32:16 +1000

    docs: Add jsdoc/single command, which generates jsdocs for a single package.
    affects: ak-pony-button

---
+++ build/bin/jsdoc.single.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+BASEDIR=$(dirname $0)
+PKG="$1"
+# shift removes first command line arg (the package name)
+shift || true
+$BASEDIR/_scope_command.sh "$PKG" "webpack --config ./docs/webpack.config.js"
---
+++ packages/ak-pony-button/docs/webpack.config.js
@@ -0,0 +1,47 @@
+const JsDocPlugin = require('jsdoc-webpack-plugin');
+
+module.exports = {
+  entry: './docs/index.js',
+  output: {
+    path: './docs/out/scripts',
+    filename: 'index.js',
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      },
+      {
+        test: /\.less$/,
+        loader: 'css?modules&camelCase!less',
+      },
+      {
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: 'es2015',
+        },
+      },
+      { // this is for the v1 CustomElement polyfill and named-slots
+        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        include: /(webcomponents\.js\/src|skatejs-named-slots\/src)/,
+        query: {
+          presets: 'es2015',
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new JsDocPlugin({
+      conf: './docs/jsdoc-conf.json',
+    }),
+  ],
+};

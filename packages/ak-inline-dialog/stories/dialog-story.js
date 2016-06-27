5efb0c78e8b1e3b906699cc804d0a60bf2a0fff2
Nadia Makarevich <nmakarevich@atlassian.com>
2016-06-27 15:00:32 +1000

    feat: add storybook
    affects: ak-inline-dialog

---
+++ packages/ak-inline-dialog/stories/dialog-story.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@kadira/storybook';
+import reactify from 'akutil-react';
+import InlineDialog from '../src/index';
+import { define } from 'skatejs';
+
+const React = window.React;
+const ReactDOM = window.ReactDOM;
+
+let Dialog;
+Dialog = reactify(window.uniqueWebComponent(InlineDialog, define), {
+  React,
+  ReactDOM,
+});
+
+storiesOf('ak-dialog', module)
+  .add('Just open dialog', () => {
+    let target = '#target';
+    let position = 'right top';
+    let open = true;
+    return (<div style={{ position: 'relative' }}>
+      <button id="target">test target div</button>
+      <Dialog open={open} target={target} position={position}>Test text</Dialog>
+    </div>);
+  });

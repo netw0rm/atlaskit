dc344e04c905f3c67021038228558581283596d9
Ben Wong <benwong@atlassian.com>
2016-06-28 14:54:47 +1000

    docs: Add placeholder console output

--- build/bin/release.docs.sh
+++ build/bin/release.docs.sh
@@ -7,7 +7,11 @@ shift || true
 while read line; do
   PACKAGE=$(echo $line | cut -d@ -f1)
 
-  npm run jsdoc/build/single $PACKAGE
+  echo "[Release docs]: Building docs for $PACKAGE"
+
+  npm run jsdoc/build/single "$PACKAGE"
+
+  echo "[Release docs]: Releasing docs for $PACKAGE"
 
   # and then release what is in packages/$PACKAGE/docs/out
 done

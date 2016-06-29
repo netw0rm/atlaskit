3c1887e6bbc295976eecb794f6ab42d3d107661d
Ben Wong <benwong@atlassian.com>
2016-06-29 16:51:41 +1000

    docs: Add ak-pony-button stories, and embed story iframe in docs markdown file
    affects: ak-pony-button

--- packages/ak-pony-button/docs/tutorials/Usage.md
+++ packages/ak-pony-button/docs/tutorials/Usage.md
@@ -3,3 +3,13 @@ Tutorial content goes here.
 ### A heading
 
 This is a Markdown file.
+
+The `iframe` below relies on the `ak-pony-button` React Storybook running locally on port 9001.
+
+Example for a button with a name:
+
+```
+<ak-pony-button name="Randy"></ak-pony-button>
+```
+
+<iframe title="iframe example" width="480" height="390" src="//localhost:9001/iframe.html?dataId=ed8df051-a556-428e-8833-0a3e1de82d28" frameborder="0" allowfullscreen></iframe>
---
+++ packages/ak-pony-button/stories/pony-button-story.js
@@ -0,0 +1,20 @@
+import { storiesOf } from '@kadira/storybook';
+import reactify from 'akutil-react';
+import ponyButtonWc from '../src/index';
+import { define } from 'skatejs';
+
+const React = window.React;
+const ReactDOM = window.ReactDOM;
+
+const PonyButton = reactify(window.uniqueWebComponent(ponyButtonWc, define), {
+  React,
+  ReactDOM,
+});
+
+storiesOf('ak-pony-button', module)
+  .add('with name', () => (
+    <PonyButton name="Randy" />
+  ))
+  .add('with no name', () => (
+    <PonyButton />
+  ));

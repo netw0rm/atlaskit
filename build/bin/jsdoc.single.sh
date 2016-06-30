e3946b7218d8b96fa58394e85377022a80ebd127
Ben Wong <benwong@atlassian.com>
2016-06-30 12:25:42 +1000

    docs: Move JSDoc webpack config into the root config file
    affects: ak-pony-button

--- build/bin/jsdoc.single.sh
+++ build/bin/jsdoc.single.sh
@@ -5,4 +5,4 @@ BASEDIR=$(dirname $0)
 PKG="$1"
 # shift removes first command line arg (the package name)
 shift || true
-$BASEDIR/_scope_command.sh "$PKG" "webpack-dev-server --config ./docs/webpack.config.js"
+$BASEDIR/_scope_command.sh "$PKG" "webpack-dev-server --config ../../webpack.config.js --inline --docs $@"

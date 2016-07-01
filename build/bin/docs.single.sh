a68e84153b9e7d03cd18a9a91603e6bec5decd76
Ben Wong <benwong@atlassian.com>
2016-07-01 15:09:13 +1000

    docs: Automatically build and stage docs Markdown file in pre-commit hook
    affects: atlas-animation, ak-avatar, ak-inline-dialog, ak-layer

--- build/bin/docs.single.sh
+++ build/bin/docs.single.sh
@@ -6,3 +6,4 @@ PKG="$1"
 # shift removes first command line arg (the package name)
 shift || true
 ./node_modules/.bin/jsdoc2md "./packages/$PKG/src/index.js" > "./packages/$PKG/API.md"
+git add "./packages/$PKG/API.md"

c6a22365e5640641b07b0b597453422e0dbcbd47
Joscha Feth <jfeth@atlassian.com>
2016-07-05 14:15:52 +1000

    refactor: modularize

--- build/storybook/webpack.config.js
+++ build/storybook/webpack.config.js
@@ -1,27 +1,6 @@
-const path = require('path');
 const webpack = require('webpack');
 const webpackConfig = require('../../webpack.config.js');
-const lerna = require('lerna');
-const lernaPackageUtilities = require('lerna/lib/PackageUtilities');
-
-/**
-* Filters packages by their name based on the given glob and then gets the folder name
-*
-* @param {String} [packageGlob] The glob to match the package names against.
-*                               Falsy value means no packages are matched.
-* @return {Array} An array of folder names (the basename of the package folder beneath ../packages).
-*                 Empty array if none matched.
-*/
-function getPackageRestrictions(packageGlob) {
-  if (packageGlob) {
-    const packagesPath = lerna.getPackagesPath(path.join(__dirname, '..', '..'));
-    const packages = lernaPackageUtilities
-      .filterPackages(lerna.getPackages(packagesPath), packageGlob);
-    return packages.map((p) => path.basename(p.location));
-  }
-
-  return [];
-}
+const getPackageRestrictions = require('./getPackageRestrictions.js');
 
 module.exports = (storybookBaseConfig, configType) => {
   // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'

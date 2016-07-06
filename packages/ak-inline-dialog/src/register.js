2ba8cf2c03b936e0989a67a823ecf1944c7ac5f7
Nadia Makarevich <nmakarevich@atlassian.com>
2016-07-06 14:46:18 +1000

    feat: introducing register.js
    affects: ak-inline-dialog

---
+++ packages/ak-inline-dialog/src/register.js
@@ -0,0 +1,8 @@
+import { define } from 'skatejs';
+import definition, { AnimmyTestDefinition } from './index.js';
+
+/* The constructor for our component */
+define('ak-inline-dialog', definition);
+define('ak-animtest', AnimmyTestDefinition);
+
+export default definition;

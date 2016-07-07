520fa3f84c2794b3430847bb0e213343aa64709f
Joscha Feth <jfeth@atlassian.com>
2016-07-07 12:35:28 +1000

    test: fix for FF & co.

--- packages/akutil-component-template/test/index.js
+++ packages/akutil-component-template/test/index.js
@@ -11,6 +11,7 @@ const expect = chai.expect; // eslint-disable-line no-unused-vars
 describe('akutil-component-template', () => {
   it('should be possible to create a component', () => {
     const component = document.createElement('akutil-component-template');
-    component.innerText.should.match(/I am an .+? element!/);
+    // TODO: Talk to Trey on how to make this better - helper fn?
+    (component.innerHTML || component.shadowRoot.innerHTML).should.match(/I am an .+? element!/);
   });
 });

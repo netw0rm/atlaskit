feb0d229b64b92841a6aea4460202710a7cf8fba
Joscha Feth <jfeth@atlassian.com>
2016-07-07 12:51:07 +1000

    test: use skate symbols to access shadowRoot

--- packages/akutil-component-template/test/index.js
+++ packages/akutil-component-template/test/index.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-
+import { symbols } from 'skatejs';
 import '../src/index.register.js';
 
 chai.use(chaiAsPromised);
@@ -11,7 +11,6 @@ const expect = chai.expect; // eslint-disable-line no-unused-vars
 describe('akutil-component-template', () => {
   it('should be possible to create a component', () => {
     const component = document.createElement('akutil-component-template');
-    // TODO: Talk to Trey on how to make this better - helper fn?
-    (component.innerHTML || component.shadowRoot.innerHTML).should.match(/I am an .+? element!/);
+    component[symbols.shadowRoot].innerHTML.should.match(/I am an .+? element!/);
   });
 });

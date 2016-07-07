ff12b75548aa99b0f913a6ac163ae3ebf8b17f19
Joscha Feth <jfeth@atlassian.com>
2016-07-07 12:52:33 +1000

    chore: use dots reporter for CI

--- build/bin/test.karma.nofail.sh
+++ build/bin/test.karma.nofail.sh
@@ -7,7 +7,7 @@ COMPONENT_DIR=$(basename `pwd`)
 
 {
     if [ -d "test" ]; then
-        karma start ../../karma.conf.js --single-run --reporters=progress,junit
+        karma start ../../karma.conf.js --single-run --reporters=dots,junit
     else
         echo -e "\033[34m No 'test' dir in packages/$COMPONENT_DIR; Skipping tests."
         echo -e "\033[0m"

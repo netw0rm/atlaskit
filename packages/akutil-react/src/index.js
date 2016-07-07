76cb57e8017f16eed252460502faa68f973ca4ea
Joscha Feth <jfeth@atlassian.com>
2016-07-07 10:56:59 +1000

    docs: fix jsdoc signature

--- packages/akutil-react/src/index.js
+++ packages/akutil-react/src/index.js
@@ -2,8 +2,8 @@ import reactify from 'skatejs-react-integration';
 
 /**
  * Turn an atlaskit web component into a react component
- * @param webComponent the atlaskit web component
- * @param options
+ * @param {!object} webComponent the atlaskit web component
+ * @param {!{React: object, ReactDOM: object}} options
  *          - React: the instance of React to register with
  *          - ReactDOM: the instance of ReactDOM to register with
  */

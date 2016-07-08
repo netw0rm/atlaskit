39e0ddd760a37d30c64756b48876819f38b33aa1
Joscha Feth <jfeth@atlassian.com>
2016-07-08 11:31:05 +1000

    refactor: fix class name

--- packages/akutil-component-template/stories/example-story.js
+++ packages/akutil-component-template/stories/example-story.js
@@ -20,7 +20,7 @@ storiesOf(name, module)
   ))
   .add('an akutil-component-template that removes itself when being clicked', () => {
     const removeMe = (e) => e.currentTarget.parentNode.removeChild(e.currentTarget);
-    const cls = styles.akAvatar;
+    const cls = styles.akutilComponentTemplate;
     return (<Component id="myComponent" className={cls} onClick={removeMe} />);
   })
   .addMonkeyTest('a akutil-component-template with monkey testing', () => (

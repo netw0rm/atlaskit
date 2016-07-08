a9d052f6dcc489df119b14d9b61cb6a0a52a39b9
Ben Wong <benwong@atlassian.com>
2016-07-08 16:12:53 +1000

    docs: Move dmd overrides into overrides folder

--- build/bin/docs.single.sh
+++ build/bin/docs.single.sh
@@ -8,6 +8,14 @@ shift || true
 
 # Concatenate USAGE docs and JSDoc output
 $BASEDIR/_scope_command.sh "$PKG" "mkdir -p ./docs/.tmp"
-$BASEDIR/_scope_command.sh "$PKG" "../../node_modules/.bin/jsdoc2md --plugin dmd-bitbucket --template ../../docs/component-template.hbs --src ./src/index.js --helper ../../docs/helpers/*.js --partial ../../docs/partials/**/*.hbs --member-index-format list --name-format --no-gfm" | sed 1,3d >  "./packages/$PKG/docs/.tmp/README.md"
+$BASEDIR/_scope_command.sh "$PKG" "../../node_modules/.bin/jsdoc2md
+--plugin dmd-bitbucket
+--template ../../docs/component-template.hbs
+--src ./src/index.js
+--helper ../../docs/overrides/helpers/*.js
+--partial ../../docs/overrides/partials/**/*.hbs
+--member-index-format list
+--name-format
+--no-gfm" | sed 1,3d >  "./packages/$PKG/docs/.tmp/README.md"
 $BASEDIR/_scope_command.sh "$PKG" "cat ./docs/USAGE.md ./docs/.tmp/README.md" | sed 1,3d > "packages/$PKG/README.md"
 $BASEDIR/_scope_command.sh "$PKG" "rm -rf ./docs/.tmp"

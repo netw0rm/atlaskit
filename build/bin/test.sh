089c8e3655e33d56275e058a8077ed8a8780726e
Ben Gummer <bgummer@atlassian.com>
2016-07-12 16:16:36 +1000

    chore(tests): eventually return error when local unit test fails

--- build/bin/test.sh
+++ build/bin/test.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 set -e
 
-node ./node_modules/lerna/bin/lerna.js exec -- ../../build/bin/test.karma.nofail.sh
+FAILED_CI_FILE=.ci_failed
+
+# Clears the file each time we run
+rm -f $FAILED_CI_FILE
+
+# Run the local unit tests, temporarily ignore failures
+FAILED_CI_FILE=$FAILED_CI_FILE node ./node_modules/lerna/bin/lerna.js exec -- ../../build/bin/test.karma.nofail.sh
+
+# Check if any components failed local unit tests
+if [ -f $FAILED_CI_FILE ]; then
+    echo "The following components failed local unit tests:"
+    cat $FAILED_CI_FILE
+    exit 1
+fi
+
+echo "Yay! All local unit tests passed"

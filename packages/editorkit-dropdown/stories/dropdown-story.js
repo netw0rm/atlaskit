2e066bf3a603fa739fc74497cf6ed165607c65c9
Steve Mao <maochenyan@gmail.com>
2016-07-14 16:11:01 +1000

    chore(dropdown): add buggy stories

--- packages/editorkit-dropdown/stories/dropdown-story.js
+++ packages/editorkit-dropdown/stories/dropdown-story.js
@@ -15,7 +15,13 @@ storiesOf(name, module)
     <Component style={{ position: 'absolute', left: 100 }} />
   ))
   .add('a editorkit-dropdown at the bottom', () => (
+    <Component style={{ position: 'absolute', left: 100, top: 600 }} />
+  ))
+  .add('1. buggy editorkit-dropdown at the bottom', () => (
     <div style={{ marginLeft: 100, marginTop:700 }}>
       <Component />
     </div>
+  ))
+  .add('2. buggy editorkit-dropdown at the bottom', () => (
+    <Component style={{ position: 'absolute', left: 100, bottom: 10 }} />
   ));

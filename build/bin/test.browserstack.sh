4b9065194696b829419382624b7e659b9ebbd94a
Joscha Feth <jfeth@atlassian.com>
2016-07-19 17:21:23 +1000

    chore: re-add BROWSERSTACK=1

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -6,7 +6,7 @@ FAILED_CI_FILE=.ci_failed
 rm -f $FAILED_CI_FILE
 
 # Run the Browserstack tests 1 component at a time, temporarily ignore failures
-FAILED_CI_FILE=$FAILED_CI_FILE `npm bin`/lerna exec --concurrency 1 -- ../../build/bin/test.sh
+FAILED_CI_FILE=$FAILED_CI_FILE BROWSERSTACK=1 `npm bin`/lerna exec --concurrency 1 -- ../../build/bin/test.sh
 
 # Check if any components failed on Browserstack
 if [ -f $FAILED_CI_FILE ]; then

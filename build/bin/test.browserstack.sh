f0c3d91a6c511d91d94a9264ff972ba4b45c4685
Joscha Feth <jfeth@atlassian.com>
2016-07-19 17:11:33 +1000

    chore: use fixed retry-cli

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -6,7 +6,7 @@ FAILED_CI_FILE=.ci_failed
 rm -f $FAILED_CI_FILE
 
 # Run the Browserstack tests 1 component at a time, temporarily ignore failures
-FAILED_CI_FILE=$FAILED_CI_FILE BROWSERSTACK=1 `npm bin`/lerna exec --concurrency 1 -- ../../build/bin/test.sh
+FAILED_CI_FILE=$FAILED_CI_FILE `npm bin`/lerna exec --concurrency 1 -- ../../build/bin/test.sh
 
 # Check if any components failed on Browserstack
 if [ -f $FAILED_CI_FILE ]; then
@@ -19,6 +19,8 @@ if [ -f $FAILED_CI_FILE ]; then
     printf "\033[0m"
     exit 1
 fi
-echo           "----------------------------------"
-printf "\033[32mYay! All Browserstack tests passed\033[0m\n"
-echo           "----------------------------------"
+printf "\033[32m"
+echo "----------------------------------"
+echo "Yay! All Browserstack tests passed"
+echo "----------------------------------"
+printf "\033[0m"

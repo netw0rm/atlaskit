56db6552ffd74e358d0b1bc2ce57b34905253087
Matias Corbanini <mcorbanini@atlassian.com>
2016-07-19 17:27:01 +1000

    feat(): Disabled Button

--- packages/ak-button/stories/example-story.js
+++ packages/ak-button/stories/example-story.js
@@ -15,8 +15,42 @@ storiesOf(name, module)
     <AkButton label="Button" />
   ))
   .add('a primary ak-button', () => (
-    <AkButton label="Button" primary />
+    <AkButton label="Primary Button" primary />
   ))
+  .add('a disabled ak-button', () => {
+    class MyComponent extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          disable: false,
+        };
+      }
+
+      handleClick() {
+        this.setState({ disable: !this.state.disable });
+      }
+
+      render() {
+        return (
+          <div>
+            Disable
+            <input
+              type="checkbox"
+              onChange={this.handleClick.bind(this)}  // eslint-disable-line react/jsx-no-bind
+              defaultChecked={this.state.disable}
+            />
+            <AkButton
+              disabled={this.state.disable}
+              label="Button"
+              onClick={action('clicking the WebComponent')}
+            />
+          </div>
+        );
+      }
+    }
+
+    return <MyComponent />;
+  })
   .add('an ak-button that emits an action when it is clicked', () => (
     <AkButton id="myComponent" label="Button" onClick={action('clicking the WebComponent')} />
   ));

c359a65ecfe26bf4ecb952d8a55fe1e6e3257edb
Ben Wong <benwong@atlassian.com>
2016-07-20 18:20:04 +1000

    docs(docs): Check for existing files before docs generation, only generate README if they exist
    affects: ak-avatar, akutil-component-template

--- build/bin/generate.readme.sh
+++ build/bin/generate.readme.sh
@@ -5,22 +5,37 @@ printf "\033[34m"
 echo "Generating README.md..."
 printf "\033[0m"
 
-# Generate API docs
-API="$(../../node_modules/.bin/jsdoc2md \
---plugin dmd-bitbucket ak-dmd-plugin \
---src ./src/index.js \
---member-index-format list \
---name-format)"
-
-if [[ $API == *"ERROR, Cannot find class"* ]]
-then
-  API=""
+# Get usage docs
+if compgen -G "docs/USAGE\.md" > /dev/null; then
+  USAGE="$(cat ./docs/USAGE.md)"
 else
-  API="\n$API"
+  USAGE=""
+fi
+
+# Generate API docs
+if compgen -G "*/index\.js" > /dev/null; then
+
+  API="$(../../node_modules/.bin/jsdoc2md \
+  --plugin dmd-bitbucket ak-dmd-plugin \
+  --src ./src/index.js \
+  --member-index-format list \
+  --name-format)"
+
+  if [[ $API == *"ERROR, Cannot find class"* ]]
+  then
+    API=""
+  else
+    API="\n$API"
+  fi
+
+else
+  API=""
 fi
 
 # Concatenate USAGE docs and JSDoc output
-(
-  cat ./docs/USAGE.md
-  printf "$API"
-) > README.md
+if [ -n "$USAGE" ] || [ -n "$API" ]; then
+  (
+    printf "$USAGE"
+    printf "$API"
+  ) > README.md
+fi

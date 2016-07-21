15751dc17505b1a5b1249cb3383354aff2afa2bb
Bradley Ayers <bradley.ayers@gmail.com>
2016-07-21 16:26:02 +1000

    fix(package): Add ak- prefix to editor-*, and fix licensing.
    affects: ak-editor-button, ak-editor-content, ak-editor-icon, ak-editor-toolbar, ak-editor-toolbar-text-formatting, akutil-shared-styles

---
+++ packages/ak-editor-content/stories/content-story.js
@@ -0,0 +1,192 @@
+import { storiesOf } from '@kadira/storybook';
+import ContentComponent from '../src/index';
+import { define, vdom } from 'skatejs'; // eslint-disable-line no-unused-vars
+const { React, ReactDOM } = window;
+import reactify from 'akutil-react';
+
+const Content = reactify(ContentComponent, { React, ReactDOM });
+
+storiesOf('ak-editor-content', module)
+  .add('Empty', () => (
+    <Content />
+  ))
+  .add('div', () => (
+    <Content>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: 'Hello world!',
+        }}
+      />
+    </Content>
+  ))
+  .add('p', () => (
+    <Content>
+      <p
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: 'Hello world!',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p', () => (
+    <Content>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + p', () => (
+    <Content>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + div', () => (
+    <Content>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><div>Hello world!</div>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + div + p', () => (
+    <Content>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><div>Hello world!</div><p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div (open top)', () => (
+    <Content openTop>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: 'Hello world!',
+        }}
+      />
+    </Content>
+  ))
+  .add('p (open top)', () => (
+    <Content openTop>
+      <p
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: 'Hello world!',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p (open top)', () => (
+    <Content openTop>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + p (open top)', () => (
+    <Content openTop>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + div (open top)', () => (
+    <Content openTop>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><div>Hello world!</div>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + div + p (open top)', () => (
+    <Content openTop>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><div>Hello world!</div><p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div (open bottom)', () => (
+    <Content openBottom>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: 'Hello world!',
+        }}
+      />
+    </Content>
+  ))
+  .add('p (open bottom)', () => (
+    <Content openBottom>
+      <p
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: 'Hello world!',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p (open bottom)', () => (
+    <Content openBottom>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + p (open bottom)', () => (
+    <Content openBottom>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + div (open bottom)', () => (
+    <Content openBottom>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><div>Hello world!</div>',
+        }}
+      />
+    </Content>
+  ))
+  .add('div > p + div + p (open bottom)', () => (
+    <Content openBottom>
+      <div
+        contentEditable
+        dangerouslySetInnerHTML={{
+          __html: '<p>Hello world!</p><div>Hello world!</div><p>Hello world!</p>',
+        }}
+      />
+    </Content>
+  ));
---
+++ packages/ak-editor-toolbar/stories/toolbar-story.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@kadira/storybook';
+import ToolbarComponent from '../src/index';
+import ToolbarButtonComponent from 'ak-editor-button';
+import TextFormattingComponent from 'ak-editor-toolbar-text-formatting';
+import IconComponent from 'ak-editor-icon';
+import { define, vdom } from 'skatejs'; // eslint-disable-line no-unused-vars
+const { React, ReactDOM } = window;
+import reactify from 'akutil-react';
+
+const Toolbar = reactify(ToolbarComponent, { React, ReactDOM });
+const ToolbarButton = reactify(ToolbarButtonComponent, { React, ReactDOM });
+const Icon = reactify(IconComponent, { React, ReactDOM });
+const TextFormatting = reactify(TextFormattingComponent, { React, ReactDOM });
+
+storiesOf('ak-editor-toolbar', module)
+  .add('Empty', () => (
+    <Toolbar />
+  ))
+  .add('Single button', () => (
+    <Toolbar>
+      <ToolbarButton><Icon glyph="bold" /></ToolbarButton>
+    </Toolbar>
+  ))
+  .add('Text formatting', () => (
+    <Toolbar>
+      <TextFormatting />
+    </Toolbar>
+  ));

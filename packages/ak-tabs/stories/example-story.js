6023cda90733aedfcb1918730341ea5577096c89
Ben Wong <benwong@atlassian.com>
2016-07-26 14:15:47 +1000

    chore: Add story for tab with many items
    affects: ak-tabs

--- packages/ak-tabs/stories/example-story.js
+++ packages/ak-tabs/stories/example-story.js
@@ -10,10 +10,30 @@ const Component = reactify(Tabs, {
 });
 
 storiesOf(name, module)
-  .add('a simple ak-tabs', () => (
+  .add('simple ak-tabs', () => (
     <Component>
       <ak-tab label="Tab 1">Tab 1 content</ak-tab>
       <ak-tab selected label="Tab 2">Tab 2 content</ak-tab>
       <ak-tab label="Tab 3">Tab 3 content</ak-tab>
+    </Component>
+  ))
+  .add('ak-tabs with many items', () => (
+    <Component>
+      <ak-tab label="Long tab name 1">Tab 1 content</ak-tab>
+      <ak-tab selected label="Long tab name 2">Tab 2 content</ak-tab>
+      <ak-tab label="Long tab name 3">Tab 3 content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
+      <ak-tab label="Another tab">Tab content</ak-tab>
     </Component>
   ));

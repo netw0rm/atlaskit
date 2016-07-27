e5083c8c9df59530f7a175bda1661c1ea5aaffca
Matias Corbanini <mcorbanini@atlassian.com>
2016-07-27 16:38:27 +1000

    refactor(): added some tests

--- packages/ak-editor-button/test/index.js
+++ packages/ak-editor-button/test/index.js
@@ -15,11 +15,17 @@ describe('ak-editor-button', () => {
   const shadowDomQuery = (elem, classes) =>
     elem[symbols.shadowRoot].querySelector(classes);
 
-  it('should be possible to create a component', () => {
-    let component;
+  const getShadowButtonElem = (elem) =>
+    shadowDomQuery(elem, `.${classKeys.root} button`);
+
+  it('should not throws when component is instanciated', () => {
     expect(() => {
-      component = new AkEditorButton();
+      new AkEditorButton(); // eslint-disable-line no-new
     }).not.to.throw(Error);
+  });
+
+  it('should be possible to create a component', () => {
+    const component = new AkEditorButton();
     expect(component).to.be.defined;
     expect(shadowDomQuery(component, `.${classKeys.root}`)).to.be.defined;
     expect(component.tagName.toLowerCase()).to.equal(name);
@@ -43,27 +49,51 @@ describe('ak-editor-button', () => {
       });
     });
 
-    describe('when active attribute is set', () => {
-      beforeEach(() => state(component, { active: true }));
+    describe('active attribute', () => {
+      const selector = `.${classKeys.root}.${classKeys.active}`;
+      beforeEach(() =>
+        state(component, { active: true })
+      );
 
-      it('container should have active class', () => {
-        const classes = `.${classKeys.root}.${classKeys.active}`;
-        expect(shadowDomQuery(component, classes)).not.to.be.null;
-      });
+      it('container should have active class', () =>
+        expect(shadowDomQuery(component, selector)).not.to.be.null
+      );
 
-      it('container should not have active class after attribute is removed', () => {
+      it('container should not have active class after it is removed', () => {
         state(component, { active: false });
-        const classes = `.${classKeys.root}.${classKeys.active}`;
-        expect(shadowDomQuery(component, classes)).to.be.null;
+        expect(shadowDomQuery(component, selector)).to.be.null;
       });
     });
 
     describe('when disabled attribute is set', () => {
-      beforeEach(() => state(component, { disabled: true }));
+      const selector = `.${classKeys.root} button[disabled]`;
+      beforeEach(() =>
+        state(component, { disabled: true })
+      );
 
-      it('button should have disabled attribute', () => {
-        const classes = `.${classKeys.root} button[disabled]`;
-        expect(shadowDomQuery(component, classes)).not.to.be.null;
+      it('button should have disabled attribute', () =>
+        expect(shadowDomQuery(component, selector)).not.to.be.null
+      );
+
+      it('button should not have disabled attribute after it is removed', () => {
+        state(component, { disabled: false });
+        expect(shadowDomQuery(component, selector)).to.be.null;
+      });
+
+      describe('onclick event', () => {
+        const checkOnClickEvent = shouldBeCalled => {
+          const onclick = sinon.spy();
+          state(component, { onclick });
+          state(getShadowButtonElem(component), { onclick });
+          getShadowButtonElem(component).click();
+          expect(onclick.called).to.equals(shouldBeCalled);
+        };
+
+        it('button should not trigger onclick event', () => checkOnClickEvent(false));
+        it('button should trigger onclick event after it is removed', () => {
+          state(component, { disabled: false });
+          checkOnClickEvent(true);
+        });
       });
     });
   });

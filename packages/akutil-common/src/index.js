82db32540d880df7682b6f6d921c2ccfa38ea906
Nadia Makarevich <nmakarevich@atlassian.com>
2016-08-02 13:12:48 +1000

    fix: KeyPressHandler after tests were added
    affects: akutil-common

--- packages/akutil-common/src/index.js
+++ packages/akutil-common/src/index.js
@@ -1,35 +1,6 @@
 import { enumeration } from './properties';
 import Alignment from './Alignment';
 import keyCode from 'keycode';
-
-
-class KeyPressHandler {
-  constructor(key, callback) {
-    this.keyListeners = {};
-    this.keyListeners[keyCode(key)] = callback;
-
-    this.listener = (e) => {
-      if (this.keyListeners[e.keyCode]) {
-        callback();
-      }
-    };
-
-    document.addEventListener('keydown', this.listener);
-  }
-
-  add(key, callback) {
-    this.keyListeners[keyCode(key)] = callback;
-  }
-
-  destroy(key) {
-    if (!key) {
-      this.keyListeners = undefined;
-      document.removeEventListener('keydown', this.listener);
-    } else {
-      delete this.keyListeners[key];
-    }
-  }
-}
-
+import KeyPressHandler from './KeyPressHandler';
 
 export { enumeration, Alignment, keyCode, KeyPressHandler };

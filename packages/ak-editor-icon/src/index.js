2ab4c7b70ed15e9038754493addae0a39564abe5
Joscha Feth <jfeth@atlassian.com>
2016-08-03 15:48:27 +1000

    chore(linting): Remove unnecessary linting overrides
    affects: ak-avatar, ak-editor-icon

--- packages/ak-editor-icon/src/index.js
+++ packages/ak-editor-icon/src/index.js
@@ -1,7 +1,7 @@
 import { define, vdom, prop } from 'skatejs';
 import shadowStyles from './shadow.less';
 import 'style!./host.less';
-import icons from './pack-svgs!./'; // eslint-disable-line import/no-unresolved
+import icons from './pack-svgs!./';
 
 // Because we dynamically write a CSS style for the fill colour, and we're
 // dealing with polyfill'd shadow DOM that doesn't enforce CSS boundaries, we

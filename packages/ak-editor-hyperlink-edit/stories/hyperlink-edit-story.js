4e9f477fd1b4acdf293956e7b58cd38b48331394
Bradley Ayers <bradley.ayers@gmail.com>
2016-08-04 13:24:49 +1000

    feat: Add ak-editor-hyperlink-edit
    affects: ak-editor-hyperlink-edit

---
+++ packages/ak-editor-hyperlink-edit/stories/hyperlink-edit-story.js
@@ -0,0 +1,200 @@
+import { storiesOf } from '@kadira/storybook';
+import HyperlinkEdit from '../src';
+import ContentComponent from 'ak-editor-content';
+import { vdom } from 'skatejs';
+const { React, ReactDOM } = window;
+import styles from './styles.less';
+import reactify from 'akutil-react';
+
+import { ProseMirror } from 'prosemirror/dist/edit';
+import { DOMFromPos } from 'prosemirror/dist/edit/dompos';
+import { schema } from 'prosemirror/dist/schema-basic';
+const Content = reactify(ContentComponent, { React, ReactDOM });
+
+class PoppedDemo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { unlinked: false };
+  }
+
+  componentDidMount() {
+    this.hyperlinkEdit = new HyperlinkEdit();
+    this.hyperlinkEdit.href = this.props.href;
+    this.hyperlinkEdit.attachTo = this.span;
+    this.hyperlinkEdit.canUnlink = this.props.canUnlink;
+    this.hyperlinkEdit.addEventListener('unlink', () => {
+      this.hyperlinkEdit.parentNode.removeChild(this.hyperlinkEdit);
+      this.setState({ unlinked: true });
+    });
+    document.body.appendChild(this.hyperlinkEdit);
+  }
+
+  componentWillUnmount() {
+    if (this.hyperlinkEdit) {
+      this.hyperlinkEdit.dismiss();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <style>{styles.toString()}</style>
+        <p contentEditable>Lorem ipsum ipsum ipsum ipsum ipsum ipsum <span
+          className={this.state.unlinked ? null : styles.locals.hyperlink}
+          ref={(span) => { this.span = span; }}
+        >epsum</span> lopsom gibson gibson gibson gibson gibson.</p>
+      </div>
+    );
+  }
+}
+
+PoppedDemo.defaultProps = {
+  href: 'https://example.com',
+  canUnlink: true,
+  canOpen: true,
+};
+
+storiesOf('ak-editor-hyperlink-edit', module)
+  .add('Default', () => {
+    class Demo extends React.Component {
+      componentWillUnmount() {
+        if (this.hyperlinkEdit) {
+          this.hyperlinkEdit.dismiss();
+        }
+      }
+
+      editLink(target) {
+        this.hyperlinkEdit = new HyperlinkEdit();
+        this.hyperlinkEdit.href = 'https://example.com';
+        this.hyperlinkEdit.attachTo = target;
+        document.body.appendChild(this.hyperlinkEdit);
+      }
+
+      render() {
+        return (
+          <div>
+            <style>{styles.toString()}</style>
+            <p contentEditable>Lorem ipsum <span
+              className={styles.locals.hyperlink}
+              onClick={(e) => this.editLink(e.target)}
+            >epsum</span> lopsom gibson.</p>
+          </div>
+        );
+      }
+    }
+
+    return <Demo />;
+  })
+  .add('Popped', () => <PoppedDemo />)
+  .add('Popped (no open)', () => <PoppedDemo href={null} />)
+  .add('Popped (no unlink)', () => <PoppedDemo canUnlink={false} />)
+  .add('ProseMirror', () => {
+    const getDomElement = (pm, pos) => {
+      const { node, offset } = DOMFromPos(pm, pos); // eslint-disable-line new-cap
+      if (node.childNodes.length === 0) {
+        return node.parentNode;
+      }
+      return node.childNodes[offset];
+    };
+
+    class Demo extends React.Component {
+      hyperlinkEditor(elem, onUnlink) {
+        const href = elem.getAttribute('href');
+        const popup = new HyperlinkEdit();
+        popup.href = href;
+        popup.textInputPlaceholder = 'Give link a title';
+        popup.textInputValue = href;
+        popup.attachTo = elem;
+        popup.addEventListener('unlink', onUnlink);
+        return {
+          element: popup,
+          dismiss: () => {
+            popup.removeEventListener(onUnlink);
+            popup.dismiss();
+          },
+        };
+      }
+
+      componentDidMount() {
+        const doc = schema.node('doc', null,
+          schema.node('paragraph', null, [
+            schema.text('Lorem ipsum solem golum '),
+            schema.text('molum', [
+              schema.mark('link', { href: 'http://example.com' }),
+            ]),
+            schema.text('molum', [
+              schema.mark('link', { href: 'http://example.net' }),
+            ]),
+            schema.text(' gibson tepsum lopsom wundsom.'),
+          ])
+        );
+
+        const pm = this.pm = new ProseMirror({
+          place: this.editorElement,
+          doc,
+          plugins: [],
+        });
+
+        let selectedLink = null;
+
+        pm.updateScheduler([pm.on.selectionChange], () => {
+          const { head, anchor } = pm.selection;
+          const isCollapsed = head === anchor;
+          const pos = anchor - 1;
+          const node = pm.doc.nodeAt(pos);
+          const element = getDomElement(pm, pos);
+          const isLink = schema.marks.link.isInSet(node.marks);
+          const nextSelectedLink = (isCollapsed && isLink) ? element : null;
+
+          if (nextSelectedLink !== selectedLink) {
+            selectedLink = nextSelectedLink;
+            if (this.popup) {
+              this.popup.dismiss();
+            }
+
+            if (nextSelectedLink) {
+              this.popup = this.hyperlinkEditor(element, () => {
+                const p = pm.doc.resolve(anchor);
+                const from = p.pos - p.nodeBefore.nodeSize;
+                const to = p.pos + p.nodeAfter.nodeSize;
+                pm.tr.removeMark(from, to, schema.marks.link).apply();
+              });
+              document.body.appendChild(this.popup.element);
+            }
+          }
+        });
+      }
+
+      render() {
+        return (
+          <div ref={(div) => { if (div) { this.editorElement = div.firstChild; } }}>
+            <Content />
+          </div>
+        );
+      }
+
+      componentWillUpdate(nextProps, nextState) {
+        if (this.state.selectedLink !== nextState.selectedLink) {
+          if (this.popup) {
+            this.popup.dismiss();
+            delete this.popup;
+          }
+
+          if (nextState.selectedLink) {
+            this.popup = this.hyperlinkEditor();
+            this.popup = new HyperlinkEdit();
+            this.popup.href = nextState.selectedLink.getAttribute('href');
+            this.popup.textInputPlaceholder = 'Give link a title';
+            this.popup.textInputValue = nextState.selectedLink.getAttribute('href');
+            this.popup.attachTo = nextState.selectedLink;
+            this.popup.addEventListener('unlink', () => {
+
+            });
+            document.body.appendChild(this.popup);
+          }
+        }
+      }
+    }
+
+    return <Demo />;
+  });

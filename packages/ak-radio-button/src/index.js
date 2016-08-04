d7ec3877ad75cad9e42e7f82a51278503ef0ad90
Ben Gummer <bgummer@atlassian.com>
2016-08-04 10:52:17 +1000

    chore(wip): wip

--- packages/ak-radio-button/src/index.js
+++ packages/ak-radio-button/src/index.js
@@ -3,9 +3,7 @@ import 'style!./host.less';
 
 import { emit, vdom, define } from 'skatejs';
 import shadowStyles from './shadow.less';
-import Button from 'ak-button';
-
-const Paragraph = (props, chren) => <p {...props}>{chren()}</p>;
+import AkButton from 'ak-button';
 
 /**
  * @description Create instances of the component programmatically, or using markup.
@@ -16,16 +14,11 @@ const Paragraph = (props, chren) => <p {...props}>{chren()}</p>;
 export default define('ak-radio-button', {
   render(elem) {
     return (
-      // JSX requires that there only be a single root element.
-      // Incremental DOM doesn't require this.
       <div>
-        {/* This is required for elements in the shadow root to be styled.
-           This is wrapped in the <div /> because you can't have more than one
-           root element.
-        */}
         <style>{shadowStyles.toString()}</style>
-        <Button>Ben</Button>
-        <Paragraph className={shadowStyles.locals.myClassName}>My name is {elem.name}!</Paragraph>
+        <AkButton selected>
+          {elem.name}
+        </AkButton>
       </div>
     );
   },

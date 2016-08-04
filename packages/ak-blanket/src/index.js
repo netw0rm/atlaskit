b99f78207e72c5ac6f56591b31be9bd14fe83088
Trey Shugart <treshugart@gmail.com>
2016-08-04 12:30:24 +1000

    refactor: Update to Skate 1.x.
    affects: ak-blanket

--- packages/ak-blanket/src/index.js
+++ packages/ak-blanket/src/index.js
@@ -29,8 +29,8 @@ export default define('ak-blanket', {
       <div>
         <style>{shadowStyles.toString()}</style>
         <div
-          onclick={handleBlur(elem)}
-          ontouch={handleBlur(elem)}
+          on-click={handleBlur(elem)}
+          on-touch={handleBlur(elem)}
           class={classes}
         >
         </div>
@@ -47,10 +47,7 @@ export default define('ak-blanket', {
      * @example @html <ak-blanket tinted></ak-blanket>
      * @example @js component.tinted = true
      */
-    tinted: prop.boolean({
-      attribute: true,
-      default: false,
-    }),
+    tinted: prop.boolean({ attribute: true }),
     /**
      * @description If click on the blanket emits a 'blanket-click' event.
      * @memberof Blanket
@@ -60,9 +57,6 @@ export default define('ak-blanket', {
      * @example @html <ak-blanket clickable></ak-blanket>
      * @example @js component.clickable = true
      */
-    clickable: prop.boolean({
-      attribute: true,
-      default: false,
-    }),
+    clickable: prop.boolean({ attribute: true }),
   },
 });

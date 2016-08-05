e3af6b0b0fb7b99ffc133d5e05bc750ebdbaac30
Ben Gummer <bgummer@atlassian.com>
2016-08-05 14:02:04 +1000

    chore(git): fix git dep ssh issues

--- build/bin/setup_hosts.sh
+++ build/bin/setup_hosts.sh
@@ -4,5 +4,5 @@ set -e
 mkdir -p /root/.ssh/
 touch /root/.ssh/known_hosts
 
-ssh-keyscan -H bitbucket.org >> /root/.ssh/known_hosts
-ssh-keyscan -H github.com >> /root/.ssh/known_hosts
+ssh-keyscan -t rsa -H bitbucket.org >> /root/.ssh/known_hosts
+ssh-keyscan -t rsa -H github.com >> /root/.ssh/known_hosts

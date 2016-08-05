baa39a2eb33f99db070e43bef0e6b78021a89a5d
Joshua Nelson <jonelson@atlassian.com>
2016-08-05 12:03:01 +1000

    style(component): add constant for touch
    affects: ak-navigation

--- packages/ak-navigation/src/touch.js
+++ packages/ak-navigation/src/touch.js
@@ -1,14 +1,15 @@
 const swipeLeft = Symbol();
 const swipeRight = Symbol();
 const noSwipe = Symbol();
+const touchThreshold = 20;
 
 export default function getSwipeType(touchstart, touchend) {
   const startTouch = touchstart.targetTouches[0];
   const endTouch = touchend.changedTouches[0];
   const delta = startTouch.screenX - endTouch.screenX;
-  if (delta > 20) {
+  if (delta > touchThreshold) {
     return swipeRight;
-  } else if (delta < -20) {
+  } else if (delta < -touchThreshold) {
     return swipeLeft;
   }
 

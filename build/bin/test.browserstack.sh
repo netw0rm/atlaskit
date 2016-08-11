1947ff42a25ae0124ad91a28b451af69a72f04bb
Ben Gummer <bgummer@atlassian.com>
2016-08-11 17:21:09 +1000

    chore(ci): fix up file names

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -1,29 +1,10 @@
-#!/usr/bin/env bash
-
-FAILED_CI_FILE=.ci_failed
-
-# Clears the file each time we run
-rm -f $FAILED_CI_FILE
+#!/bin/sh
+set -e
 
 HEAD_SHA=$(git rev-parse HEAD | cut -c1-6)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-# Run the Browserstack tests 1 component at a time, temporarily ignore failures
-FAILED_CI_FILE=$FAILED_CI_FILE HEAD_SHA=$HEAD_SHA CURRENT_BRANCH=$CURRENT_BRANCH BROWSERSTACK=1 `npm bin`/lerna exec --concurrency 1 -- ../../build/bin/test.sh --no-auto-watch
+KARMA_CONF="karma.conf.browserstack.master.js"
 
-# Check if any components failed on Browserstack
-if [ -f $FAILED_CI_FILE ]; then
-    echo
-    printf "\033[31m"
-    echo "The following components failed Browserstack tests:"
-    echo "---------------------------------------------------"
-    cat $FAILED_CI_FILE
-    echo "---------------------------------------------------"
-    printf "\033[0m"
-    exit 1
-fi
-printf "\033[32m"
-echo "----------------------------------"
-echo "Yay! All Browserstack tests passed"
-echo "----------------------------------"
-printf "\033[0m"
+# Run the Browserstack tests 1 component at a time, temporarily ignore failures
+HEAD_SHA=$HEAD_SHA CURRENT_BRANCH="$CURRENT_BRANCH" BROWSERSTACK=1 karma start $KARMA_CONF --reporters=dots --no-auto-watch --single-run $@
--- karma.conf.browserstack.master.js
+++ karma.conf.browserstack.master.js
@@ -1,11 +1,47 @@
 const baseConfig = require('./karma.conf.browserstack.js');
-const browserStackBrowsers = require('./build/lib/browserstack.browsers.master.js');
+const browserStackBrowsers = require('./build/lib/browserstack.browsers.branch.js');
+const fs = require('fs');
+const glob = require('glob');
+
+function concatTests(entryFile) {
+  let entryContents = '';
+  glob.sync('./packages/*/test/**/*.js').forEach((testFile) => {
+    entryContents += `require('${testFile}');\n`;
+  });
+  fs.writeFileSync(entryFile, entryContents);
+}
 
 module.exports = (config) => {
   baseConfig(config);
 
+  const webpackAndSourcemap = ['webpack', 'sourcemap'];
+  const entryFile = 'browserstack-entry.js';
+
+  concatTests(entryFile);
+
   Object.assign(config, {
     customLaunchers: browserStackBrowsers,
     browsers: Object.keys(browserStackBrowsers),
+
+    files: [
+      'packages/akutil-polyfills/src/index.js',
+      entryFile,
+    ],
+
+    preprocessors: {
+      'packages/akutil-polyfills/src/index.js': webpackAndSourcemap,
+      [entryFile]: webpackAndSourcemap,
+    },
+  });
+
+  config.webpack.module.loaders.push({
+    loader: 'babel-loader',
+    test: new RegExp(entryFile),
+    query: {
+      presets: [
+        'es2015',
+        'stage-0',
+      ],
+    },
   });
 };

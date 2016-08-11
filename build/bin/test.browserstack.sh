fbdc407cf55a8b52d43361d60f6987da43ecb6dd
Ben Gummer <bgummer@atlassian.com>
2016-08-12 05:51:03 +1000

    chore(ci): use retry-cli in BrowserStack

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -4,7 +4,5 @@ set -e
 HEAD_SHA=$(git rev-parse HEAD | cut -c1-6)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-KARMA_CONF="karma.conf.browserstack.master.js"
-
-# Run the Browserstack tests 1 component at a time, temporarily ignore failures
-HEAD_SHA=$HEAD_SHA CURRENT_BRANCH="$CURRENT_BRANCH" BROWSERSTACK=1 karma start $KARMA_CONF --reporters=dots --no-auto-watch --single-run $@
+# Run the Browserstack tests
+HEAD_SHA=$HEAD_SHA CURRENT_BRANCH="$CURRENT_BRANCH" BROWSERSTACK=1 build/bin/test.sh
--- build/bin/test.sh
+++ build/bin/test.sh
@@ -1,39 +1,26 @@
 #!/usr/bin/env bash
-
-# This runs Karma (for CI via Lerna) but pipes errors to a temporarily
-# file so that Lerna doesn't stop.
+set -e
 
 COMPONENT_DIR=$(basename `pwd`)
 
-{
-    if [ -d "test" ]; then
-        echo
-        echo -e "\033[34mTesting $COMPONENT_DIR\033[0m"
 
-        if [ -n "$FAILED_CI_FILE" ]; then
-          retry="retry --retries=2 -- "
-        else
-          retry=""
-        fi
+if [ -d "test" ]; then
+    echo
+    echo -e "\033[34mTesting $COMPONENT_DIR\033[0m"
 
-        if [ -n "$BROWSERSTACK" ]; then
-          if [ -n "$BRANCH_BUILD" ]; then
-            KARMA_CONF="../../karma.conf.browserstack.branch.js"
-          else
-            KARMA_CONF="../../karma.conf.browserstack.master.js"
-          fi
-        else
-          KARMA_CONF="../../karma.conf.js"
-        fi
-
-        $retry karma start $KARMA_CONF --single-run --reporters=dots,junit $@
+    if [ -n "$BROWSERSTACK" ]; then
+      retry="retry --retries=2 -- "
+      if [ -n "$BRANCH_BUILD" ]; then
+        KARMA_CONF="../../karma.conf.browserstack.branch.js"
+      else
+        KARMA_CONF="../../karma.conf.browserstack.master.js"
+      fi
     else
-        echo -e "\033[34mNo 'test' dir in packages/$COMPONENT_DIR; Skipping tests.\033[0m"
+      retry=""
+      KARMA_CONF="../../karma.conf.js"
     fi
-} || {
-    if [ -n "$FAILED_CI_FILE" ]; then
-        echo $COMPONENT_DIR >> ../../$FAILED_CI_FILE
-    else
-      exit 1
-    fi
-}
+
+    $retry karma start $KARMA_CONF --single-run --reporters=dots,junit $@
+else
+    echo -e "\033[34mNo 'test' dir in packages/$COMPONENT_DIR; Skipping tests.\033[0m"
+fi

1cbc27e7122d35c82dfaec58b5ded4a3750c3785
Ben Gummer <bgummer@atlassian.com>
2016-08-12 09:44:02 +1000

    chore(tests): use mocha skip instead of commenting

--- packages/ak-page/test/index.js
+++ packages/ak-page/test/index.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-// import { symbols } from 'skatejs';
-// import WebComponentTemplate from '../src/index.js';
+import { symbols } from 'skatejs';
+import WebComponentTemplate from '../src/index.js';
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -9,8 +9,8 @@ const expect = chai.expect; // eslint-disable-line no-unused-vars
 
 
 describe('ak-page', () => {
-  it('should be possible to create a component', () => {
-    // const component = new WebComponentTemplate();
-    // component[symbols.shadowRoot].innerHTML.should.match(/I am an .+? element!/);
+  it.skip('should be possible to create a component', () => {
+    const component = new WebComponentTemplate();
+    component[symbols.shadowRoot].innerHTML.should.match(/I am an .+? element!/);
   });
 });

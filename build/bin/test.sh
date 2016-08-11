bca51a657bd67cb2c61f704b048e2ee87041136d
Ben Gummer <bgummer@atlassian.com>
2016-08-12 06:12:30 +1000

    chore(tests): fix test entrypoint

--- build/bin/test.sh
+++ build/bin/test.sh
@@ -4,23 +4,24 @@ set -e
 COMPONENT_DIR=$(basename `pwd`)
 
 
-if [ -d "test" ]; then
-    echo
-    echo -e "\033[34mTesting $COMPONENT_DIR\033[0m"
+echo
+echo -e "\033[34mTesting $COMPONENT_DIR\033[0m"
 
-    if [ -n "$BROWSERSTACK" ]; then
-      retry="retry --retries=2 -- "
-      if [ -n "$BRANCH_BUILD" ]; then
-        KARMA_CONF="../../karma.conf.browserstack.branch.js"
-      else
-        KARMA_CONF="../../karma.conf.browserstack.master.js"
-      fi
-    else
-      retry=""
-      KARMA_CONF="../../karma.conf.js"
-    fi
+if [ -n "$BROWSERSTACK" ]; then
+  retry="retry --retries=2 -- "
+  if [ -n "$BRANCH_BUILD" ]; then
+    KARMA_CONF="./karma.conf.browserstack.branch.js"
+  else
+    KARMA_CONF="./karma.conf.browserstack.master.js"
+  fi
+else
+  retry=""
+  KARMA_CONF="../../karma.conf.js"
+fi
 
+# Run the tests if in Browserstack mode, or if test/ dir exists for Lerna
+if [ -n "$BROWSERSTACK" ] || [ -d "test" ]; then
     $retry karma start $KARMA_CONF --single-run --reporters=dots,junit $@
 else
-    echo -e "\033[34mNo 'test' dir in packages/$COMPONENT_DIR; Skipping tests.\033[0m"
+    echo -e "\033[34mNo 'test' dir in $COMPONENT_DIR; Skipping tests.\033[0m"
 fi

fa52991be6ab21fa17ff499a44383515db3ef33e
Ben Gummer <bgummer@atlassian.com>
2016-08-12 11:24:18 +1000

    chore(babel): add Symbol polyfill

--- packages/akutil-polyfills/src/index.js
+++ packages/akutil-polyfills/src/index.js
@@ -3,3 +3,4 @@ import 'webcomponents.js/src/WeakMap/WeakMap.js';  // for Android 4.0
 import 'webcomponents.js/src/MutationObserver/MutationObserver.js';  // for Android 4.0
 import 'skatejs-named-slots';
 import 'webcomponents.js/src/CustomElements/v1/CustomElements.js';
+import 'core-js/library/fn/symbol'; // because Babel uses Symbol

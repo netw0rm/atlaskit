471a8fc6172e356e1e098d9bd80863b03d4bd34f
Nadia Makarevich <nmakarevich@atlassian.com>
2016-08-15 11:05:01 +1000

    test(component): item tests done
    affects: ak-dropdown

--- packages/ak-dropdown/test/item.js
+++ packages/ak-dropdown/test/item.js
@@ -9,12 +9,38 @@ chai.should();
 const expect = chai.expect;
 
 describe('ak-dropdown-item:', () => {
-  it('should be possible to create a component', () => {
+  describe('general behavior:', () => {
     let component;
-    expect(() => (component = new Item())).not.to.throw(Error);
-    expect(component.tagName.toLowerCase()).to.equal('ak-dropdown-item');
-  });
+    let itemContainer;
 
+    beforeEach(() => {
+      component = new Item();
+      itemContainer = document.createElement('div');
+      itemContainer.appendChild(component);
+    });
+    it('should be possible to create a component', (done) => {
+      // testing to see that skate did its job as expected
+      // (in case some breaking changes in it that affect rendering)
+      setTimeout(() => {
+        expect(component.tagName.toLowerCase()).to.equal('ak-dropdown-item');
+        expect(component[symbols.shadowRoot]).to.be.defined;
+        expect(component[symbols.shadowRoot].firstChild).to.be.defined;
+      });
+      setTimeout(done);
+    });
+
+    it('click on a component should emit `ak-dropdown-selected` event', (done) => {
+      const event = new CustomEvent('click', {});
+      let called = false;
+      itemContainer.appendChild(component);
+      itemContainer.addEventListener('ak-dropdown-selected', () => (called = true));
+      setTimeout(() => {
+        component[symbols.shadowRoot].firstChild.dispatchEvent(event);
+      });
+      setTimeout(() => expect(called).to.equal(true));
+      setTimeout(() => done());
+    });
+  });
   describe('keyboard events:', () => {
     const eventsMap = {
       up: 'ak-dropdown-item-up',
@@ -24,13 +50,13 @@ describe('ak-dropdown-item:', () => {
       tab: 'ak-dropdown-item-tab',
     };
     let component;
-    let componentParent;
+    let itemContainer;
     let event;
 
     beforeEach(() => {
       component = new Item();
-      componentParent = document.createElement('div');
-      componentParent.appendChild(component);
+      itemContainer = document.createElement('div');
+      itemContainer.appendChild(component);
       event = new CustomEvent('keydown', {
         bubbles: true,
         cancelable: true,
@@ -40,86 +66,37 @@ describe('ak-dropdown-item:', () => {
       it(`keypress event on the ${key} key should emit ${eventsMap[key]} event`, (done) => {
         event.keyCode = keyCode(key);
         let called = false;
-        componentParent.addEventListener(eventsMap[key], () => (called = true));
+        itemContainer.addEventListener(eventsMap[key], () => (called = true));
         setTimeout(() => {
           component[symbols.shadowRoot].firstChild.dispatchEvent(event);
-          expect(called).to.be.true;
-          done();
-        }, 0);
+        });
+        setTimeout(() => expect(called).to.be.true);
+        setTimeout(() => done());
       });
 
       it('selected event should not be emitted on a disabled element', (done) => {
         component.disabled = true;
         event.keyCode = keyCode('enter');
         let called = false;
-        componentParent.addEventListener(eventsMap[key], () => (called = true));
+        itemContainer.addEventListener(eventsMap[key], () => (called = true));
         setTimeout(() => {
           component[symbols.shadowRoot].firstChild.dispatchEvent(event);
-          expect(called).to.be.false;
-          done();
-        }, 0);
+        });
+        setTimeout(() => expect(called).to.be.false);
+        setTimeout(() => done());
       });
 
       it('selected event should not be emitted on a selected element', (done) => {
         component.selected = true;
         event.keyCode = keyCode('enter');
         let called = false;
-        componentParent.addEventListener(eventsMap[key], () => (called = true));
+        itemContainer.addEventListener(eventsMap[key], () => (called = true));
         setTimeout(() => {
           component[symbols.shadowRoot].firstChild.dispatchEvent(event);
-          expect(called).to.be.false;
-          done();
-        }, 0);
+        });
+        setTimeout(() => expect(called).to.be.false);
+        setTimeout(() => done());
       });
     });
   });
-
-  // describe('keyboard events', () => {
-  //   let component;
-  //   let componentParent;
-  //   const event = new CustomEvent('keydown', {
-  //     bubbles: true,
-  //     cancelable: true,
-  //   });
-  //   let keyPressCallback;
-  //
-  //   beforeEach(() => {
-  //     component = new Item();
-  //     componentParent = document.createElement('div');
-  //     componentParent.appendChild(component);
-  //     keyPressCallback = sinon.spy();
-  //   });
-  //
-  //   it('should emit proper events on keyboard press', (done) => {
-  //     event.keyCode = keyCode('up');
-  //     let called = false;
-  //     componentParent.addEventListener('ak-dropdown-item-up', () => {
-  //       called = true;
-  //     });
-  //
-  //     setTimeout(() => {
-  //       component[symbols.shadowRoot].firstChild.dispatchEvent(event);
-  //       setTimeout(() => {
-  //         expect(called).to.be.true;
-  //         done();
-  //       }, 0);
-  //     }, 0);
-  //   });
-  //
-  //   it('should emit proper events on keyboard press', (done) => {
-  //     event.keyCode = keyCode('down');
-  //     let called = false;
-  //     componentParent.addEventListener('ak-dropdown-item-down', () => {
-  //       called = true;
-  //     });
-  //     debugger;
-  //     setTimeout(() => {
-  //       component[symbols.shadowRoot].firstChild.dispatchEvent(event);
-  //       setTimeout(() => {
-  //         expect(called).to.be.true;
-  //         done();
-  //       }, 0);
-  //     }, 0);
-  //   });
-  // });
 });

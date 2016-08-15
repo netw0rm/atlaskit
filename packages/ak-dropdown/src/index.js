44c313cb531a4409a4cdb9243439fa5204a4d5e8
Nadia Makarevich <nmakarevich@atlassian.com>
2016-08-15 14:07:54 +1000

    refactor(component): simplify event handlers in the dropdown
    affects: ak-dropdown

--- packages/ak-dropdown/src/index.js
+++ packages/ak-dropdown/src/index.js
@@ -2,59 +2,53 @@
 import 'style!./host.less';
 
 import { vdom, define } from 'skatejs';
-import { KeyPressHandler } from 'akutil-common';
 import './item';
 import './list';
 import './trigger';
-
-let keyPress;
+import keyCode from 'keycode';
 
 function toggleDialog(elem, value) {
   const list = elem.childNodes[1];
   const trigger = elem.childNodes[0];
-  return () => {
-    const isOpen = value === undefined ? !list.open : value;
+  const isOpen = value === undefined ? !list.open : value;
 
-    if (isOpen) {
-      list.open = isOpen;
-      trigger.opened = isOpen;
-      const l = list.childNodes.length;
-      list.childNodes.forEach((item, i) => {
-        item.focused = false;
-        if (item.first && i) {
-          item.first = false;
-        }
-        if (item.last && i !== l - 1) {
-          item.last = false;
-        }
-      });
-      list.childNodes[0].focused = true;
-      list.childNodes[0].first = true;
-      list.childNodes[l - 1].last = true;
-    } else {
-      list.childNodes.forEach((item) => {
-        item.focused = false;
-      });
-      list.open = isOpen;
-      trigger.opened = isOpen;
-    }
-  };
+  if (isOpen) {
+    list.open = isOpen;
+    trigger.opened = isOpen;
+    const l = list.childNodes.length;
+    list.childNodes.forEach((item, i) => {
+      item.focused = false;
+      if (item.first && i) {
+        item.first = false;
+      }
+      if (item.last && i !== l - 1) {
+        item.last = false;
+      }
+    });
+    list.childNodes[0].focused = true;
+    list.childNodes[0].first = true;
+    list.childNodes[l - 1].last = true;
+  } else {
+    list.childNodes.forEach((item) => {
+      item.focused = false;
+    });
+    list.open = isOpen;
+    trigger.opened = isOpen;
+  }
 }
 
-function selectItem(elem) {
+function selectItem(elem, event) {
   const list = elem.childNodes[1];
-  return (e) => {
-    const l = list.childNodes.length;
-    for (let i = 0; i < l; i++) {
-      const item = list.childNodes[i];
-      if (item.selected) {
-        item.selected = false;
-      }
+  const l = list.childNodes.length;
+  for (let i = 0; i < l; i++) {
+    const item = list.childNodes[i];
+    if (item.selected) {
+      item.selected = false;
     }
+  }
 
-    e.detail.item.selected = true;
-    toggleDialog(elem, false)();
-  };
+  event.detail.item.selected = true;
+  toggleDialog(elem, false);
 }
 
 function isChildOf(child, parent) {
@@ -67,58 +61,62 @@ function isChildOf(child, parent) {
   return isChildOf(child.parentNode, parent);
 }
 
-function handleClickOutside(elem) {
-  return (e) => {
-    if (e.target !== elem && !isChildOf(e.target, elem)) {
-      toggleDialog(elem, false)();
-    }
-  };
-}
-
 function focusPrev(elem) {
   const list = elem.childNodes[1];
-  return () => {
-    const l = list.childNodes.length;
-    for (let i = 0; i < l; i++) {
-      const item = list.childNodes[i];
-      if (item.focused && i) {
-        item.focused = false;
-        list.childNodes[i - 1].focused = true;
-        break;
-      }
+  const l = list.childNodes.length;
+  for (let i = 0; i < l; i++) {
+    const item = list.childNodes[i];
+    if (item.focused && i) {
+      item.focused = false;
+      list.childNodes[i - 1].focused = true;
+      break;
     }
-  };
+  }
 }
 
 function focusNext(elem) {
   const list = elem.childNodes[1];
-  return () => {
-    const l = list.childNodes.length;
-    for (let i = 0; i < l; i++) {
-      const item = list.childNodes[i];
-      if (item.focused && (i < (l - 1))) {
-        item.focused = false;
-        list.childNodes[i + 1].focused = true;
-        break;
-      }
+  const l = list.childNodes.length;
+  for (let i = 0; i < l; i++) {
+    const item = list.childNodes[i];
+    if (item.focused && (i < (l - 1))) {
+      item.focused = false;
+      list.childNodes[i + 1].focused = true;
+      break;
+    }
+  }
+}
+
+function handleClickOutside(elem) {
+  return (e) => {
+    if (e.target !== elem && !isChildOf(e.target, elem)) {
+      toggleDialog(elem, false);
+    }
+  };
+}
+
+function handleKeyPress(elem) {
+  return (event) => {
+    if (event.keyCode === keyCode('escape')) {
+      toggleDialog(elem, false);
     }
   };
 }
 
 export default define('ak-dropdown', {
   attached(elem) {
-    elem.addEventListener('ak-dropdown-trigger-activated', toggleDialog(elem));
-    elem.addEventListener('ak-dropdown-selected', selectItem(elem));
-    elem.addEventListener('ak-dropdown-item-up', focusPrev(elem));
-    elem.addEventListener('ak-dropdown-item-down', focusNext(elem));
-    elem.addEventListener('ak-dropdown-item-tab', toggleDialog(elem, false));
+    elem.addEventListener('ak-dropdown-trigger-activated', () => toggleDialog(elem));
+    elem.addEventListener('ak-dropdown-selected', (e) => selectItem(elem, e));
+    elem.addEventListener('ak-dropdown-item-up', () => focusPrev(elem));
+    elem.addEventListener('ak-dropdown-item-down', () => focusNext(elem));
+    elem.addEventListener('ak-dropdown-item-tab', () => toggleDialog(elem, false));
 
-    keyPress = new KeyPressHandler('ESCAPE', toggleDialog(elem, false));
     document.addEventListener('click', handleClickOutside(elem));
+    document.addEventListener('keypress', handleKeyPress(elem));
   },
   detached() {
-    keyPress.destroy();
     document.removeEventListener('click', handleClickOutside);
+    document.removeEventListener('click', handleKeyPress);
   },
   render() {
     return (

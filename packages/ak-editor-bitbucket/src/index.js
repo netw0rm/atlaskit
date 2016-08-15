f912755f19bccf621de6fb5efdf08c6a8ae79f84
Steve Mao <maochenyan@gmail.com>
2016-08-15 16:39:39 +1000

    skatejs skip does not work

--- packages/ak-editor-bitbucket/src/index.js
+++ packages/ak-editor-bitbucket/src/index.js
@@ -18,24 +18,10 @@ import { hyperlinkTransformer, markdownTransformer } from './paste-handlers';
 import BlockTypePlugin from 'atlassian-editorkit-block-type-plugin';
 import invert from 'lodash.invert';
 
-// A hack to target the content element until https://github.com/skatejs/skatejs/issues/721
-// is fixed.
 const contentClassName = `__content__${Date.now()}`;
 const initEditorSymbol = '__init_editor__';
 const pmSymbol = '__pm__';
 const readySymbol = '__ready__';
-
-const prosemirrorBlockToToolbarMap = {
-  paragraph: 'normalText',
-  // heading 1 (displayed in the blockType button) is actually heading 2
-  // heading 1 is reserved and not used in the editor
-  heading2: 'heading1',
-  heading3: 'heading2',
-  heading4: 'heading3',
-  code_block: 'monospace',
-};
-
-const toolbarToProsemirrorMap = invert(prosemirrorBlockToToolbarMap);
 
 export default define('ak-editor-bitbucket', {
   rendered(elem) {
@@ -48,85 +34,20 @@ export default define('ak-editor-bitbucket', {
 
   render(elem) {
     return (
-      <div className={shadowStyles.locals.root}>
-        <style>{shadowStyles.toString()}</style>
-        <Toolbar>
-          <ToolbarBlockType
-            disabled={!elem.canChangeBlockType}
-            selectedFont={elem.selectedFont}
-            onSelectFont={(event) => {
-              const font = event.detail.font;
-
-              const matches = toolbarToProsemirrorMap[font].match(/([a-zA-Z_]+)(\d*)/);
-              const blockType = matches[1];
-              const level = matches[2];
-
-              elem.blockTypePluginInstance.changeBlockType(blockType, { level });
-            }}
-          />
-          <ToolbarTextFormatting />
-          <ToolbarHyperlink />
-        </Toolbar>
-        <Content
-          skip
-          className={contentClassName}
-          openTop
-          openBottom
-        />
-        <Footer openTop />
+      <div>
+        <div class="prosemirror" />
+        <button onclick={() => elem.abc = 'foo'}>foo</button>
       </div>
     );
   },
 
   props: {
-    /**
-     * The initial markdown value for the editor.
-     *
-     * Changes to this value are not reflected in the editor. This property
-     * follows the "uncontrolled" component pattern in React. See
-     * https://facebook.github.io/react/docs/forms.html#uncontrolled-components
-     * for details.
-     */
-    defaultValue: prop.string({ attribute: true }),
+    abc: prop.string({ attribute: true }),
   },
 
   prototype: {
-    /**
-     * Focus the editor.
-     */
-    focus() {
-      const pm = this[pmSymbol];
-      pm.focus();
-    },
-
-    /**
-     * Clear the content of the editor, making it an empty document.
-     */
-    clear() {
-      const pm = this[pmSymbol];
-      pm.tr.delete(0, pm.doc.content.size).apply();
-    },
-
-    /**
-     * Return the current markdown value from the editor.
-     * @returns {string}
-     */
-    get value() {
-      const pm = this[pmSymbol];
-      return markdownSerializer.serialize(pm.doc);
-    },
-
-    /**
-     * Returns true if the editor has been initialised and is ready for
-     * interaction.
-     * @returns {boolean}
-     */
-    get ready() {
-      return this[readySymbol] || false;
-    },
-
     [initEditorSymbol]() {
-      const contentElement = this[symbols.shadowRoot].querySelector(`.${contentClassName}`);
+      const contentElement = this[symbols.shadowRoot].querySelector(`.prosemirror`);
       let blockTypePluginInstance;
       const pm = new ProseMirror({
         place: contentElement,
@@ -143,35 +64,6 @@ export default define('ak-editor-bitbucket', {
           ),
         ],
       });
-
-      blockTypePluginInstance.onChange(state => {
-        const name = state.selectedBlockType;
-        const blockType = prosemirrorBlockToToolbarMap[name];
-
-        this.selectedFont = blockType;
-        this.canChangeBlockType = state.enabled;
-      });
-
-      this.blockTypePluginInstance = blockTypePluginInstance;
-
-      // avoid invoking keyboard shortcuts in BB
-      pm.wrapper.addEventListener('keypress', e => e.stopPropagation());
-      pm.wrapper.addEventListener('keydown', e => e.stopPropagation());
-
-      // add the keymap
-      pm.addKeymap(buildKeymap(pm.schema));
-
-      // add paste handlers
-      pm.on.transformPasted.add(() => markdownTransformer(pm.schema));
-      pm.on.transformPasted.add(() => hyperlinkTransformer(pm));
-
-      // add node life cycle handler
-      pm.on.flush.add(() => nodeLifecycleHandler(pm));
-
-      // 'change' event is public API
-      pm.on.change.add(() => emit(this, 'change'));
-
-      this[pmSymbol] = pm;
     },
   },
 });

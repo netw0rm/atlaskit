54ef9845be4568c1fc17fde9fc20a61bb3dbb322
Nadia Makarevich <nmakarevich@atlassian.com>
2016-08-16 15:12:59 +1000

    fix(component): show/hide with styles instead of render
    affects: ak-dropdown

--- packages/ak-dropdown/src/list.js
+++ packages/ak-dropdown/src/list.js
@@ -6,36 +6,35 @@ const listWidthGap = 10;
 
 export default {
   render(elem) {
-    if (!elem.open) {
-      return '';
-    }
     let target = elem.parentNode.childNodes[0];
     const styles = {
       minWidth: `${target.getBoundingClientRect().width + listWidthGap}px`,
     };
 
     return (
-      <Layer
-        position="bottom left"
-        target={target}
-        ref={(layer) => {
-          setTimeout(() => {
-            if (elem.open && layer.alignment) {
-                // by default dropdown has opacity 0
-                // and only with attribute 'positioned' it has opacity 1
-                // this behavior is to avoid 'flashing' of dropdown
-                // when it's initially positioning itself on a page
-              elem.setAttribute('positioned', true);
-            }
-          }, 100);
-        }
-        }
-      >
-        <div className={shadowListStyles.locals.list} style={styles}>
-          <style>{shadowListStyles.toString()}</style>
-          <slot />
-        </div>
-      </Layer>
+      <div style={{ display: elem.open ? 'block' : 'none' }}>
+        <Layer
+          position="bottom left"
+          target={target}
+          ref={(layer) => {
+            setTimeout(() => {
+              if (elem.open && layer.alignment) {
+                  // by default dropdown has opacity 0
+                  // and only with attribute 'positioned' it has opacity 1
+                  // this behavior is to avoid 'flashing' of dropdown
+                  // when it's initially positioning itself on a page
+                elem.setAttribute('positioned', true);
+              }
+            });
+          }
+          }
+        >
+          <div className={shadowListStyles.locals.list} style={styles}>
+            <style>{shadowListStyles.toString()}</style>
+            <slot />
+          </div>
+        </Layer>
+      </div>
     );
   },
   props: {

2b69c6fa29b6e18e52481f188a1f68600437a1b0
Lakshan Perera <lperera@atlassian.com>
2016-08-16 15:01:37 +1000

    fix(bitbucket): linting on markdown-serializer.

--- packages/ak-editor-bitbucket/src/markdown-serializer.js
+++ packages/ak-editor-bitbucket/src/markdown-serializer.js
@@ -1,79 +1,82 @@
-/* eslint-disable */
 import { MarkdownSerializer } from 'prosemirror/dist/markdown';
 
 // :: MarkdownSerializer
 // A serializer for the [basic schema](#schema).
 export const markdownSerializer = new MarkdownSerializer({
   blockquote(state, node) {
-    state.wrapBlock("> ", null, node, () => state.renderContent(node))
+    state.wrapBlock('> ', null, node, () => state.renderContent(node));
   },
   code_block(state, node) {
     if (node.attrs.params == null) {
-      state.wrapBlock("    ", null, node, () => state.text(node.textContent, false))
+      state.wrapBlock('    ', null, node, () => state.text(node.textContent, false));
     } else {
-      state.write("```" + node.attrs.params + "\n")
-      state.text(node.textContent, false)
-      state.ensureNewLine()
-      state.write("```")
-      state.closeBlock(node)
+      state.write(`\`\`\`${node.attrs.params}\n`);
+      state.text(node.textContent, false);
+      state.ensureNewLine();
+      state.write('```');
+      state.closeBlock(node);
     }
   },
   heading(state, node) {
-    state.write(state.repeat("#", node.attrs.level) + " ")
-    state.renderInline(node)
-    state.closeBlock(node)
+    state.write(`${state.repeat('#', node.attrs.level)} `);
+    state.renderInline(node);
+    state.closeBlock(node);
   },
   horizontal_rule(state, node) {
-    state.write(node.attrs.markup || "---")
-    state.closeBlock(node)
+    state.write(node.attrs.markup || '---');
+    state.closeBlock(node);
   },
   bullet_list(state, node) {
-    state.renderList(node, "  ", () => (node.attrs.bullet || "*") + " ")
+    state.renderList(node, '  ', () => `${(node.attrs.bullet || '*')} `);
   },
   ordered_list(state, node) {
-    let start = node.attrs.order || 1
-    let maxW = String(start + node.childCount - 1).length
-    let space = state.repeat(" ", maxW + 2)
+    const start = node.attrs.order || 1;
+    const maxW = String(start + node.childCount - 1).length;
+    const space = state.repeat(' ', maxW + 2);
     state.renderList(node, space, i => {
-      let nStr = String(start + i)
-      return state.repeat(" ", maxW - nStr.length) + nStr + ". "
-    })
+      const nStr = String(start + i);
+      return `${state.repeat(' ', maxW - nStr.length)}${nStr}. `;
+    });
   },
   list_item(state, node) {
-    state.renderContent(node)
+    state.renderContent(node);
   },
   paragraph(state, node) {
     // for empty paragraphs add a new line,
     // without trying to render the node.
     if (node.content.content.length === 0) {
-      state.write("‌\n");
+      state.write('‌\n');
     } else {
       state.renderInline(node);
       state.closeBlock(node);
-    };
+    }
   },
 
   image(state, node) {
-    state.write("![" + state.esc(node.attrs.alt || "") + "](" + state.esc(node.attrs.src) +
-                (node.attrs.title ? " " + state.quote(node.attrs.title) : "") + ")")
+    const alt = state.esc(node.attrs.alt || '');
+    const src = state.esc(node.attrs.src);
+    const title = node.attrs.title ? ` ${state.quote(node.attrs.title)}` : '';
+    state.write(`![${alt}](${src} ${title})`);
   },
   hard_break(state) {
-    state.write("‌\n")
+    state.write('‌\n');
   },
   text(state, node) {
-    state.text(node.text)
+    state.text(node.text);
   },
   entity_inline(state, node) {
     state.text(node.attrs.data);
-  }
-}, {
-  em: {open: "*", close: "*", mixable: true},
-  strong: {open: "**", close: "**", mixable: true},
-  link: {
-    open: "[",
-    close(state, mark) {
-      return "](" + state.esc(mark.attrs.href) + (mark.attrs.title ? " " + state.quote(mark.attrs.title) : "") + ")"
-    }
   },
-  code: {open: "`", close: "`"}
-})
+}, {
+  em: { open: '*', close: '*', mixable: true },
+  strong: { open: '**', close: '**', mixable: true },
+  link: {
+    open: '[',
+    close(state, mark) {
+      const href = state.esc(mark.attrs.href);
+      const title = mark.attrs.title ? ` ${state.quote(mark.attrs.title)}` : '';
+      return `](${href}${title})`;
+    },
+  },
+  code: { open: '`', close: '`' },
+});

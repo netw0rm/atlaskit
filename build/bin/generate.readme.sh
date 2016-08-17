e9bb0862d88da45893ae96ac92c79d04e25d1f0e
Ben Wong <benwong@atlassian.com>
2016-08-17 18:43:11 +1000

    chore(docs): Support generating documentation for components with child components
    Will try to generate documentation for any .js files in the src/children dir

--- build/bin/generate.readme.sh
+++ build/bin/generate.readme.sh
@@ -13,13 +13,14 @@ else
 fi
 
 # Generate API docs
+API=""
 if compgen -G "*/index\.js" > /dev/null; then
 
   API="$(../../node_modules/.bin/jsdoc2md \
-  --plugin dmd-bitbucket ak-dmd-plugin \
-  --src ./src/index.js \
-  --member-index-format list \
-  --name-format)"
+    --plugin dmd-bitbucket ak-dmd-plugin \
+    --src ./src/index.js \
+    --member-index-format list \
+    --name-format)"
 
   if [[ $API == *"ERROR, Cannot find class"* ]]
   then
@@ -31,6 +32,28 @@ if compgen -G "*/index\.js" > /dev/null; then
 else
   API=""
 fi
+
+if [ -d ./src/children ]
+then
+  for file in $(find ./src/children -name "*.js"); do
+    NEXT="$(../../node_modules/.bin/jsdoc2md \
+      --plugin dmd-bitbucket ak-dmd-plugin \
+      --src $file \
+      --member-index-format list \
+      --name-format)"
+
+    if [[ $NEXT == *"ERROR, Cannot find class"* ]]
+    then
+      NEXT=""
+    else
+      NEXT="$NEXT\n"
+    fi
+
+    API="$API\n$NEXT"
+  done
+fi
+
+
 
 # Concatenate USAGE docs and JSDoc output
 if [ -n "$USAGE" ] || [ -n "$API" ]; then

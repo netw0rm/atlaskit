fe16f9a43d63549d67dd8cf075201e7e5ceebe8b
Joscha Feth <jfeth@atlassian.com>
2016-08-19 11:13:37 +1000

    fix(style): allow custom fill
    affects: ak-tag

--- packages/ak-tag/src/button.js
+++ packages/ak-tag/src/button.js
@@ -3,12 +3,14 @@ import shadowStyles from './shadow.less';
 
 import X from './X';
 
+const AK_COLOR_N500 = '#42526E'; // @ak-color-N500
+
 const button = (props) => (
   <button
     className={shadowStyles.locals.button}
     aria-label={props.text}
   >
-    <X />
+    <X fill={AK_COLOR_N500} />
   </button>
 );
 button.propTypes = {

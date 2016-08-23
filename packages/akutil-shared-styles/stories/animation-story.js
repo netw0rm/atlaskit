510fbb4cdef0a71b7400c762c9a93d6fdc84388d
Luke Batchelor <lbatchelor@atlassian.com>
2016-08-23 10:44:56 +1000

    chore(component): Added classNames and set up basic story for animations
    affects: akutil-shared-styles

--- packages/akutil-shared-styles/stories/animation-story.js
+++ packages/akutil-shared-styles/stories/animation-story.js
@@ -1,12 +1,26 @@
+import classNames from 'classnames';
 import { storiesOf } from '@kadira/storybook';
 import storyStyles from 'style!./stories.less';
 import { name } from '../package.json';
 
-const boxClass = storyStyles.locals.box;
+const containerClass = storyStyles.container;
+const boxClass = storyStyles.box;
+const boldClass = classNames(boxClass, storyStyles.bold);
+const optimisticClass = classNames(boxClass, storyStyles.optimistic);
+const combinedClass = classNames(boxClass, storyStyles.combined);
+const buttonContainerClass = storyStyles.buttonContainer;
+
 
 storiesOf(name, module)
   .add('Bold, Optimistic and Combined curves', () => (
-    <div className={boxClass}>
-      Something something
+    <div className={containerClass}>
+      <div className={boldClass}></div>
+      <div className={optimisticClass}></div>
+      <div className={combinedClass}></div>
+      <div className={buttonContainerClass}>
+        <input type="button" value="Bold" id="boldButton" />
+        <input type="button" value="Optimistic" id="optimisticButton" />
+        <input type="button" value="Combined" id="combinedButton" />
+      </div>
     </div>
   ));

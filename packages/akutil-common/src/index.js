0ae78eb68bfd7bba676c5c9dbb455a506f28c8f7
Ben Wong <benwong@atlassian.com>
2016-08-24 11:37:35 +1000

    feat(package): Fix missing changes from merge
    affects: ak-tabs, akutil-common
    
    Should add debounce helper, but not have afterMutations

--- packages/akutil-common/src/index.js
+++ packages/akutil-common/src/index.js
@@ -1,8 +1,7 @@
 import { enumeration } from './properties';
 import Alignment from './Alignment';
-import { afterMutations } from './testHelpers';
 import debounce from './debounce';
 import keyCode from 'keycode';
 import KeyPressHandler from './KeyPressHandler';
 
-export { enumeration, Alignment, afterMutations, debounce, keyCode, KeyPressHandler };
+export { enumeration, Alignment, debounce, keyCode, KeyPressHandler };

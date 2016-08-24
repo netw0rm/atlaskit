26b9ff936b0fd2259f69ebf81e94d35cb7db4bea
Ben Gummer <bengummer@gmail.com>
2016-08-24 11:52:38 +1000

    chore: remove unwanted tests

--- packages/ak-button-group/test/index.js
+++ packages/ak-button-group/test/index.js
@@ -2,7 +2,6 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { symbols } from 'skatejs';
 import ButtonGroup from '../src/index.js';
-import RadioButton from '../../ak-radio-button/src/index.js';
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -30,43 +29,6 @@ describe('ak-button-group', () => {
     setFixtures([btnGroup]);
     waitForRender(btnGroup, () => {
       btnGroup.hasOwnProperty(symbols.shadowRoot).should.equal(true);
-    });
-  });
-
-  describe('with radio buttons', () => {
-    let btnGroup;
-
-    beforeEach((done) => {
-      btnGroup = new ButtonGroup();
-      for (let i = 0; i < 3; i++) {
-        const radioBtn = new RadioButton();
-        radioBtn.innerText = `Radio ${i}`;
-        btnGroup.appendChild(radioBtn);
-      }
-
-      setFixtures([btnGroup]);
-      waitForRender(btnGroup, done);
-    });
-
-    it('should contain the expected number radio buttons', () => {
-      btnGroup.children.length.should.equal(3);
-    });
-
-    it('should not modify radio button selected state when loaded', () => {
-      btnGroup.childNodes.forEach(radioBtn => {
-        radioBtn.selected.should.equal(false);
-      });
-    });
-
-    it('should update selected prop on all radios when one is clicked', () => {
-      const radioBtns = btnGroup.childNodes;
-      radioBtns.forEach(radioBtnToSelect => {
-        radioBtnToSelect.click();
-
-        radioBtns.forEach(loopRadioBtn => {
-          loopRadioBtn.selected.should.equal(loopRadioBtn === radioBtnToSelect);
-        });
-      });
     });
   });
 });

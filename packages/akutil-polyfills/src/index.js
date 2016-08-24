c76796d2ed6cb752eae0c9c3408ffc2462e5f838
Joshua Nelson <jonelson@atlassian.com>
2016-08-24 16:48:04 -0700

    chore(temp): temprorary workaround while actual fix is in PR
    affects: akutil-polyfills
    
    see https://github.com/skatejs/web-components/issues/2

--- packages/akutil-polyfills/src/index.js
+++ packages/akutil-polyfills/src/index.js
@@ -1,2 +1,9 @@
 import 'es6-symbol/implement';
 import 'skatejs-web-components';
+
+// Workaround for https://github.com/skatejs/web-components/issues/2
+// TODO: when https://github.com/skatejs/web-components/issues/2 is resolved, merge it back into skatejs-web-components
+// see https://github.com/webcomponents/webcomponentsjs/pull/594
+HTMLElement.prototype = Object.create(HTMLElement.prototype,
+  { constructor: { configurable: true, value: HTMLElement, writable: true } }
+);

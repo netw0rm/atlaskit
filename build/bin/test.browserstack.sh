a4a4cb2a1e0f7e117b564070fa774bab5825a07c
Joscha Feth <jfeth@atlassian.com>
2016-08-25 18:58:16 +1000

    chore: make more readable

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -5,4 +5,12 @@ HEAD_SHA=$(git rev-parse --short HEAD)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Run the Browserstack tests
-HEAD_SHA=$HEAD_SHA CURRENT_BRANCH="$CURRENT_BRANCH" BROWSERSTACK=1 build/bin/test.sh
+HEAD_SHA=$HEAD_SHA \
+CURRENT_BRANCH="$CURRENT_BRANCH" \
+retry \
+--retries=2 \
+-- \
+karma start \
+karma.conf.browserstack.js \
+--single-run \
+--reporters=dots,junit

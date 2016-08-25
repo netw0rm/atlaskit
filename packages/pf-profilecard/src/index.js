55f8b8b49f457bc94e0d689a671d1d1b9538420f
Thorsten Basse <tbasse@atlassian.com>
2016-08-25 10:30:23 +1000

    feat(package): add pre-release version of profile-card
    affects: pf-profilecard

---
+++ packages/pf-profilecard/src/index.js
@@ -0,0 +1,11 @@
+import 'style!./host.less';
+
+import ProfileCardResourced from './wc/pf-profilecard-resourced';
+import ProfileCard from './wc/pf-profilecard';
+
+export {
+  ProfileCard,
+  ProfileCardResourced,
+};
+
+export default ProfileCard;

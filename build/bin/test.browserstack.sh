1ccb52504c5e7a94dc4769be6eff43fc6b2afdf6
Joscha Feth <jfeth@atlassian.com>
2016-08-29 17:39:32 +1000

    chore: print test run number

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -3,6 +3,7 @@ set -e
 
 HEAD_SHA=$(git rev-parse --short HEAD)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TRY=0
 
 # Run the Browserstack tests
 HEAD_SHA=$HEAD_SHA \
@@ -10,6 +11,8 @@ CURRENT_BRANCH="$CURRENT_BRANCH" \
 retry \
 --retries=2 \
 -- \
+TRY=$((TRY + 1)) \
+echo "\033[34mTest run #${TRY}\033[0m" && \
 karma start \
 karma.conf.browserstack.js \
 --single-run \

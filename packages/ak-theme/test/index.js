d9c6262d9952a11bc883707db35680074d23663f
Trey Shugart <treshugart@gmail.com>
2016-08-30 12:35:28 +1000

    test: Create base test file to iterate on.
    affects: ak-theme, akutil-common

--- packages/ak-theme/test/index.js
+++ packages/ak-theme/test/index.js
@@ -1,28 +1,15 @@
 import { afterMutations } from 'akutil-common-test';
-import { symbols } from 'skatejs';
-const { shadowRoot } = symbols;
-import chai from 'chai';
-import sinonChai from 'sinon-chai';
-import chaiAsPromised from 'chai-as-promised';
-import Component from '../src';
+import Component from '..';
 
-chai.use(sinonChai);
-chai.use(chaiAsPromised);
-chai.should();
-
-const expect = chai.expect;
+const { expect } = window.chai;
 
 describe('ak-theme', () => {
   let component;
 
   beforeEach((done) => {
     component = new Component();
-
-    afterMutations(
-      // append component to the body to ensure it has been rendered.
-      () => document.body.appendChild(component),
-      done
-    );
+    document.body.appendChild(component);
+    afterMutations(done);
   });
 
   afterEach(() => {
@@ -30,22 +17,6 @@ describe('ak-theme', () => {
   });
 
   it('should be possible to create a component', () => {
-    expect(component[shadowRoot].innerHTML).to.match(/My name is .+?!/);
-  });
-
-  describe('name prop', () => {
-    it('should modify the rendered name', (done) => {
-      const newName = 'InigoMontoya';
-      const expectedInnerHTML = `My name is ${newName}!`;
-      const paragraph = component[shadowRoot].querySelector('p');
-
-      // afterMutations will pause between each function passed to it to ensure the component has
-      // re-rendered before starting the next step.
-      afterMutations(
-        () => { component.name = newName; },
-        () => expect(paragraph.innerHTML).to.equal(expectedInnerHTML),
-        done
-      );
-    });
+    expect(component).to.not.be.visible;
   });
 });

98b8bd90dfbd3c1852ba7ac81a364a4a41be219a
Luke Batchelor <lbatchelor@atlassian.com>
2016-08-30 11:01:28 +1000

    docs(component): Added jsdoc annotations for akutil-common-test helpers
    affects: akutil-common, akutil-common-test

--- packages/akutil-common-test/src/getShadowRoot.js
+++ packages/akutil-common-test/src/getShadowRoot.js
@@ -1,5 +1,11 @@
 import { symbols } from 'skatejs';
 
+/**
+ * @description A simple helper method to retrieve a shadowRoot from a component
+ * @param {Node} component A node to pull the shadowRoot from.
+ * @example @js const elem = document.querySelector('.fixture').firstChild;
+ *  const sr = getShadowRoot(elem);
+ */
 function getShadowRoot(component) {
   return component[symbols.shadowRoot];
 }

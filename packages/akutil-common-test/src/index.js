076a3636f456df0c3a5c388f811f57c9445931d3
Joscha Feth <jfeth@atlassian.com>
2016-08-31 19:53:32 +1000

    feat: Add web component locator method
    affects: akutil-common-test

--- packages/akutil-common-test/src/index.js
+++ packages/akutil-common-test/src/index.js
@@ -3,5 +3,6 @@ import afterMutations from './after-mutations';
 import hasClass from './hasClass';
 import { keydown, keyup } from './user-interaction';
 import getShadowRoot from './getShadowRoot';
+import locateWebComponent from './locateWebComponent';
 
-export { afterMutations, waitUntil, hasClass, getShadowRoot, keydown, keyup };
+export { afterMutations, waitUntil, hasClass, getShadowRoot, keydown, keyup, locateWebComponent };

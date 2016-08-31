4091271eab12ea12d38a90bbe5090f4680ae340e
Matias Corbanini <mcorbanini@atlassian.com>
2016-08-31 10:16:38 +1000

    docs(component): JsDoc updated
    affects: akutil-common-test

--- packages/akutil-common-test/src/hasClass.js
+++ packages/akutil-common-test/src/hasClass.js
@@ -1,9 +1,10 @@
 /**
- * @description A simple helper method to check whether a node has a certain className on it.
- * @param {Node} component A node to check for a className on.
- * @param {String} className The className to check for the existance of
+ * @description A simple helper method to check whether a node has a set of classNames on it.
+ * @param {Node} component A node to check for classNames.
+ * @param {...[String]} classes A list of classnames to check for the existance of
  * @example @js const elem = document.querySelector('.fixture').firstChild;
  *  const elemIsHidden = hasClass(elem, 'hidden');
+ *  const elemIsSelectedAndHidden = hasClass(elem, 'hidden', 'selected');
  */
 export default function hasClass(component, ...classes) {
   const componentClasses = component.classList;

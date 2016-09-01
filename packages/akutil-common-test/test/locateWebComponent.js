77d4756673e5bc63fc5a32ec9cf121e09f9b0847
Joscha Feth <jfeth@atlassian.com>
2016-09-01 12:21:43 +1000

    test: refactor tests to use their own WC
    affects: akutil-common-test

--- packages/akutil-common-test/test/locateWebComponent.js
+++ packages/akutil-common-test/test/locateWebComponent.js
@@ -1,35 +1,56 @@
 import locateWebComponent from '../src/index.locateWebComponent';
-import Avatar from 'ak-avatar';
+import { getShadowRoot, waitUntil } from '../src';
+import { define } from 'skatejs';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 chai.should();
 chai.use(sinonChai);
 
+const componentPrefix = 'x-foo';
+
+function registerFooComponent() {
+  return define(componentPrefix, {
+    render() {
+    },
+  });
+}
+
 describe('locateWebComponent', () => {
   let component;
-  beforeEach((done) => {
-    component = new Avatar();
-    document.body.appendChild(component);
-    setTimeout(done);
+  let container;
+  const Foo = registerFooComponent();
+
+  beforeEach(() => {
+    component = new Foo();
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    container.appendChild(component);
+    return waitUntil(() => !!getShadowRoot(component));
   });
+
   afterEach(() => {
-    document.body.removeChild(component);
+    document.body.removeChild(container);
   });
 
   it('should be possible to locate a webcomponent by its name', () => {
-    locateWebComponent('ak-avatar').should.be.deep.equal([component]);
+    locateWebComponent('x-foo').should.be.deep.equal([component]);
     locateWebComponent('not-existent').should.be.deep.equal([]);
   });
 
   it('should be possible to locate a webcomponent by its prefix', () => {
-    locateWebComponent('ak-av').should.be.deep.equal([component]);
+    locateWebComponent('x-f').should.be.deep.equal([component]);
+    const Foo2 = registerFooComponent();
+    const component2 = new Foo2();
+    container.appendChild(component2);
+    component2.tagName.should.not.be.equal('x-foo');
+    locateWebComponent('x-foo').should.be.deep.equal([component, component2]);
   });
 
   it('should be possible to locate a webcomponent by its prefix in a given parent element', () => {
-    locateWebComponent('ak-avatar', document.body).should.be.deep.equal([component]);
+    locateWebComponent('x-foo', container).should.be.deep.equal([component]);
     const div = document.createElement('div');
     document.body.appendChild(div);
-    locateWebComponent('ak-avatar', div).should.be.deep.equal([]);
+    locateWebComponent('x-foo', div).should.be.deep.equal([]);
     document.body.removeChild(div);
   });
 });

707c72d353d83697a3b8550e3ee7d62662020f84
Steve Mao <maochenyan@gmail.com>
2016-09-01 14:10:35 +1000

    fix(editorIcon): event detail syntax

--- packages/ak-editor-toolbar-text-formatting/src/index.js
+++ packages/ak-editor-toolbar-text-formatting/src/index.js
@@ -7,7 +7,7 @@ import Icon from 'ak-editor-icon';
 /* eslint-disable react/prop-types */
 const ToggleButton = (props) => (
   <Button
-    onClick={() => !props.disabled && props.emit('toggletextformatting', props.name)}
+    onClick={() => !props.disabled && props.emit('toggletextformatting', { detail: props.name })}
     className={shadowStyles.locals.button}
     active={props.active}
     disabled={props.disabled}

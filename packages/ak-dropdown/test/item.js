dcddc7726858a9a9a33ddf6482bae403cf5abfb8
Nadia Makarevich <nmakarevich@atlassian.com>
2016-09-01 12:11:32 +1000

    test(tests): tests refactoring after a review
    affects: ak-dropdown

--- packages/ak-dropdown/test/item.js
+++ packages/ak-dropdown/test/item.js
@@ -2,8 +2,9 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { Item, events as dropdownEvents } from '../src';
 import keyCode from 'keycode';
-import { symbols, props } from 'skatejs';
+import { props } from 'skatejs';
 import 'custom-event-polyfill';
+import { waitUntil, getShadowRoot } from 'akutil-common-test';
 
 const defaultHeight = 30;
 const defaultGap = 10;
@@ -15,11 +16,10 @@ const expect = chai.expect;
 describe('ak-dropdown-item', () => {
   let itemContainer;
 
-  beforeEach((done) => {
+  beforeEach(() => {
     itemContainer = document.createElement('div');
     itemContainer.style.width = '300px';
     document.body.appendChild(itemContainer);
-    setTimeout(done);
   });
 
   afterEach(() => {
@@ -29,23 +29,23 @@ describe('ak-dropdown-item', () => {
   describe('general behavior', () => {
     let component;
 
-    beforeEach((done) => {
+    beforeEach(() => {
       component = new Item();
       itemContainer.appendChild(component);
-      setTimeout(done);
+      return waitUntil(() => getShadowRoot(component));
     });
 
     it('should be possible to create a component', () => {
       // testing to see that skate did its job as expected
       // (in case some breaking changes in it affect rendering)
-      expect(component[symbols.shadowRoot]).to.be.defined;
-      expect(component[symbols.shadowRoot].firstChild).to.be.defined;
+      expect(getShadowRoot(component)).to.be.defined;
+      expect(getShadowRoot(component).firstChild).to.be.defined;
     });
 
     it('click on a component should emit `selected` event', () => {
       const clickSpy = sinon.spy();
       itemContainer.addEventListener('selected', clickSpy);
-      component[symbols.shadowRoot].firstChild.click();
+      getShadowRoot(component).firstChild.click();
 
       expect(clickSpy.called).to.equal(true);
     });
@@ -54,7 +54,7 @@ describe('ak-dropdown-item', () => {
       const clickSpy = sinon.spy();
       itemContainer.addEventListener('selected', clickSpy);
       props(component, { disabled: true });
-      component[symbols.shadowRoot].firstChild.click();
+      getShadowRoot(component).firstChild.click();
 
       expect(clickSpy.called).to.equal(false);
     });
@@ -63,7 +63,7 @@ describe('ak-dropdown-item', () => {
       const clickSpy = sinon.spy();
       itemContainer.addEventListener('selected', clickSpy);
       props(component, { selected: true });
-      component[symbols.shadowRoot].firstChild.click();
+      getShadowRoot(component).firstChild.click();
 
       expect(clickSpy.called).to.equal(false);
     });
@@ -73,13 +73,15 @@ describe('ak-dropdown-item', () => {
     let component;
     let componentDomElem;
 
-    beforeEach((done) => {
+    beforeEach(() => {
       component = '<ak-dropdown-item><div>some text</div></ak-dropdown-item>';
       itemContainer.innerHTML = component;
-      setTimeout(() => {
+
+      return waitUntil(() =>
+        itemContainer.firstChild.getAttribute('defined') !== null
+      ).then(() => {
         component = itemContainer.firstChild;
-        componentDomElem = component[symbols.shadowRoot].firstChild;
-        done();
+        componentDomElem = getShadowRoot(component).firstChild;
       });
     });
 
@@ -95,7 +97,7 @@ describe('ak-dropdown-item', () => {
     });
 
     it(`gap between default slot and left edge of the component should be ${defaultGap}`, () => {
-      const rectComponent = component[symbols.shadowRoot].firstChild.getBoundingClientRect();
+      const rectComponent = getShadowRoot(component).firstChild.getBoundingClientRect();
       const rectDiv = component.childNodes[0].getBoundingClientRect();
       const gapLeft = rectDiv.left - rectComponent.left;
 
@@ -105,7 +107,7 @@ describe('ak-dropdown-item', () => {
     it(`gap between default slot and right edge should be at least ${defaultGap}`, () => {
       component.innerHTML = `<div>test text test texttest texttest texttest texttest
       texttest texttest texttest texttest texttest texttest text</div>`;
-      const rectComponent = component[symbols.shadowRoot].firstChild.getBoundingClientRect();
+      const rectComponent = getShadowRoot(component).firstChild.getBoundingClientRect();
       const rectDiv = component.childNodes[0].getBoundingClientRect();
       const gapRight = rectComponent.left + rectComponent.width - rectDiv.left - rectDiv.width;
 
@@ -117,16 +119,17 @@ describe('ak-dropdown-item', () => {
     let component;
     let componentDomElem;
 
-    beforeEach((done) => {
+    beforeEach(() => {
       const html = '<div slot="left" style="height:40px;width:40px;"></div><div>some text</div>';
       component = `<ak-dropdown-item>${html}</ak-dropdown-item>`;
       itemContainer.innerHTML = component;
 
       // wait until the component is rendered
-      setTimeout(() => {
+      return waitUntil(() =>
+        itemContainer.firstChild.getAttribute('defined') !== null
+      ).then(() => {
         component = itemContainer.firstChild;
-        componentDomElem = component[symbols.shadowRoot].firstChild;
-        done();
+        componentDomElem = getShadowRoot(component).firstChild;
       });
     });
 
@@ -135,7 +138,7 @@ describe('ak-dropdown-item', () => {
     });
 
     it(`gap between left slot and left edge of the component should be ${defaultGap}`, () => {
-      const rectComponent = component[symbols.shadowRoot].firstChild.getBoundingClientRect();
+      const rectComponent = getShadowRoot(component).firstChild.getBoundingClientRect();
       const rectDiv = component.firstChild.getBoundingClientRect();
       const gap = rectDiv.left - rectComponent.left;
 
@@ -163,7 +166,7 @@ describe('ak-dropdown-item', () => {
     let event;
     let calledSpy;
 
-    beforeEach((done) => {
+    beforeEach(() => {
       component = new Item();
       itemContainer.appendChild(component);
       event = new CustomEvent('keydown', {
@@ -172,14 +175,14 @@ describe('ak-dropdown-item', () => {
       });
       document.body.appendChild(itemContainer);
       calledSpy = sinon.spy();
-      setTimeout(done);
+      return waitUntil(() => getShadowRoot(component));
     });
 
     Object.keys(eventsMap).forEach((key) => {
       it(`keypress event on the ${key} key should emit ${eventsMap[key]} event`, () => {
         event.keyCode = keyCode(key);
         itemContainer.addEventListener(eventsMap[key], calledSpy);
-        component[symbols.shadowRoot].firstChild.dispatchEvent(event);
+        getShadowRoot(component).firstChild.dispatchEvent(event);
 
         expect(calledSpy.called).to.equal(true);
       });
@@ -188,7 +191,7 @@ describe('ak-dropdown-item', () => {
         event.keyCode = keyCode('enter');
         itemContainer.addEventListener(eventsMap[key], calledSpy);
         props(component, { disabled: true });
-        component[symbols.shadowRoot].firstChild.dispatchEvent(event);
+        getShadowRoot(component).firstChild.dispatchEvent(event);
 
         expect(calledSpy.called).to.equal(false);
       });
@@ -197,7 +200,7 @@ describe('ak-dropdown-item', () => {
         event.keyCode = keyCode('enter');
         itemContainer.addEventListener(eventsMap[key], calledSpy);
         props(component, { selected: true });
-        component[symbols.shadowRoot].firstChild.dispatchEvent(event);
+        getShadowRoot(component).firstChild.dispatchEvent(event);
 
         expect(calledSpy.called).to.equal(false);
       });

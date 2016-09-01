aee4b146e547316bc234142a7164d6380cfa8a61
Trey Shugart <treshugart@gmail.com>
2016-09-01 13:21:42 +1000

    test: Add test stubs for themeable and themes.
    affects: ak-theme
    
    AK-39

---
+++ packages/ak-theme/test/themeable.js
@@ -0,0 +1,3 @@
+describe('ak-theme, { themeable }', () => {
+
+});
---
+++ packages/ak-theme/test/themes.js
@@ -0,0 +1,7 @@
+import themes from '../src/themes';
+
+describe('ak-theme, { themes }', () => {
+  it('should be an object', () => {
+    expect(themes).to.be.an('object');
+  });
+});

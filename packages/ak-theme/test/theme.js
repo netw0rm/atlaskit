545242dac7531999294ad515efbbbc1cade85457
Trey Shugart <treshugart@gmail.com>
2016-09-02 17:04:57 +1000

    test: Stub out tests for ak-theme.
    affects: ak-theme
    
    AK-39

--- packages/ak-theme/test/theme.js
+++ packages/ak-theme/test/theme.js
@@ -18,4 +18,60 @@ describe('ak-theme', () => {
   it('should not be visible', () => {
     expect(window.getComputedStyle(theme).display).to.equal('none');
   });
+
+  it('allVars should be an object', () => {
+
+  });
+
+  it('allVars should contain mixed in themes', () => {
+
+  });
+
+  it('id should be string', () => {
+
+  });
+
+  it('id should be an attribute', () => {
+
+  });
+
+  it('mixin should be a string', () => {
+
+  });
+
+  it('mixin should be an attribute', () => {
+
+  });
+
+  it('mixins should be an array', () => {
+
+  });
+
+  it('ownVars should be an object', () => {
+
+  });
+
+  it('ownVars should not contain mixed in themes', () => {
+
+  });
+
+  it('should emit an event when attached', () => {
+
+  });
+
+  it('should emit an event when detached', () => {
+
+  });
+
+  it('should empty the old theme vars if the id changes', () => {
+
+  });
+
+  it('should set the new theme vars if the id changes', () => {
+
+  });
+
+  it('should not re-render after the initial render', () => {
+
+  });
 });

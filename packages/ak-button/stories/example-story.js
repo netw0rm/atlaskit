a5208579b513a1c09584b27d0626856ce20c7592
Matias Corbanini <mcorbanini@atlassian.com>
2016-09-02 11:19:04 +1000

    fix(story): Added class to ak-button in storybook to prevent style missmatch
    affects: ak-button

--- packages/ak-button/stories/example-story.js
+++ packages/ak-button/stories/example-story.js
@@ -2,9 +2,12 @@ import { storiesOf, action } from '@kadira/storybook';
 import reactify from 'akutil-react';
 import AkButtonTemplate, { APPEARANCE } from '../src/index';
 const { React, ReactDOM } = window;
+import hostStyles from '../src/host.less';
 import buttonStatesExample from './AkButtonStates';
 import { name } from '../package.json';
 import IconTemplate from 'ak-icon';
+
+const buttonClass = hostStyles.locals.akButton;
 
 const AkButton = reactify(AkButtonTemplate, {
   React,
@@ -27,6 +30,7 @@ const GLYPHS = [
 const AkButtonStates = buttonStatesExample({
   React,
   AkButton,
+  buttonClass,
   APPEARANCE,
   Icon,
   GLYPHS: [false].concat(GLYPHS),
@@ -34,58 +38,70 @@ const AkButtonStates = buttonStatesExample({
 
 storiesOf(name, module)
   .add('a standard ak-button', () => (
-    <AkButton>
+    <AkButton className={buttonClass}>
       Button
     </AkButton>
   ))
   .add('a primary ak-button', () => (
-    <AkButton appearance={APPEARANCE.PRIMARY}>Primary Button</AkButton>
+    <AkButton className={buttonClass} appearance={APPEARANCE.PRIMARY}>Primary Button</AkButton>
   ))
   .add('a disabled ak-button', () =>
-    <AkButton disabled onclick={action('clicking the WebComponent')}>
+    <AkButton className={buttonClass} disabled onclick={action('clicking the WebComponent')}>
       Button
     </AkButton>
   )
   .add('a subtle ak-button', () =>
-    <AkButton appearance={APPEARANCE.SUBTLE} >
+    <AkButton className={buttonClass} appearance={APPEARANCE.SUBTLE} >
       Button
     </AkButton>
   )
   .add('a selected button', () =>
-    <AkButton selected>
+    <AkButton className={buttonClass} selected>
       Button
     </AkButton>
   )
   .add('a link button', () =>
-    <AkButton appearance={APPEARANCE.LINK}>
+    <AkButton className={buttonClass} appearance={APPEARANCE.LINK}>
       Button
     </AkButton>
   )
   .add('an ak-button that emits an action when it is clicked', () => (
-    <AkButton id="myComponent" onClick={action('clicking the WebComponent')}>Button</AkButton>
+    <AkButton className={buttonClass} onClick={action('clicking the WebComponent')}>
+      Button
+    </AkButton>
   ))
   .add('compact buttons with all attributes', () =>
     <div>
       <style>{"ak-button {margin-left: 10px;}"}</style>
-      <AkButton compact>
+      <AkButton className={buttonClass} compact>
         Button
       </AkButton>
-      <AkButton compact appearance="primary">
+      <AkButton className={buttonClass} compact appearance="primary">
         Button
       </AkButton>
-      <AkButton compact appearance="subtle">
+      <AkButton className={buttonClass} compact appearance="subtle">
         Button
       </AkButton>
-      <AkButton compact selected>
+      <AkButton className={buttonClass} compact selected>
         Button
       </AkButton>
-      <AkButton compact disabled onclick={action('clicking the WebComponent')}>
+      <AkButton
+        className={buttonClass}
+        compact
+        disabled
+        onclick={action('clicking the WebComponent')}
+      >
         Button
       </AkButton>
     </div>
   )
   .add('an ak-button that emits an action when it is clicked', () => (
-    <AkButton id="myComponent" onClick={action('clicking the WebComponent')}>Button</AkButton>
+    <AkButton
+      className={buttonClass}
+      onClick={action('clicking the WebComponent')}
+    >
+      Button
+    </AkButton>
   ))
   .add('an ak-button with icons', () => {
     const buttonStyles = { 'margin-right': '10px', display: 'inline-flex' };
@@ -97,6 +113,7 @@ storiesOf(name, module)
               {[APPEARANCE.STANDARD, APPEARANCE.PRIMARY, APPEARANCE.SUBTLE]
                 .map(appearance => (
                   <AkButton
+                    className={buttonClass}
                     style={buttonStyles}
                     onclick={action('clicking the WebComponent')}
                     appearance={appearance}
@@ -107,11 +124,11 @@ storiesOf(name, module)
                   )
                 )
                 .concat([
-                  <AkButton style={buttonStyles} selected>
+                  <AkButton className={buttonClass} style={buttonStyles} selected>
                     <Icon slot="before" key={glyph} glyph={glyph} />
                     Button
                   </AkButton>,
-                  <AkButton style={buttonStyles} disabled>
+                  <AkButton className={buttonClass} style={buttonStyles} disabled>
                     <Icon slot="before" key={glyph} glyph={glyph} />
                       Button
                   </AkButton>,

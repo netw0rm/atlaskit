37ce532ecdced9a9cecf6b360b885631975973c7
Ben Gummer <bgummer@atlassian.com>
2016-09-05 09:35:13 +1000

    feat(label): click on label triggers input focus

--- packages/ak-text-field/src/index.js
+++ packages/ak-text-field/src/index.js
@@ -1,7 +1,21 @@
 import 'style!./host.less';
 // import classNames from 'classnames';
-import { emit, vdom, define } from 'skatejs';
+import { symbols, vdom, define } from 'skatejs';
 import shadowStyles from './shadow.less';
+
+function handleLabelClick(elem) {
+  return () => {
+    const nodes = elem[symbols.shadowRoot].querySelector('slot').assignedNodes();
+
+    // Loop over light DOM nodes and focus on first input
+    for (let i = 0; i < nodes.length; i++) {
+      if (nodes[i].tagName === 'INPUT') {
+        nodes[i].focus();
+        break;
+      }
+    }
+  };
+}
 
 /**
  * @description Create instances of the component programmatically, or using markup.
@@ -12,15 +26,12 @@ import shadowStyles from './shadow.less';
 export default define('ak-text-field', {
   render(elem) {
     return (
-      // JSX requires that there only be a single root element.
-      // Incremental DOM doesn't require this.
       <div className={shadowStyles.locals.defaultSlotWrapper}>
-        {/* This is required for elements in the shadow root to be styled.
-           This is wrapped in the <div /> because you can't have more than one
-           root element.
-        */}
         <style>{shadowStyles.toString()}</style>
-        <label className={shadowStyles.locals.label}>{elem.label}</label>
+        <label
+          onclick={handleLabelClick(elem)}
+          className={shadowStyles.locals.label}
+        >{elem.label}</label>
         <slot className={shadowStyles.locals.defaultSlotElement} />
       </div>
     );
@@ -35,31 +46,6 @@ export default define('ak-text-field', {
      */
     name: {
       default: 'TextField',
-    },
-  },
-  prototype: {
-    /**
-     * @description Fire an event containing the name of the element.
-     * @memberof TextField
-     * @function
-     * @instance
-     * @fires TextField#announce-name
-     * @return {TextField} The TextField element.
-     * @example @js component.announce(); // Fires the announce-name event.
-     */
-    announce() {
-      /**
-       * @event TextField#announce-name
-       * @memberof TextField
-       * @description Fired when the `announce` method is called.
-       * @property {String} detail.name The name of the component.
-       */
-      emit(this, 'announce-name', {
-        detail: {
-          name: this.name,
-        },
-      });
-      return this;
     },
   },
 });

da820cae1e1de51339d474992fc35d906cca0f9e
Lakshan Perera <lperera@atlassian.com>
2016-09-05 10:51:25 +1000

    feat(ak-editor-plugin-bb-mentions): Hydrate nodes on flush
    affects: ak-editor-plugin-bb-mentions
    
    This provides a way to hydrate mention nodes by calling render or autocomplete handlers when the
    
    editor contnet are flushed to DOM.

--- packages/ak-editor-plugin-bb-mentions/src/index.ts
+++ packages/ak-editor-plugin-bb-mentions/src/index.ts
@@ -5,6 +5,8 @@ import { mentionRule, emoticonRule } from './input-rules';
 
 class BitbucketMentionsPluginState {
   pm: ProseMirror;
+  renderHandler: (el: HTMLElement, pm: ProseMirror) => void
+  autocompleteHandler: (el: HTMLElement, pm: ProseMirror) => void
 
   constructor(pm: ProseMirror) {
     this.pm = pm;
@@ -15,6 +17,35 @@ class BitbucketMentionsPluginState {
         inputRules.ensure(pm).addRule(rule);
       });
     }
+
+    this.pm.on.flush.add(() => this.hydrateNodes());
+  }
+
+  hydrateNodes(): void {
+    const pm = this.pm;
+    const wrapper = pm.wrapper;
+    const elements = wrapper.querySelectorAll('[editor-hydrate]');
+
+    if (!elements.length) {
+      return
+    }
+
+    Array.prototype.forEach.call(elements, (el: HTMLElement) => {
+      el.removeAttribute('editor-hydrate');
+
+      const nodeType = el.getAttribute('editor-node-type');
+      if (nodeType === 'mention') {
+        const data = el.getAttribute('editor-data');
+
+        // if theres data set, render the element
+        // otherwise prompt for autocomplete.
+        if (data && data.length) {
+          this.renderHandler(el, pm);
+        } else {
+          this.autocompleteHandler(el, pm);
+        }
+      }
+    });
   }
 }
 

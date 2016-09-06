a1e9d3216ad54166405556d70ee17669d45f65c3
Lakshan Perera <lperera@atlassian.com>
2016-09-06 13:41:51 +1000

    fix(ak-editor-plugin-mentions): Removed unnecessary type declarations.
    affects: ak-editor-plugin-mentions

--- packages/ak-editor-plugin-mentions/src/input-rules.ts
+++ packages/ak-editor-plugin-mentions/src/input-rules.ts
@@ -2,10 +2,10 @@ import { ProseMirror, InputRule } from 'ak-editor-prosemirror';
 
 const makeNode = (pm: ProseMirror) => pm.schema.nodes.mention.create({ data: '' });
 
-export const mentionRule : InputRule = new InputRule(/(?:^|\s)@$/, '@', (pm: ProseMirror, match: string[], pos: number) => {
+export const mentionRule = new InputRule(/(?:^|\s)@$/, '@', (pm: ProseMirror, match: string[], pos: number) => {
   return pm.tr.replaceWith(pos-1, pos, makeNode(pm)).apply();
 });
 
-export const emoticonRule : InputRule = new InputRule(/(?:^|\s):$/, ':', (pm: ProseMirror, match: string[], pos: number) => {
+export const emoticonRule = new InputRule(/(?:^|\s):$/, ':', (pm: ProseMirror, match: string[], pos: number) => {
   return pm.tr.replaceWith(pos-1, pos, makeNode(pm)).apply();
 });

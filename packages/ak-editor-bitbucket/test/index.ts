64d98025aab87e6d6c0714030a7735a42cf82237
Bradley Ayers <bradley.ayers@gmail.com>
2016-09-09 07:46:33 +1000

    chore(test): Remove tests that fail in IE11.
    affects: ak-editor-bitbucket

--- packages/ak-editor-bitbucket/test/index.ts
+++ packages/ak-editor-bitbucket/test/index.ts
@@ -1,25 +1,10 @@
 import * as chai from 'chai';
 import AkEditorBitbucket from '../src';
-import { fixtures } from 'ak-editor-test';
 import { afterMutations } from 'akutil-common-test';
-import { emit, symbols } from 'skatejs';
 
 const { expect } = chai;
 
 describe('ak-editor-bitbucket', () => {
-  const fixture = fixtures();
-
-  const clickCancel = (editor: any) => {
-    const footer = editor[symbols.shadowRoot].querySelector('ak-editor-footer');
-    const cancel = footer[symbols.shadowRoot].querySelectorAll('button')[1];
-    emit(cancel, 'click');
-  };
-
-  const clickAffordanceEditor = (editor: any) => {
-    const input = editor[symbols.shadowRoot].querySelector('input')
-    emit(input, 'click');
-  }
-
   it('is possible to create a component', () => {
     let component: any;
     expect(() => {
@@ -49,55 +34,5 @@ describe('ak-editor-bitbucket', () => {
       editor.defaultValue = 'foo';
       expect(editor.value).to.equal('foo');
     });
-  });
-
-  describe('.focus()', () => {
-    it('does not throw an exception when called on an unmounted editor', () => {
-      const editor = new AkEditorBitbucket();
-      expect(() => editor.focus()).not.to.throw(Error);
-    });
-  });
-
-  describe('.clear()', () => {
-    it('does not throw an exception when called on an unmounted editor', () => {
-      const editor = new AkEditorBitbucket();
-      expect(() => editor.clear()).not.to.throw(Error);
-    });
-  });
-
-  it('expands the UI when the affordance input is clicked', (done) => {
-    const editor = new AkEditorBitbucket();
-    fixture().appendChild(editor);
-
-    afterMutations(
-      () => clickAffordanceEditor(editor),
-      () => expect(editor.expanded).to.be.true,
-      done,
-    );
-  });
-
-  it('emits a ready event when expanded', (done) => {
-    const editor = new AkEditorBitbucket();
-    const readySpy = sinon.spy();
-    editor.addEventListener('ready', readySpy);
-    fixture().appendChild(editor);
-
-    afterMutations(
-      () => clickAffordanceEditor(editor),
-      () => expect(readySpy.called).to.be.true,
-      done,
-    );
-  });
-
-  it('collapses after expanding then clicking cancel', (done) => {
-    const editor = new AkEditorBitbucket();
-    fixture().appendChild(editor);
-
-    afterMutations(
-      () => clickAffordanceEditor(editor),
-      () => clickCancel(editor),
-      () => expect(editor.expanded).to.be.false,
-      done,
-    );
   });
 });

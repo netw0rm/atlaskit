25e3a6d8b61a82d3710b4febe522f7f1ef150efe
Nadia Makarevich <nmakarevich@atlassian.com>
2016-09-09 10:01:20 +1000

    test(tests): disable unnecessary tests
    affects: ak-heatmap

--- packages/ak-heatmap/test/index.js
+++ packages/ak-heatmap/test/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { waitUntil, getShadowRoot } from 'akutil-common-test';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
@@ -31,42 +32,35 @@ describe('ak-heatmap', () => {
     it('should export a base component', () => {
       (new MyComponent).should.be.an.instanceof(Component);
     });
-
-    it('should have an events export with defined events', () => {
-      events.should.be.defined;
-      Object.keys(events).should.be.deep.equal([
-        'announceName',
-      ]);
-    });
   });
-
-  describe('logic', () => {
-    let component;
-    let shadowRoot;
-
-    beforeEach(() => setupComponent().then(newComponent => {
-      component = newComponent;
-      shadowRoot = getShadowRoot(component);
-    }));
-    afterEach(() => tearDownComponent(component));
-
-    it('should be possible to create a component', () => {
-      expect(shadowRoot.innerHTML).to.match(/My name is .+?!/);
-    });
-
-    describe('name prop', () => {
-      it('should modify the rendered name', () => {
-        const newName = 'InigoMontoya';
-        const expectedInnerHTML = `My name is ${newName}!`;
-        const paragraph = shadowRoot.querySelector('p');
-
-        const nameHasBeenModifiedCorrectly = () => (paragraph.innerHTML === expectedInnerHTML);
-
-        component.name = newName;
-
-        // here we can wrap our assertions in promises and just check that the promise was fulfilled
-        return waitUntil(nameHasBeenModifiedCorrectly).should.be.fulfilled;
-      });
-    });
-  });
+  //
+  // describe('logic', () => {
+  //   let component;
+  //   let shadowRoot;
+  //
+  //   beforeEach(() => setupComponent().then(newComponent => {
+  //     component = newComponent;
+  //     shadowRoot = getShadowRoot(component);
+  //   }));
+  //   afterEach(() => tearDownComponent(component));
+  //
+  //   it('should be possible to create a component', () => {
+  //     expect(shadowRoot.innerHTML).to.match(/My name is .+?!/);
+  //   });
+  //
+  //   describe('name prop', () => {
+  //     it('should modify the rendered name', () => {
+  //       const newName = 'InigoMontoya';
+  //       const expectedInnerHTML = `My name is ${newName}!`;
+  //       const paragraph = shadowRoot.querySelector('p');
+  //
+  //       const nameHasBeenModifiedCorrectly = () => (paragraph.innerHTML === expectedInnerHTML);
+  //
+  //       component.name = newName;
+  //
+  //       // here we can wrap our assertions in promises and just check that the promise was fulfilled
+  //       return waitUntil(nameHasBeenModifiedCorrectly).should.be.fulfilled;
+  //     });
+  //   });
+  // });
 });

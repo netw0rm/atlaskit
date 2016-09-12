2022b402624dead2a39e47e02f906d253992082f
Steve Mao <maochenyan@gmail.com>
2016-09-12 13:55:02 +1000

    feat(bitbucket): also remove `sliceContents`

--- packages/ak-editor-bitbucket/src/paste-handlers.ts
+++ packages/ak-editor-bitbucket/src/paste-handlers.ts
@@ -4,18 +4,8 @@ import { Slice } from 'ak-editor-prosemirror';
 
 export const markdownTransformer = function(schema: any, slice: any) {
   var sliceContents = '';
-  const canParseAsMarkdown = slice.content.content.every((node: any) => {
-    // we attempt to convert to Markdown if only all nodes are paragraphs
-    if (node.type.name !== "paragraph") return false;
 
-    sliceContents = sliceContents + node.toDOM().innerText + '\n';
-    // all nodes in a paragraph should either be text (without any marks) and hard breaks.
-    return node.content.content.every((child: any) => {
-      return ['text', 'hard_break'].indexOf(child.type.name) > -1 && !child.marks.length
-    });
-  });
-
-  if (canParseAsMarkdown && sliceContents.length) {
+  if (sliceContents.length) {
     return new Slice(sliceContents, slice.openLeft, slice.openRight, slice.possibleParent);
   } else {
     return slice

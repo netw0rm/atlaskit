99ea3842174abf5480a4761b82715b0c47d6d154
Artur Bodera <abodera@atlassian.com>
2016-09-12 15:15:47 +1000

    test(component): Add basic post-render tests of the editor.
    affects: ak-editor-bitbucket, ak-editor-toolbar, ak-editor-toolbar-block-type

--- packages/ak-editor-bitbucket/test/index.ts
+++ packages/ak-editor-bitbucket/test/index.ts
@@ -1,8 +1,25 @@
 import * as chai from 'chai';
 import AkEditorBitbucket from '../src';
-import { afterMutations } from 'akutil-common-test';
+import { afterMutations, waitUntil } from 'akutil-common-test';
 
 const { expect } = chai;
+
+function activateEditorByClicking(editor: AkEditorBitbucket) : void {
+  const clickEvent = new MouseEvent("click", {
+    view: window,
+    bubbles: true,
+    cancelable: true
+  });
+  const inputEl = editor.shadowRoot.querySelector('* > input');
+  expect(inputEl).to.not.be.null;
+  expect(inputEl.dispatchEvent(clickEvent)).to.be.true;
+}
+
+function waitForEditorToBeReady(editor: AkEditorBitbucket) : Promise<boolean> {
+  return waitUntil(() => {
+    return editor && !!editor._ready;
+  });
+}
 
 describe('ak-editor-bitbucket', () => {
   it('is possible to create a component', () => {
@@ -21,6 +38,66 @@ describe('ak-editor-bitbucket', () => {
   it('does not be expanded by default', () => {
     const editor = new AkEditorBitbucket();
     expect(editor.expanded).to.be.false;
+  });
+
+  describe('default config', function(){
+    let tmpContainer : any;
+    let editor : any;
+
+    this.timeout(30000);
+
+    beforeEach((done) => {
+      tmpContainer = document.createElement('div');
+      tmpContainer.innerHTML = `<ak-editor-bitbucket></ak-editor-bitbucket>`;
+      document.body.appendChild(tmpContainer);
+
+      afterMutations(
+        () => (editor = tmpContainer.firstChild),
+        done
+      );
+    });
+
+    afterEach(() => {
+      document.body.removeChild(tmpContainer);
+    });
+
+    it('should expand after clicking the input element', (done) => {
+      activateEditorByClicking(editor);
+      expect(editor.expanded).to.be.true;
+      done();
+    });
+
+    it('should have toolbar with all default elements', () => {
+      activateEditorByClicking(editor);
+      return waitForEditorToBeReady(editor).then(() => {
+        [
+          'ak-editor-toolbar',
+          'ak-editor-toolbar-block-type',
+          'ak-editor-toolbar-lists',
+          'ak-editor-toolbar-hyperlink',
+          'ak-editor-toolbar-text-formatting'
+        ].forEach((selector) => {
+          expect(editor.shadowRoot.querySelector(selector)).to.not.be.null;
+        });
+      });
+    });
+
+    it('should have options in block type dropdown', () => {
+      activateEditorByClicking(editor);
+
+      return waitForEditorToBeReady(editor).then(() => {
+        const bt = editor.shadowRoot.querySelector('ak-editor-toolbar-block-type');
+        expect(bt).to.not.be.null;
+
+        const fs = bt.shadowRoot.querySelector('ak-editor-toolbar-block-type-font-select');
+        expect(fs).to.not.be.null;
+
+        return waitUntil(() => {
+          return bt.shadowRoot.querySelectorAll('ak-editor-toolbar-block-type-option').length >= 2;
+        }, 2000);
+      });
+    });
+
   });
 
   describe('.value', () => {

6094783e99dee8e45cafd3f754188dd3d9aeb852
Joscha Feth <joscha@feth.com>
2016-09-14 19:09:53 +1000

    test: Make sure there is no extraneous exports
    affects: ak-tabs

--- packages/ak-tabs/test/test-exports.js
+++ packages/ak-tabs/test/test-exports.js
@@ -8,6 +8,14 @@ chai.should();
 
 describe(name, () => {
   describe('exports', () => {
+    it('should have a well-defined set of exports', () => {
+      Object.keys(exports).should.be.deep.equal([
+        'Tab',
+        'events',
+        'default',
+      ]);
+    });
+
     it('should export a base component', () => {
       Tabs.should.be.equals(exports.default);
       (new Tabs).should.be.an.instanceof(Component);

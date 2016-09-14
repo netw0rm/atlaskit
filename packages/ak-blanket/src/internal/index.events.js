d0c409ed77552eee510d879dd22773dbc176778c
Maria del Mar Mallorquin <mmallorquincanet@atlassian.com>
2016-09-14 15:16:25 +1000

    docs: Add a cross-link from the event to the controlling property
    affects: ak-blanket

--- packages/ak-blanket/src/internal/index.events.js
+++ packages/ak-blanket/src/internal/index.events.js
@@ -1,5 +1,6 @@
 /**
 * This event is emitted when the blanket is clicked.
+* The emission of this event can be controlled by the {@link Blanket#clickable} property.
 *
 * @event Blanket#activate
 */

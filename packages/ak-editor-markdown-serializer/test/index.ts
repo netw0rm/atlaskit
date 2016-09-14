b1f7acf0cbf145f6ce9e2947e98a0d85b0fee039
Lakshan Perera <lperera@atlassian.com>
2016-09-14 17:21:00 +1000

    feat: WIP: Markdown serializer package for editor.
    affects: ak-editor-markdown-serializer, ak-editor-prosemirror

---
+++ packages/ak-editor-markdown-serializer/test/index.ts
@@ -0,0 +1,52 @@
+import markdownSerializer from '../src/index';
+
+describe('nodes', () => {
+  it('should serialize a blockquote', () => {
+  });
+
+  it('should serialize a code block', () => {
+  });
+
+  it('should serialize headings (level 1 - 6)', () => {
+  });
+
+  it('should serialize horizontal_rule', () => {
+  });
+
+  it('should serialize bullet list', () => {
+  });
+
+  it('should serialize ordered list', () => {
+  });
+
+  it('should serialize paragraph', () => {
+  });
+
+  it('should serialize image', () => {
+  });
+
+  it('should serialize hard_break', () => {
+  });
+
+  it('should serialize text', () => {
+  });
+});
+
+describe('marks', () => {
+  it('should serialize em', () => {
+  });
+
+  it('should serialize strong', () => {
+  });
+
+  it('should serialize link', () => {
+  });
+
+  it('should serialize code', () => {
+  });
+});
+
+describe('mixed content', () => {
+  it('should serialize dragons and demons', () => {
+  });
+});

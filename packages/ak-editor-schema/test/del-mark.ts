90d6d64c3ff129f782487ed61ea63fab6a4878b4
Lakshan Perera <lperera@atlassian.com>
2016-09-14 13:24:56 +1000

    test(ak-editor-schema): Del mark tests were modified to use the fromHTML helper.
    affects: ak-editor-schema

--- packages/ak-editor-schema/test/del-mark.ts
+++ packages/ak-editor-schema/test/del-mark.ts
@@ -3,47 +3,36 @@ import { Fragment, Node } from 'ak-editor-prosemirror';
 import * as chai from 'chai';
 import { expect } from 'chai';
 import { chaiPlugin } from 'ak-editor-test';
+import { fromHTML, toHTML } from 'ak-editor-test';
 
 chai.use(chaiPlugin);
-
-const parse = (html: string) => {
-  const el = document.createElement('div');
-  el.innerHTML = html;
-  return schema.parseDOM(el);
-}
-
-const toHTML = (node: Node) => {
-  const el = document.createElement('div');
-  el.appendChild(node.toDOM());
-  return el.innerHTML
-}
 
 describe('Del mark - parsing from DOM', () => {
   const del = schema.marks['del'].create({});
 
   it('supports <del> tag', () => {
-    const doc = parse(`
+    const doc = fromHTML(`
       <p><del>text</del></p>
     `);
     expect(doc).to.have.textWithMarks('text', [ del ]);
   });
 
   it('supports <s> tag', () => {
-    const doc = parse(`
+    const doc = fromHTML(`
       <p><s>text</s></p>
     `);
     expect(doc).to.have.textWithMarks('text', [ del ]);
   });
 
   it('supports <strike> tag', () => {
-    const doc = parse(`
+    const doc = fromHTML(`
       <p><strike>text</strike></p>
     `);
     expect(doc).to.have.textWithMarks('text', [ del ]);
   });
 
   it('supports line-through text decoration', () => {
-    const doc = parse(`
+    const doc = fromHTML(`
       <p><span style="text-decoration: line-through">text</span></p>
     `);
     expect(doc).to.have.textWithMarks('text', [ del ]);

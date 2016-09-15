53c19d001357667ba5ce213ad7717d2fdcea0c50
Steve Mao <maochenyan@gmail.com>
2016-09-15 14:43:58 +1000

    feat(bitbucket): use subscribe for all plugins

--- packages/ak-editor-bitbucket/src/index.tsx
+++ packages/ak-editor-bitbucket/src/index.tsx
@@ -423,7 +423,7 @@ class AkEditorBitbucket extends Component {
     });
 
     // Hyperlink plugin wiring
-    HyperlinkPlugin.get(pm).onChange(state => {
+    HyperlinkPlugin.get(pm).subscribe(state => {
       this._canLinkHyperlink = state.enabled as boolean;
       this._hyperLinkActive = state.active as boolean;
       this._hyperLinkElement = state.element as HTMLElement;
@@ -437,7 +437,7 @@ class AkEditorBitbucket extends Component {
     ImageUploadPlugin.get(pm).pasteAdapter.add(handler);
 
     // Block type plugin wiring
-    BlockTypePlugin.get(pm).onChange(state => {
+    BlockTypePlugin.get(pm).subscribe(state => {
       const blockType = state.selectedBlockType;
       const font = getFont({ blockType }, this._fonts);
       this._selectedFont = font;
@@ -445,13 +445,13 @@ class AkEditorBitbucket extends Component {
     });
 
     // Lists
-    ListsPlugin.get(pm).onChange(state => {
+    ListsPlugin.get(pm).subscribe(state => {
       this._bulletListActive = Boolean(state.active && state.type === 'bullet_list');
       this._numberListActive = Boolean(state.active && state.type === 'ordered_list');
     });
 
     // Text formatting
-    TextFormattingPlugin.get(pm).onChange(state => {
+    TextFormattingPlugin.get(pm).subscribe(state => {
       this._strongActive = state.strongActive;
       this._emActive = state.emActive;
       this._underlineActive = state.underlineActive;

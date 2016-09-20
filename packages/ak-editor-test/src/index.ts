c98a0b81f287dbaab5d8e62d8de6fc90e31e6ef2
Artur Bodera <abodera@atlassian.com>
2016-09-20 14:57:50 +1000

    test: fix failing tests on IE.

--- packages/ak-editor-test/src/index.ts
+++ packages/ak-editor-test/src/index.ts
@@ -1,3 +1,4 @@
+import './polyfill';
 import * as base64fileconverter from './base64fileconverter';
 import { offsetRefs, BuilderContent, coerce } from './schema-builder';
 import { ProseMirror } from 'ak-editor-prosemirror';

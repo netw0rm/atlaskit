18959a5a18376284376471229bee88a173c976d6
Nadia Makarevich <nmakarevich@atlassian.com>
2016-09-20 13:16:40 +1000

    test(tests): dropdown checkbox item tests added
    affects: ak-dropdown

--- packages/ak-dropdown/test/index.js
+++ packages/ak-dropdown/test/index.js
@@ -1,20 +1,23 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import Dropdown, * as exports from '../src';
-import { props, Component } from 'skatejs';
+import { props, emit, Component } from 'skatejs';
 import { name } from '../package.json';
 import { afterMutations, getShadowRoot, checkVisibility, waitUntil } from 'akutil-common-test';
+import { selected as selectedEvent } from '../src/internal/events';
 
 chai.use(chaiAsPromised);
 chai.should();
 const expect = chai.expect;
 
-function setupComponent() {
+function setupComponent(content) {
+  const html = content || `<ak-dropdown-item>124</ak-dropdown-item>
+    <ak-dropdown-item>444</ak-dropdown-item>`;
+
   const component = new Dropdown();
   component.innerHTML = `
     <ak-dropdown-trigger-button slot="trigger">test</ak-dropdown-trigger-button>
-    <ak-dropdown-item>124</ak-dropdown-item>
-    <ak-dropdown-item>444</ak-dropdown-item>
+    ${html}
   `;
   const componentHasShadowRoot = () => !!getShadowRoot(component);
 
@@ -25,6 +28,16 @@ function setupComponent() {
 
 function tearDownComponent(component) {
   document.body.removeChild(component);
+}
+
+function checkSelectedItems(items, ...index) {
+  [...items].forEach((el, i) => {
+    if (index.indexOf(i) > -1) {
+      expect(items[i].selected).to.equal(true);
+    } else {
+      expect(items[i].selected).to.equal(false);
+    }
+  });
 }
 
 describe('ak-dropdown', () => {
@@ -71,12 +84,14 @@ describe('ak-dropdown', () => {
       expect(getShadowRoot(component)).to.be.defined;
       expect(getShadowRoot(component).firstChild).to.be.defined;
     });
+
     it('open property controls open state', () => {
       props(component, { open: false });
       expect(checkVisibility(component.children[1])).to.equal(false);
       props(component, { open: true });
       expect(checkVisibility(component.children[0])).to.equal(true);
     });
+
     it('position is reflected to inner layer', (done) => {
       // we can't just do querySelector('ak-layer') here, ak-layer is defined a few times
       // and doesn't have a nice clear tag name anymore
@@ -87,6 +102,60 @@ describe('ak-dropdown', () => {
         () => (expect(layer.position).to.equal('bottom left')),
         () => (component.position = 'top left'),
         () => (expect(layer.position).to.equal('top left')),
+        done
+      );
+    });
+  });
+
+  describe('select item', () => {
+    let component;
+    let items;
+    const html = `<ak-dropdown-item>first</ak-dropdown-item>
+                  <ak-dropdown-item>second</ak-dropdown-item>
+                  <ak-dropdown-item>third</ak-dropdown-item>`;
+    beforeEach(() => setupComponent(html).then(newComponent => {
+      component = newComponent;
+      items = component.querySelectorAll('ak-dropdown-item');
+    }));
+    afterEach(() => tearDownComponent(component));
+
+    it('should be possible to select an item', (done) => {
+      const item = items[0];
+
+      // mock the event from the item
+      emit(item, selectedEvent, { detail: { item } });
+
+      afterMutations(
+        () => expect(items[0].selected).to.equal(true),
+        done
+      );
+    });
+
+    it('only one item should be selected', (done) => {
+      // mock the event from the item
+      emit(items[0], selectedEvent, { detail: { item: items[0] } });
+
+      afterMutations(
+        () => checkSelectedItems(items, 0),
+        () => emit(items[1], selectedEvent, { detail: { item: items[1] } }),
+        () => checkSelectedItems(items, 1),
+        done
+      );
+    });
+
+    it('any checkbox item can be selected and unselected simultaneously', (done) => {
+      items.forEach(item => (props(item, { checkbox: true })));
+
+      // mock the event from the item
+      emit(items[0], selectedEvent, { detail: { item: items[0] } });
+      emit(items[2], selectedEvent, { detail: { item: items[2] } });
+
+      afterMutations(
+        () => checkSelectedItems(items, 0, 2),
+        () => emit(items[0], selectedEvent, { detail: { item: items[0] } }),
+        () => checkSelectedItems(items, 2),
+        () => emit(items[0], selectedEvent, { detail: { item: items[2] } }),
+        () => checkSelectedItems(items),
         done
       );
     });

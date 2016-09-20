f559e90f18754ac9ab583af57a6e4fa1a179b284
Trey Shugart <treshugart@gmail.com>
2016-09-20 17:23:25 +1000

    chore: Add comment explaining why the tagName() test is the way it is.
    affects: ak-theme
    
    AK-39

--- packages/ak-theme/test/util.js
+++ packages/ak-theme/test/util.js
@@ -2,6 +2,9 @@ import { tagName, themeNameFromNode } from '../src/util';
 
 describe('ak-theme, { themes }', () => {
   it('tagName', () => {
+    // We're testing exact tag names at the moment. However, once
+    // https://github.com/skatejs/skatejs/issues/769 is implemented we should change the expected
+    // tagName to be the non-unique name of a Skate component.
     expect(tagName(document.createElement('x-a1b2c3d4-a1b2c3d4'))).to.equal('x-a1b2c3d4-a1b2c3d4');
     expect(tagName(document.createElement('x-a1b2c3d4-a1b2c3d4'))).to.equal('x-a1b2c3d4-a1b2c3d4');
   });

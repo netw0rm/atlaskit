cd428cb627771cd68a6b0b7ed08a91dc70182d38
Artur Bodera <abodera@atlassian.com>
2016-09-22 08:55:31 +1000

    test: add basic editor expansion and activation tests.

--- packages/ak-editor-bitbucket/test/index.ts
+++ packages/ak-editor-bitbucket/test/index.ts
@@ -1,12 +1,66 @@
 import * as chai from 'chai';
 import AkEditorBitbucket from '../src';
-import { afterMutations } from 'akutil-common-test';
+import { afterMutations, waitUntil, getShadowRoot  } from 'akutil-common-test';
 import { symbols } from 'skatejs';
 import { fixtures, RewireSpy } from 'ak-editor-test';
 import sinonChai from 'sinon-chai';
 
 chai.use(sinonChai);
 const { expect } = chai;
+
+const RewireSpy = () => {
+  const resetAfter: any[] = [];
+
+  afterEach(() => resetAfter.map(({ module, name }) => module.__ResetDependency__(name)));
+
+  return (module: any, name: string) => {
+    const func = module.__GetDependency__(name);
+    const spy = sinon.spy(func);
+    module.__Rewire__(name, spy);
+    resetAfter.push({ module, name });
+    return spy;
+  }
+};
+
+
+function activateEditorByClicking(editor: AkEditorBitbucket) : void {
+  const inputEl = getShadowRoot(editor).querySelector('* > input');
+  expect(inputEl).to.not.be.null;
+  emit(inputEl, 'click');
+}
+
+function waitForEditorToBeReady(editor: AkEditorBitbucket) : Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    const successFn = () => {
+      clearTimeout(failTimer);
+      resolve(editor);
+    };
+
+    const failTimer = setTimeout(() => {
+      editor.removeEventListener('ready', successFn);
+      reject('the editor didn\'t become ready in 1.5s');
+    }, 1500);
+
+    editor.addEventListener('ready', successFn, { once: true });
+  });
+}
+
+function buildExpandedEditor(fixture : any) : Promise<AkEditorBitbucket> {
+  return new Promise(function(resolve) {
+    fixture.innerHTML = `<ak-editor-bitbucket></ak-editor-bitbucket>`;
+
+    afterMutations(
+      () => {
+        // TODO: this should not be required when using "expanded" attribute
+        activateEditorByClicking(fixture.firstChild);
+        waitForEditorToBeReady(fixture.firstChild).then(() => {
+          resolve(fixture.firstChild);
+        });
+      }
+    );
+  });
+}
+
 
 describe('ak-editor-bitbucket', () => {
   const fixture = fixtures();
@@ -88,6 +142,9 @@ describe('ak-editor-bitbucket', () => {
 
       editor.defaultValue = content;
 
+      // TODO: Delete this after merging a different PR.
+      editor._justToggledExpansion = true;
+
       afterMutations(
         () => { editor.expanded = true; },
         () => {
@@ -96,6 +153,68 @@ describe('ak-editor-bitbucket', () => {
         },
         done
       );
+    });
+  });
+
+  describe('collapsed editor', () => {
+    let tmpContainer: any;
+    let editor: any;
+
+    beforeEach((done) => {
+      tmpContainer = fixture();
+      tmpContainer.innerHTML = `<ak-editor-bitbucket></ak-editor-bitbucket>`;
+
+      afterMutations(
+        () => (editor = tmpContainer.firstChild),
+        done
+      );
+    });
+
+    it('should not be expanded by default', () => {
+      expect(editor.expanded).to.be.false;
+    });
+
+    it('should expand after clicking the input element', () => {
+      activateEditorByClicking(editor);
+      expect(editor.expanded).to.be.true;
+    });
+  });
+
+  describe('editor toolbar', () => {
+    it('should have all default elements', () => {
+      return buildExpandedEditor(fixture()).then((editor) => {
+        [
+          'ak-editor-toolbar',
+          'ak-editor-toolbar-block-type',
+          'ak-editor-toolbar-lists',
+          'ak-editor-toolbar-hyperlink',
+          'ak-editor-toolbar-text-formatting'
+        ].forEach((selector) => {
+          // debugger;
+          expect(getShadowRoot(editor).querySelector(selector)).to.not.be.null;
+        });
+      });
+    });
+
+    it('should have options in block type dropdown', () => {
+      return buildExpandedEditor(fixture()).then((editor) => {
+        const bt = getShadowRoot(editor).querySelector('ak-editor-toolbar-block-type');
+        expect(bt).to.not.be.null;
+
+        // on Firefox, shadowRoot is not available right away so we have to wait for it
+        return waitUntil(() => {
+          return !!getShadowRoot(bt);
+        }).then(() => {
+          const fs = getShadowRoot(bt).querySelector('ak-editor-toolbar-block-type-select');
+          expect(fs).to.not.be.null;
+
+          const btShadowRoot = getShadowRoot(bt);
+          return waitUntil(() => {
+            // it takes roughly 3 iterations to render all elements and attach them to <ul>
+            return btShadowRoot.querySelectorAll('ak-editor-toolbar-block-type-option').length >= 2;
+          });
+        });
+      });
     });
   });
 });

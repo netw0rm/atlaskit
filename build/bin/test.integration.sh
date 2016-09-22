dfb610bf62f6f6d8df2665f5c5c58ecbf77e2216
Joscha Feth <joscha@feth.com>
2016-09-23 01:54:46 +1000

    chore: use npm-run-alll & run in pr

--- build/bin/test.integration.sh
+++ build/bin/test.integration.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-WAITON_LOC="`npm bin`/wait-on"
-
-npm run storybook& # runs in background
-STORYBOOK_PID=$!
-# give storybook 20 seconds, before checking every half second for a total of 2 minutes max if it is up
-$WAITON_LOC -d 20000 -i 500 -t 120000 http-get://0.0.0.0:9001/
-npm run test/integration/browserstack
-kill $STORYBOOK_PID
+npm-run-all --race --parallel \
+"storybook" \
+"test/integration/browserstack/wait"

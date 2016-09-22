90bfd9d0f7051152b73741ad9626775b830e4cfe
Luke Batchelor <lbatchelor@atlassian.com>
2016-09-22 07:08:20 +0000

    fileToScope.js edited online with Bitbucket Added jsdoc annotation

--- packages/ak-icon/src/fileToScope.js
+++ packages/ak-icon/src/fileToScope.js
@@ -1,3 +1,8 @@
+/**
+ * @description Takes a filename and returns
+ * @params {string} file Path to an icon in the form './pathTo/icon.js'
+ * @return {string} the path without the leading `./` and trailing '.js'
+ */
 export default function fileToScope(file) {
   return file.replace(/^\.\//, '').replace(/\.js$/, '');
 }

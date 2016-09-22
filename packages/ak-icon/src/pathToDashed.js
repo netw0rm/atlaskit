db60a8296367af46cbb0511cb1cd5a1681487bd5
Luke Batchelor <lbatchelor@atlassian.com>
2016-09-22 07:11:53 +0000

    pathToDashed.js edited online with Bitbucket Added jsdoc annotations

--- packages/ak-icon/src/pathToDashed.js
+++ packages/ak-icon/src/pathToDashed.js
@@ -1,1 +1,6 @@
+/**
+ * @description Takes a filename and replaces forward slashes with dashes.
+ * @params {string} file Path to an icon in the form 'path/to/icon'
+ * @return {string} the path with forward slashes replaced with dashes, e.g 'path-to-icon'
+ */
 export default (file) => file.split('/').join('-');

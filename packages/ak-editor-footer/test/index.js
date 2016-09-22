84cc41803fdfb5ea2e64c8d5cae781f97ec6e1d7
Matias Corbanini <mcorbanini@atlassian.com>
2016-09-22 16:37:20 +1000

    fix(component): save/cancel buttons changed to use ak-button & ak-button-group
    affects: ak-editor-footer

--- packages/ak-editor-footer/test/index.js
+++ packages/ak-editor-footer/test/index.js
@@ -1,11 +1,13 @@
 import { name } from '../package.json';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import sinonChai from 'sinon-chai';
 import { emit } from 'skatejs';
 import Component from '../src';
 import shadowStyles from '../src/shadow.less';
-import { afterMutations, getShadowRoot } from 'akutil-common-test';
+import { waitUntil, getShadowRoot } from 'akutil-common-test';
 
+chai.use(sinonChai);
 chai.use(chaiAsPromised);
 chai.should();
 const expect = chai.expect;
@@ -16,6 +18,7 @@ describe(name, () => {
   beforeEach(() => {
     component = new Component();
     document.body.appendChild(component);
+    return waitUntil(() => getShadowRoot(component) !== null);
   });
 
   afterEach(() => document.body.removeChild(component));
@@ -24,61 +27,22 @@ describe(name, () => {
     expect(component.tagName).to.match(new RegExp(`^${name}`, 'i'));
   });
 
-  describe('button events', () => {
-    it('should trigger save event', done => {
-      let clicked = false;
-      component.addEventListener('save', () => { clicked = true; });
-      afterMutations(
-        () => {
-          const button = getShadowRoot(component)
-            .querySelector(`.${shadowStyles.locals.saveButton}`);
+  describe('button events', () =>
+    [
+      { event: 'save', selector: `.${shadowStyles.locals.saveButton}` },
+      { event: 'cancel', selector: `.${shadowStyles.locals.cancelButton}` },
+      { event: 'insertmention', selector: `.${shadowStyles.locals.iconButton}:nth-child(1)` },
+      { event: 'insertimage', selector: `.${shadowStyles.locals.iconButton}:nth-child(2)` },
+    ].forEach(data =>
+      describe('should trigger', () => {
+        it(`${data.event} event`, () => {
+          const spy = sinon.spy();
+          component.addEventListener(data.event, spy);
+          const button = getShadowRoot(component).querySelector(data.selector);
           emit(button, 'click');
-        },
-        () => expect(clicked).to.equal(true),
-        done
-      );
-    });
-
-    it('should trigger cancel event', done => {
-      let clicked = false;
-      component.addEventListener('cancel', () => { clicked = true; });
-      afterMutations(
-        () => {
-          const button = getShadowRoot(component)
-            .querySelector(`.${shadowStyles.locals.cancelButton}`);
-          emit(button, 'click');
-        },
-        () => expect(clicked).to.equal(true),
-        done
-      );
-    });
-  });
-
-  describe('insert events', () => {
-    it('should trigger mention event', done => {
-      let clicked = false;
-      component.addEventListener('insertmention', () => { clicked = true; });
-      afterMutations(
-        () => {
-          const insert = getShadowRoot(component).querySelectorAll('button')[2];
-          emit(insert, 'click');
-        },
-        () => expect(clicked).to.equal(true),
-        done
-      );
-    });
-
-    it('should trigger image event', done => {
-      let clicked = false;
-      component.addEventListener('insertimage', () => { clicked = true; });
-      afterMutations(
-        () => {
-          const insert = getShadowRoot(component).querySelectorAll('button')[3];
-          emit(insert, 'click');
-        },
-        () => expect(clicked).to.equal(true),
-        done
-      );
-    });
-  });
+          expect(spy).to.have.been.called;
+        });
+      })
+    )
+  );
 });

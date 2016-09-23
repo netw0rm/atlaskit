a4a0b5db4fba28dee99f912dfa52c924b0d60358
Steve Mao <maochenyan@gmail.com>
2016-09-23 15:49:31 +1000

    test(component): add tests for context
    affects: ak-editor-toolbar-block-type
    
    Will leave the click tests to integration test

--- packages/ak-editor-toolbar-block-type/test/index.js
+++ packages/ak-editor-toolbar-block-type/test/index.js
@@ -12,4 +12,18 @@ describe(name, () => {
     const component = new Component();
     expect(component.tagName).to.match(new RegExp(`^${name}`, 'i'));
   });
+
+  it('should have default context as document', () => {
+    const component = new Component();
+    expect(component.context).to.equal(window.document);
+  });
+
+  it('should change context to other div', () => {
+    const component = new Component();
+
+    const div = document.createElement('div');
+    component.context = div;
+
+    expect(component.context).to.equal(div);
+  });
 });

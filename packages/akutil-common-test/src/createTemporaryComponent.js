1fff893abec246d039ae28a97a1fee959e8c4a7c
Joscha Feth <jfeth@atlassian.com>
2016-09-25 12:58:42 +0000

    docs: add JSDoc for ceateTemporary
    affects: akutil-common-test

--- packages/akutil-common-test/src/createTemporaryComponent.js
+++ packages/akutil-common-test/src/createTemporaryComponent.js
@@ -2,6 +2,12 @@ import { define } from 'skatejs';
 import { waitUntil, getShadowRoot } from './';
 import uid from 'uid';
 
+/**
+* Creates a temporary component from a SkateJS module definition
+*
+* @param {object} definition The SkateJS definition to base the temporary component upon
+* @return {Promise.<Component>} A promise resolving to a ready-to-use instance of the created temporary SkateJS WebComponent. 
+*/
 export default function createTemporary(definition) {
   const TemporaryWebComponent = define(`x-${uid()}`, definition);
   const component = new TemporaryWebComponent();

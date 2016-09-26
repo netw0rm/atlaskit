cfecbb2313eb88f11490e1ead0a5e0f4be1baa31
Joscha Feth <joscha@feth.com>
2016-09-27 00:41:38 +1000

    docs: Add hint about test fixture
    affects: akutil-shared-styles

--- packages/akutil-shared-styles/test/exports.js
+++ packages/akutil-shared-styles/test/exports.js
@@ -10,6 +10,17 @@ import * as lessVars from '../src';
 
 describe('exports', () => {
   it('should have well-defined exports', () => {
+    // If you find yourself here and wonder why this list is not auto-generated, then bear in
+    // mind that tests are supposed to tell you when a piece of software breaks.
+    // As the sole purpose of this component is providing icons:
+    //
+    // * changing a variable is a patch
+    // * adding a variable is a feature
+    // * removing a variable is breaking change
+    // * renaming a variable is a breaking change
+    //
+    // If we were to auto-generate this list, then renaming, adding or removing would NOT
+    // break any tests and thus not hint the developer at what kind of change he/she is making
     Object.keys(lessVars).should.be.deep.equal([
       'akBorderRadius',
       'akEditorInactiveForeground',

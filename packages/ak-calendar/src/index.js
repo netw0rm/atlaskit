2fe8c47c230c552bb25602e1c624382020a15dc1
Trey Shugart <treshugart@gmail.com>
2016-09-26 10:48:52 +1000

    chore: Add some base styling.
    affects: ak-calendar

--- packages/ak-calendar/src/index.js
+++ packages/ak-calendar/src/index.js
@@ -1,11 +1,8 @@
-import 'style!./host.less';
-import classNames from 'classnames';
-import { emit, vdom, define } from 'skatejs';
-import shadowStyles from './shadow.less';
-import * as events from './internal/events';
-const { announceName: announceNameEvent } = events;
+/** @jsx h */
 
-const Paragraph = (props, chren) => <p {...props}>{chren()}</p>;
+import { define, h, vdom } from 'skatejs';
+import { style } from 'akutil-common';
+import * as events from './internal/events';
 
 /**
  * @description Create instances of the component programmatically, or using markup.
@@ -14,60 +11,16 @@ const Paragraph = (props, chren) => <p {...props}>{chren()}</p>;
  * const component = new Calendar();
  */
 export default define('ak-calendar', {
-  render(elem) {
-    const paragraphClasses = classNames({
-      [shadowStyles.locals.myClassName]: true,
-      anotherClass: false,
+  render() {
+    style(vdom, {
+      ':host': {
+        'background-color': 'white',
+        display: 'block',
+        height: '200px',
+        width: '200px',
+      },
     });
-    return (
-      // JSX requires that there only be a single root element.
-      // Incremental DOM doesn't require this.
-      <div>
-        {/* This is required for elements in the shadow root to be styled.
-           This is wrapped in the <div /> because you can't have more than one
-           root element.
-        */}
-        <style>{shadowStyles.toString()}</style>
-        <Paragraph className={paragraphClasses}>My name is {elem.name}!</Paragraph>
-      </div>
-    );
-  },
-  props: {
-    /**
-     * @description The name of the Calendar element.
-     * @memberof Calendar
-     * @instance
-     * @type {string}
-     * @default Calendar
-     */
-    name: {
-      default: 'Calendar',
-    },
-  },
-  prototype: {
-    /**
-     * @description Fire an event containing the name of the element.
-     * @memberof Calendar
-     * @function
-     * @instance
-     * @fires Calendar#announce-name
-     * @return {Calendar} The Calendar element.
-     * @example @js component.announce(); // Fires the announce-name event.
-     */
-    announce() {
-      /**
-       * @event Calendar#announce-name
-       * @memberof Calendar
-       * @description Fired when the `announce` method is called.
-       * @property {String} detail.name The name of the component.
-       */
-      emit(this, announceNameEvent, {
-        detail: {
-          name: this.name,
-        },
-      });
-      return this;
-    },
+    return <div />;
   },
 });
 
--- packages/ak-calendar/stories/example-story.js
+++ packages/ak-calendar/stories/example-story.js
@@ -1,47 +1,12 @@
-import { storiesOf, action } from '@kadira/storybook';
-import reactify from 'akutil-react';
-import WebComponent, { events } from '../src';
-import React from 'react';
 import { name } from '../package.json';
-import styles from 'style!./../src/host.less';
+import { storiesOf } from '@kadira/storybook';
+import React from 'react';
+import reactify from 'akutil-react';
+import WebComponent from '../src';
 
-const Component = reactify(WebComponent);
+const ReactComponent = reactify(WebComponent);
 
 storiesOf(name, module)
   .add('a simple ak-calendar', () => (
-    <Component />
-  ))
-  .add('a simple ak-calendar with a name', () => (
-    <Component name="MyComponent" />
-  ))
-  .add('a simple ak-calendar that uses an emitted event', () => (
-    <Component
-      name="MyComponent"
-      onClick={function onClick() { this.announce(); }}
-      onAnnounce-name={action(events.announceName)}
-    />
-  ))
-  .add('an ak-calendar that emits an action when it is clicked', () => (
-    <Component id="myComponent" onClick={action('clicking the WebComponent')} />
-  ))
-  .add('an ak-calendar that removes itself when being clicked', () => {
-    const removeMe = (e) => e.currentTarget.parentNode.removeChild(e.currentTarget);
-    const cls = styles.akutilComponentTemplate;
-    return (<Component id="myComponent" className={cls} onClick={removeMe} />);
-  })
-  .addMonkeyTest('a ak-calendar with monkey testing', () => (
-    // Use this to add a story that has fuzzy testing attached.
-    <Component />
-  ))
-  .addMonitored('an ak-calendar with monitored performance', () => (
-    // Use this to add a story that has a little fps/memory gauge that allows you
-    // to monitor performance whilst developing
-    <Component />
-  ), () => {
-    // This is where the actual work is done - anything in here will be monitored by the stats
-    // view and displayed, so this is where you want to do your animation work, etc.
-    const x = Math.random() * 1000000;
-    for (let i = 0; i < x; i++) {
-      Math.random(); // burn some CPU cycles
-    }
-  });
+    <ReactComponent />
+  ));

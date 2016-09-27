2312b059011e39bca0e0d585acf18b5d7fb0ce4a
Joscha Feth <jfeth@atlassian.com>
2016-09-27 08:59:50 +0000

    test: _helpers.js must only use require.ensure on call
    affects: ak-icon

--- packages/ak-icon/test/_helpers.js
+++ packages/ak-icon/test/_helpers.js
@@ -1,12 +1,15 @@
 import { baseIconChunkName, glyphFolderName } from '../bin/constants';
 import fileToScope from '../src/fileToScope';
 
-const req = require.context(`../${glyphFolderName}`, true, /^.*\.js/);
-export const getGlyphs = () => req.keys().reduce((prev, file) => {
-  if (file.indexOf(baseIconChunkName) !== -1) {
-    // ignore our base chunk
+export const getGlyphs = () => {
+
+  const req = require.context(`../${glyphFolderName}`, true, /^.*\.js/);
+  return req.keys().reduce((prev, file) => {
+    if (file.indexOf(baseIconChunkName) !== -1) {
+      // ignore our base chunk
+      return prev;
+    }
+    prev[fileToScope(file)] = req(file).default;
     return prev;
-  }
-  prev[fileToScope(file)] = req(file).default;
-  return prev;
-}, {});
+  }, {});
+};

c7d6c5d84e33f992d151c53324761d4cbc7d3e55
Joscha Feth <jfeth@atlassian.com>
2016-09-27 09:48:20 +0000

    style: remove padding
    affects: ak-icon

--- packages/ak-icon/test/_helpers.js
+++ packages/ak-icon/test/_helpers.js
@@ -2,7 +2,6 @@ import { baseIconChunkName, glyphFolderName } from '../bin/constants';
 import fileToScope from '../src/fileToScope';
 
 export const getGlyphs = () => {
-
   const req = require.context(`../${glyphFolderName}`, true, /^.*\.js/);
   return req.keys().reduce((prev, file) => {
     if (file.indexOf(baseIconChunkName) !== -1) {

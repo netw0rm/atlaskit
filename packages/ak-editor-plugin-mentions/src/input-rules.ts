a4a0822684d84502bcdfc0bfff3f8271a4bac19c
Lakshan Perera <lperera@atlassian.com>
2016-09-27 15:17:21 +1000

    fix(package): Change the input rules that recoginize @mention at the start of the context.
    affects: ak-editor-plugin-mentions

--- packages/ak-editor-plugin-mentions/src/input-rules.ts
+++ packages/ak-editor-plugin-mentions/src/input-rules.ts
@@ -1,6 +1,6 @@
 import { ProseMirror, InputRule } from 'ak-editor-prosemirror';
 
-export const mentionRule = new InputRule(/(?:^|\s)@$/, '@', (pm: ProseMirror, match: string[], pos: number) => {
+export const mentionRule = new InputRule(/(\s+@)|^@$/, '@', (pm: ProseMirror, match: string[], pos: number) => {
   const node = pm.schema.nodes.mention.create({ id: '' });
   return pm.tr.replaceWith(pos-1, pos, node).apply();
 });
--- packages/ak-editor-plugin-mentions/test/input-rules.ts
+++ packages/ak-editor-plugin-mentions/test/input-rules.ts
@@ -36,4 +36,12 @@ describe('ak-editor-plugin-mentions - input rules', () => {
     const cursorFocus: ResolvedPos = pm.selection.$to;
     expect(cursorFocus.nodeBefore).not.to.be.of.nodeType(Mention);
   });
+
+  it('should replace "@" at the start of the content', () => {
+    const pm = makeEditor();
+    pm.input.insertText(0, 0, '@');
+
+    const cursorFocus: ResolvedPos = pm.selection.$to;
+    expect(cursorFocus.nodeBefore).to.be.of.nodeType(Mention);
+  });
 });

8afe6d983620c3a332b76a3d92e6c5deb7a05929
Ben Wong <benwong@atlassian.com>
2016-09-27 16:56:05 +1000

    docs(component): Clean up documentation for validator component
    affects: ak-validator

--- packages/ak-validator/src/index.js
+++ packages/ak-validator/src/index.js
@@ -19,9 +19,9 @@ const baseDefinition = {
 
 /**
  * Define a new validator component.
- * @param {string} tagName The tag name of the new validator component.
+ * @param {string} tagName The tag name of the new validator component
  * @param {validatorFunction} validatorFunction The validator function
- * @param {Object=} props Any properties that should be
+ * @param {Object=} props Properties that should be defined on the validator component
  * @returns {*}
  */
 const defineValidator = (tagName, validatorFunction, props = {}) => {
@@ -33,16 +33,32 @@ const defineValidator = (tagName, validatorFunction, props = {}) => {
   return define(tagName, newDefinition);
 };
 
+/**
+ * @description Minimum length validator.
+ * @class ValidatorMinLength
+ * @example @js import ValidatorMinLength from 'ak-validator';
+ * const myValidator = new ValidatorMinLength();
+ */
 const ValidatorMinLength = defineValidator(`${prefix}min-length`,
   (value) => (value.length > this.minLength),
   { minLength: prop.number({}) }
 );
-
+/**
+ * @description Maximum length validator.
+ * @class ValidatorMaxLength
+ * @example @js import ValidatorMaxLength from 'ak-validator';
+ * const myValidator = new ValidatorMaxLength();
+ */
 const ValidatorMaxLength = defineValidator(`${prefix}max-length`,
   (value) => (value.length > this.maxLength),
   { maxLength: prop.number({}) }
 );
-
+/**
+ * @description Required validator.
+ * @class ValidatorRequired
+ * @example @js import ValidatorRequired from 'ak-validator';
+ * const myValidator = new ValidatorRequired();
+ */
 const ValidatorRequired = defineValidator(`${prefix}required`, {}, (value) => !!value);
 
 export default defineValidator;

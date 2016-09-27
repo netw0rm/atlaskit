1669a015cadddce45418e537be2b3e96546278cd
Joscha Feth <joscha@feth.com>
2016-09-27 21:14:45 +1000

    test: require.context only works with string
    affects: ak-icon

--- packages/ak-icon/test/_helpers.js
+++ packages/ak-icon/test/_helpers.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import { baseIconChunkName, glyphFolderName } from '../bin/constants';
 import fileToScope from '../src/fileToScope';
 
 export const getGlyphs = () => {
-  const req = require.context(`../${glyphFolderName}`, true, /^.*\.js/);
+  // TODO use `../${glyphFolderName}` here as soon as webpack supports require.context with vars
+  const req = require.context('../glyph', true, /^.*\.js/);
   return req.keys().reduce((prev, file) => {
     if (file.indexOf(baseIconChunkName) !== -1) {
       // ignore our base chunk

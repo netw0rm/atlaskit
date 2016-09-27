c362bdb359ffb0e55c0af7426b10388977c66c2e
Joscha Feth <jfeth@atlassian.com>
2016-09-27 18:22:01 +1000

    fix: Allow class definitions, make tearDownComponent more resilient, add tests
    affects: akutil-common-test

--- packages/akutil-common-test/src/createTemporaryComponent.js
+++ packages/akutil-common-test/src/createTemporaryComponent.js
@@ -4,17 +4,20 @@ import uid from 'uid';
 /**
 * Creates a temporary component from a SkateJS module definition
 *
-* @param {object} define The SkateJS define function. This needs to be passed in so that vdom
+* @param {Function} define The SkateJS define function. This needs to be passed in so that vdom
 * doesn't get confused between your components skate and the skate from akutil-common-test.
-* @param {object} definition The SkateJS definition to base the temporary component upon
-* @param {object} [target=document.body] The target element to which the component
+* @param {object|Component} definition The SkateJS definition to base the temporary component upon
+* @param {Node} [target=document.body] The target element to which the component
 * should be appended
 * @return {Promise.<Component>} A promise resolving to a ready-to-use instance of the created
 * temporary SkateJS WebComponent.
 */
 export default (define, definition, target = document.body) => {
-  if (!define || !definition) {
-    return undefined;
+  if (typeof define !== 'function') {
+    throw new Error('Given define is not a function');
+  }
+  if (typeof definition !== 'object' && typeof definition !== 'function') {
+    throw new Error('Definition must be an object or constructor');
   }
   const TemporaryWebComponent = define(`x-${uid()}`, definition);
   const component = new TemporaryWebComponent();

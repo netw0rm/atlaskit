8c08bebbeaf961dc1f3ae4266b11345db140ed56
Joscha Feth <jfeth@atlassian.com>
2016-09-28 16:32:46 +1000

    refactor: Move require.context outside the function
    affects: ak-icon

--- packages/ak-icon/test/_helpers.js
+++ packages/ak-icon/test/_helpers.js
@@ -2,15 +2,14 @@
 import { baseIconChunkName, glyphFolderName } from '../bin/constants';
 import fileToScope from '../src/fileToScope';
 
-export const getGlyphs = () => {
-  // TODO use `../${glyphFolderName}` here as soon as webpack supports require.context with vars
-  const req = require.context('../glyph', true, /^.*\.js/);
-  return req.keys().reduce((prev, file) => {
-    if (file.indexOf(baseIconChunkName) !== -1) {
-      // ignore our base chunk
-      return prev;
-    }
-    prev[fileToScope(file)] = req(file).default;
+// TODO use `../${glyphFolderName}` here as soon as webpack supports require.context with vars
+const req = require.context('../glyph', true, /^.*\.js/);
+
+export const getGlyphs = () => req.keys().reduce((prev, file) => {
+  if (file.indexOf(baseIconChunkName) !== -1) {
+    // ignore our base chunk
     return prev;
-  }, {});
-};
+  }
+  prev[fileToScope(file)] = req(file).default;
+  return prev;
+}, {});

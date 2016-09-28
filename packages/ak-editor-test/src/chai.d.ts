d31ae2b0db5f4d0d54f2612e19a2aed06bebb006
Bradley Ayers <bradley.ayers@gmail.com>
2016-09-28 17:07:05 +1000

    fix(types): Fix chai.d.ts TypeScript declaration to use prosemirror directly.
    affects: ak-editor-test

--- packages/ak-editor-test/src/chai.d.ts
+++ packages/ak-editor-test/src/chai.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../ak-editor-prosemirror/src/prosemirror.d.ts" />
 
 declare namespace Chai {
-  import { NodeType, Mark } from 'ak-editor-prosemirror';
+  import { NodeType, Mark } from 'prosemirror/dist/model';
   interface Assertion {
     nodeType(nodeType: NodeType): Assertion;
     textWithMarks(text: string, marks: Mark[]): Assertion;

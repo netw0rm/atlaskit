76c2f80b6ef34d3a635369d91001f712f5dc1ca6
Nadia Makarevich <nmakarevich@atlassian.com>
2016-09-30 13:25:35 +1000

    fix(component): use forEach instead of find
    affects: ak-layer

--- packages/ak-layer/src/internal/helpers.js
+++ packages/ak-layer/src/internal/helpers.js
@@ -2,8 +2,13 @@ import Alignment from './Alignment';
 
 function popperPositionToAlignmentPosition(position) {
   const positionMap = Alignment.attachmentMap;
-  const alignmentPosition = Object.keys(positionMap)
-    .find(positionKey => positionMap[positionKey].position === position);
+  let alignmentPosition;
+
+  Object.keys(positionMap).forEach((positionKey) => {
+    if (positionMap[positionKey].position === position) {
+      alignmentPosition = positionMap[positionKey].position;
+    }
+  });
 
   return alignmentPosition;
 }

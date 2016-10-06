19644194667a088944d5dc7ae49e8f5f3c9dc73c
Joscha Feth <jfeth@atlassian.com>
2016-10-06 16:14:40 +1100

    chore: also do not link locally

--- build/bin/postinstall.sh
+++ build/bin/postinstall.sh
@@ -6,16 +6,12 @@ CHALK="`npm bin`/chalk"
 VALIDATE_COMMIT_MSG_LOC="`npm bin`/validate-commit-msg"
 BASEDIR=$(dirname $0)
 
+$CHALK --no-stdin -t "{blue Installing packages...}"
+$LERNA_LOC exec -- npm install
+
 if [[ -z "$BITBUCKET_COMMIT" ]]; then
   # we are in a local env
-  $CHALK --no-stdin -t "{blue Lerna bootstrap...}"
-  $LERNA_LOC bootstrap
-
   $CHALK --no-stdin -t "{blue Installing hooks...}"
   node $BASEDIR/pre-commit.install.js
   $VALIDATE_COMMIT_MSG_LOC
-else
-  # we are running in CI (do not link, as we can't guarantee that cross-dependencies still work)
-  $CHALK --no-stdin -t "{blue Installing packages...}"
-  $LERNA_LOC exec -- npm install
 fi

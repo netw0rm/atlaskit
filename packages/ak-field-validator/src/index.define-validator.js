a154b53151e7238d815e8a22a3a4c6d6820ce9c7
Ben Wong <benwong@atlassian.com>
2016-10-06 16:20:53 +1100

    docs(docs): Tweak docs for defineValidator arguments
    affects: ak-field-validator

--- packages/ak-field-validator/src/index.define-validator.js
+++ packages/ak-field-validator/src/index.define-validator.js
@@ -2,19 +2,24 @@ import { define, vdom } from 'skatejs';
 import ValidatorBase from './internal/validator-base';
 
 /**
+ * @description Validator function that returns whether a value is valid.
+ * @see defineValidator
  * @callback validatorFunction
  * @param value The value to validate
  * @param elem The validator element
  */
 
 /**
+ * @description Function that returns the default error message that should be shown if no content
+ * is provided
+ * @see defineValidator
  * @callback defaultMessageFunction
  * @param elem The validator element
  */
 
 /**
- * Define a new validator component
- * @param {string} tagName The
+ * @description Factory function to define a new validator component
+ * @param {string} tagName The tag name for the new validator component.
  * @param {validatorFunction} validatorFunction A function that returns whether the value is valid
  * @param {Object=} props Any additional properties that should be available to the component.
  * See https://github.com/skatejs/skatejs#props for more information.

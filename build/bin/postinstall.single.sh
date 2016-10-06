c81683aa8a5f4a7edc4279607d791a1a910029d9
Joscha Feth <joscha@feth.com>
2016-10-07 02:08:42 +1100

    chore: make sure prefix is colored

--- build/bin/postinstall.single.sh
+++ build/bin/postinstall.single.sh
@@ -6,7 +6,7 @@ CHALK="`npm bin`/chalk"
 popd > /dev/null
 
 PKG=$(node -e 'console.log(require("./package.json").name)')
-PREFIX=$($CHALK --no-stdin -t "{white.bold [$PKG]}")
-$CHALK --no-stdin -t "{blue {white.bold [$PKG]} Installing packages...}"
 export FORCE_COLOR=1
-npm install --silent | sed "s/^/$PREFIX /"
+PREFIX=$($CHALK --no-stdin -t "{white.bold [$PKG]}")
+$CHALK --no-stdin -t "{blue $PREFIX Installing packages...}"
+npm install --no-progress --silent | sed "s/^/$PREFIX /"

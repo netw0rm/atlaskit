0dc3a588749d9e99a98e20221417c66504cb9579
Joscha Feth <jfeth@atlassian.com>
2016-10-10 06:41:15 +0000

    chore: typo in cloudfront_invalidate.sh

--- build/bin/cloudfront_invalidate.sh
+++ build/bin/cloudfront_invalidate.sh
@@ -3,7 +3,8 @@ set -e
 
 CLOUDFRONT_INVALIDATION_PATTERN="/$CDN_URL_SCOPE/*"
 
-echo "Invalidating CloudFront cache for distribution '$CLOUDFRONT_DISTRIBUTION'} and pattern '$CLOUDFRONT_INVALIDATION_PATTERN'"
+
+echo "Invalidating CloudFront cache for distribution '$CLOUDFRONT_DISTRIBUTION' and pattern '$CLOUDFRONT_INVALIDATION_PATTERN'"
 
 AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY" \
 AWS_SECRET_ACCESS_KEY="$AWS_SECRET_KEY" \

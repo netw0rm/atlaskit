458a3db9e1df58fb806eff01caf438e391c9eb9c
Joshua Nelson <jonelson@atlassian.com>
2016-10-10 12:25:14 +1100

    chore(story): forgot to add the icons.js file for the last commit
    affects: ak-navigation

---
+++ packages/ak-navigation/stories/icons.js
@@ -0,0 +1,29 @@
+import reactify from 'akutil-react';
+
+import CalendarIcon from 'ak-icon/glyph/confluence/calendar';
+import CanvasIcon from 'ak-icon/glyph/confluence/canvas';
+import PageIcon from 'ak-icon/glyph/confluence/page';
+import PersonIcon from 'ak-icon/glyph/confluence/person';
+import QuoteIcon from 'ak-icon/glyph/confluence/quote';
+import OverviewIcon from 'ak-icon/glyph/bitbucket/overview';
+import BitbucketIcon from 'ak-icon/glyph/bitbucket/logo';
+import HelpIcon from 'ak-icon/glyph/bitbucket/help';
+import QuestionIcon from 'ak-icon/glyph/question';
+import SearchIcon from 'ak-icon/glyph/search';
+import JiraIcon from 'ak-icon/glyph/jira/logo';
+import CreateIcon from 'ak-icon/glyph/bitbucket/create';
+
+export default {
+  CalendarIcon: reactify(CalendarIcon),
+  CanvasIcon: reactify(CanvasIcon),
+  PageIcon: reactify(PageIcon),
+  PersonIcon: reactify(PersonIcon),
+  QuoteIcon: reactify(QuoteIcon),
+  OverviewIcon: reactify(OverviewIcon),
+  BitbucketIcon: reactify(BitbucketIcon),
+  HelpIcon: reactify(HelpIcon),
+  QuestionIcon: reactify(QuestionIcon),
+  SearchIcon: reactify(SearchIcon),
+  JiraIcon: reactify(JiraIcon),
+  CreateIcon: reactify(CreateIcon),
+};

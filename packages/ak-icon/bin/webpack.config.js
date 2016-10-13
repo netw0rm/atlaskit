21a8c621be83a459256bfce8227270ce7c842d8b
Joscha Feth <jfeth@atlassian.com>
2016-10-13 13:49:25 +1100

    fix: If we transpile at runtime we package core-js into each icon - not good, so let's just not use
    affects: ak-icon

--- packages/ak-icon/bin/webpack.config.js
+++ packages/ak-icon/bin/webpack.config.js
@@ -48,7 +48,6 @@ module.exports = (tmpFolder, entry) => ({
             'stage-0',
           ],
           plugins: [
-            'transform-runtime',
             ['incremental-dom', {
               components: true,
               hoist: true,

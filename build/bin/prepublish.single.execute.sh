e109255012a1c2f861f98b8b8d7b651dd39aa4fd
Joscha Feth <jfeth@atlassian.com>
2016-10-14 11:03:09 +1100

    chore: do not generate bundles if we are in bootstrap

--- build/bin/prepublish.single.execute.sh
+++ build/bin/prepublish.single.execute.sh
@@ -5,11 +5,19 @@ pushd ../.. > /dev/null
 CHALK="`npm bin`/chalk"
 popd > /dev/null
 
-$CHALK --no-stdin -t "{blue Generating UMD bundle...}"
-webpack --config ../../build/webpack/production-umd.js $@
+if [ -z ${RUN_DURING_INSTALL+x} ];
+then
+  # We are not running during install (e.g. we are running in dist mode, so we generate the bundles)
 
-$CHALK --no-stdin -t "{blue Generating CJS bundle...}"
-webpack --config ../../build/webpack/production-cjs.js $@
+  $CHALK --no-stdin -t "{blue Generating CJS bundle...}"
+  webpack --config ../../build/webpack/production-cjs.js $@
 
-$CHALK --no-stdin -t "{blue Generating bundle with dependencies...}"
-webpack --config ../../build/webpack/production-with-deps.js $@
+  $CHALK --no-stdin -t "{blue Generating UMD bundle...}"
+  webpack --config ../../build/webpack/production-umd.js $@
+
+  $CHALK --no-stdin -t "{blue Generating bundle with dependencies...}"
+  webpack --config ../../build/webpack/production-with-deps.js $@
+else
+  # We are running in install mode, no need to generate bundles
+  $CHALK --no-stdin -t "{blue Running in install mode, not generating bundles}"
+fi

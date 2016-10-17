aafd7848d4562493fe385b29a420db9228716888
Matias Corbanini <mcorbanini@atlassian.com>
2016-10-17 15:15:51 +1100

    test: initial tests added & small structure refactor
    affects: ak-calendar

--- packages/ak-calendar/test/_helpers.js
+++ packages/ak-calendar/test/_helpers.js
@@ -1,17 +1,32 @@
 // any file prefixed with _ will be ignored by Karma when picking up test files
 import { waitUntil, getShadowRoot } from 'akutil-common-test';
+import { define, vdom } from 'skatejs';
+import styles from '../src/styles';
+import { style } from 'akutil-common';
+
+const div = document.createElement('div');
 
 function setupComponent(Component) {
   const component = new Component();
   const componentHasShadowRoot = () => !!getShadowRoot(component);
-
-  document.body.appendChild(component);
+  div.appendChild(component);
+  document.body.appendChild(div);
 
   return waitUntil(componentHasShadowRoot).then(() => component);
 }
 
-function tearDownComponent(component) {
-  document.body.removeChild(component);
+function tearDownComponent() {
+  document.body.removeChild(div);
 }
 
-export { setupComponent, tearDownComponent };
+function stylesWrapperConstructor(Constructor) {
+  // Wrapper to set computed shadow dom css in the component
+  return define(`${Constructor.name}-styles-wrapper-constructo`, class extends Constructor {
+    static render(elem) {
+      elem.css = style(vdom, styles);
+      return super.render(elem);
+    }
+  });
+}
+
+export { setupComponent, tearDownComponent, stylesWrapperConstructor };

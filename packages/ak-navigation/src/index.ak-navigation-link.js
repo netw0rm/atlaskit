54902c15eee5e85eebddafed00c52c93f73c9483
Joshua Nelson <jonelson@atlassian.com>
2016-10-17 11:54:13 +1100

    refactor(component): use mouse down for blur
    affects: ak-navigation

--- packages/ak-navigation/src/index.ak-navigation-link.js
+++ packages/ak-navigation/src/index.ak-navigation-link.js
@@ -48,7 +48,7 @@ export default define('ak-navigation-link', {
         <a
           className={classNames(shadowStyles.locals.link)}
           href={elem.href || false}
-          ref={(a) => { a.addEventListener('mouseup', () => a.blur()); }}
+          onmousedown={e => e.preventDefault()}
           tabindex="0"
         >
           <slot

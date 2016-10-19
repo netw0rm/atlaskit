1dc9ee568e1dc4879b82e5c3bedfbf32f58d112c
Luke Batchelor <lbatchelor@atlassian.com>
2016-10-19 13:16:02 +0000

    generate.readme.html.sh edited online with Bitbucket

--- build/bin/generate.readme.html.sh
+++ build/bin/generate.readme.html.sh
@@ -8,5 +8,13 @@ popd > /dev/null
 PKG=$($BASEDIR/_get_package_name.sh)
 
 if [ -e "README.md" ]; then
+  # add the npm styles to the readme.html
+  echo "<head><link ref=\"stylesheet\" href=\"https://www.npmjs.com/static/css/index.css\"></style></head>"\
+    "<body><div class=\"container\">" >> ../../$OUTDIR/$PKG.html 
+  
   marky-markdown README.md > ../../$OUTDIR/$PKG.html
+  
+  # close off the html
+  echo "</div></body>" >> ../../$OUTDIR/$PKG.html
+  ec
 fi

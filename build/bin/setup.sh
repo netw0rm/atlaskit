526c05a5a869203893d27ed79d5786d132be1b91
Joscha Feth <jfeth@atlassian.com>
2016-10-20 13:50:18 +1100

    chore: add yarn.js bin

--- build/bin/setup.sh
+++ build/bin/setup.sh
@@ -3,6 +3,8 @@ set -e
 
 # use yarn in stead of npm
 ln -s /root/.yarn/bin/yarn /usr/bin/npm
+# TODO remove this when fixed in yarn (was introduced in 0.16)
+ln -s /root/.yarn/bin/yarn /usr/bin/yarn.js
 
 yarn config set progress false
 yarn config set color always

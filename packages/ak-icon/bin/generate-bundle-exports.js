c64c198e6bce9aeaa32c1be56e709bceb6b2ee73
Luke Batchelor <lbatchelor@atlassian.com>
2016-10-25 18:50:53 +1100

    refactor(component): Refactored generate-bundle-exports to use getAllIconPaths.js
    affects: ak-icon

--- packages/ak-icon/bin/generate-bundle-exports.js
+++ packages/ak-icon/bin/generate-bundle-exports.js
@@ -1,43 +1,36 @@
 #!/usr/bin/env node
 
-const glob = require('glob');
-const path = require('path');
 const fileToScope = require('./fileToScope');
 const pathToDashed = require('./pathToDashed');
 const iconNameToComponentName = require('./iconNameToComponentName');
+const getAllIconsPaths = require('./getAllIconsPaths');
 const { tmpFolderName } = require('./constants');
 
 const tempFolder = `../src/${tmpFolderName}/`;
-const sourceFolder = path.join(__dirname, '..', 'src');
 
 const pathToExport = p => iconNameToComponentName(pathToDashed(fileToScope(p, tempFolder)));
 
-glob(`${tempFolder}**/*.js`, {
-  cwd: sourceFolder,
-}, (err, files) => {
-  if (err) {
-    throw err;
-  }
-  /* eslint-disable no-console */
-  console.log('// NOTE: This file is generated from the glyphs found inside this Component');
-  console.log('//       DO NOT MODIFY THIS FILE AS YOUR CHANGES WILL BE OVERRIDDEN');
-  console.log();
-  console.log("import { size } from './Icon';");
 
-  files.forEach((file) => {
-    const componentName = pathToExport(file);
-    const tmpComponentName = `tmp${componentName}`;
-    const fileWithoutJs = file.substring(0, file.length - 3);
-    console.log(`import ${tmpComponentName} from '${fileWithoutJs}';`);
-  });
+const allIconPaths = getAllIconsPaths();
+/* eslint-disable no-console */
+console.log('// NOTE: This file is generated from the glyphs found inside this Component');
+console.log('//       DO NOT MODIFY THIS FILE AS YOUR CHANGES WILL BE OVERRIDDEN');
+console.log();
+console.log("import { size } from './Icon';");
 
-  console.log();
-
-  files.forEach((file) => {
-    const componentName = pathToExport(file);
-    const tmpComponentName = `tmp${componentName}`;
-    console.log(`export const ${componentName} = ${tmpComponentName};`);
-  });
-
-  console.log('export { size };');
+allIconPaths.forEach((file) => {
+  const componentName = pathToExport(file);
+  const tmpComponentName = `tmp${componentName}`;
+  const fileWithoutJs = file.substring(0, file.length - 3);
+  console.log(`import ${tmpComponentName} from '${fileWithoutJs}';`);
 });
+
+console.log();
+
+allIconPaths.forEach((file) => {
+  const componentName = pathToExport(file);
+  const tmpComponentName = `tmp${componentName}`;
+  console.log(`export const ${componentName} = ${tmpComponentName};`);
+});
+
+console.log('export { size };');

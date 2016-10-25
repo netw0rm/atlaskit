15d07ef37cb840bd10519513ed13adfb2f18887a
Luke Batchelor <lbatchelor@atlassian.com>
2016-10-25 18:55:43 +1100

    refactor(component): Adds getAllIconPaths.js to ak-icon
    affects: ak-icon

---
+++ packages/ak-icon/bin/getAllIconsPaths.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const glob = require('glob');
+const path = require('path');
+
+const { tmpFolderName } = require('./constants');
+
+const tempFolder = `../src/${tmpFolderName}/`;
+const sourceFolder = path.join(__dirname, '..', 'src');
+
+module.exports = () => {
+  const filesToExport = glob.sync(`${tempFolder}**/*.js`, {
+    cwd: sourceFolder,
+  });
+  return filesToExport;
+};

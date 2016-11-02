349701973108aeafc6f52c8b81d694d6bbb8d1ac
Joshua Nelson <jonelson@atlassian.com>
2016-11-02 12:08:20 +1100

    feat(util): meta keys and target can now be specified in user interaction
    affects: akutil-common-test
    
    BREAKING CHANGE: target is now no longer the third argument, there are two arguments: the key, and
    
    options = { target, modifiers } . See the jsdoc for more details

--- packages/akutil-common-test/src/user-interaction.js
+++ packages/akutil-common-test/src/user-interaction.js
@@ -1,38 +1,62 @@
 import 'custom-event-polyfill';
 import keyCode from 'keycode';
 
-function sendKeyEvent(key, target, type) {
+function sendKeyEvent(key, { modifiers, target }, type) {
   const event = new CustomEvent(type, {
     bubbles: true,
     cancelable: true,
   });
   event.keyCode = keyCode(key);
+  if (modifiers) {
+    event.metaKey = modifiers.metaKey;
+    event.altKey = modifiers.altKey;
+    event.shiftKey = modifiers.shiftKey;
+    event.ctrlKey = modifiers.ctrlKey;
+  }
   (target || document).dispatchEvent(event);
 }
 
 /**
  * Simulate a user's keydown input
  * @param key – the key to press, will be passed to keycode
- * @param target – a DOM element to trigger the event on. If undefined, triggered on the document.
- */
-export function keydown(key, target) {
-  sendKeyEvent(key, target, 'keydown');
+ * @param options {Object} – options for the event.
+ * @param options.target – a DOM element to trigger the event on. Default:triggered on the document.
+ * @param options.modifiers {Object} – modifiers for the event (see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
+ * @param options.modifiers.altKey {Boolean}
+ * @param options.modifiers.metaKey {Boolean}
+ * @param options.modifiers.shiftKey {Boolean}
+ * @param options.modifiers.ctrlKey {Boolean}
+ * */
+export function keydown(key, options) {
+  sendKeyEvent(key, options || {}, 'keydown');
 }
 
 /**
  * Simulate a user's keyup input
  * @param key – the key to press, will be passed to keycode
- * @param target – a DOM element to trigger the event on. If undefined, triggered on the document.
+ * @param options {Object} – options for the event.
+ * @param options.target – a DOM element to trigger the event on. Default:triggered on the document.
+ * @param options.modifiers {Object} – modifiers for the event (see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
+ * @param options.modifiers.altKey {Boolean}
+ * @param options.modifiers.metaKey {Boolean}
+ * @param options.modifiers.shiftKey {Boolean}
+ * @param options.modifiers.ctrlKey {Boolean}
  */
-export function keyup(key, target) {
-  sendKeyEvent(key, target, 'keyup');
+export function keyup(key, options) {
+  sendKeyEvent(key, options || {}, 'keyup');
 }
 
 /**
  * Simulate a user's keypress input
  * @param key – the key to press, will be passed to keycode
- * @param target – a DOM element to trigger the event on. If undefined, triggered on the document.
+ * @param options {Object} – options for the event.
+ * @param options.target – a DOM element to trigger the event on. Default:triggered on the document.
+ * @param options.modifiers {Object} – modifiers for the event (see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
+ * @param options.modifiers.altKey {Boolean}
+ * @param options.modifiers.metaKey {Boolean}
+ * @param options.modifiers.shiftKey {Boolean}
+ * @param options.modifiers.ctrlKey {Boolean}
  */
-export function keypress(key, target) {
-  sendKeyEvent(key, target, 'keypress');
+export function keypress(key, options) {
+  sendKeyEvent(key, options || {}, 'keypress');
 }
--- packages/akutil-common-test/test/user-interaction.js
+++ packages/akutil-common-test/test/user-interaction.js
@@ -7,32 +7,63 @@ import { keydown, keyup, keypress } from '../src';
 chai.should();
 chai.use(sinonChai);
 
-describe('keydown', () => {
-  it('fire keydown events', () => {
-    const spy = sinon.spy();
-    document.addEventListener('keydown', spy);
-    keydown('[');
-    document.removeEventListener('keydown', spy);
-    spy.should.have.been.calledOnce;
-  });
-});
-
-describe('keyup', () => {
-  it('fire keyup events', () => {
-    const spy = sinon.spy();
-    document.addEventListener('keyup', spy);
-    keyup('[');
-    document.removeEventListener('keyup', spy);
-    spy.should.have.been.calledOnce;
-  });
-});
-
-describe('keypress', () => {
-  it('fire keypress events', () => {
-    const spy = sinon.spy();
-    document.addEventListener('keypress', spy);
-    keypress('[');
-    document.removeEventListener('keypress', spy);
-    spy.should.have.been.calledOnce;
+describe('Keyboard interaction', () => {
+  const utils = { keydown, keyup, keypress };
+  ['keydown', 'keyup', 'keypress'].forEach((type) => {
+    const key = utils[type];
+    describe(`(${type})`, () => {
+      it(`can fire ${type} events`, () => {
+        const spy = sinon.spy();
+        document.addEventListener(type, spy);
+        key('[');
+        document.removeEventListener(type, spy);
+        spy.should.have.been.calledOnce;
+      });
+      it('meta keys can be specified', () => {
+        let shiftPressed;
+        const spy = (event) => {
+          shiftPressed = event.shiftKey;
+        };
+        document.addEventListener(type, spy);
+        key('[', {
+          modifiers: {
+            shiftKey: true,
+          },
+        });
+        document.removeEventListener(type, spy);
+        expect(shiftPressed).to.equal(true);
+      });
+      it('meta keys are initially off', () => {
+        let modifiers;
+        const spy = (event) => {
+          modifiers = {
+            shiftKey: event.shiftKey,
+            metaKey: event.metaKey,
+            altKey: event.altKey,
+            ctrlKey: event.ctrlKey,
+          };
+        };
+        document.addEventListener(type, spy);
+        key('[');
+        document.removeEventListener(type, spy);
+        expect(!!modifiers.shiftKey).to.equal(false, 'shiftKey is off by default');
+        expect(!!modifiers.metaKey).to.equal(false, 'metaKey is off by default');
+        expect(!!modifiers.altKey).to.equal(false, 'altKey is off by default');
+        expect(!!modifiers.ctrlKey).to.equal(false, 'ctrlKey is off by default');
+      });
+      it('target can be specified', () => {
+        let target;
+        const div = document.createElement('div');
+        const spy = (event) => {
+          target = event.target;
+        };
+        div.addEventListener(type, spy);
+        key('[', {
+          target: div,
+        });
+        div.removeEventListener(type, spy);
+        expect(target).to.equal(div);
+      });
+    });
   });
 });

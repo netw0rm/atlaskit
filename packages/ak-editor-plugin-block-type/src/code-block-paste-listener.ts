4cea26bd05b0704a31ce7a97557ea98b50904ed6
Steve Mao <maochenyan@gmail.com>
2016-11-02 14:09:17 +1100

    perf(plugin): cache pm.selection earlier
    affects: ak-editor-plugin-block-type

--- packages/ak-editor-plugin-block-type/src/code-block-paste-listener.ts
+++ packages/ak-editor-plugin-block-type/src/code-block-paste-listener.ts
@@ -2,7 +2,8 @@ import { ProseMirror, TextSelection } from 'ak-editor-prosemirror';
 
 export default (pm: ProseMirror) => {
   pm.root.addEventListener('paste', (event: ClipboardEvent) => {
-    if (pm.selection.$head.node(1).type.name !== 'code_block') {
+    const sel = pm.selection;
+    if (sel.$head.node(1).type.name !== 'code_block') {
       return;
     }
 
@@ -13,7 +14,6 @@ export default (pm: ProseMirror) => {
 
     const textNode = pm.schema.nodes.text.create({}, text);
 
-    const sel = pm.selection;
     const tr = pm.tr.replaceWith(sel.$from.pos, sel.$to.pos, textNode);
     const posAftePaste = tr.map(sel.$to.pos);
     tr.setSelection(new TextSelection(tr.doc.resolve(posAftePaste)));

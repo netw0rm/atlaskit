6c90c64bfac44f523912f15399d4fa74cfdad807
Ben Wong <benwong@atlassian.com>
2016-11-07 17:22:47 +1100

    chore(component): Fix stories for form submission and autofill
    affects: ak-field-text

--- packages/ak-field-text/stories/example-story.js
+++ packages/ak-field-text/stories/example-story.js
@@ -24,7 +24,7 @@ function generateFormWithInput(opts) {
       action={formTestUrl}
       method="post"
       style={{
-        'background-color': 'white',
+        backgroundColor: 'white',
         padding: '40px',
         width: '500px',
       }}
@@ -47,19 +47,20 @@ function submitTestForm(useNativeSubmitBtn) {
       <form
         action={formTestUrl}
         method="get"
-        style={{ 'background-color': 'white', padding: '40px', width: '500px' }}
+        style={{ backgroundColor: 'white', padding: '40px', width: '500px' }}
         target="myFrame"
       >
         <h2>Submit test</h2>
         <p>Note: Ensure that you are not using HTTPS for this story.</p>
-        {generateInput({ label: 'First name', id: 'fname' })}
-        {generateInput({ label: 'Last name', id: 'lname' })}
-        {generateInput({ type: 'email', label: 'Email', id: 'email' })}
-        {generateInput({ label: 'Full name', id: 'name' })}
+        {generateInput({ label: 'First name', name: 'fname' })}
+        {generateInput({ label: 'Last name', name: 'lname' })}
+        {generateInput({ type: 'email', label: 'Email', name: 'email' })}
+        {generateInput({ label: 'Full name', name: 'name' })}
         <p>
           {submitBtn}
         </p>
       </form>
+      <p>The data submitted by the form will appear below:</p>
       <iframe src="" name="myFrame" style={{ width: '50%', height: '300px' }} />
     </div>
   );
@@ -103,15 +104,15 @@ storiesOf(name, module)
       action={formTestUrl}
       method="post"
       style={{
-        'background-color': 'white',
+        backgroundColor: 'white',
         padding: '40px',
       }}
     >
       <h2>Autofill test</h2>
-      {generateInput({ label: 'First name', id: 'fname' })}
-      {generateInput({ label: 'Last name', id: 'lname' })}
-      {generateInput({ type: 'email', label: 'Email', id: 'email' })}
-      {generateInput({ label: 'Full name', id: 'name' })}
+      {generateInput({ label: 'First name', name: 'fname' })}
+      {generateInput({ label: 'Last name', name: 'lname' })}
+      {generateInput({ type: 'email', label: 'Email', name: 'email' })}
+      {generateInput({ label: 'Full name', name: 'name' })}
       <p>
         <ReactButton type="submit" appearance="primary">Submit</ReactButton>
       </p>

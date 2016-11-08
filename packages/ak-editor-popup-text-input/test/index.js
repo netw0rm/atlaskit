c59e0d113141ce0d4a0a363061931894df06d046
Steve Mao <maochenyan@gmail.com>
2016-11-08 13:30:07 +1100

    test(popup-text-input): add keyboard event tests
    affects: ak-editor-popup, ak-editor-popup-text-input

--- packages/ak-editor-popup-text-input/test/index.js
+++ packages/ak-editor-popup-text-input/test/index.js
@@ -1,17 +1,42 @@
+import { createEvent } from 'ak-editor-test';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import sinonChai from 'sinon-chai';
 
 import { name } from '../package.json';
 import Component from '../src';
 
 
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 chai.should();
 const expect = chai.expect;
 
 describe(name, () => {
+  function spyOnKeyup(keyCode) {
+    const spy = sinon.spy();
+    const component = new Component();
+    const div = document.createElement('div');
+    div.innerHTML = component;
+    div.addEventListener('enterKeyup', spy);
+
+    const event = createEvent('keyup');
+    event.keyCode = keyCode;
+    component.dispatchEvent(event);
+  }
+
   it('should be possible to create a component', () => {
     const component = new Component();
     expect(component.tagName).to.match(new RegExp(`^${name}`, 'i'));
+  });
+
+  it('should emit enterKeyup event on enter keyup', () => {
+    const spy = spyOnKeyup(13);
+    expect(spy).to.have.callCount(1);
+  });
+
+  it('should emit escKeyup event on ESC keyup', () => {
+    const spy = spyOnKeyup(27);
+    expect(spy).to.have.callCount(1);
   });
 });

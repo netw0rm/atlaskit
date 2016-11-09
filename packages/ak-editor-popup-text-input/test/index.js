9c94014853869b00acaba4cb17394ea1664d3ee9
Steve Mao <maochenyan@gmail.com>
2016-11-09 15:22:42 +1100

    test(input): fix tests for popup text input
    affects: ak-editor-popup, ak-editor-popup-text-input

--- packages/ak-editor-popup-text-input/test/index.js
+++ packages/ak-editor-popup-text-input/test/index.js
@@ -1,4 +1,5 @@
-import { createEvent } from 'ak-editor-test';
+import { createEvent, fixtures } from 'ak-editor-test';
+import { afterMutations } from 'akutil-common-test';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
@@ -6,23 +7,30 @@ import sinonChai from 'sinon-chai';
 import { name } from '../package.json';
 import Component from '../src';
 
-
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
 chai.should();
 const expect = chai.expect;
 
+const fixture = fixtures();
+
 describe(name, () => {
-  function spyOnKeyup(keyCode) {
+  function spyOnKeyup(keyCode, eventName) {
     const spy = sinon.spy();
     const component = new Component();
-    const div = document.createElement('div');
-    div.innerHTML = component;
-    div.addEventListener('enterKeyup', spy);
+    const tmpContainer = fixture();
+    tmpContainer.appendChild(component);
+    tmpContainer.addEventListener(eventName, spy);
 
     const event = createEvent('keyup');
     event.keyCode = keyCode;
-    component.dispatchEvent(event);
+
+    return new Promise((resolve) => {
+      afterMutations(() => {
+        component.shadowRoot.querySelector('input').dispatchEvent(event);
+        resolve(spy);
+      });
+    });
   }
 
   it('should be possible to create a component', () => {
@@ -30,13 +38,17 @@ describe(name, () => {
     expect(component.tagName).to.match(new RegExp(`^${name}`, 'i'));
   });
 
-  it('should emit enterKeyup event on enter keyup', () => {
-    const spy = spyOnKeyup(13);
-    expect(spy).to.have.callCount(1);
-  });
+  it('should emit enterKeyup event on enter keyup', () =>
+     spyOnKeyup(13, 'enterKeyup')
+      .then((spy) => {
+        expect(spy).to.have.callCount(1);
+      })
+  );
 
-  it('should emit escKeyup event on ESC keyup', () => {
-    const spy = spyOnKeyup(27);
-    expect(spy).to.have.callCount(1);
-  });
+  it('should emit escKeyup event on ESC keyup', () =>
+     spyOnKeyup(27, 'escKeyup')
+      .then((spy) => {
+        expect(spy).to.have.callCount(1);
+      })
+  );
 });

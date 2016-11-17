864db3381c28f961e97034990e8ead3452753ed5
Steve Mao <maochenyan@gmail.com>
2016-11-18 10:38:36 +1100

    test(hyperlink): move hyperlink test to the correct section
    affects: ak-editor-bitbucket

--- packages/ak-editor-bitbucket/test/index.ts
+++ packages/ak-editor-bitbucket/test/index.ts
@@ -68,27 +68,6 @@ function waitUntilPMReady(editor: typeof AkEditorBitbucket) : Promise<HTMLElemen
 describe('ak-editor-bitbucket', () => {
   const rewireSpy = RewireSpy();
 
-  it.skip('should add a href on enter', (done) => {
-    const href = 'https://www.atlassian.com';
-    buildExpandedEditor(fixture(), `<p>foo</p>`)
-      .then((editor) => {
-        editor._pm.setTextSelection(1, 4);
-
-        return waitUntilPMReady(editor).then(() => {
-          afterMutations(
-            () => {
-              // IE 11 needs one more tick to render
-            },
-            () => {
-              emit(document, 'addHyperlink', { detail: { value: href } });
-              expect(editor._pm.doc).to.deep.equal(doc(p(a({ href })('foo'))));
-            },
-            done
-          );
-        });
-      });
-  });
-
   it('is possible to create a component', () => {
     let editor: any;
     expect(() => {
@@ -285,6 +264,27 @@ describe('ak-editor-bitbucket', () => {
       const textInput = locateWebComponent('ak-editor-popup-text-input', edit.shadowRoot)[0]
       return textInput.shadowRoot.querySelector('input');
     }
+
+    it('should add a href on enter', (done) => {
+      const href = 'https://www.atlassian.com';
+      buildExpandedEditor(fixture(), `<p>foo</p>`)
+        .then((editor) => {
+          editor._pm.setTextSelection(1, 4);
+
+          return waitUntilPMReady(editor).then(() => {
+            afterMutations(
+              () => {
+                // IE 11 needs one more tick to render
+              },
+              () => {
+                emit(document, 'addHyperlink', { detail: { value: href } });
+                expect(editor._pm.doc).to.deep.equal(doc(p(a({ href })('foo'))));
+              },
+              done
+            );
+          });
+        });
+    });
 
     it('should contain the right href value', (done) => {
       const href = 'https://www.atlassian.com';

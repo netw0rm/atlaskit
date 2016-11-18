f5a529de6607267aada1e13febd132a8df2698b1
Ben Gummer <bgummer@atlassian.com>
2016-11-18 16:19:33 +1100

    chore: clean up tests for PR

--- packages/ak-badge/test/indexSpec.jsx
+++ packages/ak-badge/test/indexSpec.jsx
@@ -8,15 +8,19 @@ import styles from '../src/style.less';
 chai.should();
 
 describe('ak-badge', () => {
+  function valueSpan(element) {
+    return element.find(`.${styles.locals.value}`);
+  }
+
   describe('value property', () => {
     it('should be visibly displayed', () => {
-      shallow(<AkBadge value={5} />).find('span > span').text().should.equal('5');
+      valueSpan(shallow(<AkBadge value={5} />)).text().should.equal('5');
     });
     it('should only accept positive numbers', () => {
-      shallow(<AkBadge value={-5} />).find('span > span').text().should.equal('0');
+      valueSpan(shallow(<AkBadge value={-5} />)).text().should.equal('0');
     });
     it('should show show Infinity as the ∞ character', () => {
-      shallow(<AkBadge value={Infinity} max={Infinity} />).find('span > span').text().should.equal('∞');
+      valueSpan(shallow(<AkBadge value={Infinity} max={Infinity} />)).text().should.equal('∞');
     });
     it('should trigger onValueUpdated when value prop changed with a number', (done) => {
       const badge = shallow(
@@ -34,25 +38,33 @@ describe('ak-badge', () => {
   });
   describe('max property', () => {
     it('should constrain to 99+ when not specified', () => {
-      shallow(<AkBadge value={101} />).find('span > span').text().should.equal('99+');
+      valueSpan(shallow(<AkBadge value={101} />)).text().should.equal('99+');
     });
     it('should constrain the value when set', () => {
-      shallow(<AkBadge value={200} max={100} />).find('span > span').text().should.equal('100+');
-      shallow(<AkBadge value={Number.MAX_VALUE} />).find('span > span').text().should.equal('99+');
+      valueSpan(shallow(<AkBadge value={200} max={100} />)).text().should.equal('100+');
+    });
+    it('should pass the value through when max === 0', () => {
+      valueSpan(
+        shallow(<AkBadge value={Number.MAX_VALUE} max={0} />)
+      ).text().should.equal(`${Number.MAX_VALUE}`);
     });
     it('should not constrain if equal to value', () => {
-      shallow(<AkBadge value={200} max={200} />).find('span > span').text().should.equal('200');
+      valueSpan(shallow(<AkBadge value={200} max={200} />)).text().should.equal('200');
     });
   });
   describe('appearance property', () => {
     it('should be "default" when not set', () => {
-      shallow(<AkBadge />).find('span > span').hasClass(styles.locals.default).should.equal(true);
+      valueSpan(shallow(<AkBadge />)).hasClass(styles.locals.default).should.equal(true);
     });
     it('should change when set to an approved value', () => {
-      shallow(<AkBadge appearance="removed" />).find('span > span').hasClass(styles.locals.removed).should.equal(true);
+      valueSpan(
+        shallow(<AkBadge appearance="removed" />)
+      ).hasClass(styles.locals.removed).should.equal(true);
     });
     it('should revert to "default" when set to an invalid value', () => {
-      shallow(<AkBadge appearance="foo" />).find('span > span').hasClass(styles.locals.default).should.equal(true);
+      valueSpan(
+        shallow(<AkBadge appearance="foo" />)
+      ).hasClass(styles.locals.default).should.equal(true);
     });
   });
 });

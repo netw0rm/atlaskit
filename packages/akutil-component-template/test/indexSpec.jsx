16f33268bd12c2839742746dd4fe0ed5fe682025
Matias Corbanini <mcorbanini@atlassian.com>
2016-11-18 13:20:43 +1100

    refactor(component): make a better test example
    affects: akutil-component-template

--- packages/akutil-component-template/test/indexSpec.jsx
+++ packages/akutil-component-template/test/indexSpec.jsx
@@ -1,15 +1,18 @@
 import chai from 'chai';
+import React from 'react';
 import chaiAsPromised from 'chai-as-promised';
 import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
 
 import { name } from '../package.json';
 
 chai.use(chaiAsPromised);
-chai.use(chaiEnzyme);
-chai.should();
+chai.use(chaiEnzyme());
+
+const Component = () => <div className="test">test</div>;
 
 describe(name, () => {
-  it('should work', () => {
-    true.should.be.true;
-  });
+  it('should work', () =>
+    expect(mount(<Component />)).to.have.className('test')
+  );
 });

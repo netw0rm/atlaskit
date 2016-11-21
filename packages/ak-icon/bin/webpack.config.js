32d788efdcd70bb2f3bfd9f36b335a2930c35746
Ben Wong <benwong@atlassian.com>
2016-11-21 16:55:06 +1100

    chore(package): Add query to ak-icon webpack config
    affects: ak-icon

--- packages/ak-icon/bin/webpack.config.js
+++ packages/ak-icon/bin/webpack.config.js
@@ -45,6 +45,14 @@ module.exports = (tmpFolder, entry) => ({
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
+        query: {
+          babelrc: false,
+          presets: [
+            'es2015',
+            'react',
+            'stage-0',
+          ],
+        },
       },
     ],
   },

8c11779c7eed2d1095e5d450b518fec3dee98d98
Joshua Nelson <jonelson@atlassian.com>
2016-11-22 16:14:54 +1100

    feat(exports): add global item export
    affects: ak-navigation

--- packages/ak-navigation/src/index.jsx
+++ packages/ak-navigation/src/index.jsx
@@ -3,3 +3,4 @@ export { default as AkContainerItem } from './components/js/ContainerItem';
 export { default as AkContainerHeader } from './components/js/ContainerHeader';
 export { default as AkContainerNavigation } from './components/js/ContainerNavigation';
 export { default as AkGlobalNavigation } from './components/js/GlobalNavigation';
+export { default as AkGlobalItem } from './components/js/GlobalItem';

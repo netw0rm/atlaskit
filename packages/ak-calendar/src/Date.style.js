43a3c9f44b128f5ee83f538d072b715089ec3603
Trey Shugart <treshugart@gmail.com>
2016-11-22 16:14:02 +1100

    refactor: Simplify syntax.
    affects: ak-calendar
    
    AK-822

--- packages/ak-calendar/src/Date.style.js
+++ packages/ak-calendar/src/Date.style.js
@@ -66,33 +66,29 @@ function getHoverColor({ previouslySelected, selected }) {
   return akColorN0;
 }
 
-export default function (props) {
-  return merge(
-    style({
-      backgroundColor: getBackgroundColor(props),
-      border: `2px solid ${getBorderColor(props)}`,
-      borderRadius: 4,
-      color: getColor(props),
-      cursor: getCursor(props),
-      display: 'table-cell',
-      fontSize: 12,
-      fontWeight: 'lighter',
-      padding: '2px 5px',
-      position: 'relative',
-    }),
-    hover({
-      backgroundColor: getHoverBackgroundColor(props),
-      color: getHoverColor(props),
-    })
-  );
-}
+export default props => merge(
+  style({
+    backgroundColor: getBackgroundColor(props),
+    border: `2px solid ${getBorderColor(props)}`,
+    borderRadius: 4,
+    color: getColor(props),
+    cursor: getCursor(props),
+    display: 'table-cell',
+    fontSize: 12,
+    fontWeight: 'lighter',
+    padding: '2px 5px',
+    position: 'relative',
+  }),
+  hover({
+    backgroundColor: getHoverBackgroundColor(props),
+    color: getHoverColor(props),
+  })
+);
 
-export function underline({ disabled }) {
-  return style({
-    borderBottom: `1px solid ${disabled ? akColorN400 : akColorB75}`,
-    bottom: -2,
-    left: '12.5%',
-    position: 'absolute',
-    width: '75%',
-  });
-}
+export const underline = ({ disabled }) => style({
+  borderBottom: `1px solid ${disabled ? akColorN400 : akColorB75}`,
+  bottom: -2,
+  left: '12.5%',
+  position: 'absolute',
+  width: '75%',
+});

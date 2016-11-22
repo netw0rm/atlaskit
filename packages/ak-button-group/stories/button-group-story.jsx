46c645afd2dae767eda96aefeaa1c9578138596d
Nadia Makarevich <nmakarevich@atlassian.com>
2016-11-22 12:53:23 +1100

    style(component): remove unnecessary component
    affects: ak-button-group

--- packages/ak-button-group/stories/button-group-story.jsx
+++ packages/ak-button-group/stories/button-group-story.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 
 import ReactAkButtonGroup from '../src';
 import { name } from '../package.json';
-//
+
 const ReactIcon = reactify(CharlieIcon);
 const ReactDropdown = reactify(Dropdown);
 

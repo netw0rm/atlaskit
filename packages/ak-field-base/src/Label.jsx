eb9a8cf6c032d9e55600d85a68440d1108d1f06f
Matias Corbanini <mcorbanini@atlassian.com>
2016-11-22 11:44:30 +1100

    fix(component): Label specs done
    affects: ak-field-base

--- packages/ak-field-base/src/Label.jsx
+++ packages/ak-field-base/src/Label.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import styles from './shadow.less';
+import styles from './styles.less';
 
 /*
   eslint-disable react/prop-types, jsx-a11y/label-has-for, jsx-a11y/no-static-element-interactions
@@ -9,7 +9,6 @@ export default (props) => {
   const labelClasses = classNames(styles.locals.labelText, {
     [styles.locals.hidden]: props.hideLabel,
   });
-  const children = props.children;
   // we render the label in a span that is in a div so that the label itself will be display: block
   // but we can put the click handler on the span so that clicking white space after the label
   // doesnt call anything
@@ -17,9 +16,9 @@ export default (props) => {
     <label className={styles.locals.label}>
       <div className={labelClasses}>
         <span onClick={props.onLabelClick}>{props.label}</span>
-        {props.required ? <span class={styles.locals.required}>*</span> : null}
+        {props.required ? <span className={styles.locals.required}>*</span> : null}
       </div>
-      {children ? children() : null}
+      {props.children}
     </label>
   );
 };
--- packages/ak-field-base/test/LabelSpec.jsx
+++ packages/ak-field-base/test/LabelSpec.jsx
@@ -1,19 +1,61 @@
 import chai, { expect } from 'chai';
 import React from 'react';
 import chaiAsPromised from 'chai-as-promised';
+import sinonChai from 'sinon-chai';
+import { shallow } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
 import Label from '../src/Label';
-import { shallow } from 'enzyme';
+import styles from '../src/styles.less';
 
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 chai.use(chaiEnzyme());
 
 describe('ak-field-base', () =>
   describe('Label', () => {
-    describe('by default', () => {
+    describe('by default', () =>
       it('should render a label element', () =>
         expect(shallow(<Label />)).to.have.descendants('label')
-      );
+      )
+    );
+
+    describe('label prop', () => {
+      it('should be reflected in the label element', () => {
+        const label = 'This is a label';
+        const wrapper = shallow(<Label label={label} />);
+        expect(wrapper.find('label')).to.have.text(label);
+      });
     });
+
+    describe('required prop', () => {
+      it('should append an asterisk to the content', () =>
+        expect(shallow(<Label required />).find(`.${styles.locals.required}`)).to.have.text('*')
+      );
+
+      it('should not append an asterisk to the content if required is not set', () => {
+        expect(shallow(<Label />)).to.not.have.descendants(`.${styles.locals.required}`);
+        expect(shallow(<Label />)).to.not.have.text('*');
+      });
+    });
+
+    describe('onLabelClick prop', () =>
+      it('should fire handler when the span is clicked', () => {
+        const handler = sinon.spy();
+        const wrapper = shallow(<Label onLabelClick={handler} />);
+        wrapper.find('span').simulate('click');
+        expect(handler).to.have.been.calledOnce;
+      })
+    );
+
+    describe('.children', () =>
+      it('should render any children passed to it', () => {
+        const wrapper = shallow(
+          <Label>
+            <div className="foo">Here is some child content!</div>
+          </Label>
+        );
+        expect(wrapper.find('div.foo')).to.be.present;
+      })
+    );
   })
 );

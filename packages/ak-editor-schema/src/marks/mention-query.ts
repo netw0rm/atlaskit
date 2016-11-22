3df0be6950ad66e69bbcee18550260865acb10ab
Oscar Wallhult <owallhult@atlassian.com>
2016-11-22 11:43:56 +1100

    feat: Mention-query-mark and tests
    affects: ak-editor-schema

--- packages/ak-editor-schema/src/marks/mention-query.ts
+++ packages/ak-editor-schema/src/marks/mention-query.ts
@@ -1,11 +1,18 @@
-import { Mark, MarkType } from 'ak-editor-prosemirror';
+import { Mark, MarkType, Schema } from 'ak-editor-prosemirror';
 
 export class MentionQueryMarkType extends MarkType {
+  constructor(name: string, rank: number, schema: Schema) {
+    if (name !== 'mention_query') {
+      throw new Error("MentionQueryMarkType must be named 'mention_query'.");
+    }
+    super(name, rank, schema);
+  }
+
   get inclusiveRight() {
     return true;
   }
 
-  toDOM() { return ['span', { style: /*'background: #e8eaed; border-radius: 20px; padding: 0 5px; */'color: #165ecc', 'data-mention-query': true }]; }
+  toDOM() { return ['span', { 'data-mention-query': true, 'style': 'color: rgb(22, 94, 204)' }]; }
 }
 
 export interface MentionQueryMark extends Mark {

1bece961a9b5bc7f64e21da1e09e2bb7e0e2f523
Alex Reardon <areardon@atlassian.com>
2016-11-23 15:16:10 +1100

    chore(component): Updating root ak-button to use new conventions
    affects: ak-button

--- packages/ak-button/src/index.jsx
+++ packages/ak-button/src/index.jsx
@@ -10,80 +10,78 @@ import Link from './Link';
 
 /* eslint-disable react/no-unused-prop-types */
 export default class AkButton extends Component {
-  static get propTypes() {
-    return {
-      /**
-       * @description Predefined appearances of an ak-button. One of:
-       * 'primary', 'default', 'subtle', 'compact'
-       * @memberof Button
-       * @default 'standard'
-       * @type {string}
-       */
-      appearance: PropTypes.oneOf(appearance.values),
-      /**
-       * @description Type of the ak-button. One of:
-       * 'button', 'submit'.
-       * @memberof Button
-       * @default button
-       * @type {string}
-       */
-      type: PropTypes.oneOf(type.values),
-      /**
-       * @description href of the ak-button.
-       * If href is set, button will redirect to href url when clicked.
-       * @memberof Button
-       * @default button
-       * @type {string}
-       */
-      href: PropTypes.string,
-      /**
-       * @description Standard target attribute for hyperlinks
-       * @memberof Button
-       * @type {string}
-       */
-      target: PropTypes.string,
-      /**
-       * @description Option to disable button and every click event
-       * @memberof Button
-       * @default false
-       * @type {boolean}
-       */
-      isDisabled: PropTypes.bool,
-      /**
-       * @description Option to change button's padding. One of:
-       * 'none', 'compact', 'default'
-       * @memberof Button
-       * @default 'default'
-       * @type {string}
-       */
-      spacing: PropTypes.oneOf(spacing.values),
-      /**
-       * @description Option to make a button selected
-       * @memberof Button
-       * @default false
-       * @type {boolean}
-       */
-      isSelected: PropTypes.bool,
-      /**
-       * @description Option to make have a dark look and feel of a button.
-       * @memberof Button
-       * @default false
-       * @type {boolean}
-       */
-      theme: PropTypes.oneOf(theme.values),
-      /**
-       * @description iconBefore
-       * @memberof Button
-       * @type {element}
-       */
-      iconBefore: PropTypes.element,
-      /**
-       * @description iconAfter
-       * @memberof Button
-       * @type {element}
-       */
-      iconAfter: PropTypes.element,
-    };
+  static propTypes = {
+    /**
+     * @description Predefined appearances of an ak-button. One of:
+     * 'primary', 'default', 'subtle', 'compact'
+     * @memberof Button
+     * @default 'standard'
+     * @type {string}
+     */
+    appearance: PropTypes.oneOf(appearance.values),
+    /**
+     * @description Type of the ak-button. One of:
+     * 'button', 'submit'.
+     * @memberof Button
+     * @default button
+     * @type {string}
+     */
+    type: PropTypes.oneOf(type.values),
+    /**
+     * @description href of the ak-button.
+     * If href is set, button will redirect to href url when clicked.
+     * @memberof Button
+     * @default button
+     * @type {string}
+     */
+    href: PropTypes.string,
+    /**
+     * @description Standard target attribute for hyperlinks
+     * @memberof Button
+     * @type {string}
+     */
+    target: PropTypes.string,
+    /**
+     * @description Option to disable button and every click event
+     * @memberof Button
+     * @default false
+     * @type {boolean}
+     */
+    isDisabled: PropTypes.bool,
+    /**
+     * @description Option to change button's padding. One of:
+     * 'none', 'compact', 'default'
+     * @memberof Button
+     * @default 'default'
+     * @type {string}
+     */
+    spacing: PropTypes.oneOf(spacing.values),
+    /**
+     * @description Option to make a button selected
+     * @memberof Button
+     * @default false
+     * @type {boolean}
+     */
+    isSelected: PropTypes.bool,
+    /**
+     * @description Option to make have a dark look and feel of a button.
+     * @memberof Button
+     * @default false
+     * @type {boolean}
+     */
+    theme: PropTypes.oneOf(theme.values),
+    /**
+     * @description iconBefore
+     * @memberof Button
+     * @type {element}
+     */
+    iconBefore: PropTypes.element,
+    /**
+     * @description iconAfter
+     * @memberof Button
+     * @type {element}
+     */
+    iconAfter: PropTypes.element,
   }
 
   static defaultProps = {

7e316af3ed2e746ebfed1e7ab5ef64f13d121670
Steve Mao <maochenyan@gmail.com>
2016-11-24 15:51:51 +1100

    refactor(var): cache the properties of `pm.selection`
    affects: ak-editor-bitbucket

--- packages/ak-editor-bitbucket/src/keymap.js
+++ packages/ak-editor-bitbucket/src/keymap.js
@@ -102,7 +102,7 @@ export function buildKeymap(schema, mapKeys) {
       bind("Shift-Ctrl-\\", setBlockType(node));
       // https://github.com/ProseMirror/prosemirror/issues/419
       bind("Enter", (pm, apply) => {
-        let {$from} = pm.selection;
+        let {$from, $head, empty} = pm.selection;
 
         if (!$from.parent.type.isCode) {
           return false;
@@ -110,10 +110,10 @@ export function buildKeymap(schema, mapKeys) {
 
         if (apply !== false) {
           if (
-            pm.selection.$head.parent.textContent.slice(-1) === "\n"
-            && pm.selection.empty
+            $head.parent.textContent.slice(-1) === "\n"
+            && empty
             // nodeSize includes newlines
-            && pm.selection.$head.parentOffset === pm.selection.$head.parent.nodeSize - 2
+            && $head.parentOffset === $head.parent.nodeSize - 2
           ) {
             commands.deleteCharBefore(pm);
             return false;

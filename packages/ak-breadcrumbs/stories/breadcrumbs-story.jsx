4a6375c6e25bf4bc88241b0344a6f1a8c91e471d
Ben Wong <benwong@atlassian.com>
2016-11-25 17:12:38 +1100

    fix(component): Fix spacing for slash separator
    affects: ak-breadcrumbs

--- packages/ak-breadcrumbs/stories/breadcrumbs-story.jsx
+++ packages/ak-breadcrumbs/stories/breadcrumbs-story.jsx
@@ -19,6 +19,18 @@ storiesOf(name, module)
   .add('ak-breadcrumbs with no items', () => (
     <AkBreadcrumbs />
   ))
+  .add('ak-breadcrumbs with one item', () => (
+    <AkBreadcrumbs>
+      <AkBreadcrumbsItem href="/page">Page</AkBreadcrumbsItem>
+    </AkBreadcrumbs>
+  ))
+  .add('ak-breadcrumbs with one item with very long text', () => (
+    <AkBreadcrumbs>
+      <AkBreadcrumbsItem href="/supercalifragilisticexpialidocious">
+        Supercalifragilisticexpialidocious
+      </AkBreadcrumbsItem>
+    </AkBreadcrumbs>
+  ))
   .add('ak-breadcrumbs with many items', () => (
     <AkBreadcrumbs>
       <AkBreadcrumbsItem href="/item">Item 1</AkBreadcrumbsItem>
@@ -35,15 +47,12 @@ storiesOf(name, module)
   .add('ak-breadcrumbs with many items, inside small container', () => (
     <div style={{ maxWidth: '500px', border: '1px solid black' }}>
       <AkBreadcrumbs >
-        <AkBreadcrumbsItem href="/item">Item 1</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 2</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 3</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 4</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 5</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 6</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 7</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 8</AkBreadcrumbsItem>
-        <AkBreadcrumbsItem href="/item">Item 9</AkBreadcrumbsItem>
+        <AkBreadcrumbsItem href="/pages">Pages</AkBreadcrumbsItem>
+        <AkBreadcrumbsItem href="/pages/home">Home</AkBreadcrumbsItem>
+        <AkBreadcrumbsItem href="/pages/adg3">ADG 3 - New site</AkBreadcrumbsItem>
+        <AkBreadcrumbsItem href="/pages/daccontent">design.atlassian.com content</AkBreadcrumbsItem>
+        <AkBreadcrumbsItem href="/pages/product-design">Product design (draft)</AkBreadcrumbsItem>
+        <AkBreadcrumbsItem href="/pages/patternsdesign">Patterns design (draft)</AkBreadcrumbsItem>
       </AkBreadcrumbs>
     </div>
   ));

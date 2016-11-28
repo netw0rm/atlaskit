0b1f41e57eaa4cef40a68c18416e662d68c5ddac
Steve Mao <maochenyan@gmail.com>
2016-11-28 16:29:02 +1100

    test(marks): add tests for toggling marks
    affects: ak-editor-bitbucket

--- packages/ak-editor-bitbucket/test/__keymap.ts
+++ packages/ak-editor-bitbucket/test/__keymap.ts
@@ -1,39 +1,52 @@
 import { keydown } from 'akutil-common-test';
 
 describe.skip('ak-editor-bitbucket keymap', () => {
-  it('should change paragraph to h1', () => {
+  describe('block type', () => {
+    it('should change paragraph to h1', () => {
+    });
+
+    it('should change paragraph to h2', () => {
+    });
+
+    it('should change paragraph to h3', () => {
+    });
+
+    it('should change paragraph to h4', () => {
+    });
+
+    it('should change paragraph to h5', () => {
+    });
+
+    it('should change h1 to paragraph', () => {
+    });
+
+    it('should change h1 to code_block', () => {
+    });
+
+    it('should change h1 to blockquote', () => {
+    });
+
+    it('should change h1 to bullet_list', () => {
+    });
+
+    it('should change h1 to ordered_list', () => {
+    });
+
+    it('should not do anything when changing paragraph to paragraph', () => {
+    });
+
+    it('should change to paragraph first when changing h1 to blockquote', () => {
+    });
   });
 
-  it('should change paragraph to h2', () => {
-  });
+  describe('marks', () => {
+    it('should toggle bold', () => {
+    });
 
-  it('should change paragraph to h3', () => {
-  });
+    it('should toggle italic', () => {
+    });
 
-  it('should change paragraph to h4', () => {
-  });
-
-  it('should change paragraph to h5', () => {
-  });
-
-  it('should change h1 to paragraph', () => {
-  });
-
-  it('should change h1 to code_block', () => {
-  });
-
-  it('should change h1 to blockquote', () => {
-  });
-
-  it('should change h1 to bullet_list', () => {
-  });
-
-  it('should change h1 to ordered_list', () => {
-  });
-
-  it('should not do anything when changing paragraph to paragraph', () => {
-  });
-
-  it('should change to paragraph first when changing h1 to blockquote', () => {
+    it('should toggle code', () => {
+    });
   });
 });

d7e3ff9d432c7bad96e4dd0c06fca08b65710a1a
Ben Wong <benwong@atlassian.com>
2016-11-29 12:36:27 +1100

    chore(component): Remove unnecessary styles, use class reference instead of element
    affects: ak-breadcrumbs

--- packages/ak-breadcrumbs/src/BreadcrumbsItem.jsx
+++ packages/ak-breadcrumbs/src/BreadcrumbsItem.jsx
@@ -31,7 +31,12 @@ export default class BreadcrumbsItem extends PureComponent {
   render() {
     return (
       <div className={styles.locals.item}>
-        <Button appearance="link" spacing="compact" href={this.props.href}>
+        <Button
+          appearance="link"
+          className={styles.locals.itemButton}
+          spacing="compact"
+          href={this.props.href}
+        >
           {this.props.children}
         </Button>
       </div>

d5bada71d5a9359736011bd5c254410737714022
Ben Gummer <bgummer@atlassian.com>
2016-12-01 14:35:42 +1100

    fix(platform): convert ak-blanket to React
    affects: ak-blanket
    
    BREAKING CHANGE: the default export web component has been replaced with a React component. The
    
    events export has been removed, use the React onBlanketClicked handler instead.

--- packages/ak-blanket/src/internal/index.events.js
+++
@@ -1,8 +0,0 @@
-/**
-* This event is emitted when the blanket is clicked.
-* The emission of this event can be controlled by the {@link Blanket#clickable} property.
-*
-* @event Blanket#activate
-*/
-// eslint-disable-next-line import/prefer-default-export
-export const activate = 'activate';

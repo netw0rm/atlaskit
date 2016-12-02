41a05ef0aeae56b352e954a2f272982a7fb07938
Luke Batchelor <lbatchelor@atlassian.com>
2016-12-02 16:10:07 +1100

    refactor(component): Base functionality of the smart and dumb tooltip component
    affects: ak-tooltip

---
+++ packages/ak-tooltip/src/Tooltip.jsx
@@ -0,0 +1,96 @@
+import React, { PureComponent, PropTypes } from 'react';
+import Layer from 'ak-layer';
+
+import styles from 'style!./styles.less';
+
+
+function positionToPopperPosition(position) {
+  const allowedPositions = {
+    top: 'top center',
+    bottom: 'bottom center',
+    left: 'left middle',
+    right: 'right middle',
+  };
+  if (allowedPositions[position]) {
+    return allowedPositions[position];
+  }
+  return allowedPositions.bottom;
+}
+
+/* eslint-disable react/no-unused-prop-types */
+
+/**
+ * @description Create instances of the component programmatically, or using markup.
+ * @class Tooltip
+ * @example @html <ak-tooltip id='myTooltip'></ak-tooltip>
+ * @example @js import Tooltip from 'ak-tooltip';
+ * const tooltip = new Tooltip();
+ * tooltip.id = 'myTooltip';
+ */
+export default class Tooltip extends PureComponent {
+  static propTypes = {
+    /**
+     * @description The location of where the tooltip will appear, relative to the component it
+     * is bound to. This is usually set by an ak-tooltip-trigger.
+     * Allowed values: top, bottom, left and right.
+     * @memberof Tooltip
+     * @instance
+     * @type {string}
+     * @default bottom
+    */
+    position: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+    /**
+     * @description The text to display in the tooltip when a user hovers or focuses on the
+     * wrapped element.
+     * This is normally set by an ak-tooltip-trigger.
+     * @memberof Tooltip
+     * @instance
+     * @type {string}
+     * @default none
+    */
+    description: PropTypes.string,
+    /**
+     * @description Whether or not the tooltip is open and visible on the page.
+     * This is normally set by an ak-tooltip-trigger.
+     * @memberof Tooltip
+     * @instance
+     * @type {boolean}
+     * @default none
+    */
+    visible: PropTypes.bool,
+    onMouseOver: PropTypes.func,
+    onMouseOut: PropTypes.func,
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    position: 'bottom',
+    description: '',
+    visible: false,
+    onMouseOver: () => {},
+    onMouseOut: () => {},
+    children: null,
+  }
+
+  render() {
+    const props = this.props;
+    const content = props.visible ?
+      (<div className={styles.tooltip}>{props.description}</div>) :
+      null;
+
+    return (
+      <div onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut} data-foo="f">
+        <Layer
+          position={positionToPopperPosition(props.position)}
+          autoPosition
+          content={content}
+        >
+          {this.props.children}
+        </Layer>
+      </div>
+    );
+  }
+}
+
+/* eslint-enable react/no-unused-prop-types */
+
---
+++ packages/ak-tooltip/src/index.jsx
@@ -0,0 +1,51 @@
+import React, { PureComponent, PropTypes } from 'react';
+import StatelessTooltip from './Tooltip';
+
+/* eslint-disable react/no-unused-prop-types */
+/* export the smart (useful) component by default. Class name doesnt matter as user's will name it
+   at import time */
+export default class Tooltip extends PureComponent {
+  static propTypes = {
+    description: PropTypes.string,
+    position: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    description: '',
+    position: 'bottom',
+    children: null,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  setVisible = (visible) => {
+    this.setState({
+      visible,
+    });
+  }
+
+  render() {
+    const props = this.props;
+
+    return (<StatelessTooltip
+      visible={this.state.visible}
+      description={props.description}
+      position={props.position}
+      onMouseOver={() => this.setVisible(true)}
+      onMouseOut={() => this.setVisible(false)}
+    >
+      {props.children}
+    </StatelessTooltip>);
+  }
+}
+
+export { StatelessTooltip };
+
+/* eslint-enable react/no-unused-prop-types */
+

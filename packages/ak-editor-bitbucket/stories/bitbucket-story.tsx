f4add320173e456b59582a45e23802b0339c7c99
Artur Bodera <abodera@atlassian.com>
2016-12-07 17:10:51 +1100

    test: add storybook story for setting from html.

--- packages/ak-editor-bitbucket/stories/bitbucket-story.tsx
+++ packages/ak-editor-bitbucket/stories/bitbucket-story.tsx
@@ -5,6 +5,7 @@ import { base64fileconverter } from 'ak-editor-test';
 import { default as AkTabs, Tab as AkTab} from 'ak-tabs';
 import Editor from '../src';
 import FacadeInput from '../src/hacks/facade-input';
+import exampleHTML from './exampleHTML';
 
 const Tabs = reactify(AkTabs);
 const Tab = reactify(AkTab);
@@ -136,4 +137,88 @@ storiesOf('ak-editor-bitbucket', module)
         <Demo />
       </div>
     );
-  });
+  })
+  .add('Set from HTML', () => {
+    type Props = {};
+    type State = { hasError?: boolean };
+    class Demo extends PureComponent<Props, State> {
+      state: State = { hasError: false };
+      textarea?: HTMLTextAreaElement;
+      editor?: Editor;
+
+      setEditorContentFromHtml = () => {
+        const { textarea, editor } = this;
+        
+        if (!textarea || !editor) {
+          return;
+        }
+
+        this.setState({ hasError: false });
+
+        try {
+          editor.setFromHtml(textarea.value);
+        } catch (e) {
+          this.setState({ hasError: true });
+          console.error('Error when setting from HTML', e);
+        }
+      }
+
+      handleTextareaRef = (ref: HTMLTextAreaElement | null) => {
+        if (!ref) {
+          this.textarea = undefined;
+          return;
+        }
+        this.textarea = ref;
+        ref.value = exampleHTML;
+        this.setEditorContentFromHtml();
+      }
+
+      handleEditorRef = (ref: Editor | null) => {
+        if (!ref) {
+          this.editor = undefined;
+          return;
+        }
+
+        this.editor = ref;
+
+        // TODO: we don't currently have a good method of knowing that the editor can receive content
+        setTimeout(() => {
+          this.setEditorContentFromHtml();
+        }, 500);
+      }
+
+      render() {
+        return (
+          <div ref="root" style={{ display: 'flex', alignItems: 'stretch', minHeight: '100%' }}>
+            <div style={{ flex: 3, display: 'flex', alignItems: 'stretch' }}>
+              <textarea 
+                style={{ 
+                  flex: 1, 
+                  fontFamily: 'monospace', 
+                  fontSize: '90%', 
+                  outline: 'none', 
+                  border: '0 none', 
+                  padding: '5px 10px', 
+                  borderRight: '10px solid rgb(247, 247, 247)',
+                  background: this.state.hasError ? 'red' : 'white'
+                }} 
+                ref={this.handleTextareaRef}
+                onKeyUp={this.setEditorContentFromHtml}
+              />
+            </div>
+            <div style={{ flex: 2, display: 'flex', flexDirection: 'column', alignItems: 'stretch', alignContent: 'stretch' }}>
+              <Editor
+                ref={this.handleEditorRef}
+                isExpandedByDefault
+              />
+            </div>
+          </div>
+        );
+      }
+    }
+
+    return (
+      <Demo />
+    );
+  })
+;
---
+++ packages/ak-editor-bitbucket/stories/exampleHTML.ts
@@ -0,0 +1,65 @@
+export default 
+`<p>Regular, 
+  <strong>Strong</strong>, 
+  <em>Italic</em>, 
+  <em><strong>Strong Italic</strong></em>
+</p>
+
+<hr>
+
+<p><a href="http://atlassian.com">Regular link</a>,
+  <a href="http://atlassian.com"><strong>strong link</strong></a>,
+  <a href="http://atlassian.com"><em><strong>strong italic link</strong></em></a>
+</p>
+
+<hr>
+
+<ul>
+    <li>Bullet list item 1</li>
+    <li>Bullet list item 2</li>
+</ul>
+
+<hr>
+
+<ol>
+    <li>Number list item 1</li>
+    <li>Number list item 2</li>
+</ol>
+
+<hr>
+
+<pre><code>private handleChange = () => {
+  const { onChange } = this.props;
+  if (onChange) {
+    onChange(this);
+  }
+}</code></pre>
+
+<hr>
+
+<blockquote>
+  <p>Block quote first paragraph</p>
+  <p>Regular, 
+    <strong>Strong</strong>, 
+    <em>Italic</em>, 
+    <em><strong>Strong Italic</strong></em>
+  </p>
+  <ul>
+    <li>Bullet list item 1</li>
+    <li>Bullet list item 2</li>
+  </ul>
+  <ol>
+    <li>Number list item 1</li>
+    <li>Number list item 2</li>
+  </ol>
+</blockquote>
+
+<hr>
+
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+`;

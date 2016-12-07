c774c52c1eeb5a2d1085581d937e5f1ae652f23f
Artur Bodera <abodera@atlassian.com>
2016-12-08 10:37:33 +1100

    test: move enzyme config to separate file.

--- build/karma/react/all.js
+++ build/karma/react/all.js
@@ -2,24 +2,14 @@ const path = require('path');
 
 const karmaConf = require('../base');
 const assignPattern = require('../assignPattern');
+const setUpEnzyme = require('../setUpEnzyme');
 
 
 module.exports = (config) => {
   karmaConf(config);
+  setUpEnzyme(config);
   assignPattern(config, path.join(__dirname, 'all.entry.js'));
   config.set({
     browsers: ['jsdom'],
-  });
-
-  // Following is copied from https://github.com/airbnb/enzyme/blob/master/docs/guides/karma.md
-  // and then modified to match https://github.com/lelandrichardson/enzyme-example-karma-webpack due
-  // to running into the issue here https://github.com/producthunt/chai-enzyme/issues/46
-  Object.assign(config.webpack, {
-    devtool: 'inline-source-map', // just do inline source maps instead of the default
-    externals: {
-      'react/addons': true,
-      'react/lib/ExecutionEnvironment': true,
-      'react/lib/ReactContext': true,
-    },
   });
 };

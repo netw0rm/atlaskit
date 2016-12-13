a0ed121a9a74ebc6875a752008db1ab8ee500dae
James Russell <jrussell@atlassian.com>
2016-12-13 16:26:55 +1100

    fix(component): Does not display grey background in field base when hovering and invalid; improves t
    affects: ak-field-base

--- packages/ak-field-base/test/ContentSpec.jsx
+++ packages/ak-field-base/test/ContentSpec.jsx
@@ -4,6 +4,7 @@ import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
 import { shallow } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
+import WarningIcon from 'ak-icon/glyph/warning';
 import Content from '../src/Content';
 import { locals } from '../src/styles.less';
 import { compact, subtle } from '../src/internal/appearances';
@@ -19,35 +20,46 @@ const {
   readOnly: isReadOnlyClass,
 } = locals;
 
+const defaultProps = {
+  isDisabled: false,
+  isFocused: false,
+  isInvalid: false,
+  isReadOnly: false,
+};
+
 describe('ak-field-base', () => {
   describe('Content', () => {
     describe('by default', () =>
       it('should render a content', () =>
-        expect(shallow(<Content />)).to.have.descendants(`.${contentClass}`)
+        expect(shallow(<Content {...defaultProps} />)).to.have.descendants(`.${contentClass}`)
       )
     );
 
     describe('isReadOnly prop = true', () =>
       it('should render with the .isReadOnly class', () =>
-        expect(shallow(<Content isReadOnly />)).to.have.descendants(`.${isReadOnlyClass}`)
+        expect(shallow(<Content {...defaultProps} isReadOnly />)).to.have.descendants(`.${isReadOnlyClass}`)
       )
     );
 
     describe('isFocused prop = true', () => {
       it('should render the slotwrapper with the .isFocused class', () =>
-        expect(shallow(<Content isFocused />)).to.have.descendants(`.${isFocusedClass}`)
+        expect(shallow(<Content {...defaultProps} isFocused />)).to.have.descendants(`.${isFocusedClass}`)
       );
     });
 
-    describe('isInvalid prop = true', () =>
-      it('should render with the isFocused styles and not the isInvalid styles', () =>
-        expect(shallow(<Content isInvalid />)).to.have.descendants(`.${isInvalidClass}`)
-      )
-    );
+    describe('isInvalid prop = true', () => {
+      it('should render with the isInvalid styles', () =>
+        expect(shallow(<Content {...defaultProps} isInvalid />)).to.have.descendants(`.${isInvalidClass}`)
+      );
+
+      it('should render the warning icon', () =>
+        expect(shallow(<Content {...defaultProps} isInvalid />)).to.have.descendants(WarningIcon)
+      );
+    });
 
     describe('isFocused prop = true AND isInvalid prop = true', () =>
       it('should render with the isFocused styles and not the isInvalid styles', () => {
-        const wrapper = shallow(<Content isFocused isInvalid />);
+        const wrapper = shallow(<Content {...defaultProps} isFocused isInvalid />);
         expect(wrapper).to.have.descendants(`.${isFocusedClass}`);
         expect(wrapper).to.not.have.descendants(`.${isInvalidClass}`);
       })
@@ -56,12 +68,12 @@ describe('ak-field-base', () => {
     describe('rightGutter prop', () => {
       it('should render properly', () => {
         const div = <div id="right">test</div>;
-        const wrapper = shallow(<Content rightGutter={div} />);
+        const wrapper = shallow(<Content {...defaultProps} rightGutter={div} />);
         expect(wrapper.find(`.${locals.rightGutterWrapper}`)).to.contain(div);
       });
 
       it('should not render rightGutterWrapper when prop is not set', () =>
-        expect(shallow(<Content />)).to.not.have.descendants(`.${locals.rightGutterWrapper}`)
+        expect(shallow(<Content {...defaultProps} />)).to.not.have.descendants(`.${locals.rightGutterWrapper}`)
       );
     });
 
@@ -69,7 +81,7 @@ describe('ak-field-base', () => {
       [compact, subtle].forEach(appearance =>
         describe(appearance, () =>
           it(`should render the content with the .${appearance} class`, () =>
-            expect(shallow(<Content appearance={appearance} />)).to.have.descendants(`.${locals[appearance]}`)
+            expect(shallow(<Content {...defaultProps} appearance={appearance} />)).to.have.descendants(`.${locals[appearance]}`)
           )
         )
       );

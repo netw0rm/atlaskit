1b0a73ac0a3ff04b446f9dfb58237390fbedad72
Huanhuan Huang <hhuang@atlassian.com>
2016-12-15 10:05:24 +1100

    feat(package): Added ak-editor-plugin-image-upload to core
    affects: ak-editor-core

--- packages/ak-editor-core/test/_schema-builder.ts
+++ packages/ak-editor-core/test/_schema-builder.ts
@@ -1,9 +1,11 @@
 import { nodeFactory, markFactory } from 'ak-editor-test';
 import {
   DocNodeType,
+  BlockQuoteNodeType,
+  ImageNodeType,
   ParagraphNodeType,
+  Text,
   LinkMarkType,
-  Text
 } from 'ak-editor-schema';
 import { Schema } from 'ak-editor-prosemirror';
 
@@ -13,6 +15,9 @@ export const schema = new Schema({
     unlinkable: { type: ParagraphNodeType, content: 'text*', group: 'block' },
     linkable: { type: ParagraphNodeType, content: 'text<link>*', group: 'block' },
     text: { type: Text },
+    blockquote: { type: BlockQuoteNodeType, content: 'text*', group: 'block' },
+    images: { type: ParagraphNodeType, content: '(text | image)*', group: 'block' },
+    image: { type: ImageNodeType },
   },
   
   marks: {
@@ -21,6 +26,11 @@ export const schema = new Schema({
 });
 
 export const doc = nodeFactory(schema.nodes.doc);
+
 export const unlinkable = nodeFactory(schema.nodes.unlinkable);
 export const linkable = nodeFactory(schema.nodes.linkable);
 export const link = (attrs: { href: string }) => markFactory(schema.marks.link, attrs);
+
+export const noimages = nodeFactory(schema.nodes.blockquote);
+export const images = nodeFactory(schema.nodes.images);
+export const image = (attrs: { src: string }) => nodeFactory(schema.nodes.image, attrs)();

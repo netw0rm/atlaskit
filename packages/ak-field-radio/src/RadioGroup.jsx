a619be2e3e346792f50b047a47d9b15509640d3a
Ben Wong <benwong@atlassian.com>
2016-12-19 16:32:33 +1100

    feat(component): Use field base in ak-field-radio
    affects: ak-field-radio

--- packages/ak-field-radio/src/RadioGroup.jsx
+++ packages/ak-field-radio/src/RadioGroup.jsx
@@ -1,23 +1,28 @@
 import React, { PropTypes, PureComponent } from 'react';
+import FieldBase from 'ak-field-base';
 import styles from 'style!./styles.less';
 import Radio from './Radio';
 
 /* eslint-disable-next-line react/prefer-stateless-function */
 export default class RadioGroup extends PureComponent {
   static propTypes = {
+    disabled: PropTypes.bool,
     items: PropTypes.arrayOf(PropTypes.shape({
       disabled: PropTypes.bool,
-      label: PropTypes.node,
+      label: PropTypes.node.isRequired,
       name: PropTypes.string,
       value: PropTypes.string,
     })),
     label: PropTypes.node,
-    value: PropTypes.string,
     onRadioChange: PropTypes.func.isRequired,
+    required: PropTypes.bool,
+    value: PropTypes.string,
   }
 
   static defaultProps = {
+    disabled: false,
     items: [],
+    required: false,
   }
 
   renderItems = () => (
@@ -37,10 +42,16 @@ export default class RadioGroup extends PureComponent {
 
   render() {
     return (
-      <div className={styles.radioGroup}>
-        {this.props.label ? <div>{this.props.label}</div> : null}
-        {this.renderItems()}
-      </div>
+      <FieldBase
+        appearance="none"
+        disabled={this.props.disabled}
+        label={this.props.label}
+        required={this.props.required}
+      >
+        <div className={styles.radioGroup}>
+          {this.renderItems()}
+        </div>
+      </FieldBase>
     );
   }
 }

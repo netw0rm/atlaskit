9bb98ad89ef4e635001cdeeaeab650b1e0af0e01
Bradley Ayers <bradley.ayers@gmail.com>
2016-12-21 20:52:40 +1100

    fix: Export privately used types to allow .d.ts creation.
    affects: ak-editor-core, ak-editor-cq, ak-editor-jira

--- packages/ak-editor-jira/src/html/weak-map.ts
+++ packages/ak-editor-jira/src/html/weak-map.ts
@@ -1,11 +1,11 @@
-interface WeakMap<K, V> {
+export interface WeakMap<K, V> {
     delete(key: K): boolean;
     get(key: K): V | undefined;
     has(key: K): boolean;
     set(key: K, value?: V): this;
 }
 
-interface WeakMapConstructor {
+export interface WeakMapConstructor {
     new (): WeakMap<any, any>;
     new <K, V>(entries?: [K, V][]): WeakMap<K, V>;
     readonly prototype: WeakMap<any, any>;

08f9add282506d815b8146ade8ed838f73a4e0af
Ben Gummer <bgummer@atlassian.com>
2016-12-22 15:36:01 +1100

    refactor(template): convert akutil-component-template to React
    affects: akutil-component-template
    
    BREAKING CHANGE: default export is now React component, was Skate component

--- packages/akutil-component-template/stories/example-story.js
+++ packages/akutil-component-template/stories/example-story.js
@@ -1,40 +1,24 @@
 import { storiesOf, action } from '@kadira/storybook';
 import React from 'react';
-import reactify from 'akutil-react';
 
-import WebComponent, { events } from '../src';
+import Component from '../src';
 import { name } from '../package.json';
-import styles from '../src/shadow.less';
-
-const Component = reactify(WebComponent);
 
 storiesOf(name, module)
-  .add('a simple akutil-component-template', () => (
+  .add('simple akutil-component-template', () => (
     <Component />
   ))
-  .add('a simple akutil-component-template with a name', () => (
-    <Component name="MyComponent" />
+  .add('with a prop set', () => (
+    <Component audienceName="MyComponent" />
   ))
-  .add('a simple akutil-component-template that uses an emitted event', () => (
-    <Component
-      name="MyComponent"
-      onClick={function onClick() { this.announce(); }}
-      onAnnounce-name={action(events.announceName)}
-    />
+  .add('with a handler prop', () => (
+    <Component onTextClicked={action('Clicked!')} />
   ))
-  .add('an akutil-component-template that emits an action when it is clicked', () => (
-    <Component id="myComponent" onClick={action('clicking the WebComponent')} />
-  ))
-  .add('an akutil-component-template that removes itself when being clicked', () => {
-    const removeMe = e => e.currentTarget.parentNode.removeChild(e.currentTarget);
-    const cls = styles.locals.akutilComponentTemplate;
-    return (<Component id="myComponent" className={cls} onClick={removeMe} />);
-  })
-  .addMonkeyTest('a akutil-component-template with monkey testing', () => (
+  .addMonkeyTest('with monkey testing', () => (
     // Use this to add a story that has fuzzy testing attached.
     <Component />
   ))
-  .addMonitored('an akutil-component-template with monitored performance', () => (
+  .addMonitored('with monitored performance', () => (
     // Use this to add a story that has a little fps/memory gauge that allows you
     // to monitor performance whilst developing
     <Component />
--- packages/akutil-component-template/test/indexSpec.jsx
+++ packages/akutil-component-template/test/indexSpec.jsx
@@ -1,20 +1,32 @@
 import chai from 'chai';
 import React from 'react';
-import chaiAsPromised from 'chai-as-promised';
 import chaiEnzyme from 'chai-enzyme';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import styles from 'style!../src/styles.less';
+import Component from '../src';
 
 import { name } from '../package.json';
 
-chai.use(chaiAsPromised);
 chai.use(chaiEnzyme());
-
-const { expect } = chai;
-
-const Component = () => <div className="test">test</div>;
+chai.should();
 
 describe(name, () => {
-  it('should work', () =>
-    expect(mount(<Component />)).to.have.className('test')
+  it('should render with correct CSS class name', () =>
+    shallow(
+      <Component />
+    ).should.have.exactly(1).descendants(`.${styles.root}`)
   );
+
+  describe('audienceName prop', () => {
+    it('should default to "world"', () =>
+      shallow(
+        <Component />
+      ).find(`.${styles.root}`).should.have.text('Hello world!')
+    );
+    it('should render supplied prop when provided', () =>
+      shallow(
+        <Component audienceName="Sideshow Bob" />
+      ).find(`.${styles.root}`).should.have.text('Hello Sideshow Bob!')
+    );
+  });
 });

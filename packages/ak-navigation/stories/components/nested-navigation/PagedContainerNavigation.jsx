63b0a49843d3f8a3f458abd734ab8e63f0f8c30b
Joshua Nelson <jonelson@atlassian.com>
2017-01-03 10:14:47 +1100

    chore(component): polish story to have disabled links
    affects: ak-navigation

--- packages/ak-navigation/stories/components/nested-navigation/PagedContainerNavigation.jsx
+++ packages/ak-navigation/stories/components/nested-navigation/PagedContainerNavigation.jsx
@@ -7,6 +7,11 @@ const pageStyle = {
   height: 300,
 };
 
+const disabledLinkStyles = {
+  color: 'grey',
+  cursor: 'default',
+};
+
 export default class PagedContainerNavigation extends PureComponent {
   static propTypes = {
     children: PropTypes.arrayOf(PropTypes.node),
@@ -40,6 +45,14 @@ export default class PagedContainerNavigation extends PureComponent {
     };
   }
 
+  isNextEnabled() {
+    return this.state.selectedIndex < (this.props.children.length - 1);
+  }
+
+  isPrevEnabled() {
+    return this.state.selectedIndex > 0;
+  }
+
   render() {
     return (
       <div>
@@ -47,32 +60,26 @@ export default class PagedContainerNavigation extends PureComponent {
           pages={this.props.children}
           selectedIndex={this.state.selectedIndex}
         />
-        {
-            this.state.selectedIndex > 0 ? (
-              <a
-                href="#prev"
-                onClick={() => this.setState({
-                  selectedIndex: Math.max(this.state.selectedIndex - 1, 0),
-                })}
-              >
-                <AkContainerItem icon={<ArrowleftIcon label="Previous" />} text="Previous" />
-              </a>
-            ) : null
-          }
-        {
-            this.state.selectedIndex < (this.props.children.length - 1) ? (
-              <a
-                href="#next"
-                onClick={() => this.setState({
-                  selectedIndex: Math.min(
-                    this.state.selectedIndex + 1,
-                    this.props.children.length - 1),
-                })}
-              >
-                <AkContainerItem icon={<ArrowrightIcon label="Next" />} text="Next" />
-              </a>
-            ) : null
-          }
+        <a
+          style={!this.isPrevEnabled() ? disabledLinkStyles : {}}
+          href="#prev"
+          onClick={() => this.isPrevEnabled() && this.setState({
+            selectedIndex: Math.max(this.state.selectedIndex - 1, 0),
+          })}
+        >
+          <AkContainerItem icon={<ArrowleftIcon label="Previous" />} text="Previous" />
+        </a>
+        <a
+          style={!this.isNextEnabled() ? disabledLinkStyles : {}}
+          href="#next"
+          onClick={() => this.isNextEnabled() && this.setState({
+            selectedIndex: Math.min(
+              this.state.selectedIndex + 1,
+              this.props.children.length - 1),
+          })}
+        >
+          <AkContainerItem icon={<ArrowrightIcon label="Next" />} text="Next" />
+        </a>
       </div>
     );
   }

27882e7f542e164b579a9388bccfa62123898c7e
Luke Batchelor <lbatchelor@atlassian.com>
2017-01-05 14:51:05 +1100

    refactor(component): Deletes some old webcomponents we dont need anymore
    Deletes: ak-component-base, ak-dropdown, ak-field-validator, ak-inline-dialog, ak-page, ak-tabs,
    
    akutil-polyfills, pf-profile-card

--- packages/ak-dropdown/src/internal/childrenHaveSlot.js
+++
@@ -1,3 +0,0 @@
-export default function childrenHaveSlot(list, slotName) {
-  return [...list].some(el => (el.getAttribute && el.getAttribute('slot') === slotName));
-}
--- packages/ak-dropdown/src/internal/isDescendantOf.js
+++
@@ -1,9 +0,0 @@
-export default function isDescendantOf(child, parent) {
-  if (child.parentNode === parent) {
-    return true;
-  } else if (child.parentNode === null) {
-    return false;
-  }
-
-  return isDescendantOf(child.parentNode, parent);
-}

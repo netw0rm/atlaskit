30eca22267044bc06f0716020fe7ef5340243451
Ben Gummer <bgummer@atlassian.com>
2017-01-06 15:02:22 +1100

    fix(docs): use storybook for ak-button-group readme
    affects: ak-button-group

---
+++ packages/ak-button-group/stories/00-readme-story.jsx
@@ -0,0 +1,28 @@
+import { storiesOf } from '@kadira/storybook';
+import React from 'react';
+import Readme, { Code, Props } from 'akutil-readme';
+
+import ButtonGroupOverviewExample from './examples/ButtonGroupOverview'; // eslint-disable-line import/no-duplicates
+import ButtonGroupOverviewExampleRaw from '!raw!./examples/ButtonGroupOverview'; // eslint-disable-line import/no-duplicates, import/first
+
+import { name, description } from '../package.json';
+import ButtonGroup from '../src';
+
+const buttonGroupPropDescriptions = {
+  children: 'Button elements to be displayed inside the group',
+};
+
+storiesOf(name, module)
+  .add('ButtonGroup readme', () => (
+    <div>
+      <Readme
+        component={name}
+        description={description}
+      >
+        <Code code={ButtonGroupOverviewExampleRaw}>
+          {ButtonGroupOverviewExample}
+        </Code>
+        <Props component={ButtonGroup} descriptions={buttonGroupPropDescriptions} />
+      </Readme>
+    </div>
+  ));

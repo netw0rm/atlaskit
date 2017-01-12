9f84688383723fc0fad0dc5254edad0865ba545b
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-01-12 14:17:26 +1100

    feat(component): Protect hyperlink popup input styles from being redeclared
    affects: ak-editor-core
    
    In some cases e.g. bitbucket has styles that matches on input css selector we want our styles to
    
    have bigger proirity. In order to achive this, had to make selector for input inside hyperlink popup
    
    more specific and add reset for some properties e.g. border-radius, padding, etc.
    
    Closes: FAB-1786

--- packages/ak-editor-core/src/ui/PanelTextInput/styles.ts
+++ packages/ak-editor-core/src/ui/PanelTextInput/styles.ts
@@ -2,14 +2,19 @@ import { akEditorPopupText } from 'ak-editor-shared-styles';
 import { style } from 'typestyle';
 
 export const input = style({
-  background: 'transparent',
-  border: 0,
-  color: akEditorPopupText,
-  flexGrow: 1,
-  fontSize: '13px',
-  lineHeight: '20px',
-
   $nest: {
+    'input&': {
+      background: 'transparent',
+      border: 0,
+      borderRadius: 0,
+      boxSizing: 'content-box',
+      color: akEditorPopupText,
+      flexGrow: 1,
+      fontSize: '13px',
+      lineHeight: '20px',
+      padding: 0,
+    },
+
     '&:focus': {
       outline: 'none',
     },

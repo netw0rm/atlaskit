68aaff232724a1f8287f0df23e14ad17f6c48128
Ben Wong <benwong@atlassian.com>
2017-01-18 15:35:15 +1100

    test(component): Add additional tests for ak-tabs keyboard nav
    affects: ak-tabs

--- packages/ak-tabs/test/indexSpec.jsx
+++ packages/ak-tabs/test/indexSpec.jsx
@@ -1,9 +1,10 @@
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 import React, { Component } from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import Tabs, { StatelessTabs } from '../src';
+import styles from '../src/styles.less';
 import { sampleTabs, sampleTabsNoSelection, sampleTabsDefaultSelected } from './_constants';
 import { name } from '../package.json';
 
@@ -60,6 +61,47 @@ describe(name, () => {
           expect(wrapper.find(StatelessTabs).prop('tabs').filter(tab => tab.isSelected).length)
             .to.equal(1);
           expect(wrapper.find(StatelessTabs).prop('tabs')[0].isSelected).to.equal(true);
+        });
+      });
+    });
+
+    describe('behaviour', () => {
+      describe('keyboard navigation', () => {
+        describe('with 3 tabs, when the 2nd tab is selected', () => {
+          let wrapper;
+          const simulateKeyboardNav = (key) => {
+            wrapper.find(`.${styles.locals.akTabLabelSelected}`).simulate('keyDown', { key });
+          };
+
+          beforeEach(() => {
+            wrapper = mount(<Tabs tabs={sampleTabsDefaultSelected} />);
+          });
+
+          it('pressing LEFT arrow selects the first tab', () => {
+            expect(wrapper.state().selectedTab).to.equal(1);
+            simulateKeyboardNav('ArrowLeft');
+            expect(wrapper.state().selectedTab).to.equal(0);
+          });
+
+          it('pressing the RIGHT arrow selects the last tab', () => {
+            expect(wrapper.state().selectedTab).to.equal(1);
+            simulateKeyboardNav('ArrowRight');
+            expect(wrapper.state().selectedTab).to.equal(2);
+          });
+
+          it('pressing the LEFT arrow twice leaves selection on the first tab', () => {
+            expect(wrapper.state().selectedTab).to.equal(1);
+            simulateKeyboardNav('ArrowLeft');
+            simulateKeyboardNav('ArrowLeft');
+            expect(wrapper.state().selectedTab).to.equal(0);
+          });
+
+          it('pressing the RIGHT arrow twice leaves selection on the last tab', () => {
+            expect(wrapper.state().selectedTab).to.equal(1);
+            simulateKeyboardNav('ArrowRight');
+            simulateKeyboardNav('ArrowRight');
+            expect(wrapper.state().selectedTab).to.equal(2);
+          });
         });
       });
     });

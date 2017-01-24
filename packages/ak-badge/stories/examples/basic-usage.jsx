444fcb52b57d9eb31bf35feff6f86e29e3554730
Luke Batchelor <lbatchelor@atlassian.com>
2017-01-25 10:34:54 +1100

    style(lint): Reordered props for linting
    affects: ak-badge

--- packages/ak-badge/stories/examples/basic-usage.jsx
+++ packages/ak-badge/stories/examples/basic-usage.jsx
@@ -6,7 +6,7 @@ export default (
     <Badge value="5" />
     <Badge appearance="primary" value={-5} />
     <Badge appearance="important" value="25" />
-    <Badge appearance="added" value="3000" max="99" />
+    <Badge appearance="added" max="99" value="3000" />
     <Badge appearance="removed" />
     <Badge appearance="default" theme="dark" />
   </div>

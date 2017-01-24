90c28ca8be7160729f1df8df69c44652ac3b7256
Matias Corbanini <mcorbanini@atlassian.com>
2017-01-24 15:26:06 +1100

    fix: wip

--- packages/ak-droplist-item/__mocks__/styleMock.js
+++ packages/ak-droplist-item/__mocks__/styleMock.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  item: 'item',
+  disabled: 'disabled',
+  active: 'active',
+  checked: 'checked',
+  hidden: 'hidden',
+  checkradio: 'checkradio',
+  elemBefore: 'elemBefore',
+  itemContent: 'itemContent',
+  elemAfter: 'elemAfter',
+};
--- packages/ak-droplist-item/__tests__/index.jsx
+++ packages/ak-droplist-item/__tests__/index.jsx
@@ -2,91 +2,97 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 
 import { name } from '../package.json';
-import { locals as styles } from '../src/styles.less';
+import styles from '../__mocks__/styleMock';
 import Item, { SecondaryText } from '../src';
+import Element from '../src/internal/Element';
+
+import Radio from 'ak-icon/glyph/radio';
+import Checkbox from 'ak-icon/glyph/checkbox';
+
 
 describe(name, () => {
-  it('should be possible to create a component', () => {
-    console.log(shallow(<Item />).html());
-    expect(shallow(<Item />).exists()).toBeTruthy();
+  it('should be possible to create a component', () =>
+    expect(shallow(<Item />).isEmpty()).toBeFalsy()
+  );
+
+  it('should be "link" item by default', () =>
+    expect(shallow(<Item />).find(Element).prop('type')).toBe('link')
+  );
+
+  describe('rendering', () => {
+    it('should render content that is passed to the item', () =>
+      expect(mount(<Item>sample</Item>).text()).toBe('sample')
+    );
+
+    it('should render elemBefore for all items', () => {
+      const Icon = <div id="icon">icon</div>;
+      expect(shallow(<Item elemBefore={Icon}>sample</Item>).contains(Icon)).toBeTruthy();
+      expect(shallow(<Item elemBefore={Icon} type="checkbox">sample</Item>).contains(Icon)).toBeTruthy();
+      expect(shallow(<Item elemBefore={Icon} type="radio">sample</Item>).contains(Icon)).toBeTruthy();
+    });
+
+    it('should render elemAfter for all items', () => {
+      const Icon = (<div id="icon">icon</div>);
+      expect(shallow(<Item elemAfter={Icon}>sample</Item>).contains(Icon)).toBeTruthy();
+      expect(shallow(<Item elemAfter={Icon} type="checkbox">sample</Item>).contains(Icon)).toBeTruthy();
+      expect(shallow(<Item elemAfter={Icon} type="radio">sample</Item>).contains(Icon)).toBeTruthy();
+    });
+
+    it('should render icon for the radio or checkbox element', () => {
+      expect(shallow(<Item type="checkbox" />).find(Checkbox).isEmpty()).toBeFalsy();
+      expect(shallow(<Item type="radio" />).find(Radio).isEmpty()).toBeFalsy();
+    });
+
+    it('should NOT render icon for the link element', () => {
+      const wrapper = shallow(<Item type="link" />);
+      expect(wrapper.find(Checkbox).isEmpty()).toBeTruthy();
+      expect(wrapper.find(Radio).isEmpty()).toBeTruthy();
+    });
   });
 
-  // it('should be "link" item by default', () => {
-  //   expect(shallow(<Item />).instance().props.type).to.equal('link');
-  // });
+  describe('classes', () => {
+    it('should have "item" class by default', () => {
+      expect(shallow(<Item type="link" />).find(Element).hasClass(styles.item)).toBeTruthy();
+      expect(shallow(<Item type="checkbox" />).find(Element).hasClass(styles.item)).toBeTruthy();
+      expect(shallow(<Item type="radio" />).find(Element).hasClass(styles.item)).toBeTruthy();
+    });
 
-  // describe('rendering', () => {
-  //   it('should render content that is passed to the item', () => {
-  //     expect(mount(<Item>sample</Item>)).to.have.text('sample');
-  //   });
+    it('should have "disabled" class when disabled', () => {
+      expect(shallow(<Item type="link" isDisabled />).find(Element).hasClass(styles.disabled)).toBeTruthy();
+      expect(shallow(<Item type="radio" isDisabled />).find(Element).hasClass(styles.disabled)).toBeTruthy();
+      expect(shallow(<Item type="checkbox" isDisabled />).find(Element).hasClass(styles.disabled)).toBeTruthy();
+    });
 
-  //   it('should render elemBefore for all items', () => {
-  //     const Icon = (<div id="icon">icon</div>);
-  //     expect(mount(<Item elemBefore={Icon}>sample</Item>)).to.contain(Icon);
-  //     expect(mount(<Item elemBefore={Icon} type="checkbox">sample</Item>)).to.contain(Icon);
-  //     expect(mount(<Item elemBefore={Icon} type="radio">sample</Item>)).to.contain(Icon);
-  //   });
+    it('should have "active" class when link item is active', () =>
+      expect(shallow(<Item type="link" isActive />).find(Element).hasClass(styles.active)).toBeTruthy()
+    );
 
-  //   it('should render elemAfter for all items', () => {
-  //     const Icon = (<div id="icon">icon</div>);
-  //     expect(mount(<Item elemAfter={Icon}>sample</Item>)).to.contain(Icon);
-  //     expect(mount(<Item elemAfter={Icon} type="checkbox">sample</Item>)).to.contain(Icon);
-  //     expect(mount(<Item elemAfter={Icon} type="radio">sample</Item>)).to.contain(Icon);
-  //   });
+    it('should have "active" class when option item is selected', () =>
+      expect(shallow(<Item type="option" isSelected />).find(Element).hasClass(styles.active)).toBeTruthy()
+    );
 
-  //   it('should render icon for the radio or checkbox element', () => {
-  //     expect(mount(<Item type="radio" />)).to.have.descendants(`.${styles.checkradio}`);
-  //     expect(mount(<Item type="checkbox" />)).to.have.descendants(`.${styles.checkradio}`);
-  //   });
+    it('should NOT have "active" class for any other item types', () => {
+      expect(shallow(<Item type="radio" isActive />).find(Element).hasClass(styles.active)).toBeFalsy();
+      expect(shallow(<Item type="checkbox" isActive />).find(Element).hasClass(styles.disabled)).toBeFalsy();
+    });
 
-  //   it('should NOT render icon for the link element', () => {
-  //     expect(mount(<Item type="link" />)).to.not.have.descendants(`.${styles.checkradio}`);
-  //   });
-  // });
+    it('should have "checked" class when checkbox or radio is checked', () => {
+      expect(shallow(<Item type="checkbox" isChecked />).find(Element).hasClass(styles.checked)).toBeTruthy();
+      expect(shallow(<Item type="radio" isChecked />).find(Element).hasClass(styles.checked)).toBeTruthy();
+    });
 
-  // describe('classes', () => {
-  //   it('should have "item" class by default', () => {
-  //     expect(mount(<Item type="link" />).find(`.${styles.item}`)).to.exist;
-  //     expect(mount(<Item type="checkbox" />).find(`.${styles.item}`)).to.exist;
-  //     expect(mount(<Item type="radio" />).find(`.${styles.item}`)).to.exist;
-  //   });
+    it('should NOT have "checked" class for any other items', () => {
+      expect(shallow(<Item type="link" isChecked />).find(Element).hasClass(styles.checked)).toBeFalsy();
+    });
 
-  //   it('should have "disabled" class when disabled', () => {
-  //     expect(mount(<Item type="link" isDisabled />).find(`.${styles.disabled}`)).to.exist;
-  //     expect(mount(<Item type="radio" isDisabled />).find(`.${styles.disabled}`)).to.exist;
-  //     expect(mount(<Item type="checkbox" isDisabled />).find(`.${styles.disabled}`)).to.exist;
-  //   });
+    it('should have "hidden" class when item is hidden', () => {
+      expect(shallow(<Item type="link" isHidden />).find(Element).hasClass(styles.hidden)).toBeTruthy();
+      expect(shallow(<Item type="checkbox" isHidden />).find(Element).hasClass(styles.hidden)).toBeTruthy();
+      expect(shallow(<Item type="radio" isHidden />).find(Element).hasClass(styles.hidden)).toBeTruthy();
+    });
+  });
 
-  //   it('should have "active" class when link item is active', () => {
-  //     expect(mount(<Item type="link" isActive />).find(`.${styles.active}`)).to.exist;
-  //   });
-
-  //   it('should have "active" class when option item is selected', () => {
-  //     expect(mount(<Item type="option" isSelected />).find(`.${styles.active}`)).to.exist;
-  //   });
-
-  //   it('should NOT have "active" class for any other item types', () => {
-  //     expect(mount(<Item type="radio" isActive />).find(`.${styles.active}`)).to.not.exist;
-  //     expect(mount(<Item type="checkbox" isActive />).find(`.${styles.disabled}`)).to.not.exist;
-  //   });
-
-  //   it('should have "checked" class when checkbox or radio is checked', () => {
-  //     expect(mount(<Item type="checkbox" isChecked />).find(`.${styles.checked}`)).to.exist;
-  //     expect(mount(<Item type="radio" isChecked />).find(`.${styles.checked}`)).to.exist;
-  //   });
-
-  //   it('should NOT have "checked" class for any other items', () => {
-  //     expect(mount(<Item type="link" isChecked />).find(`.${styles.checked}`)).to.not.exist;
-  //   });
-
-  //   it('should have "hidden" class when item is hidden', () => {
-  //     expect(mount(<Item type="link" isHidden />).find(`.${styles.hidden}`)).to.exist;
-  //     expect(mount(<Item type="checkbox" isHidden />).find(`.${styles.hidden}`)).to.exist;
-  //     expect(mount(<Item type="radio" isHidden />).find(`.${styles.hidden}`)).to.exist;
-  //   });
-  // });
-
-  // describe('events', () => {
+  describe('events', () => {
   //   let onActivate;
 
   //   beforeEach(() => {
@@ -96,38 +102,43 @@ describe(name, () => {
   //     onActivate.reset();
   //   });
 
-  //   describe('onActivate', () => {
-  //     let wrapper;
-  //     beforeEach(() => {
-  //       onActivate = sinon.spy();
-  //       wrapper = mount(<Item onActivate={onActivate} />).find(`.${styles.item}`);
-  //     });
+    describe.only('onActivate', () => {
+      let wrapper;
+      let onActivate;
+      beforeEach(() => {
+        onActivate = jest.fn();
 
-  //     it('should be activated when enter is pressed', () => {
-  //       wrapper.simulate('keyPress', { key: 'Enter' });
-  //       expect(onActivate.calledOnce).to.be.true;
-  //     });
+// myMock('1');
+// myMock('a', 'b');
+// console.log(myMock.mock.calls);
+        wrapper = mount(<Item onActivate={onActivate} />).find(`.${styles.item}`);
+      });
 
-  //     it('should be activated when space is pressed', () => {
-  //       wrapper.simulate('keyPress', { key: ' ' });
-  //       expect(onActivate.calledOnce).to.be.true;
-  //     });
+      it('should be activated when enter is pressed', () => {
+        wrapper.simulate('keyPress', { key: 'Enter' });
+        expect(onActivate.mock.calls.length).toBe(1);
+      });
 
-  //     it('should be activated when clicked', () => {
-  //       wrapper.simulate('click');
-  //       expect(onActivate.calledOnce).to.be.true;
-  //     });
+      // it('should be activated when space is pressed', () => {
+      //   wrapper.simulate('keyPress', { key: ' ' });
+      //   expect(onActivate.calledOnce).to.be.true;
+      // });
 
-  //     it('should not be activated when disabled', () => {
-  //       const disabledWrapper =
-  //         mount(<Item onActivate={onActivate} isDisabled />).find(`.${styles.item}`);
-  //       disabledWrapper.simulate('click');
-  //       disabledWrapper.simulate('keyPress', { key: 'Enter' });
-  //       disabledWrapper.simulate('keyPress', { key: ' ' });
-  //       expect(onActivate.called).to.be.false;
-  //     });
-  //   });
-  // });
+      // it('should be activated when clicked', () => {
+      //   wrapper.simulate('click');
+      //   expect(onActivate.calledOnce).to.be.true;
+      // });
+
+      // it('should not be activated when disabled', () => {
+      //   const disabledWrapper =
+      //     mount(<Item onActivate={onActivate} isDisabled />).find(`.${styles.item}`);
+      //   disabledWrapper.simulate('click');
+      //   disabledWrapper.simulate('keyPress', { key: 'Enter' });
+      //   disabledWrapper.simulate('keyPress', { key: ' ' });
+      //   expect(onActivate.called).to.be.false;
+      // });
+    });
+  });
 
   // it('should focus itself when the isFocused property is set to true', () => {
   //   const wrapper = mount(<Item isFocused />).find(`.${styles.item}`);

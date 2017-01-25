3ea3d7ac7f27cd79a22ae77dfad413d7a3fcfabc
Oscar Wallhult <owallhult@atlassian.com>
2017-01-25 13:02:17 +1100

    feat(component): Adding EmojisPlugin
    affects: ak-editor-core

---
+++ packages/ak-editor-core/src/schema/marks/emoji-query.ts
@@ -0,0 +1,31 @@
+import { akColorB400 } from 'akutil-shared-styles';
+import { style } from 'typestyle';
+import { Mark, MarkType, Schema } from '../../prosemirror';
+
+const emojiQueryStyle = style({
+  color: akColorB400
+});
+
+export class EmojiQueryMarkType extends MarkType {
+  constructor(name: string, rank: number, schema: Schema) {
+    if (name !== 'emoji_query') {
+      throw new Error('EmojiQueryMarkType must be named "emoji_query".');
+    }
+    super(name, rank, schema);
+  }
+
+  get inclusiveRight() {
+    return true;
+  }
+
+  toDOM(): [string, any] {
+    return ['span', {
+      'data-emoji-query': true,
+      'class': emojiQueryStyle
+    }];
+  }
+}
+
+export interface EmojiQueryMark extends Mark {
+  type: EmojiQueryMarkType;
+}

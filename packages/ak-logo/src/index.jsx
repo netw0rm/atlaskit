ca201b9a885c37a59c1feaec5edf8ff376c0c076
Dean Pisani <dpisani@atlassian.com>
2017-01-27 13:08:05 +1100

    fix(component): Pass props from product logo component to the underlying logo component
    affects: ak-logo

--- packages/ak-logo/src/index.jsx
+++ packages/ak-logo/src/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { JiraLogoIcon } from 'ak-icon';
 import JiraLogoText from 'babel-loader!svg-to-jsx-loader!./logoTexts/jira.svg';
 import Logo from './Logo';
 
-const JiraLogo = () => (<Logo icon={<JiraLogoIcon />} logoText={<JiraLogoText />} />);
+const logoPropTypes = {
+  isCollapsed: PropTypes.bool,
+};
+
+const logoDefaultProps = {
+  isCollapsed: false,
+};
+
+const JiraLogo = props =>
+  (<Logo icon={<JiraLogoIcon />} logoText={<JiraLogoText />} {...props} />);
+
+JiraLogo.propTypes = logoPropTypes;
+JiraLogo.defaultProps = logoDefaultProps;
 
 export default JiraLogo;

af7388df8676cf5c6067c2536807d1fcfbdde3b5
Marcus Ellis <mellis@atlassian.com>
2017-02-01 14:44:23 -0600

    test(icon): removing file extensions from test
    affects: @atlaskit/icon

--- packages/icon/test/indexSpec.jsx
+++ packages/icon/test/indexSpec.jsx
@@ -149,9 +149,9 @@ describe(name, () => {
           'hipchat/preferences',
           'hipchat/room-directory',
           'hipchat/room-menu',
-          'hipchat/upgrade-available-settings.svg',
-          'hipchat/upgrade-available.svg',
-          'hipchat/upgrade-min.svg',
+          'hipchat/upgrade-available-settings',
+          'hipchat/upgrade-available',
+          'hipchat/upgrade-min',
           'hipchat/video',
           'hipchat/warning-circle',
 

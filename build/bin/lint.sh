3a5a24cccc80f9c059d68409f8f71ddb148fbe27
Luke Batchelor <lbatchelor@atlassian.com>
2017-02-02 12:55:05 +1100

    build(build): Completely removes build status from linting

--- build/bin/lint.sh
+++ build/bin/lint.sh
@@ -6,8 +6,6 @@ NODE_MODULES=`npm root`
 
 chalk --no-stdin -t "{blue Start linting...}"
 
-set +e
-
 # Run eslint and tslint in parallel for speed!
 
 eslint --color --format "$NODE_MODULES/eslint-friendly-formatter" . --ext .js --ext .jsx &
@@ -17,12 +15,4 @@ tslint --project tsconfig.json --format stylish &
 tslint_pid=$!
 
 wait $eslint_pid
-eslint_exit=$?
-
 wait $tslint_pid
-tslint_exit=$?
-
-# if either lint fails, return non-zero status to fail build
-if [ "$eslint_exit" -ne "0" ] || [ "$tslint_exit" -ne "0" ]; then
-  exit 1
-fi

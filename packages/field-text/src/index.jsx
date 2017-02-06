c3c23140d69889667830bc1a97040f657f357c5d
Nadia Makarevich <nmakarevich@atlassian.com>
2017-02-07 07:44:08 +1100

    fix(component): fix onFocus and onBlur handlers
    affects: @atlaskit/field-base, @atlaskit/field-text

--- packages/field-text/src/index.jsx
+++ packages/field-text/src/index.jsx
@@ -5,19 +5,17 @@ export { FieldText };
 
 /* eslint-disable react/prop-types */
 export default class extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.value,
-    };
+  static defaultProps = {
+    onChange: () => {},
+  }
+
+  state = {
+    value: this.props.value,
   }
 
   handleOnChange = (e) => {
     this.setState({ value: e.target.value });
-
-    if (this.props.onChange) {
-      this.props.onChange(e);
-    }
+    this.props.onChange(e);
   }
 
   render() {

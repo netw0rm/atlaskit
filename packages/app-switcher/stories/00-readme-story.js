5a6a7b3bafa37dc43cc09e401c0ebab5c27e9c5e
James McArthur <jmcarthur@atlassian.com>
2017-02-07 13:21:02 +1100

    docs: Add props to readme story
    affects: @atlaskit/app-switcher

--- packages/app-switcher/stories/00-readme-story.js
+++ packages/app-switcher/stories/00-readme-story.js
@@ -1,7 +1,8 @@
 import { storiesOf } from '@kadira/storybook';
 import React from 'react';
-import Readme, { Code, Description } from '@atlaskit/util-readme';
+import Readme, { Code, Props, Description } from '@atlaskit/util-readme';
 
+import AppSwitcher from '../src'
 import { name } from '../package.json';
 
 /* eslint-disable import/first, import/no-duplicates */
@@ -13,6 +14,13 @@ const AppSwitcherDescription = (<Description>
   <p>This component provides the app switcher used in Atlassian Cloud products.</p>
 </Description>);
 
+const propDescriptions = {
+  recentContainers: 'Array of recent containers. If this array is empty the recent container section will be hidden.',
+  linkedApplications: 'Map containing an array of linked applications. See prop-types.js for details on the exact format of this property.',
+  suggestedApplication: 'Map containing which application to suggest to the user. See prop-types.js for details on the exact format of this property.',
+  i18n: 'Mapping of i18n keys to translations.',
+};
+
 storiesOf(name, module)
   .add('AppSwitcher Readme', () => (
     <div>
@@ -23,6 +31,7 @@ storiesOf(name, module)
         <Code code={BasicUsageExampleRaw}>
           {BasicUsageExample}
         </Code>
+        <Props component={AppSwitcher} descriptions={propDescriptions} />
       </Readme>
     </div>
   ));

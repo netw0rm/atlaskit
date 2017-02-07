5029d717511c0825882347d809242ff1bd821d19
James McArthur <jmcarthur@atlassian.com>
2017-02-07 13:21:48 +1100

    fix(Remove unnecessary attributes from SVG markup):
    affects: @atlaskit/app-switcher

--- packages/app-switcher/src/components/HomeIcon.jsx
+++ packages/app-switcher/src/components/HomeIcon.jsx
@@ -3,8 +3,8 @@ import React, { PureComponent } from 'react';
 /* eslint-disable react/prefer-stateless-function */
 export default class HomeIcon extends PureComponent {
   render = () => (
-    <svg width="32px" height="32px" viewBox="12 8 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-      <g id="home" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(12.000000, 8.000000)">
+    <svg width="32px" height="32px" viewBox="12 8 32 32">
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" transform="translate(12.000000, 8.000000)">
         <rect id="Rectangle-4" fill="#0049B0" x="0" y="0" width="32" height="32" rx="3" />
         <path d="M8,16.2957144 C8,16.0190634 8.15266705,15.6421264 8.35407245,15.440721 L15.6459275,8.14886593 C15.8414764,7.95331712 16.152667,7.94746053 16.3540725,8.14886593 L23.6459275,15.440721 C23.8414764,15.6362698 24,16.0142629 24,16.2957144 L24,23.4990791 C24,23.7757301 23.7721195,24 23.5089948,24 L18.4910052,24 C18.2198305,24 18,23.7739098 18,23.501094 L18,19.7947935 C18,19.7947935 17,18.7947935 16,18.7947935 C15,18.7947935 14,19.7947935 14,19.7947935 L14,23.501094 C14,23.7766322 13.7721195,24 13.5089948,24 L8.49100518,24 C8.21983051,24 8,23.7805305 8,23.4990791 L8,16.2957144 Z" id="Rectangle-34" fill="#FFFFFF" />
       </g>

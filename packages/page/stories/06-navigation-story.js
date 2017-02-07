f3e66bb640c5811375a5751e190787b2977cfe54
Joshua Nelson <jonelson@atlassian.com>
2017-02-07 11:47:58 -0600

    refactor(stories): refactor the stories to use styled components, and add an overflow example to the
    affects: @atlaskit/page

--- packages/page/stories/06-navigation-story.js
+++ packages/page/stories/06-navigation-story.js
@@ -1,13 +1,15 @@
 import { storiesOf } from '@kadira/storybook';
 import React from 'react';
 import Navigation from '@atlaskit/navigation';
+import styled from 'styled-components';
+import Lorem from 'react-lorem-component';
 
 import Page, { Grid, GridColumn } from '../src';
 import { name } from '../package.json';
 
-const dummyStyles = {
-  background: '#fea',
-};
+const Dummy = styled.div`
+  background: #fea;
+`;
 
 storiesOf(name, module)
   .add('navigation', () => (
@@ -15,34 +17,32 @@ storiesOf(name, module)
       navigation={<Navigation />}
     >
       <Grid>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
+        <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
+        <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
+        <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
+        <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
       </Grid>
       <Grid>
-        <GridColumn medium={3}><div style={dummyStyles}>3 col</div></GridColumn>
-        <GridColumn medium={3}><div style={dummyStyles}>3 col</div></GridColumn>
-        <GridColumn medium={3}><div style={dummyStyles}>3 col</div></GridColumn>
-        <GridColumn medium={3}><div style={dummyStyles}>3 col</div></GridColumn>
+        <GridColumn medium={4}><Dummy>4 col</Dummy></GridColumn>
+        <GridColumn medium={4}><Dummy>4 col</Dummy></GridColumn>
+        <GridColumn medium={4}><Dummy>4 col</Dummy></GridColumn>
       </Grid>
       <Grid>
-        <GridColumn medium={4}><div style={dummyStyles}>4 col</div></GridColumn>
-        <GridColumn medium={4}><div style={dummyStyles}>4 col</div></GridColumn>
-        <GridColumn medium={4}><div style={dummyStyles}>4 col</div></GridColumn>
-      </Grid>
-      <Grid>
-        <GridColumn medium={5}><div style={dummyStyles}>5 col</div></GridColumn>
-        <GridColumn medium={5}><div style={dummyStyles}>5 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
+        <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
+        <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
+        <GridColumn medium={2}><Dummy>2 col</Dummy></GridColumn>
       </Grid>
       <Grid layout="fluid">
-        <GridColumn medium={5}><div style={dummyStyles}>5 col</div></GridColumn>
-        <GridColumn medium={5}><div style={dummyStyles}>5 col</div></GridColumn>
-        <GridColumn medium={2}><div style={dummyStyles}>2 col</div></GridColumn>
+        <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
+        <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
+        <GridColumn medium={2}><Dummy>2 col</Dummy></GridColumn>
       </Grid>
-      <p>This story is used to verify that page works correctly with navigation.</p>
+      <Grid>
+        <GridColumn medium={2}><Dummy>LHS</Dummy></GridColumn>
+        <GridColumn medium={8}>
+          <Dummy><Lorem count={30} /></Dummy>
+        </GridColumn>
+        <GridColumn medium={2}><Dummy>RHS</Dummy></GridColumn>
+      </Grid>
     </Page>
   ));

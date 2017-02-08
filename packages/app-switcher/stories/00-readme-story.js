3a373fc41f05dc9698b1eb1fd198318f46f30f55
James McArthur <jmcarthur@atlassian.com>
2017-02-08 11:02:00 +1100

    docs: Add details about AppSwitcher props to readme story
    affects: @atlaskit/app-switcher

--- packages/app-switcher/stories/00-readme-story.js
+++ packages/app-switcher/stories/00-readme-story.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@kadira/storybook';
 import React from 'react';
-import Readme, { Code, Props, Description } from '@atlaskit/util-readme';
+import Readme, { Code, Description, Heading, Props } from '@atlaskit/util-readme';
 
 import AppSwitcher from '../src';
 import { name } from '../package.json';
@@ -16,10 +16,138 @@ const AppSwitcherDescription = (<Description>
 
 const propDescriptions = {
   recentContainers: 'Array of recent containers. If this array is empty the recent container section will be hidden.',
-  linkedApplications: 'Map containing an array of linked applications. See prop-types.js for details on the exact format of this property.',
-  suggestedApplication: 'Map containing which application to suggest to the user. See prop-types.js for details on the exact format of this property.',
+  linkedApplications: 'Map containing an array of linked applications.',
+  suggestedApplication: 'Map containing which application to suggest to the user.',
   i18n: 'Mapping of i18n keys to translations.',
 };
+
+const recentContainerPropDescriptions = [
+  {
+    name: 'name',
+    type: 'String',
+    description: 'Name of the container.',
+  },
+  {
+    name: 'url',
+    type: 'String',
+    description: 'Link to the container.',
+  },
+  {
+    name: 'iconUrl',
+    type: 'String',
+    description: 'Link to the container\'s icon. Should be 32px square.',
+  },
+  {
+    name: 'type',
+    type: 'String',
+    description: 'Type of the container. Currently, "confluence-space" and "jira-project" are supported.',
+  },
+];
+
+const linkedApplicationsPropDescriptions = [
+  {
+    name: 'configureLink',
+    type: 'Boolean|String',
+    description: 'A link to configure the applinks for this instance OR false if the user does ' +
+                 'not have permission to configure applinks',
+  },
+  {
+    name: 'apps',
+    type: 'Array',
+    description: 'An array of the application links to display. Each array item is a map with the following keys:',
+    children: [
+      {
+        name: 'name',
+        type: 'String',
+        description: 'Display name of the application.',
+      },
+      {
+        name: 'url',
+        type: 'String',
+        description: 'URL of the application.',
+      },
+    ],
+  },
+];
+
+const suggestedApplicationPropDescriptions = [
+  {
+    name: 'show',
+    type: 'Boolean',
+    description: 'Whether to show the suggested application section.',
+  },
+  {
+    name: 'application',
+    type: 'String',
+    description: 'Application to suggest. "jira" and "confluence" are currently supported.',
+  },
+  {
+    name: 'url',
+    type: 'String',
+    description: 'Link for the suggested application.',
+  },
+  {
+    name: 'onDontShowAgainClick',
+    type: 'Function',
+    description: 'Called when the "Don\'t show this again" link is clicked.',
+  },
+];
+
+const i18nPropDescriptions = [
+  {
+    name: 'home',
+    description: 'Home',
+  },
+  {
+    name: 'apps',
+    description: 'Apps',
+  },
+  {
+    name: 'configure',
+    description: 'Configure',
+  },
+  {
+    name: 'try.other.apps',
+    description: 'Try Other Atlassian Apps',
+  },
+  {
+    name: 'don\'t.show.this.again',
+    description: 'Don’t show this again',
+  },
+  {
+    name: 'container.confluence-space',
+    description: 'Space',
+  },
+  {
+    name: 'container.jira-project',
+    description: 'Project',
+  },
+  {
+    name: 'suggested.application.description.confluence',
+    description: 'Collaboration and content sharing',
+  },
+  {
+    name: 'suggested.application.description.jira',
+    description: 'Issue & project tracking software',
+  },
+];
+
+function propDescriptionList(props) {
+  const listItems = props.map((item, index) => {
+    const children = item.children ? propDescriptionList(item.children) : null;
+    const type = item.type ? (<i>({item.type})</i>) : null;
+
+    return (
+      <li key={index}>
+        <b>{item.name}</b> {type}: {item.description} {children}
+      </li>
+    );
+  });
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
 
 storiesOf(name, module)
   .add('AppSwitcher Readme', () => (
@@ -32,6 +160,33 @@ storiesOf(name, module)
           {BasicUsageExample}
         </Code>
         <Props component={AppSwitcher} descriptions={propDescriptions} />
+        <Description>
+          <Heading type="3">recentContainers property</Heading>
+          <p>
+            The recentContainers property is an array containing the recent containers to display.
+            Each recent container item is a map with the following keys:
+          </p>
+          {propDescriptionList(recentContainerPropDescriptions)}
+
+          <Heading type="3">linkedApplications property</Heading>
+          <p>
+            The linkedApplications property is a map with the following format:
+          </p>
+          {propDescriptionList(linkedApplicationsPropDescriptions)}
+
+          <Heading type="3">suggestedApplication property</Heading>
+          <p>
+            The suggestedApplication property is a map with the following format:
+          </p>
+          {propDescriptionList(suggestedApplicationPropDescriptions)}
+
+          <Heading type="3">i18n property</Heading>
+          <p>
+            The i18n property is a mapping between translation keys and translations. It should
+            contain translations for the following keys:
+          </p>
+          {propDescriptionList(i18nPropDescriptions)}
+        </Description>
       </Readme>
     </div>
   ));

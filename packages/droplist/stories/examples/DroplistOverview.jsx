2cc1373a91f4929c7283a2123a3ac34d04feda2b
Nadia Makarevich <nmakarevich@atlassian.com>
2017-02-10 10:51:29 +1100

    refactor: remove isKeyboardInteractionDisabled from the story
    affects: @atlaskit/droplist

--- packages/droplist/stories/examples/DroplistOverview.jsx
+++ packages/droplist/stories/examples/DroplistOverview.jsx
@@ -21,7 +21,6 @@ const DroplistOverview = class extends Component {
         this.setState({ isDropdownOpen: attrs.isOpen });
       }}
       isTriggerNotTabbable
-      isKeyboardInteractionDisabled
       appearance="default"
       position="right top"
       listContext="menu"

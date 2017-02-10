63f2d78a813cad444e9c3e0cbca09813deafafc1
Jimmy Luong <jluong@atlassian.com>
2017-02-10 11:56:17 +1100

    feat(media-core): add media-core models
    affects: @atlaskit/media-core

---
+++ packages/media-core/src/index.ts
@@ -0,0 +1,36 @@
+import {MediaItemType} from './item';
+
+export * from './item';
+export * from './actions';
+export * from './config';
+export * from './mediaTypes';
+
+export interface RemoteCollection {
+  id: string;
+  apiConfig: {
+    clientId: string;
+    token: string;
+    baseURL: string;
+  };
+}
+
+export interface MediaCollectionItem {
+  id: string;
+  mediaItemType: MediaItemType;
+}
+
+export interface MediaCollection {
+  id: string;
+  items: Array<MediaCollectionItem>;
+}
+
+export interface Metadata {
+  id: string;
+  mediaType: string;
+  mimeType: string;
+  name: string;
+  processingStatus: string;
+  size: number;
+}
+
+export default {};

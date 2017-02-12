4035a376c153c43907dda79c4f424cf7b8febcad
Luke Batchelor <lbatchelor@atlassian.com>
2017-02-13 09:04:45 +1100

    build(build): Cleans up some build scripts with comments

--- build/bin/generate.readme.html.pr.sh
+++ build/bin/generate.readme.html.pr.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-CHALK="`yarn bin`/chalk"
-BUILD_SPECIFIC_URL_PART="pr/$BITBUCKET_COMMIT/$CURRENT_BUILD_TIME/docs"
+# Paths for binaries in our node_modules
+BIN_PATH=$(yarn bin)
+CHALK="$BIN_PATH/chalk"
+LERNA_LOC="$BIN_PATH/lerna"
+
+# Paths for our build scripts
 BASEDIR=$(dirname $0)
-OUTDIR=$(mktemp -d)
-export OUTDIR="$OUTDIR"
+
+# source build scripts to get functions from them
 . $BASEDIR/_build_status.sh
 . $BASEDIR/_cdn_publish_folder.sh
+
+
+OUTDIR=$(mktemp -d)
+BUILD_SPECIFIC_URL_PART="pr/$BITBUCKET_COMMIT/$CURRENT_BUILD_TIME/docs"
+
+
+# get list of changed packages in the form @atlaskit/package1,@atlaskit/package2
+PACKAGES=$($BASEDIR/_get.changed.packages.js)
 
 function docs_build_status() {
   build_status \
@@ -20,7 +32,7 @@ function docs_build_status() {
 
 function generate_docs() {
   $CHALK --no-stdin -t "{blue Generating docs HTML output from README.md files...}"
-  lerna exec -- ../../build/bin/generate.readme.html.sh
+  LERNA_LOC exec --scope="{$PACKAGES}" -- ../../build/bin/generate.readme.html.sh
 
   $CHALK --no-stdin -t "{blue Generating docs index...}"
   pushd $OUTDIR > /dev/null
@@ -28,7 +40,7 @@ function generate_docs() {
   popd > /dev/null
 }
 
-docs_build_status "INPROGRESS"
+#docs_build_status "INPROGRESS"
 generate_docs
-cdn_publish_folder "$OUTDIR" "$BUILD_SPECIFIC_URL_PART"
-docs_build_status "SUCCESSFUL"
+#cdn_publish_folder "$OUTDIR" "$BUILD_SPECIFIC_URL_PART"
+#docs_build_status "SUCCESSFUL"

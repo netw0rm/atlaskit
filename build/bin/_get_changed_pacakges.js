763a7b92ffc0f77bf798945541accb223763eb25
Luke Batchelor <lbatchelor@atlassian.com>
2017-02-14 09:43:28 +1100

    build(build): Adds generate.changed.packages.file.sh

---
+++ build/bin/_get_changed_pacakges.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+/* Writing this file in js to make it easier to read and maintain */
+
+// child_process_promise is a wrapper around nodes child_process'
+const exec = require('child-process-promise').exec;
+
+// returns a random 5 character string for use in branch names to avoid collisions (super unlikely)
+function generateRandomString() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return [...new Array(5)]
+    .map(() => alphabet.charAt(Math.random() * alphabet.length))
+    .join('');
+}
+
+/*
+  Gets a list of changed packages between current branch and master and returns them in the form
+     @atlaskit/package1,@atlaskit/package2
+   etc.
+*/
+function getChangedPackages() {
+  let changedPackages;
+
+  exec('git fetch origin')
+    .then(() => {
+      const tempBranchName = `temp_${generateRandomString()}`;
+      return exec(`git checkout -b ${tempBranchName}`);
+    })
+    .then(() => exec('git merge origin/master'))
+    .then(() => exec('git diff --name-only origin/master'))
+    .then((result) => {
+      changedPackages = result.stdout.split('\n')
+        // remove any empty strings
+        .filter(filePath => filePath.length > 0)
+        // remove files not in /packages directory
+        .filter(filePath => filePath.match(/^packages\//))
+        // get packageNames from paths
+        .map(filePath => filePath.match(/^packages\/(.+?)\//)[1])
+        // remove duplicate names (if the first index of ourself isnt our idx, we arent unique)
+        .filter((packageName, idx, arr) => arr.indexOf(packageName) === idx)
+        // add the @atlaskit scope to them
+        .map(packageName => `@atlaskit/${packageName}`);
+
+      return exec('git checkout -');
+    })
+    .then(() => {
+      // output in the form "@atlaskit/packageOne,@atlaskit/packageTwo" to allow for easy scoping
+      // by passing the list as a glob
+      console.log(`${changedPackages.join(',')}`); // eslint-disable-line no-console
+    })
+    .catch((err) => {
+      console.error(err); // eslint-disable-line no-console
+    });
+}
+
+getChangedPackages();
---
+++ build/bin/generate.changed.packages.file.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# This script creates a file with a list of all the packages that have changed between the current
+# branch and origin/master. We write to a file so that commands running in parallel don't try to
+# run git commands at the same time.
+
+# We wrap this in a script so that we have an easy "kill switch" if we need to turn this off for
+# whatever reason
+
+BASEDIR=$(dirname $0)
+
+# set this flag to true to prevent any scoping of changed packages (run all commands for all packages)
+KILL_SWITCH=false
+
+if [ "$KILL_SWITCH" = true ] ; then
+  # we echo "**," to make the globs match all packages. The comma is because the glob will be inside
+  # a pair of curly braces `--scope="{$PACKAGES}"`
+  echo "**," > changed-packages
+else
+  $BASEDIR/_get_changed_packages.js > changed-packages
+fi

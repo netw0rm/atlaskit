9d7cf59ae9d2a8c2ac4f06e3abe58582fd3555ad
Luke Batchelor <lbatchelor@atlassian.com>
2017-02-13 11:40:04 +1100

    build(build): Adds promises and master merging to changed.packages script

--- build/bin/_get.changed.packages.js
+++ build/bin/_get.changed.packages.js
@@ -1,22 +1,38 @@
 #!/usr/bin/env node
 /* Writing this file in js to make it easier to read and maintain */
 
-// child_process comes from node itself
-const exec = require('child_process').exec;
+// child_process_promise is a wrapper around nodes child_process'
+const exec = require('child-process-promise').exec;
 
-/* Gets a list of changed packages between current branch and master and returns them in the form
+// returns a random 5 character string for use in branch names to avoid collisions (super unlikely)
+function generateRandomString() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return [...new Array(5)]
+    .map(() => alphabet.charAt(Math.random() * alphabet.length))
+    .join('');
+}
+
+/*
+  Gets a list of changed packages between current branch and master and returns them in the form
      @atlaskit/package1,@atlaskit/package2
    etc.
 */
 function getChangedPackages() {
-  exec('git fetch origin && git diff --name-only origin/master', (err, stdout, stderr) => {
-    if (!err) {
+  exec('git fetch origin')
+    .then(() => {
+      const tempBranchName = `temp_${generateRandomString()}`;
+      return exec(`git checkout -b ${tempBranchName}`);
+    })
+    .then(() => exec('git merge origin/master'))
+    .then(() => exec('git diff --name-only origin/master'))
+    .then((result) => {
+      const stdout = result.stdout;
       const changedPackages = stdout.split('\n')
         // remove any empty strings
         .filter(filePath => filePath.length > 0)
-        // remove files not in /packages
+        // remove files not in /packages directory
         .filter(filePath => filePath.match(/^packages\//))
-        // get packageNames
+        // get packageNames from paths
         .map(filePath => filePath.match(/^packages\/(.+?)\//)[1])
         // remove duplicate names (if the first index of ourself isnt our idx, we arent unique)
         .filter((packageName, idx, arr) => arr.indexOf(packageName) === idx)
@@ -25,10 +41,13 @@ function getChangedPackages() {
 
       // return the result
       console.log(`${changedPackages.join(',')}`); // eslint-disable-line no-console
-    } else {
-      console.error(stderr); // eslint-disable-line no-console
-    }
-  });
+
+      return exec('git checkout -');
+    })
+    .catch((err) => {
+      console.error(err); // eslint-disable-line no-console
+    });
 }
 
 getChangedPackages();
+

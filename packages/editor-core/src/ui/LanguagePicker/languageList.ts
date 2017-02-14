314000172bfc733d2969525cc69f68b922f12a32
Huanhuan Huang <hhuang@atlassian.com>
2017-02-15 09:33:03 +1100

    feat(pakcage): Added alias to language list.
    affects: @atlaskit/editor-core

--- packages/editor-core/src/ui/LanguagePicker/languageList.ts
+++ packages/editor-core/src/ui/LanguagePicker/languageList.ts
@@ -2,75 +2,89 @@
 export const NO_LANGUAGE = 'Language';
 
 export const SUPPORTED_LANGUAGES = [
-  'PHP',
-  'Java',
-  'CSharp',
-  'Python',
-  'JavaScript',
-  'Html',
-  'C++',
-  'Ruby',
-  'Objective-C',
-  'C',
-  'Swift',
-  'LaTex',
-  'Shell',
-  'Scala',
-  'Go',
-  'ActionScript',
-  'VbNet',
-  'MATLAB',
-  'Groovy',
-  'SQL',
-  'R',
-  'Perl',
-  'Lua',
-  'Delphi',
-  'XML',
-  'TypeScript',
-  'CoffeeScript',
-  'Clojure',
-  'Haskell',
-  'Puppet',
-  'Arduino',
-  'Fortran',
-  'Erlang',
-  'TeX',
-  'PowerShell',
-  'Haxe',
-  'Elixir',
-  'Verilog',
-  'Rust',
-  'VHDL',
-  'Sass',
-  'OCaml',
-  'Dart',
-  'CSS',
-  'reStructuredText',
-  'ObjectPascal',
-  'Kotlin',
-  'D',
-  'Octave',
-  'QML',
-  'Prolog',
-  'FoxPro',
-  'Scheme',
-  'CUDA',
-  'Julia',
-  'Racket',
-  'Ada',
-  'Tcl',
-  'Mathematica',
-  'Autoit',
-  'StandardML',
-  'Objective-J',
-  'Smalltalk',
-  'Vala',
-  'ABAP',
-  'LiveScript',
-  'XQuery'];
+  makeLanguage('PHP', ['php3', 'php4', 'php5']),
+  makeLanguage('Java'),
+  makeLanguage('CSharp', ['c#']),
+  makeLanguage('Python'),
+  makeLanguage('JavaScript', ['js']),
+  makeLanguage('Html'),
+  makeLanguage('C++', ['cpp']),
+  makeLanguage('Ruby', ['rb', 'duby']),
+  makeLanguage('Objective-C', ['objectivec', 'obj-c', 'objc']),
+  makeLanguage('C'),
+  makeLanguage('Swift'),
+  makeLanguage('TeX', ['tex', 'latex']),
+  makeLanguage('Shell', ['bash', 'sh', 'ksh', 'zsh']),
+  makeLanguage('Scala'),
+  makeLanguage('Go'),
+  makeLanguage('ActionScript', ['as']),
+  makeLanguage('VbNet', ['vb.net']),
+  makeLanguage('MATLAB'),
+  makeLanguage('Groovy'),
+  makeLanguage('SQL', ['postgresql', 'postgres', 'plpgsql', 'psql', 'postgresql-console', 'postgres-console', 'tsql', 't-sql', 'mysql', 'sqlite']),
+  makeLanguage('R'),
+  makeLanguage('Perl', ['pl']),
+  makeLanguage('Lua'),
+  makeLanguage('Delphi', ['pas', 'pascal', 'objectpascal']),
+  makeLanguage('XML'),
+  makeLanguage('TypeScript', ['ts']),
+  makeLanguage('CoffeeScript', ['coffee-script', 'coffee']),
+  makeLanguage('Clojure', ['clj']),
+  makeLanguage('Haskell', ['hs']),
+  makeLanguage('Puppet'),
+  makeLanguage('Arduino'),
+  makeLanguage('Fortran'),
+  makeLanguage('Erlang'),
+  makeLanguage('PowerShell', ['posh', 'ps1', 'psm1']),
+  makeLanguage('Haxe', ['hx', 'hxsl']),
+  makeLanguage('Elixir', ['ex', 'exs']),
+  makeLanguage('Verilog', ['v']),
+  makeLanguage('Rust'),
+  makeLanguage('VHDL'),
+  makeLanguage('Sass'),
+  makeLanguage('OCaml'),
+  makeLanguage('Dart'),
+  makeLanguage('CSS'),
+  makeLanguage('reStructuredText', ['rst', 'rest']),
+  makeLanguage('ObjectPascal'),
+  makeLanguage('Kotlin'),
+  makeLanguage('D'),
+  makeLanguage('Octave'),
+  makeLanguage('QML', ['qbs']),
+  makeLanguage('Prolog'),
+  makeLanguage('FoxPro', ['vfp', 'clipper', 'xbase']),
+  makeLanguage('Scheme', ['scm']),
+  makeLanguage('CUDA', ['cu']),
+  makeLanguage('Julia', ['jl']),
+  makeLanguage('Racket', ['rkt']),
+  makeLanguage('Ada', ['ada95', 'ada2005']),
+  makeLanguage('Tcl'),
+  makeLanguage('Mathematica', ['mathematica', 'mma', 'nb']),
+  makeLanguage('Autoit'),
+  makeLanguage('StandardML', ['sml']),
+  makeLanguage('Objective-J', ['objectivej', 'obj-j', 'objj']),
+  makeLanguage('Smalltalk', ['squeak', 'st']),
+  makeLanguage('Vala', ['vapi']),
+  makeLanguage('ABAP'),
+  makeLanguage('LiveScript', ['live-script']),
+  makeLanguage('XQuery', ['xqy', 'xq', 'xql', 'xqm'])];
 
 const languageList = [NO_LANGUAGE, ...(SUPPORTED_LANGUAGES.sort())];
+
+function makeLanguage(name: string, alias?: Array<string>): Language {
+  alias = alias || [];
+
+  if (alias.length === 0 || alias.indexOf(name.toLowerCase()) === -1) {
+    alias.push(name.toLowerCase());
+  }
+
+  return {name, alias};
+}
+
+export interface Language {
+  name: string;
+  alias: Array<string>;
+}
 
 export function findMatchedLanguage(languge?: string): string {
   if (!languge) {
@@ -78,11 +92,11 @@ export function findMatchedLanguage(languge?: string): string {
   }
 
   const matches = SUPPORTED_LANGUAGES.filter((supportedLanguage) => {
-    return supportedLanguage.toLowerCase() === languge.toLowerCase();
+    return supportedLanguage.name.toLowerCase() === languge.toLowerCase() || supportedLanguage.alias.indexOf(languge.toLowerCase()) !== -1;
   });
 
   if (matches.length > 0) {
-    return matches[0];
+    return matches[0].name;
   }
 
   return NO_LANGUAGE;

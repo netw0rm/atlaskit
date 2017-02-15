dbc3dcd820689eb25c0bb256692f24eabeae9cf7
Luke Batchelor <lbatchelor@atlassian.com>
2017-02-16 09:01:19 +1100

    build(build): Adds more tests for getChangedPackages

--- test/getChangedPackages.js
+++ test/getChangedPackages.js
@@ -23,6 +23,33 @@ describe('_get_changed_packages.js', () => {
       const changedPackages = changedFilesToChangedPackages(changedFiles);
       changedPackages.should.deep.equal(['@atlaskit/foo', '@atlaskit/bar']);
     });
+
+    it('should return an empty list when no changed files are found (2 empty strings)', () => {
+      // if no changed files, we would have received just a newline character, which, when split
+      // would pass an array with two empty strings to our function. We'll test the cases of 1, 2
+      // and 0 empty strings though, just in cases
+      const changedFiles = ['', ''];
+      const changedPackages = changedFilesToChangedPackages(changedFiles);
+      changedPackages.should.deep.equal([]);
+    });
+
+    it('should return an empty list when no changed files are found (1 empty string)', () => {
+      const changedFiles = [''];
+      const changedPackages = changedFilesToChangedPackages(changedFiles);
+      changedPackages.should.deep.equal([]);
+    });
+
+    it('should return an empty list when no changed files are found (no empty strings)', () => {
+      const changedFiles = [];
+      const changedPackages = changedFilesToChangedPackages(changedFiles);
+      changedPackages.should.deep.equal([]);
+    });
+
+    it('should return an empty array if only files outside of /packages are changed', () => {
+      const changedFiles = ['Readme.md', 'bitbucket-pipelines.yml', 'test/getChangedPackages.js'];
+      const changedPackages = changedFilesToChangedPackages(changedFiles);
+      changedPackages.should.deep.equal([]);
+    });
   });
 
   describe('changedPackagesToLernaGlob', () => {

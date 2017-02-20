68d298d9fac5ccb09b64568eb0acc334608d7c78
Eduard Shvedai <eshvedai@atlassian.com>
2017-02-20 15:04:45 +1100

    test: removed the test that will be added to editor-bitbucket

--- packages/editor-core/test/plugins/markdown-inputrules/index.ts
+++ packages/editor-core/test/plugins/markdown-inputrules/index.ts
@@ -305,17 +305,6 @@ describe('markdown-inputrules', () => {
 
   describe('undo mark convertion', () => {
     context('when hits Cmd+Z', () => {
-      it('should undo codeblock', () => {
-        const { pm, sel } = editor(doc(p('{<>}foo')));
-
-        pm.input.insertText(sel, sel, '```');
-        expect(pm.doc).to.deep.equal(doc(code_block()('foo')));
-
-        pm.input.dispatchKey('Cmd-Z');
-
-        expect(pm.doc).to.deep.equal(doc(p('foo')));
-      });
-
       it('should undo previously applied mark', () => {
         const { pm, sel } = editor(doc(p('{<>}')));
 

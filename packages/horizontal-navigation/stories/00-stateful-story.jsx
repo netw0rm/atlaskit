1c81e0985dfe220992f7d1f02e3551136f8937de
Jake Furler <jfurler@atlassian.com>
2017-02-20 15:56:15 +1100

    feat(component): RAD-16 reintroducing import to fix storybook

--- packages/horizontal-navigation/stories/00-stateful-story.jsx
+++ packages/horizontal-navigation/stories/00-stateful-story.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { AtlassianLogo } from '@atlaskit/logo';
+import { name } from '../package.json';
 import AkHorizontalNavigation from '../src/index';
 
 storiesOf(name, module)

d8caa7e222784ebadc9384c9f16a48f065a7ff40
Jake Furler <jfurler@atlassian.com>
2017-02-20 15:53:55 +1100

    feat(component): RAD-16 extracted logo definition into storybook and added title

--- packages/horizontal-navigation/src/index.jsx
+++ packages/horizontal-navigation/src/index.jsx
@@ -1,19 +1,26 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 import classNames from 'classnames';
 import styles from 'style!./less/HorizontalNavigation.less';
 
 export default class HorizontalNavigation extends PureComponent {
+  static propTypes = {
+    logo: PropTypes.node,
+    title: PropTypes.string,
+  };
+
   render() {
+    const {
+      logo,
+      title,
+    } = this.props;
+
     return (<div className={classNames(styles.horizontalNavigation)}>
       <div className={styles.primaryContainer}>
-        <h1>JIRA Service Desk Cloud</h1>
+        {logo ? <span className={styles.logo}>{logo}</span> : null}
+        <div className={styles.primaryTitle}>{title}</div>
       </div>
-      <div className={styles.secondaryContainer}>
-        <form>
-          <input type="text" />
-          <input type="submit" value="Search" />
-        </form>
-      </div>
+
+      <div className={styles.secondaryContainer} />
     </div>);
   }
 }
--- packages/horizontal-navigation/stories/00-stateful-story.jsx
+++ packages/horizontal-navigation/stories/00-stateful-story.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import Readme, { Code, Props } from '@atlaskit/util-readme';
-import { name, description } from '../package.json';
-import AkHorizontalNavigation from '../src/';
-
-/* eslint-disable import/no-duplicates, import/first */
-import OverviewExample from './examples/stateful-overview';
-import OverviewExampleRaw from '!raw!./examples/stateful-overview';
-/* eslint-enable import/no-duplicates, import/first */
+import { AtlassianLogo } from '@atlaskit/logo';
+import AkHorizontalNavigation from '../src/index';
 
 storiesOf(name, module)
-  .add('HorizontalNavigation - stateful: README', () => (
-    <Readme
-      component={name}
-      description={description}
-    >
-      <Code code={OverviewExampleRaw}>
-        <OverviewExample />
-      </Code>
-      <Props component={AkHorizontalNavigation} />
-    </Readme>
-  ))
-  .add('HorizontalNavigation - stateful: basic usage', () => (
-    <AkHorizontalNavigation label="My toggle" />
+  .add('DAC', () => (
+    <AkHorizontalNavigation logo={<AtlassianLogo isCollapsed />} title="JIRA Service Desk Cloud" />
   ));

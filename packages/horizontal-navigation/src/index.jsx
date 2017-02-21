fadeca7d6b68329aabdf8cee24bcb7f9aa90c2ed
Jake Furler <jfurler@atlassian.com>
2017-02-21 17:30:59 +1100

    feat(component): RAD-16 added support for displaying links along the top nav

--- packages/horizontal-navigation/src/index.jsx
+++ packages/horizontal-navigation/src/index.jsx
@@ -6,18 +6,53 @@ export default class HorizontalNavigation extends PureComponent {
   static propTypes = {
     logo: PropTypes.node,
     title: PropTypes.string,
+    linkItems: PropTypes.arrayOf(PropTypes.string),
   };
+
+  static simpleDropdownItems = [
+    {
+      heading: 'Heading',
+      items: [
+        {
+          content: 'Some text',
+          href: '//atlassian.com',
+          target: '_blank',
+        },
+        {
+          content: 'Some text 2',
+          href: '//atlassian.com',
+          target: '_blank',
+          isDisabled: true,
+        },
+        {
+          content: 'Some text 4',
+          href: '//atlassian.com',
+          target: '_blank',
+        },
+      ],
+    },
+  ];
 
   render() {
     const {
       logo,
       title,
+      linkItems,
     } = this.props;
 
     return (<div className={classNames(styles.horizontalNavigation)}>
       <div className={styles.primaryContainer}>
         {logo ? <span className={styles.logo}>{logo}</span> : null}
         <div className={styles.primaryTitle}>{title}</div>
+        <ul className={styles.linkItems}>
+          {linkItems.map(linkItem =>
+            <li>
+              <a href={linkItem.href} className={linkItem.selected && styles.current}>
+                {linkItem.text}
+              </a>
+            </li>
+          )}
+        </ul>
       </div>
 
       <div className={styles.secondaryContainer} />
--- packages/horizontal-navigation/stories/00-stateful-story.jsx
+++ packages/horizontal-navigation/stories/00-stateful-story.jsx
@@ -4,7 +4,46 @@ import { AtlassianLogo } from '@atlaskit/logo';
 import { name } from '../package.json';
 import AkHorizontalNavigation from '../src/index';
 
+const dropdownItems = [
+  {
+    items: [
+      {
+        content: 'Some text',
+        href: '//atlassian.com',
+        target: '_blank',
+      },
+      {
+        content: 'Some text 2',
+        href: '//atlassian.com',
+        target: '_blank',
+        isDisabled: true,
+      },
+      {
+        content: 'Some text 4',
+        href: '//atlassian.com',
+        target: '_blank',
+      },
+    ],
+  },
+];
+
+const linkItems = [
+  {
+    text: 'Guides',
+    href: 'http://www.atlassian.com',
+  },
+  {
+    text: 'Reference',
+    href: 'http://www.atlassian.com',
+    selected: true,
+  },
+  {
+    text: 'Get help',
+    href: 'http://www.atlassian.com',
+  },
+];
+
 storiesOf(name, module)
   .add('DAC', () => (
-    <AkHorizontalNavigation logo={<AtlassianLogo isCollapsed />} title="JIRA Service Desk Cloud" />
+    <AkHorizontalNavigation logo={<AtlassianLogo isCollapsed />} title="JIRA Service Desk Cloud" dropdownItems={dropdownItems} linkItems={linkItems} />
   ));

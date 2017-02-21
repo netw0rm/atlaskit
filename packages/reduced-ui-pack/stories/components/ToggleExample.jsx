8eeb39ebcb286046535128d1748b359d7f30e305
Ben Gummer <bgummer@atlassian.com>
2017-02-21 16:30:31 +1100

    feat(reduced-ui): add toggle component to reduced UI pack
    affects: @atlaskit/reduced-ui-pack

---
+++ packages/reduced-ui-pack/stories/components/ToggleExample.jsx
@@ -0,0 +1,31 @@
+import React, { PropTypes, PureComponent } from 'react';
+
+let exampleCounter = 0;
+
+export default class ToggleExample extends PureComponent {
+  static propTypes = {
+    checked: PropTypes.bool.isRequired,
+    disabled: PropTypes.bool.isRequired,
+    size: PropTypes.string.isRequired,
+  }
+
+  render() {
+    exampleCounter++;
+    const exampleId = `example${exampleCounter}`;
+    const { checked, disabled, size } = this.props;
+    return (
+      <div className={`ak-field-toggle ak-field-toggle__size-${size}`}>
+        <input
+          type="checkbox"
+          name={exampleId}
+          id={exampleId}
+          value={exampleId}
+          className="ak-toggle"
+          defaultChecked={checked}
+          disabled={disabled}
+        />
+        <label htmlFor={exampleId}>Option</label>
+      </div>
+    );
+  }
+}

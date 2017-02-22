ed8d65a3af74c90518dc5a96b1c92875c0d64557
Eduard Shvedai <eshvedai@atlassian.com>
2017-02-22 13:23:32 +1100

    fix: autoconversion of code block in a nested structure
    affects: @atlaskit/editor-core

--- packages/editor-core/src/plugins/block-type/index.ts
+++ packages/editor-core/src/plugins/block-type/index.ts
@@ -402,7 +402,7 @@ export class BlockTypeState {
     let textOnly = true;
 
     this.pm.doc.nodesBetween(startPos, $from.pos, (node) => {
-      if (!node.isText && !node.isTextblock) {
+      if (node.childCount === 0 && !node.isText && !node.isTextblock) {
         textOnly = false;
       }
     });
@@ -416,7 +416,7 @@ export class BlockTypeState {
     }
 
     const fencePart = parentBlock.textContent.slice(0, $from.pos - startPos).trim();
-
+    
     const matches = /^```([^\s]+)?/.exec(fencePart);
 
     if (matches) {

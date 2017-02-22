db4da3fc5066120facb49367177640b4b9553c70
Joshua Nelson <jonelson@atlassian.com>
2017-02-22 13:48:04 +1100

    refactor(build): refactor wc tests to remove sourcemaps

--- build/karma/wc/all.js
+++ build/karma/wc/all.js
@@ -4,6 +4,7 @@ const karmaConf = require('../base');
 const addPolyFills = require('../addPolyFills');
 const setUpEnzyme = require('../setUpEnzyme');
 const assignPattern = require('../assignPattern');
+const removeSourcemaps = require('../removeSourceMaps');
 
 module.exports = (config) => {
   karmaConf(config);
@@ -12,6 +13,7 @@ module.exports = (config) => {
 
   // add the polyfill file to the test run
   addPolyFills(config);
+  removeSourcemaps(config);
 
   setUpEnzyme(config);
 };

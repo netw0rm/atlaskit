404da4114e202f7588c3629154f94b1d33951263
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-02-23 12:59:18 +1100

    fix(media-filmstrip): Avoid scrolling for Y axis events
    affects: @atlaskit/media-filmstrip

--- packages/media-filmstrip/src/filmstrip-navigator.tsx
+++ packages/media-filmstrip/src/filmstrip-navigator.tsx
@@ -69,6 +69,9 @@ export default class FilmStripNavigator extends Component<FilmstripNavigatorProp
 
   _onScroll(e) {
     e.preventDefault();
+    const isHorizontalScroll = Math.abs(e.nativeEvent.deltaX) > Math.abs(e.nativeEvent.deltaY);
+    if (!isHorizontalScroll) { return; }
+
     const showTransition = false;
     this.updateState({showTransition});
     this._setNewPosition(this.state.position + e.nativeEvent.deltaX, showTransition);

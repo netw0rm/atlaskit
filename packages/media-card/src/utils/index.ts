acd4616b1ea1c6343e0914b676edb7db34074614
Michael James <mjames@atlassian.com>
2017-02-28 10:50:27 +1100

    refactor(refactor): cleaned #toHumanReadableMediaSize util function
    affects: @atlaskit/media-card
    
    ISSUES CLOSED: FIL-3871

--- packages/media-card/src/utils/index.ts
+++ packages/media-card/src/utils/index.ts
@@ -7,7 +7,6 @@ const ONE_MEGABYTE_IN_BYTES = 1 << 20;
  * Takes a media (file) size in bytes and returns a human readable string
  */
 export function toHumanReadableMediaSize(size: number): string {
-  return size < ONE_MEGABYTE_IN_BYTES ?
-    bytes.format(size, {unitSeparator: ' ', decimalPlaces: 0}) :
-    bytes.format(size, {unitSeparator: ' ', decimalPlaces: 1});
+    const decimalPlaces = size < ONE_MEGABYTE_IN_BYTES ? 0 : 1;
+    return bytes.format(size, {unitSeparator: ' ', decimalPlaces});
 }

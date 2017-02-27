20adff8ba37eac891850fb10280d4fe84a468a0b
Eduard Shvedai <eshvedai@atlassian.com>
2017-02-27 15:13:06 +1100

    fix: lint fixes
    affects: @atlaskit/editor-bitbucket, @atlaskit/editor-core, @atlaskit/editor-cq

--- packages/editor-core/src/plugins/index.ts
+++ packages/editor-core/src/plugins/index.ts
@@ -3,7 +3,7 @@ export { default as CodeBlockPlugin } from './code-block';
 export { default as EmojisPlugin } from './emojis';
 export { default as HyperlinkPlugin } from './hyperlink';
 export { default as ImageUploadPlugin } from './image-upload';
-export { default as ListsPlugin } from './lists/index-future';
+export { default as ListsPlugin } from './lists';
 export { default as MarkdownInputRulesPlugin } from './markdown-inputrules';
 export { default as MentionsPlugin } from './mentions';
 export { default as TextFormattingPlugin } from './text-formatting';

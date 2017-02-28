702187784f2c6dd49b77fc6d3cf1319752702e82
Ben Gummer <bgummer@atlassian.com>
2017-02-28 14:30:10 +1100

    chore: update tests and stories

--- packages/logo/test/LogoSpec.jsx
+++ packages/logo/test/LogoSpec.jsx
@@ -7,5 +7,20 @@ describe('<Logo />', () => {
     it('should render an svg', () => {
       expect(shallow(<Logo logoText={<svg />} />).find('svg')).to.have.length(1);
     });
+    describe('collapseTo prop', () => {
+      it('should not collapse by default', () => {
+        const wrapper = shallow(<Logo size="small" />);
+        expect(wrapper.props().collapseTo).to.equal(undefined);
+      });
+
+      // Note: other values can be supplied, but will cause a PropType warning
+      it('should accept "type" or "icon"', () => {
+        const typeWrapper = shallow(<Logo collapseTo="type" size="small" />);
+        expect(typeWrapper.props().collapseTo).to.equal('type');
+
+        const iconWrapper = shallow(<Logo collapseTo="icon" size="small" />);
+        expect(iconWrapper.props().collapseTo).to.equal('icon');
+      });
+    });
   });
 });

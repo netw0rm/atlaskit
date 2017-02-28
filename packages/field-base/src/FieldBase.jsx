2e6f7252bd46cb19c0cc768cca50e21e6577b6a3
Luke Batchelor <lbatchelor@atlassian.com>
2017-02-28 11:10:37 +1100

    fix(docs): Removes jsdocs annoations and moves them to usage.md
    affects: @atlaskit/field-base

--- packages/field-base/src/FieldBase.jsx
+++ packages/field-base/src/FieldBase.jsx
@@ -4,142 +4,20 @@ import WarningIcon from '@atlaskit/icon/glyph/warning';
 import React, { PureComponent, PropTypes } from 'react';
 import appearances, { standard, compact, none, subtle } from './internal/appearances';
 
-/**
- * @description Create instances of the FieldBase.
- * @class FieldBase
- * @example <FieldBase label="Email" />
- */
  /* eslint-disable react/no-unused-prop-types */
 export default class FieldBase extends PureComponent {
   static propTypes = {
-    /**
-     * @description The appearance of the field.
-     *
-     * Valid values for this property are: 'standard' (default), 'compact' and 'subtle'.
-     *
-     * Compact will make the field have less padding and subtle will remove the background/border
-     * until a user hovers over it.
-     *
-     * @memberof FieldBase
-     * @type {string}
-     * @default standard
-     * @example <FieldBase appearance="compact" />
-     */
     appearance: PropTypes.oneOf(Object.keys(appearances)),
-    /**
-     * @description Whether or not a field should show a validation error.
-     *
-     * This is shown to the user through a warning icon and an orange border.
-     * A future release will allow a custom error message to be displayed.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase isInvalid />
-     */
     isInvalid: PropTypes.bool,
-    /**
-     * @description Whether or not a field should show its focused styles by default.
-     *
-     * By default, this component will automatically add and remove this prop if itself
-     * or any child of it receives focus or blur events.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example @html <FieldBase isFocused />
-     */
     isFocused: PropTypes.bool,
-    /**
-     * @description Whether or not the field is required.
-     *
-     * If set to true, an asterisk will be appended to the label text.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase label="First Name" isRequired" />
-     */
     isRequired: PropTypes.bool,
-    /**
-     * @description Whether or not a field is disabled.
-     *
-     * This is shown to the user through a disabled cursor icon when hovering over the field.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase isDisabled />
-     */
     isDisabled: PropTypes.bool,
-    /**
-     * @description Whether or not the field should have padding.
-     *
-     * Disables the field's padding css property.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase isPaddingDisabled />
-     */
     isPaddingDisabled: PropTypes.bool,
-    /**
-     * @description Whether or not the field is in read-only mode.
-     *
-     * Disables the field's hover effect to indicate that it is not editable.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase isReadOnly />
-     */
     isReadOnly: PropTypes.bool,
-    /**
-     * @description Whether or not the field should fill the width of its container.
-     *
-     * If enabled, the field will fit the width of its container even
-     * when the field content is not that wide.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase isFitContainerWidthEnabled />
-     */
     isFitContainerWidthEnabled: PropTypes.bool,
-    /**
-     * @description Callback that is called whenever the Content is focused
-     *
-     * @memberof FieldBase
-     * @type {Function}
-     * @default () => void
-     * @example <FieldBase onFocus={() => alert('content focused!')} />
-     */
     onFocus: PropTypes.func.isRequired,
-    /**
-     * @description Callback that is called whenever the Content is blured
-     *
-     * @memberof FieldBase
-     * @type {Function}
-     * @default () => void
-     * @example <FieldBase onBlur={() => alert('content blured!')} />
-     */
     onBlur: PropTypes.func.isRequired,
-    /**
-     * @description This flag will force blur on fieldbase after it is rendered.
-     *
-     * @memberof FieldBase
-     * @type {boolean}
-     * @default false
-     * @example <FieldBase shouldReset />
-     */
     shouldReset: PropTypes.bool,
-    /**
-     * @description The content that will be displayed within the field
-     *
-     * @memberof FieldBase
-     * @type {ReactNode}
-     * @example <FieldBase><div>Hi!</div></FieldBase>
-     */
     children: PropTypes.node,
   }
 

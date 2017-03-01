8ffccd66e1422c846cafbeca388b8650b5f5fcdb
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-01 17:05:42 +1100

    chore(build): fixes edge case where deleting a package would prevent storybook generation of build

--- build/bin/storybook.static.pr.sh
+++ build/bin/storybook.static.pr.sh
@@ -19,6 +19,9 @@ BUILD_SPECIFIC_URL_PART="pr/$BITBUCKET_COMMIT/$CURRENT_BUILD_TIME/storybook"
 # get list of changed packages which should have been outputted by generate.changed.packages.file.sh
 CHANGED_PACKAGES=$(cat changed-packages)
 
+$CHALK --no-stdin -t "{green -- Changed Packages --}"
+$CHALK --no-stdin -t "{green $CHANGED_PACKAGES}"
+
 function storybook_build_status() {
   build_status \
     "STORYBOOK" \

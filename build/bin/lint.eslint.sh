8b70e395870c9467eb2093c5c98f99fdc94fd492
Oscar Wallhult <owallhult@atlassian.com>
2017-03-03 11:58:19 +1100

    chore(build): fixing eslintignore

--- build/bin/lint.eslint.sh
+++ build/bin/lint.eslint.sh
@@ -4,4 +4,5 @@ export PATH="`yarn bin`:$PATH"
 NODE_MODULES=`npm root`
 
 chalk --no-stdin -t "{blue ESLinting files…}"
-eslint --color --format "$NODE_MODULES/eslint-friendly-formatter" . --ext .js --ext .jsx $@
+eslint --color --format "$NODE_MODULES/eslint-friendly-formatter" . --ext .js $@
+eslint --color --format "$NODE_MODULES/eslint-friendly-formatter" . --ext .jsx $@

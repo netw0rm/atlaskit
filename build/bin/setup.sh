be695c8447dc42df3aee1e2f55fa0329e73bc150
Oscar Wallhult <owallhult@atlassian.com>
2017-03-03 16:23:47 +1100

    chore(build): increasing memory for yarn

--- build/bin/setup.sh
+++ build/bin/setup.sh
@@ -4,6 +4,9 @@ set -e
 npm=$(which npm)
 alias npm='node --max_old_space_size=8192 $npm'
 
+yarn=$(which yarn)
+alias yarn='node --max_old_space_size=8192 $yarn'
+
 yarn config set progress false
 yarn config set color always
 yarn config set loglevel warn

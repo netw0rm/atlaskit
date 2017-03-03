dc3371f2860f484f638426e57c2228f9dd9e001b
Clifton Hensley <chensley@atlassian.com>
2017-03-03 14:53:42 +1100

    chore(build): attempting to resolve memory issues
    hackily increasing memory

--- build/bin/setup.sh
+++ build/bin/setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+npm=$(which npm)
+alias npm='node --max_old_space_size=8192 $npm'
+
 yarn config set progress false
 yarn config set color always
 yarn config set loglevel warn
--- build/webpack/karma.js
+++ build/webpack/karma.js
@@ -1,6 +1,10 @@
+const webpack = require('webpack');
 const standardConfig = require('./development.js');
 
 // We delete the entry from the normal config and let karma insert it for us
 delete standardConfig.entry;
+
+// Dedupe modules
+standardConfig.plugins.push(new webpack.optimize.DedupePlugin());
 
 module.exports = standardConfig;

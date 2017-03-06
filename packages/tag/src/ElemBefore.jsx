ab491441ba37b03279363a25dd2a4f82987db7a9
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-07 10:54:20 +1100

    chore(refactor): removes unnecessary eslint disables
    affects: @atlaskit/tag

--- packages/tag/src/ElemBefore.jsx
+++ packages/tag/src/ElemBefore.jsx
@@ -1,7 +1,6 @@
 import styles from 'style!./styles.less';
 import React, { PureComponent, PropTypes } from 'react';
 
-/* eslint-disable react/prefer-stateless-function,jsx-a11y/no-static-element-interactions */
 export default class ElemBefore extends PureComponent {
   static propTypes = {
     children: PropTypes.node,

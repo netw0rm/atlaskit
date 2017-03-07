3e745f12dc83637e7fdf261452cbc443da025162
Joshua Nelson <jonelson@atlassian.com>
2017-03-07 14:34:19 +1100

    fix(component): alignment fixes for the GlobalNavigation and ContainerNavigation
    affects: @atlaskit/navigation
    
    this brings Navigation closer to v1.3 of the spec

--- packages/navigation/src/shared-variables.js
+++ packages/navigation/src/shared-variables.js
@@ -1,10 +1,10 @@
 // NOTE: this is duplicated in shared-variables.less
 export const globalOpenWidth = 64;
-export const containerOpenWidth = 220;
+export const containerOpenWidth = 240;
 export const containerClosedWidth = 64;
 export const navigationOpenWidth = globalOpenWidth + containerOpenWidth;
 export const resizeClosedBreakpoint = globalOpenWidth + (containerOpenWidth / 2);
 export const collapseBreakpoint = globalOpenWidth + containerClosedWidth;
 export const containerNavigationNestedPageSpacing = 8;
-export const searchIconOffset = 68;
-export const createIconOffset = 108;
+export const searchIconOffset = 68 + 12;
+export const createIconOffset = 108 + 12;

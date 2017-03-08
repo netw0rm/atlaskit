66185eb7d5f8b4c116ad5431b43465aac201272b
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-09 09:25:10 +1100

    fix(component): changes export syntax to work with webpack 2
    affects: @atlaskit/pagination, @atlaskit/util-component-template

--- packages/pagination/src/index.js
+++ packages/pagination/src/index.js
@@ -1,2 +1,11 @@
+import Stateful from './stateful';
+import Stateless from './stateless';
+
+export default Stateful;
+export { Stateless as Pagination };
+
+// TODO: Find out why this  was not valid
+/*
 export default from './stateful';
 export { default as Pagination } from './stateless';
+ */
--- packages/util-component-template/src/index.js
+++ packages/util-component-template/src/index.js
@@ -1,2 +1,11 @@
+import Stateful from './stateful';
+import Stateless from './stateless';
+
+export default Stateful;
+export { Stateless as Toggle };
+
+// TODO: Find out why this  was not valid
+/*
 export default from './stateful';
 export { default as Toggle } from './stateless';
+ */

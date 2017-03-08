721fe311bfdec4cb7c10c66035c9f7112eb95596
Ben Wong <benwong@atlassian.com>
2017-03-08 17:33:15 +1100

    fix(component): clicking the warning icon when there is no warning message should focus the field
    affects: @atlaskit/field-base

--- packages/field-base/src/FieldBase.jsx
+++ packages/field-base/src/FieldBase.jsx
@@ -53,9 +53,13 @@ export default class FieldBase extends PureComponent {
 
   renderRightGutter() {
     if (!this.props.isDisabled && this.props.isInvalid) {
+      const iconWrapperClasses = classNames(styles.warningIconWrapper, {
+        [styles.noPointerEvents]: !this.props.invalidMessage,
+      });
+
       return (
         <div
-          className={styles.warningIconWrapper}
+          className={iconWrapperClasses}
           onMouseDown={this.props.onIconMouseDown}
         >
           <WarningIcon label="warning" />

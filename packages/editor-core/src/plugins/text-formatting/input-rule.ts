0069d9a7677d43d2bc0747eeb78f202332718cd3
Eduard Shvedai <eshvedai@atlassian.com>
2017-03-14 10:39:45 +1100

    fix: updated text-formatting input-rules
    affects: @atlaskit/editor-core

--- packages/editor-core/src/plugins/text-formatting/input-rule.ts
+++ packages/editor-core/src/plugins/text-formatting/input-rule.ts
@@ -1,31 +1,34 @@
-import { InputRule, inputRules, Plugin, Schema, Transaction, MarkType, Node } from '../../prosemirror';
+import { InputRule, inputRules, Plugin, Schema, Transaction, MarkType } from '../../prosemirror';
 import { analyticsService } from '../../analytics';
 
 function addMark(markType: MarkType, schema: Schema<any, any>, specialChar: string): Function {
   return (state, match, start, end): Transaction | null => {
-    const from = start;
-    const to = from + match[0].length;
+    const to = end;
+    const from = end - match[1].length + 1;
     const charSize = specialChar.length;
-    const nodes: Node[] = [];
 
-    state.doc.nodesBetween(start, end, (node) => {
-      if (node.isText) {
-        nodes.push(node);
-      }
-    });
-
-    // treat special characters inside marks as plaint text
-    if (nodes.length > 1 && nodes[0].marks.length && (nodes[0].text || '').indexOf(specialChar) > -1) {
+    if (state.schema.marks.code.isInSet(state.doc.resolve(from + 1).marks())) {
       return null;
     }
 
     analyticsService.trackEvent(`atlassian.editor.format.${markType.name}.autoformatting`);
 
-    return state.tr
-      .addMark(from, to, markType.create())
-      .delete(from, from + charSize)
-      .delete(to - charSize * 2, to - charSize)
-      .removeStoredMark(markType);
+    let tr = (
+      state.tr
+        // apply mark
+        .addMark(from, to, markType.create())
+        // delete special characters before the text
+        .delete(from, from + charSize)
+        // deactivate mark
+        .removeStoredMark(markType)
+    );
+
+    if (charSize > 1) {
+      // remove special characters after the text
+      tr = tr.delete(to - charSize * 2 + 1, to - charSize);
+    }
+
+    return tr;
   };
 };
 
@@ -50,7 +53,7 @@ export function inputRulePlugin(schema: Schema<any, any>): Plugin | undefined {
 
   if (schema.marks.strike) {
     // ~~string~~ should strikethrough the text
-    rules.push(new InputRule(/(\~\~([^\*]+)\~\~)$/, addMark(schema.marks.strike, schema, '~~')));
+    rules.push(new InputRule(/(\~\~([^\~]+)\~\~)$/, addMark(schema.marks.strike, schema, '~~')));
   }
 
   if (schema.marks.code) {

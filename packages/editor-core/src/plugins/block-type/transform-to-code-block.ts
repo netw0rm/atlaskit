53a0f2d52a23b12f27b5a43dc9af272850ea70a9
jpuri <jyotipuri@gmail.com>
2017-03-15 17:27:50 +0530

    fix(component): when converting multiple blocks to code block a single code block should get generat
    affects: @atlaskit/editor-core
    
    ISSUES CLOSED: #ED-1053

--- packages/editor-core/src/plugins/block-type/transform-to-code-block.ts
+++ packages/editor-core/src/plugins/block-type/transform-to-code-block.ts
@@ -1,5 +1,5 @@
-import { EditorTransform, ProseMirror } from '../../prosemirror';
-import { isCodeBlockNode } from '../../schema';
+import { EditorTransform, Fragment, ProseMirror, RemoveMarkStep, ReplaceStep, Slice, Step, TextSelection } from '../../prosemirror';
+import { isCodeBlockNode, isHardBreakNode, isMentionNode } from '../../schema';
 
 export default function transformToCodeBlock(pm: ProseMirror): void {
   if (!isConvertableToCodeBlock(pm)) {
@@ -14,10 +14,9 @@ export function transformToCodeBlockAction(pm: ProseMirror, attrs?: any): Editor
   const codeBlock = pm.schema.nodes.code_block;
 
   const where = $from.before($from.depth);
-  const tr = mergeClearContent(pm)
+  const tr = clearMarkupFor(pm, where);
+  return mergeContent(tr, pm.schema.nodes)
     .setNodeType(where, codeBlock, attrs);
-
-  return tr;
 }
 
 export function isConvertableToCodeBlock(pm: ProseMirror): boolean {
@@ -40,10 +39,52 @@ export function isConvertableToCodeBlock(pm: ProseMirror): boolean {
   return parentNode.canReplaceWith(index, index + 1, pm.schema.nodes.code_block);
 }
 
-function mergeClearContent(pm: ProseMirror) {
-  const { tr } = pm;
-  const { text } = pm.schema.nodes;
-  const { from, to } = tr.selection;
+function createSliceWithContent(content: string, pm: ProseMirror) {
+ return new Slice(Fragment.from(pm.schema.nodes.text.create(null, content)), 0, 0);
+}
+
+function clearMarkupFor(pm: ProseMirror, pos: number) {
+  const tr = pm.tr;
+  const node = tr.doc.nodeAt(pos)!;
+  let match = pm.schema.nodes.code_block.contentExpr.start();
+  const delSteps: Step[] = [];
+
+  for (let i = 0, cur = pos + 1; i < node.childCount; i++) {
+    const child = node.child(i);
+    const end = cur + child.nodeSize;
+
+    const allowed = match.matchType(child.type, child.attrs);
+    if (!allowed) {
+      if (isMentionNode(child)) {
+        const content = child.attrs['displayName'];
+        delSteps.push(new ReplaceStep(cur, end, createSliceWithContent(content, pm)));
+      } else if (isHardBreakNode(child)) {
+        const content = '\n';
+        delSteps.push(new ReplaceStep(cur, end, createSliceWithContent(content, pm)));
+      } else {
+        delSteps.push(new ReplaceStep(cur, end, Slice.empty));
+      }
+    } else {
+      match = allowed;
+      for (let j = 0; j < child.marks.length; j++) {
+        if (!match.allowsMark(child.marks[j])) {
+          tr.step(new RemoveMarkStep(cur, end, child.marks[j]));
+        }
+      }
+    }
+    cur = end;
+  }
+
+  for (let i = delSteps.length - 1; i >= 0; i--) {
+    tr.step(delSteps[i]);
+  }
+
+  return tr;
+}
+
+function mergeContent(tr: EditorTransform, nodes: any) {
+  const { text } = nodes;
+  const { from, to, $from, $to } = tr.selection;
   let textContent = '';
   tr.doc.nodesBetween(from, to, (node, pos) => {
     if (node.isTextblock && node.textContent) {
@@ -53,7 +94,10 @@ function mergeClearContent(pm: ProseMirror) {
       textContent += node.textContent;
     }
   });
-  const textNode = text.create({}, textContent);
-  tr.replaceSelection(textNode);
+  if (textContent.length > 0) {
+    const textNode = text.create({}, textContent);
+    tr.setSelection(new TextSelection(tr.doc.resolve($from.start(1)), tr.doc.resolve($to.end(1))));
+    tr.replaceSelection(textNode);
+  }
   return tr;
 }

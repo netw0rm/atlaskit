9bd25704e999a3bc06a53b5364260160a459a5ba
Jimmy Luong <jluong@Jimmys-MBP.home>
2017-03-16 07:52:08 +0000

    chore(stories): load @atlassian/mediaviewer for stories

--- build/storybook/webpack.config.js
+++ build/storybook/webpack.config.js
@@ -1,5 +1,7 @@
 const webpackConfig = require('../webpack/development.js');
 const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (storybookBaseConfig, configType) => { // eslint-disable-line no-unused-vars
   // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
@@ -7,7 +9,31 @@ module.exports = (storybookBaseConfig, configType) => { // eslint-disable-line n
   // 'PRODUCTION' is used when building the static version of storybook.
 
   storybookBaseConfig.devtool = webpackConfig.devtool;
-  storybookBaseConfig.plugins = storybookBaseConfig.plugins.concat(webpackConfig.plugins);
+  storybookBaseConfig.plugins = storybookBaseConfig.plugins
+    .concat(webpackConfig.plugins);
+
+  storybookBaseConfig.plugins = storybookBaseConfig.plugins
+    .concat([
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, '../../node_modules/backbone/backbone.js'),
+          to: 'lib/vendor/backbone.js',
+        },
+        {
+          from: path.join(__dirname, '../../node_modules/jquery/dist/jquery.js'),
+          to: 'lib/vendor/jquery.js',
+        },
+        {
+          from: path.join(__dirname, '../../node_modules/underscore/underscore.js'),
+          to: 'lib/vendor/underscore.js',
+        },
+        {
+          from: path.join(__dirname, '../../node_modules/@atlassian/mediaviewer/lib'),
+          to: 'lib',
+        },
+      ]),
+    ]);
+
   storybookBaseConfig.module.loaders = webpackConfig.module.loaders;
   storybookBaseConfig.postcss = webpackConfig.postcss;
 

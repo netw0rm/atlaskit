d04c5e0d5f20d76c976b67736ef268480ab57148
Huanhuan Huang <hhuang@atlassian.com>
2017-03-20 13:06:54 +1100

    feat(component): added json schema to new prosemirror.
    affects: @atlaskit/editor-core

--- packages/editor-core/stories/example-editor-story.tsx
+++ packages/editor-core/stories/example-editor-story.tsx
@@ -1,15 +1,10 @@
 import { action, storiesOf } from '@kadira/storybook';
 import * as React from 'react';
-import Thenable from 'thenable';
-import * as ajvModule from 'ajv';
 import Editor from './editor';
 import * as styles from './styles';
 import { base64fileconverter } from '../src/test-helper';
-import { highlightBlock } from 'highlight.js';
-import { OrderedMap } from '../src/prosemirror';
-import makeJsonSchema from '../src/schema/json-schema';
-import schema from './schema/schema';
 import { name } from '../package.json';
+import * as v1schema from '../src/json-schema/v1.json';
 
 const { Converter, dropHandler, pasteHandler } = base64fileconverter;
 const CANCEL_ACTION = () => action('Cancel')();
@@ -30,21 +25,6 @@ const imageUploadHandler = (e: any, fn: any) => {
   }
 };
 
-// tslint:disable-next-line:variable-name
-const Ajv = ((ajvModule as any).default || ajvModule);
-const ajv = new Ajv();
-const jsonSchema = makeJsonSchema(schema);
-const validate = ajv.compile(jsonSchema);
-
-function toJS(map: OrderedMap<any>, transform: (value: { [key: string]: any }) => any) {
-  const result: any = {};
-  map.forEach((key, value) => {
-    value['name'] = key;
-    return result[key] = transform(value);
-  });
-  return result;
-}
-
 const jsonPretty = (obj: any) => JSON.stringify(obj, null, 2);
 
 storiesOf(name, module)
@@ -59,93 +39,6 @@ storiesOf(name, module)
       />
     </div>
   ))
-  .add('JSON Schema', () => {
-    interface State {
-      docJson?: any;
-      isValid: boolean | Thenable<boolean>;
-    }
-
-    class Story extends React.PureComponent<{}, State> {
-      state: State = {
-        isValid: true
-      };
-
-      container?: HTMLDivElement;
-      editor?: Element;
-
-      componentDidMount() {
-        const { container } = this.refs;
-        if (container instanceof HTMLElement) {
-          const codes = container.querySelectorAll('code');
-          for (let i = 0; i < codes.length; i++) {
-            highlightBlock(codes[i]);
-          }
-        }
-
-        this.fetchEditorState();
-      }
-
-      render() {
-        return (
-          <div style={{ display: 'flex', flexDirection: 'column' }} ref="container">
-            <fieldset>
-              <legend>ProseMirror schema</legend>
-              <pre><code className="json">{jsonPretty({
-                nodes: toJS(schema.nodeSpec, val => ({
-                  name: val.name,
-                  content: val.content,
-                  defining: val.defining,
-                  group: val.group,
-                })),
-                marks: toJS(schema.markSpec, val => (
-                  {
-                    name: val.name,
-                    excludes: val.excludes
-                  })),
-              })}</code></pre>
-            </fieldset>
-
-            <fieldset>
-              <legend>JSON Schema</legend>
-              <pre><code className="json">{jsonPretty(jsonSchema)}</code></pre>
-            </fieldset>
-
-            <fieldset>
-              <legend>JSON</legend>
-              {this.state.docJson === null ? null :
-                <pre><code className="json">{jsonPretty(this.state.docJson)}</code></pre>
-              }
-              {this.state.isValid ? null :
-                <fieldset>
-                  <legend>Validation errors</legend>
-                  <pre>{jsonPretty(validate.errors)}</pre>
-                </fieldset>
-              }
-            </fieldset>
-          </div>
-        );
-      }
-
-      private fetchEditorState = () => {
-        const { editor } = this.refs;
-        if (editor instanceof Editor) {
-          const { doc } = editor;
-          if (doc) {
-            const docJson = doc.toJSON();
-
-            this.setState({
-              ...this.state,
-              docJson,
-              isValid: validate(docJson),
-            });
-          }
-        }
-      }
-    }
-
-    return (
-      <div style={{ padding: 20 }}>
-        <Story />
-      </div>
-    );
-  });
+  .add('v1 JSON Schema', () => (
+    <pre><code className="json">{jsonPretty(v1schema)}</code></pre>
+  ));

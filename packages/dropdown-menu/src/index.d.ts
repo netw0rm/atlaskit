5d90718e4e1a75b7bed56222009b8248459fe15c
Piotr Plewa <pplewa@atlassian.com>
2017-03-20 04:19:41 +0000

    fix(Typings): add missing dropdown menu typings
    affects: @atlaskit/dropdown-menu
    
    missing dropdown menu typings

--- packages/dropdown-menu/src/index.d.ts
+++ packages/dropdown-menu/src/index.d.ts
@@ -10,8 +10,8 @@ interface Props {
   onOpenChange?: Function;
   position?: string;
   triggerType?: 'default' | 'button';
-  triggerButtonProps: any;
-  shouldFlip: boolean;
+  triggerButtonProps?: any;
+  shouldFlip?: boolean;
 }
 
 interface State {}

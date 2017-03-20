c2e52f6ebf23794546a7cfcc05633581c18f58b0
Dmitrii Sorin <dsorin@atlassian.com>
2017-03-21 09:25:57 +1100

    test: use new API for code-block plugin tests
    affects: @atlaskit/editor-core

--- packages/editor-core/test/plugins/code-block/input-rules.ts
+++ packages/editor-core/test/plugins/code-block/input-rules.ts
@@ -1,44 +1,49 @@
 import * as chai from 'chai';
 import { expect } from 'chai';
-import MarkdownInputRulesPlugin from '../../../src/plugins/inputrules';
+import CodeBlockPlugin from '../../../src/plugins/code-block';
+
 import {
-  a, blockquote, br, chaiPlugin, code_block, doc, em, h1, h2,
-  h3, hr, img, li, makeEditor, code, ol, p, strike, strong, ul, mention
-} from '../../../test-helper';
+  br, chaiPlugin, code_block, doc, fixtures, insertText, li, makeEditor, p, ul
+} from '../../../src/test-helper';
 chai.use(chaiPlugin);
 
 describe('inputrules', () => {
-  const editor = (doc: any) => {
-    const { pm, plugin } = makeEditor({ doc, plugin: MarkdownInputRulesPlugin });
-    return { pm, plugin, sel: pm.doc.refs['<>'] };
-  };
+  const fixture = fixtures();
+  const editor = (doc: any) => makeEditor({
+    doc,
+    plugin: CodeBlockPlugin,
+    place: fixture()
+  });
 
   describe('codeblock rule', () => {
     context('when node is not convertable to code block', () => {
       it('should not convert "```" to a code block\t', () => {
-        const { pm, sel } = editor(doc(ul(li(p('{<>}hello')))));
+        const { editorView, sel } = editor(doc(ul(li(p('{<>}hello')))));
 
-        pm.input.insertText(sel, sel, '```');
-        expect(pm.doc).to.deep.equal(doc(ul(li(p('```hello')))));
+        insertText(editorView, '```', sel);
+
+        expect(editorView.state.doc).to.deep.equal(doc(ul(li(p('```hello')))));
       });
     });
 
     context('when node is convertable to code block', () => {
       context('when converted node has content', () => {
         it('should convert "```" to a code block', () => {
-          const { pm, sel } = editor(doc(p('{<>}hello', br, 'world')));
+          const { editorView, sel } = editor(doc(p('{<>}hello', br, 'world')));
 
-          pm.input.insertText(sel, sel, '```');
-          expect(pm.doc).to.deep.equal(doc(code_block()('hello\nworld')));
+          insertText(editorView, '```', sel);
+
+          expect(editorView.state.doc).to.deep.equal(doc(code_block()('hello\nworld')));
         });
       });
 
       context('when converted node has no content', () => {
         it('should not convert "```" to a code block\t', () => {
-          const { pm, sel } = editor(doc(p('{<>}')));
+          const { editorView, sel } = editor(doc(p('{<>}')));
 
-          pm.input.insertText(sel, sel, '```');
-          expect(pm.doc).to.deep.equal(doc(p('```')));
+          insertText(editorView, '```', sel);
+
+          expect(editorView.state.doc).to.deep.equal(doc(p('```')));
         });
       });
 

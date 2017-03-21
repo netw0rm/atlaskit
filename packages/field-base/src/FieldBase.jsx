78c0e211236cb2000fd7437f97e178d27ff5fca4
Ben Wong <benwong@atlassian.com>
2017-03-21 17:48:45 +1100

    feat(component): add onDialogClick property
    affects: @atlaskit/field-base
    
    Passes the handler to the inline-dialog's onContentClick property

--- packages/field-base/src/FieldBase.jsx
+++ packages/field-base/src/FieldBase.jsx
@@ -25,8 +25,9 @@ export default class FieldBase extends PureComponent {
     onBlur: PropTypes.func.isRequired,
     shouldReset: PropTypes.bool,
     children: PropTypes.node,
-    onDialogFocus: PropTypes.func,
     onDialogBlur: PropTypes.func,
+    onDialogClick: PropTypes.func,
+    onDialogFocus: PropTypes.func,
   }
 
   static defaultProps = {
@@ -44,8 +45,9 @@ export default class FieldBase extends PureComponent {
     isReadOnly: false,
     isRequired: false,
     shouldReset: false,
-    onDialogFocus: () => {},
     onDialogBlur: () => {},
+    onDialogClick: () => {},
+    onDialogFocus: () => {},
   }
 
   componentDidUpdate() {
@@ -96,8 +98,9 @@ export default class FieldBase extends PureComponent {
             isOpen={this.props.isDialogOpen && !!this.props.invalidMessage}
             content={this.props.invalidMessage}
             shouldFlip={['top']}
-            onContentFocus={this.props.onDialogFocus}
             onContentBlur={this.props.onDialogBlur}
+            onContentClick={this.props.onDialogClick}
+            onContentFocus={this.props.onDialogFocus}
           >
             <div
               className={contentClasses}

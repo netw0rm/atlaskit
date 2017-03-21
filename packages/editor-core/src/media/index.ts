04f345c1bcd21577541d27541ebb77a7dd42129d
Artur Bodera <abodera@atlassian.com>
2017-03-21 17:16:34 +1100

    chore(component): update MediaResource interface.
    affects: @atlaskit/editor-core

--- packages/editor-core/src/media/index.ts
+++ packages/editor-core/src/media/index.ts
@@ -1,12 +1,4 @@
-import { MediaCollectionItem, MediaApiConfig } from '@atlaskit/media-core';
-
-export type MediaItemId = string;
-export type MediaCollectionId = string;
-export type MediaViewContextScope = MediaCollectionItem[] | MediaCollectionId;
-
-export type IsScopedToCloudClientId = {
-  clientId: string;
-};
+import { ContextConfig } from '@atlaskit/media-core';
 
 export interface MediaResource {
   /**
@@ -18,11 +10,20 @@ export interface MediaResource {
    *     tokenProvider: tokenProvidingFunction
    *   });
    */
-  getUploadContext(): Promise<MediaApiConfig & IsScopedToCloudClientId>;
+  uploadContext?: () => Promise<ContextConfig>;
 
   /**
-   * Resolve to Media Context that allows downloading Media. Optionally accepts a list of media items or
-   * a Media Collection id which is about to be accessed. Example result:
+   * Resolve to Media Context that allows downloading Media, i.e.
+   *   resolve({
+   *     clientId: 'e3afd8e5-b7d2-4b8d-bff0-ec86e4b14595',
+   *     serviceHost: 'http://media-api.host.com',
+   *     tokenProvider: tokenProvidingFunction
+   *   });
+   */
+  viewContext?: () => Promise<ContextConfig>;
+
+  /**
+   * Resolve to Media Context that allows creating media links:
    *
    *   resolve({
    *     clientId: 'e3afd8e5-b7d2-4b8d-bff0-ec86e4b14595',
@@ -30,7 +31,9 @@ export interface MediaResource {
    *     tokenProvider: tokenProvidingFunction
    *   });
    */
-  getViewContext(scope?: MediaViewContextScope): Promise<MediaApiConfig & IsScopedToCloudClientId>;
+  linkCreateContext?: () => Promise<ContextConfig>;
 };
+
+export { ContextConfig as MediaContextConfig };
 
 export default MediaResource;

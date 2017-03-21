31fa403b4693c57b9a5d2150438ce0f4917447b6
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-21 15:25:20 +1100

    feat(component): selected items will now render their elemBefore content in the select field
    affects: @atlaskit/single-select

--- packages/single-select/src/StatelessSelect.jsx
+++ packages/single-select/src/StatelessSelect.jsx
@@ -419,8 +419,9 @@ export default class StatelessSelect extends PureComponent {
                 {
                   !this.props.hasAutocomplete || this.props.isDisabled ?
                     <div className={styles.content}>
+                      { this.props.selectedItem.elemBefore || null }
                       {
-                        this.props.selectedItem.content ||
+                        <span>{this.props.selectedItem.content}</span> ||
                         <span className={styles.placeholder}>{this.props.placeholder}</span>
                       }
                     </div> :

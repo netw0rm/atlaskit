f6748ea0e64971a42b964c81b057df04cd5b29a6
Sean Curtis <scurtis@atlassian.com>
2017-03-22 13:24:00 +1100

    feat(component): icon sizes changed to be on grid
    affects: @atlaskit/icon
    
    BREAKING CHANGE:
    
    Icon sizes changed. This does not change the default sizes, only the ones when there is a size prop
    
    specified.
    
    ISSUES CLOSED: AK-1515

--- packages/icon/src/internal/size.jsx
+++ packages/icon/src/internal/size.jsx
@@ -1,11 +1,7 @@
 const size = {
-  /** small icon */
   small: 'small',
-  /** medium icon */
   medium: 'medium',
-  /** large icon */
   large: 'large',
-  /** xlarge icon */
   xlarge: 'xlarge',
 };
 

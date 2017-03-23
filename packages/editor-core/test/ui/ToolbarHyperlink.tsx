12275d332f8443fd396297871879fefdeed52cdd
Huanhuan Huang <hhuang@atlassian.com>
2017-03-23 12:21:29 +1100

    chore(component): fixed indent of some files.
    affects: @atlaskit/editor-core

--- packages/editor-core/test/ui/ToolbarHyperlink.tsx
+++ packages/editor-core/test/ui/ToolbarHyperlink.tsx
@@ -6,36 +6,37 @@ import { TextSelection } from '../../src/prosemirror';
 import HyperlinkPlugin from '../../src/plugins/hyperlink';
 import ToolbarHyperlink from '../../src/ui/ToolbarHyperlink';
 import ToolbarButton from '../../src/ui/ToolbarButton';
-import { makeEditor } from '../../src/test-helper';
-import { doc, schema, p } from '../_schema-builder';
 import LinkIcon from '@atlaskit/icon/glyph/editor/link';
+import { doc, p, makeEditor, fixtures } from '../../src/test-helper';
 
 describe('@atlaskit/editor-core/ui/ToolbarHyperlink', () => {
-  const editor = (doc: any) => {
-    const { pm, plugin } = makeEditor({ doc, plugin: HyperlinkPlugin, schema });
-    return { pm, plugin, sel: pm.doc.refs['<>'] };
-  };
+
+  const fixture = fixtures();
+  const editor = (doc: any) => makeEditor({
+    doc,
+    plugin: HyperlinkPlugin,
+    place: fixture()
+  });
 
   it('should trigger showLinkPanel of plugin when toolbar hyperlink button is clicked', () => {
-    const { plugin } = editor(doc(p('text')));
-    const toolbarHyperlink = mount(<ToolbarHyperlink pluginState={plugin}/>);
+    const { pluginState, editorView } = editor(doc(p('text')));
+    const toolbarHyperlink = mount(<ToolbarHyperlink pluginState={pluginState} editorView={editorView}/>);
     const spyFunc = sinon.spy();
-    plugin.showLinkPanel = spyFunc;
+    pluginState.showLinkPanel = spyFunc;
     toolbarHyperlink.find(LinkIcon).simulate('click');
     expect(spyFunc.callCount).to.equal(1);
   });
 
   it('should state variable showToolbarPanel should be true when toolbar hyperlink button is clicked without a selection', () => {
-    const { plugin } = editor(doc(p('text')));
-    const toolbarHyperlink = mount(<ToolbarHyperlink pluginState={plugin}/>);
+    const { pluginState, editorView } = editor(doc(p('text')));
+    const toolbarHyperlink = mount(<ToolbarHyperlink pluginState={pluginState} editorView={editorView}/>);
     toolbarHyperlink.find(LinkIcon).simulate('click');
     expect(toolbarHyperlink.state('showToolbarPanel')).to.be.true;
   });
 
   it('should state variable showToolbarPanel should be false when toolbar hyperlink button is clicked with a selection', () => {
-    const { pm, plugin } = editor(doc(p('testing')));
-    pm.setSelection(new TextSelection(pm.doc.resolve(4), pm.doc.resolve(7)));
-    const toolbarHyperlink = mount(<ToolbarHyperlink pluginState={plugin}/>);
+    const { pluginState, editorView } = editor(doc(p('te{<}stin<}>g')));
+    const toolbarHyperlink = mount(<ToolbarHyperlink pluginState={pluginState} editorView={editorView}/>);
     toolbarHyperlink.find(ToolbarButton).simulate('click');
     expect(toolbarHyperlink.state('showToolbarPanel')).not.to.be.true;
   });

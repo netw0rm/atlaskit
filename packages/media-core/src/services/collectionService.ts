477ee0f641d31be38b3be885d7e078ca93298855
Jimmy Luong <jluong@Jimmys-MacBook-Pro.local>
2017-03-23 23:21:11 +0000

    feat(component): add details support in collections service
    affects: @atlaskit/media-core
    
    BREAKING CHANGE:
    
    CollectionService API changed

--- packages/media-core/src/services/collectionService.ts
+++ packages/media-core/src/services/collectionService.ts
@@ -1,16 +1,34 @@
-import createRequest, {CreateRequestFunc} from './util/createRequest';
-import {MediaApiConfig} from '../';
+import axios from 'axios';
+
+import { MediaApiConfig } from '../config';
+import createRequest, { CreateRequestFunc } from './util/createRequest';
 
 export type SortDirection = 'desc' | 'asc';
 
-export interface RemoteCollectionItem {
-  id: string;
-  occurrenceKey: string;
-  type: string;
-  details: {
-    name: string,
-    size: number
-  };
+export type RemoteCollectionItem = RemoteCollectionFileItem | RemoteCollectionLinkItem;
+
+export interface RemoteCollectionFileItem {
+  readonly id: string;
+  readonly occurrenceKey: string;
+  readonly type: 'file';
+  readonly details: RemoteCollectionFileItemDetails;
+}
+
+export interface RemoteCollectionFileItemDetails {
+  readonly name: string;
+  readonly size: number;
+  readonly mimeType?: string;
+}
+
+export interface RemoteCollectionLinkItem {
+  readonly id: string;
+  readonly occurrenceKey: string;
+  readonly type: 'link';
+  readonly details: RemoteCollectionLinkItemDetails;
+}
+
+export interface RemoteCollectionLinkItemDetails {
+  readonly url: string;
 }
 
 export interface RemoteCollectionItemsResponse {
@@ -21,36 +39,48 @@ export interface RemoteCollectionItemsResponse {
 }
 
 export interface CollectionService {
-  getCollectionItems(inclusiveStartKey: string): Promise<RemoteCollectionItemsResponse>;
-  collectionName: string;
+  getCollectionItems(
+    collectionName: string,
+    limit: number,
+    inclusiveStartKey?: string,
+    sortDirection?: SortDirection,
+    details?: DetailsType): Promise<RemoteCollectionItemsResponse>;
 }
 
+export type DetailsType = 'minimal' | 'full';
+
 export class MediaCollectionService implements CollectionService {
+  static defaultLimit = 10;
 
-  private request: CreateRequestFunc;
-
-  constructor(private config: MediaApiConfig,
-              public collectionName: string,
-              private clientId: string,
-              private limit: number,
-              private sortDirection: SortDirection) {
-    this.request = createRequest({
-      config: this.config,
-      clientId: this.clientId,
-      collectionName: this.collectionName
-    });
+  constructor(private config: MediaApiConfig, private clientId: string) {
   }
 
-  getCollectionItems(inclusiveStartKey: string): Promise<RemoteCollectionItemsResponse> {
-    return this.request({
-      url: `/collection/${this.collectionName}/items`,
-      params: {
-          limit: this.limit,
-          inclusiveStartKey: inclusiveStartKey,
-          sortDirection: this.sortDirection
-      }
-    })
-      .then(json => json as RemoteCollectionItemsResponse)
-    ;
+  getCollectionItems(
+    collectionName: string,
+    limit: number = MediaCollectionService.defaultLimit,
+    inclusiveStartKey?: string,
+    sortDirection?: SortDirection,
+    details?: DetailsType): Promise<RemoteCollectionItemsResponse> {
+
+    const { serviceHost, tokenProvider } = this.config;
+
+    return tokenProvider(collectionName)
+      .then(token => axios
+        .get(`/collection/${collectionName}/items`, {
+          baseURL: serviceHost,
+          headers: {
+            'X-Client-Id': this.clientId,
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          },
+          params: {
+            limit,
+            inclusiveStartKey,
+            sortDirection,
+            details
+          }
+        })
+        .then(response => response.data)
+      );
   }
 }

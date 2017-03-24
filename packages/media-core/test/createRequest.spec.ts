eaf0ca0d600dd77f56592182816f79c08933897d
James Newell <james@atlassian.com>
2017-03-24 12:13:14 +1100

    test(component): added test for token provider
    affects: @atlaskit/media-core

--- packages/media-core/test/createRequest.spec.ts
+++ packages/media-core/test/createRequest.spec.ts
@@ -18,20 +18,22 @@ describe('createRequest()', () => {
   });
 
   it('should send the client ID and auth token', () => {
+    const tokenProvider = sinon.stub().returns(Promise.resolve('ABCD'));
 
     const request = createRequest({
       clientId: '1234',
       config: {
-        tokenProvider: sinon.stub().returns(Promise.resolve('ABCD')),
+        tokenProvider,
         serviceHost: 'http://example.com'
       }
     });
 
     const promise = request({url: '/some-api/links'})
       .then(json => {
+        expect(tokenProvider.calledOnce).to.be.true;
+        expect(fakeResponses[0].url).to.equal('http://example.com/some-api/links');
         expect(fakeResponses[0].requestHeaders['X-Client-Id']).to.equal('1234');
         expect(fakeResponses[0].requestHeaders['Authorization']).to.equal('Bearer ABCD');
-        expect(fakeResponses[0].url).to.equal('http://example.com/some-api/links');
       })
     ;
 

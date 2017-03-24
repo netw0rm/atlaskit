2240a7dd57bbd196c157caf7289592b293175457
Joshua Nelson <jonelson@atlassian.com>
2017-03-24 15:33:44 +1100

    fix(refactor): refactor ContainerLogo to use styled components
    affects: @atlaskit/navigation

---
+++ packages/navigation/src/components/styled/ContainerLogoStyled.jsx
@@ -0,0 +1,7 @@
+import { akGridSizeUnitless } from '@atlaskit/util-shared-styles';
+import styled from 'styled-components';
+
+export default styled.div`
+  height: ${akGridSizeUnitless * 3}px;
+  margin: ${akGridSizeUnitless * 3.5}px 0 ${akGridSizeUnitless * 3.5}px ${akGridSizeUnitless * 3};
+`;

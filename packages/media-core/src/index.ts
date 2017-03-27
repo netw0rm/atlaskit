a6128cd9e0e3e6336a74f1a0b548d3b8ef747d58
James Newell <james@atlassian.com>
2017-03-27 14:58:36 +1100

    feat(component): modified Context.getMediaItemProvider to use a MediaItem if provided
    affects: @atlaskit/media-core
    
    Changed the CollectionService response to be similar to an array of MediaItems. Exported MediaItemProvider and UrlPreviewProvider. Exported new type, MediaItemDetails.
    
    Added tests for
    CollectionProvider.
    
    BREAKING CHANGE:
    
    Changed the CollectionService response to be similar to an array of MediaItems. Typed
    
    processingStatus.
    
    ISSUES CLOSED: FIL-3542

--- packages/media-core/src/index.ts
+++ packages/media-core/src/index.ts
@@ -5,6 +5,8 @@ export * from './mediaTypes';
 export * from './context/context';
 export * from './collection';
 export * from './trello';
+export {MediaItemProvider} from './providers';
+export {UrlPreviewProvider} from './providers/urlPreviewProvider';
 
 export interface Metadata {
   id: string;

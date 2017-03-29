2ba2fee9a2a899ff8d6b0a96899db171ee9580bc
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-29 16:14:06 +1100

    chore(build): build older!

--- build/bin/_get_running_pipelines.js
+++ build/bin/_get_running_pipelines.js
@@ -50,7 +50,7 @@ function getPipelinesResultURL(pipelineUUID) {
 // Related documentation
 // https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/pipelines/%7Bpipeline_uuid%7D/stopPipeline
 function stopPipelineBuild(pipelineUUID) {
-  const stopPipelinesEndpoint = `${pipelinesEndpoint}${pipelineUUID}/stopPipeline`;
+  const stopPipelinesEndpoint = `${pipelinesEndpoint}${pipelineUUID}/stopPipeline/`;
   // we'll return the promise and let it be caught outside
   return axios.post(stopPipelinesEndpoint, axiosRequestConfig);
 }

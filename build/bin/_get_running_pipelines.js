5ac991f51617fddafe3da316ae85a5f6ca33683a
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-29 16:55:24 +1100

    chore(build): build 1

--- build/bin/_get_running_pipelines.js
+++ build/bin/_get_running_pipelines.js
@@ -51,7 +51,7 @@ function getPipelinesResultURL(pipelineUUID) {
 // https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Busername%7D/%7Brepo_slug%7D/pipelines/%7Bpipeline_uuid%7D/stopPipeline
 function stopPipelineBuild(pipelineUUID) {
   // we'll strip the braces from the uuid
-  const safeUUID = pipelineUUID.replace('{', '').replace('}');
+  const safeUUID = pipelineUUID.replace('{', '').replace('}', '');
   const stopPipelinesEndpoint = `${pipelinesEndpoint}${safeUUID}/stopPipeline/`;
   console.log(`Stopping pipline using endpoint ${stopPipelinesEndpoint}`);
   // we'll return the promise and let it be caught outside

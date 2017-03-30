ec5972c2040ff4e129370cbed99406c3fc876ba7
Matias Corbanini <mcorbanini@atlassian.com>
2017-03-31 10:53:11 +1100

    fix: trying to run tests in pipelines without renderer

--- packages/renderer/src/marks/underline.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { PureComponent } from 'react';
-
-export default class Underline extends PureComponent<{}, {}> {
-  render() {
-    return <u>{this.props.children}</u>;
-  }
-}
--- packages/renderer/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const isTextWrapper = (type: string): type is 'textWrapper' => {
-  return type === 'textWrapper';
-};
-
-export const isText = (type: string): type is 'text' => {
-  return type === 'text';
-};

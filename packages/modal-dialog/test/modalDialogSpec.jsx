8db1ff8aae230a5a46bce83c75941e90fdd8eaae
Sean Curtis <scurtis@atlassian.com>
2017-03-31 13:39:22 +1100

    chore(build): unmounting modal-dialog test fixtures
    affects: @atlaskit/modal-dialog

--- packages/modal-dialog/test/modalDialogSpec.jsx
+++ packages/modal-dialog/test/modalDialogSpec.jsx
@@ -7,22 +7,29 @@ import styles from '../src/style.less';
 describe('ak-modal-dialog', () => {
   describe('exports', () => {
     it('should export a base component', () => {
-      shallow(<ModalDialog />).should.be.an.instanceof(Object);
+      const wrapper = shallow(<ModalDialog />);
+      wrapper.should.be.an.instanceof(Object);
+      wrapper.unmount();
     });
   });
 
   describe('props', () => {
     describe('isOpen', () => {
       it('should be hidden by default', () => {
-        shallow(<ModalDialog />).text().should.be.equal('');
+        const wrapper = shallow(<ModalDialog />);
+        wrapper.text().should.be.equal('');
+        wrapper.unmount();
       });
       it('should be visible when open = true', () => {
-        shallow(<ModalDialog isOpen />).text().should.not.equal('');
+        const wrapper = shallow(<ModalDialog isOpen />);
+        wrapper.text().should.not.equal('');
+        wrapper.unmount();
       });
       it('should become hidden when open changed from true -> false', () => {
         const wrapper = shallow(<ModalDialog isOpen />);
         wrapper.setProps({ isOpen: false });
         wrapper.text().should.be.equal('');
+        wrapper.unmount();
       });
     });
 
@@ -32,43 +39,40 @@ describe('ak-modal-dialog', () => {
       const hasClass = (wrapper, className) => wrapper.find(`.${modalPositioner}`).hasClass(className);
 
       it('should be "medium" by default', () => {
-        hasClass(shallow(<ModalDialog isOpen />), medium).should.equal(true);
+        const wrapper = shallow(<ModalDialog isOpen />);
+        hasClass(wrapper, medium).should.equal(true);
+        wrapper.unmount();
       });
 
       it('should support a custom pixel width as string', () => {
-        expect(shallow(
-          <ModalDialog
-            isOpen
-            width="300px"
-          />
-        ).find(`.${modalPositioner}`).props().style).to.deep.equal({ width: '300px' });
+        const wrapper = shallow(<ModalDialog isOpen width="300px" />);
+        expect(wrapper.find(`.${modalPositioner}`).props().style).to.deep.equal({ width: '300px' });
+        wrapper.unmount();
       });
 
       it('should support a custom pixel width as string', () => {
-        expect(shallow(
-          <ModalDialog
-            isOpen
-            width="75%"
-          />
-        ).find(`.${modalPositioner}`).props().style).to.deep.equal({ width: '75%' });
+        const wrapper = shallow(<ModalDialog isOpen width="75%" />);
+        expect(wrapper.find(`.${modalPositioner}`).props().style).to.deep.equal({ width: '75%' });
+        wrapper.unmount();
       });
 
       it('should support a custom integer width', () => {
-        expect(shallow(
-          <ModalDialog
-            isOpen
-            width={300}
-          />
-        ).find(`.${modalPositioner}`).props().style).to.deep.equal({ width: 300 });
+        const wrapper = shallow(<ModalDialog isOpen width={300} />);
+        expect(wrapper.find(`.${modalPositioner}`).props().style).to.deep.equal({ width: 300 });
+        wrapper.unmount();
       });
 
       allowedWidths.forEach((width) => {
         it(`width = "${width}" is applied uniquely`, () => {
-          hasClass(shallow(<ModalDialog isOpen width={width} />), locals[width]).should.equal(true);
+          const wrapper = shallow(<ModalDialog isOpen width={width} />);
+          hasClass(wrapper, locals[width]).should.equal(true);
+          wrapper.unmount();
 
           // Check that other widths aren't applied
           allowedWidths.filter(w => w !== width).forEach((otherWidth) => {
-            hasClass(shallow(<ModalDialog isOpen width={width} />), otherWidth).should.equal(false);
+            const wrapper2 = shallow(<ModalDialog isOpen width={width} />);
+            hasClass(wrapper, otherWidth).should.equal(false);
+            wrapper2.unmount();
           });
         });
       });
@@ -76,37 +80,43 @@ describe('ak-modal-dialog', () => {
 
     describe('header', () => {
       it('should render when set', () => {
-        shallow(
+        const wrapper = shallow(
           <ModalDialog
             header={
               <span>My header</span>
             }
             isOpen
           />
-        ).contains(<span>My header</span>).should.be.equal(true);
+        );
+        wrapper.contains(<span>My header</span>).should.be.equal(true);
+        wrapper.unmount();
       });
     });
 
     describe('footer', () => {
       it('should render when set', () => {
-        shallow(
+        const wrapper = shallow(
           <ModalDialog
             footer={
               <span>My footer</span>
             }
             isOpen
           />
-        ).contains(<span>My footer</span>).should.be.equal(true);
+        );
+        wrapper.contains(<span>My footer</span>).should.be.equal(true);
+        wrapper.unmount();
       });
     });
 
     describe('children', () => {
       it('should render when set', () => {
-        shallow(
+        const wrapper = shallow(
           <ModalDialog isOpen>
             <form>This is <strong>my</strong> form</form>
           </ModalDialog>
-        ).contains(<form>This is <strong>my</strong> form</form>).should.be.equal(true);
+        );
+        wrapper.contains(<form>This is <strong>my</strong> form</form>).should.be.equal(true);
+        wrapper.unmount();
       });
     });
 
@@ -116,6 +126,7 @@ describe('ak-modal-dialog', () => {
         const wrapper = mount(<ModalDialog isOpen onDialogDismissed={spy} />);
         wrapper.find(`.${styles.locals.modalWrapper}`).children().first().simulate('click');
         expect(spy.callCount).to.equal(1);
+        wrapper.unmount();
       });
 
       it('should trigger when blanket clicked below dialog (modalPositioner)', () => {
@@ -123,6 +134,7 @@ describe('ak-modal-dialog', () => {
         const wrapper = mount(<ModalDialog isOpen onDialogDismissed={spy} />);
         wrapper.find(`.${styles.locals.modalPositioner}`).simulate('click');
         expect(spy.callCount).to.equal(1);
+        wrapper.unmount();
       });
 
       it('should not trigger when blanket content clicked', () => {
@@ -134,6 +146,7 @@ describe('ak-modal-dialog', () => {
         );
         wrapper.find('.my-content').simulate('click');
         expect(spy.callCount).to.equal(0);
+        wrapper.unmount();
       });
     });
   });
@@ -143,18 +156,21 @@ describe('ak-modal-dialog', () => {
       const wrapper = mount(<ModalDialog isOpen footer="Footer" />);
       expect(wrapper.find(`.${styles.locals.withoutHeader}`).length).to.equal(1);
       expect(wrapper.find(`.${styles.locals.withoutFooter}`).length).to.equal(0);
+      wrapper.unmount();
     });
 
     it('should be rounded on bottom only when footer omitted but header supplied', () => {
       const wrapper = mount(<ModalDialog isOpen header="Header" />);
       expect(wrapper.find(`.${styles.locals.withoutHeader}`).length).to.equal(0);
       expect(wrapper.find(`.${styles.locals.withoutFooter}`).length).to.equal(1);
+      wrapper.unmount();
     });
 
     it('should be rounded on top + bottom when header and footer omitted', () => {
       const wrapper = mount(<ModalDialog isOpen />);
       expect(wrapper.find(`.${styles.locals.withoutHeader}`).length).to.equal(1);
       expect(wrapper.find(`.${styles.locals.withoutFooter}`).length).to.equal(1);
+      wrapper.unmount();
     });
   });
 
@@ -164,6 +180,7 @@ describe('ak-modal-dialog', () => {
       expect(wrapper.find(`.${styles.locals.withHeader}`).length).to.equal(0);
       wrapper.setProps({ header: 'Header' });
       expect(wrapper.find(`.${styles.locals.withHeader}`).length).to.equal(1);
+      wrapper.unmount();
     });
 
     it('should enable footer keyline only when footer provided', () => {
@@ -171,6 +188,7 @@ describe('ak-modal-dialog', () => {
       expect(wrapper.find(`.${styles.locals.withFooter}`).length).to.equal(0);
       wrapper.setProps({ footer: 'Header' });
       expect(wrapper.find(`.${styles.locals.withFooter}`).length).to.equal(1);
+      wrapper.unmount();
     });
   });
 });

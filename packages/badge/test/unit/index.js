bb21061f2954b5e1f52bebce224bc4c710bf0596
Luke Batchelor <lbatchelor@atlassian.com>
2017-03-31 16:50:15 +1100

    test(component): fixes tests in badge to be able to be tested in mocha with jsdocm
    affects: @atlaskit/badge

--- packages/badge/test/unit/index.js
+++ packages/badge/test/unit/index.js
@@ -1,27 +1,27 @@
 import { mount } from 'enzyme';
 import React from 'react';
 
-import AkBadge from '../src';
-import { APPEARANCE_ENUM } from '../src/Badge';
+import AkBadge from '../../src';
+import { APPEARANCE_ENUM } from '../../src/Badge';
 
 describe('ak-badge', () => {
   describe('value property', () => {
     it('should be visibly displayed', () => {
-      mount(<AkBadge value={5} />).text().should.equal('5');
+      expect(mount(<AkBadge value={5} />).text()).to.equal('5');
     });
     it('should only accept positive numbers', () => {
-      mount(<AkBadge value={-5} />).text().should.equal('0');
+      expect(mount(<AkBadge value={-5} />).text()).to.equal('0');
     });
     it('should show Infinity as the ∞ character', () => {
-      mount(<AkBadge value={Infinity} max={Infinity} />).text().should.equal('∞');
+      expect(mount(<AkBadge value={Infinity} max={Infinity} />).text()).to.equal('∞');
     });
     it('should trigger onValueUpdated when value prop changed with a number', (done) => {
       const badge = mount(
         <AkBadge
           value={1}
           onValueUpdated={(detail) => {
-            detail.oldValue.should.equal(1);
-            detail.newValue.should.equal(20);
+            expect(detail.oldValue).to.equal(1);
+            expect(detail.newValue).to.equal(20);
             done();
           }}
         />
@@ -31,39 +31,38 @@ describe('ak-badge', () => {
   });
   describe('max property', () => {
     it('should constrain to 99+ when not specified', () => {
-      mount(<AkBadge value={101} />).text().should.equal('99+');
+      expect(mount(<AkBadge value={101} />).text()).to.equal('99+');
     });
     it('should constrain the value when set', () => {
-      mount(<AkBadge value={200} max={100} />).text().should.equal('100+');
+      expect(mount(<AkBadge value={200} max={100} />).text()).to.equal('100+');
     });
     it('should pass the value through when max === 0', () => {
-      mount(<AkBadge value={Number.MAX_VALUE} max={0} />)
-      .text().should.equal(`${Number.MAX_VALUE}`);
+      expect(mount(<AkBadge value={Number.MAX_VALUE} max={0} />).text())
+        .to.equal(`${Number.MAX_VALUE}`);
     });
     it('should not constrain if equal to value', () => {
-      mount(<AkBadge value={200} max={200} />).text().should.equal('200');
+      expect(mount(<AkBadge value={200} max={200} />).text()).to.equal('200');
     });
   });
   describe('appearance property', () => {
     it('should be "default" when not set', () => {
-      mount(<AkBadge />).prop('appearance').should.equal('default');
+      expect(mount(<AkBadge />).prop('appearance')).to.equal('default');
     });
     it('should change when set to an approved value', () => {
       APPEARANCE_ENUM.values.forEach((value) => {
-        mount(<AkBadge appearance={value} />).prop('appearance').should.equal(value);
+        expect(mount(<AkBadge appearance={value} />).prop('appearance')).to.equal(value);
       });
     });
     it('should revert to "default" when set to an invalid value', () => {
-      mount(<AkBadge appearance="foo" />).getNode().validAppearance()
-        .should.equal('default');
+      expect(mount(<AkBadge appearance="foo" />).getNode().validAppearance()).to.equal('default');
     });
   });
   describe('theme property', () => {
     it('should not have dark theme when not set', () => {
-      mount(<AkBadge />).prop('theme').should.equal('default');
+      expect(mount(<AkBadge />).prop('theme')).to.equal('default');
     });
     it('should apply the dark theme class when the theme is dark', () => {
-      mount(<AkBadge theme="dark" />).prop('theme').should.equal('dark');
+      expect(mount(<AkBadge theme="dark" />).prop('theme')).to.equal('dark');
     });
   });
 });

a4438583f1c7fb2a3bf3d59f0ecb158aa0cf0257
Matias Corbanini <mcorbanini@atlassian.com>
2017-03-31 11:32:51 +1100

    fix(component): fix failing tests on pipelines by using unmount
    affects: @atlaskit/renderer

--- packages/renderer/test/marks/linkSpec.tsx
+++ packages/renderer/test/marks/linkSpec.tsx
@@ -4,14 +4,21 @@ import { expect } from 'chai';
 import Link from '../../src/marks/link';
 
 describe('<Link />', () => {
-  const mark = mount(<Link href="https://www.atlassian.com">This is a link</Link>);
+  let mark;
 
+  beforeEach(() => {
+    mark = mount(<Link href="https://www.atlassian.com">This is a link</Link>);
+  });
   it('should wrap content with <a>-tag', () => {
     expect(mark.find('a').length).to.equal(1);
   });
 
   it('should set href to attrs.href', () => {
     expect(mark.find('a').props()).to.have.property('href', 'https://www.atlassian.com');
+  });
+
+  afterEach(() => {
+    mark.unmount();
   });
 
 });

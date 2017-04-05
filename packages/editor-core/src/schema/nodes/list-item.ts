478840bdc8a98e63bc91a3a39a13b264bce09fc2
jpuri <jyotipuri@gmail.com>
2017-04-05 10:58:21 +0530

    fix(component): align margins of paragraphs inside lists to list item margins.
    affects: @atlaskit/editor-core

--- packages/editor-core/src/schema/nodes/list-item.ts
+++ packages/editor-core/src/schema/nodes/list-item.ts
@@ -1,9 +1,18 @@
 import { NodeSpec } from '../../prosemirror';
+import { style } from 'typestyle';
+
+const listStyle = style({
+  $nest: {
+    '> p:not(:first-child)': {
+      margin: '4px 0 0 0',
+    },
+  }
+});
 
 export const listItem: NodeSpec = {
   content: 'paragraph block*',
   parseDOM: [{ tag: 'li' }],
   toDOM() {
-    return ['li', 0];
+    return ['li', { class: listStyle }, 0];
   }
 };

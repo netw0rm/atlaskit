03c550f45ee86115fc23ad78c32050ed7e3663b5
Jed Watson <jed.watson@me.com>
2017-04-05 16:52:29 +1000

    feat(component): improve dynamic propType rendering with recursive pretty-printing
    affects: @atlaskit/util-readme

--- packages/util-readme/test/DynamicPropsSpec.jsx
+++ packages/util-readme/test/DynamicPropsSpec.jsx
@@ -29,6 +29,6 @@ export default class Foo extends React.PureComponent {
 }`;
     const wrapper = mount(<DynamicProps componentSrc={componentSrc} />);
     expect(wrapper.text()).to.not.contain('There are no props for this component');
-    expect(wrapper.find('table')).to.have.length(1);
+    expect(wrapper.find('h3').text()).to.contain('foo');
   });
 });

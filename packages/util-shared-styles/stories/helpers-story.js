ab02cb2030c2643d99de6c0fe696f6bc04eedaa1
jossmac <joss.mackison@gmail.com>
2017-04-05 12:01:03 +1000

    refactor(stories): generate available utils on a helper mixin, rather than hand code
    affects: @atlaskit/util-shared-styles

--- packages/util-shared-styles/stories/helpers-story.js
+++ packages/util-shared-styles/stories/helpers-story.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { storiesOf } from '@kadira/storybook';
 import { Chrome, Code, Description } from '@atlaskit/util-readme';
+import { akHelperMixins } from '@atlaskit/util-shared-styles';
 import { name } from '../package.json';
 
 const H3 = styled.h3`
@@ -15,34 +16,43 @@ import TextExample from './examples/helpers/text';
 import TextExampleRaw from '!raw!./examples/helpers/text';
 /* eslint-enable import/first, import/no-duplicates */
 
-storiesOf(name, module)
-  .add('Mixin: Helpers', () => (
-    <Chrome>
-      <h1>Helpers</h1>
-      <Description>
-        <H3>Focus Ring</H3>
-        <p>The focus ring helper exposes three utilities:</p>
-        <ul>
-          <li><code>default</code> Constant</li>
-          <li><code>none</code> Constant</li>
-          <li><code>generate</code> Function</li>
-        </ul>
-        <p>Below are some possible implementations using the focus ring helper mixin.</p>
-      </Description>
-      <Code code={FocusRingExampleRaw}>
-        {FocusRingExample}
-      </Code>
+function generateUtilList(n) {
+  return Object
+    .keys(akHelperMixins[n])
+    .map((u) => {
+      const util = akHelperMixins[n][u];
+      const type = Array.isArray(util) ? 'constant' : (typeof util);
+      return <li key={u}><code>{u}</code> {type}</li>;
+    });
+}
 
-      <Description>
-        <H3>Text</H3>
-        <p>The text helper exposes one utility:</p>
-        <ul>
-          <li><code>truncate</code> Function</li>
-        </ul>
-        <p>Below are some possible implementations using the text helper mixin.</p>
-      </Description>
-      <Code code={TextExampleRaw}>
-        {TextExample}
-      </Code>
-    </Chrome>
-  ));
+storiesOf(name, module)
+  .add('Mixin: Helpers', () => {
+    const focusRingUtils = generateUtilList('focusRing');
+    const textUtils = generateUtilList('text');
+
+    return (
+      <Chrome>
+        <h1>Helpers</h1>
+        <Description>
+          <H3>Focus Ring</H3>
+          <p>The focus ring helper exposes {focusRingUtils.length} utilities:</p>
+          <ul>{focusRingUtils}</ul>
+          <p>Below are some possible implementations using the focus ring helper mixin.</p>
+        </Description>
+        <Code code={FocusRingExampleRaw}>
+          {FocusRingExample}
+        </Code>
+
+        <Description>
+          <H3>Text</H3>
+          <p>The text helper exposes {textUtils.length} utility:</p>
+          <ul>{textUtils}</ul>
+          <p>Below are some possible implementations using the text helper mixin.</p>
+        </Description>
+        <Code code={TextExampleRaw}>
+          {TextExample}
+        </Code>
+      </Chrome>
+    );
+  });

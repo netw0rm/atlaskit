c84f64a0cf6bd614e1e243f1d809cb88e80b5ea3
Joshua Nelson <jonelson@atlassian.com>
2017-04-05 02:04:40 -0700

    chore(refactor): partially refactor nested navigation to styled components
    affects: @atlaskit/navigation

---
+++ packages/navigation/src/components/styled/ContainerNavigationNestedPageWrapper.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import {
+    akGridSizeUnitless,
+ } from '@atlaskit/util-shared-styles';
+
+export default styled.div`
+  flex: 1 0 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+  padding: 0 ${akGridSizeUnitless}px;
+`;

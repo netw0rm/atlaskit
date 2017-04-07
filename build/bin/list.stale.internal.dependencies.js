075b39663d463f155a660cd48462e19b09589ec9
Luke Batchelor <lbatchelor@atlassian.com>
2017-04-07 12:12:00 +1000

    chore(build): adds --dev flag to list.stale.dependencies script to list dev dependencies

--- build/bin/list.stale.internal.dependencies.js
+++ build/bin/list.stale.internal.dependencies.js
@@ -9,6 +9,8 @@ const chalk = require('chalk');
   This script is for finding packages that depend on non-latest versions of other internal packages.
 
   It is intended to be run adhoc to give a quick view of potential issues.
+
+  Pass the `--dev` flag to list stale dev dependencies instead
 */
 
 function getAllPackageJsons() {
@@ -27,11 +29,11 @@ function getLocalDep(packageJsons, packageName) {
   return packageJsons.find(pkg => pkg.name === packageName);
 }
 
-function getPackagesWithStaleDeps(packageJsons) {
+function getPackagesWithStaleDeps(packageJsons, depsToCheck) {
   const packagesWithStaleDeps = [];
 
   packageJsons.forEach((pkgJson) => {
-    const deps = pkgJson.dependencies;
+    const deps = pkgJson[depsToCheck] || {};
     const staleDeps = [];
     Object.keys(deps).forEach((depName) => {
       const localDep = getLocalDep(packageJsons, depName);
@@ -56,11 +58,12 @@ function getPackagesWithStaleDeps(packageJsons) {
   return packagesWithStaleDeps;
 }
 
+const depsToCheck = process.argv[2] === '--dev' ? 'devDependencies' : 'dependencies';
 const packageJsons = getAllPackageJsons();
-const packagesWithStaleDeps = getPackagesWithStaleDeps(packageJsons);
+const packagesWithStaleDeps = getPackagesWithStaleDeps(packageJsons, depsToCheck);
 
 if (packagesWithStaleDeps.length > 0) {
-  console.log('The following packages have stale local dependencies');
+  console.log(`The following packages have stale local ${depsToCheck}`);
 
   packagesWithStaleDeps
     // sort them so that worst offenders are listed first
@@ -71,7 +74,7 @@ if (packagesWithStaleDeps.length > 0) {
         .forEach(dep => console.log(`  - ${chalk.yellow(dep.name)}:${chalk.red(dep.expectedVersion)} (latest: ${chalk.green(dep.localVersion)})`));
     });
 } else {
-  console.log(chalk.green('All local dependencies are up to date!'));
+  console.log(chalk.green(`All local ${depsToCheck} are up to date!`));
 }
 
 /* eslint-enable no-console */

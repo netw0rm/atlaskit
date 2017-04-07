0706c349cebc9a40b972b4f91a9c95ff24b875bf
Vladimir Vinogradov <vvinogradov@atlassian.com>
2017-04-07 10:46:02 +1000

    WIP(component): initial component for predefined avatar view
    affects: @atlaskit/media-avatar-picker

---
+++ packages/media-avatar-picker/src/predefinedAvatarView/index.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import {PureComponent} from 'react';
+
+import {CardImageView} from '@atlaskit/media-card';
+import {PredefinedAvatarViewWrapper} from './styled';
+import {Avatar} from '../avatarList/index';
+
+import ArrowLeftIcon from '@atlaskit/icon/glyph/arrow-left';
+import Button from '@atlaskit/button';
+
+export interface BackBtnProps {
+  onClick?: () => void;
+}
+
+class BackBtn extends PureComponent<BackBtnProps, {}> {
+  render() {
+    return (
+      <Button
+        className="back-button"
+        iconAfter={<ArrowLeftIcon label="" />}
+        onClick={this.props.onClick}
+      />
+    );
+  }
+}
+
+export interface PredefinedAvatarViewProps {
+  avatars: Array<Avatar>;
+  onGoBack?: () => void;
+  onAvatarSelected?: (avatar: Avatar) => void;
+}
+
+const DEFAULT_AVATAR_DIMENSIONS = {
+  height: 76,
+  width: 76,
+};
+
+export default class PredefinedAvatarView extends PureComponent<PredefinedAvatarViewProps, {}> {
+  static defaultProps = {
+    avatars: []
+  };
+
+  render() {
+    const {avatars} = this.props;
+
+    const cards = avatars.map(
+      (avatar, idx) => {
+        const elementKey = `predefined-avatar-${idx}`;
+        return (<li key={elementKey}>
+          <CardImageView
+            mediaType="image"
+            dataURI={avatar.dataURI}
+            dimensions={DEFAULT_AVATAR_DIMENSIONS}
+            onClick={this.createOnItemClickHandler(avatar)}
+          />
+        </li>);
+      }
+    );
+
+    return (
+      <PredefinedAvatarViewWrapper>
+        <div className="header">
+          <BackBtn onClick={this.props.onGoBack} /><div className="description">Default avatars</div>
+        </div>
+        <ul>
+          {cards}
+        </ul>
+      </PredefinedAvatarViewWrapper>
+    );
+  }
+
+  createOnItemClickHandler(avatar: Avatar): (event: Event) => void {
+    return () => {
+      const { onAvatarSelected } = this.props;
+      if (onAvatarSelected) {
+        onAvatarSelected(avatar);
+      }
+    };
+  }
+}
--- packages/media-avatar-picker/stories/predefinedAvatarList/predefined-avatars-story.tsx
+++ packages/media-avatar-picker/stories/predefinedAvatarList/predefined-avatars-story.tsx
@@ -1,15 +1,25 @@
 import {storiesOf} from '@kadira/storybook';
 import * as React from 'react';
 
-import {CardImageView} from '@atlaskit/media-card';
-
 import AvatarList, {Avatar} from '../../src/avatarList';
 import PredefinedAvatarList from '../../src/predefinedAvatarList';
+import PredefinedAvatarView from '../../src/predefinedAvatarView/index';
 
-const avatarIds = [18831, 18832, 18833, 18834, 18835];
-const avatars: Array<Avatar> = avatarIds.map(id => ({
-  dataURI: `https://jdog.jira-dev.com/secure/viewavatar?avatarId=${id}&avatarType=project`,
-}));
+function generateAvatarIds(start: number, count: number): Array<number> {
+  const result: Array<number> = [];
+  for (let i = 0; i < count; ++i) {
+    result[i] = start + i;
+  }
+  return result;
+}
+
+function generateAvatars(count: number) {
+  return generateAvatarIds(18831, count).map(id => ({
+    dataURI: `https://jdog.jira-dev.com/secure/viewavatar?avatarId=${id}&avatarType=project`,
+  }));
+}
+
+const avatars: Array<Avatar> = generateAvatars(5);
 
 storiesOf('Predefined Avatars', {})
   .add('Avatar List', () => (
@@ -26,12 +36,10 @@ storiesOf('Predefined Avatars', {})
       selectedAvatar={avatars[2]}
     />);
   })
-  .add('Card Experiment', () => (
-    <CardImageView
-      dataURI="https://jdog.jira-dev.com/secure/viewavatar?avatarId=18832&avatarType=project"
-      mediaType="image"
-      selectable
-      selected
-      dimensions={{height: 50, width: 50}}
-    />
-  ));
+  .add('Predefined Avatar View', () => {
+    return (
+      <PredefinedAvatarView
+        avatars={generateAvatars(25)}
+      />
+    );
+  });

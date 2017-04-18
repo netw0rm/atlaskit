971d2a20464ad171057ed4f250ebeba0ed8317ce
ckrishnakumar <ckrishnakumar@atlassian.com>
2017-04-18 10:34:27 +1000

    chore(build): add coverage support on browserstack tests

--- build/webpack/karma.js
+++ build/webpack/karma.js
@@ -3,4 +3,12 @@ const standardConfig = require('./development.js');
 // We delete the entry from the normal config and let karma insert it for us
 delete standardConfig.entry;
 
+standardConfig.module.postLoaders = [
+  {
+    test: /^((?!\.spec\.ts).)*.ts$/,
+    exclude: /(node_modules|tests)/,
+    loader: 'istanbul-instrumenter-loader', // used to to get the code coverage for TypeScript
+  },
+];
+
 module.exports = standardConfig;

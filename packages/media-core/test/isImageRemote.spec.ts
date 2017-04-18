5536b7a7a813a20ca9e8b6956ce353eae71c8e69
Vadim Kazakov <vkazakov@atlassian.com>
2017-04-18 18:29:28 +1000

    feat(component): add utility function to detect if we need to specify crossorigin property for img e
    affects: @atlaskit/media-core

---
+++ packages/media-core/test/isImageRemote.spec.ts
@@ -0,0 +1,34 @@
+import * as chai from 'chai';
+import {isImageRemote} from '../src';
+const expect = chai.expect;
+
+describe('isImageRemote', () => {
+  it('should return false for local resource', () => {
+    const localUrl = 'data:image/png;base64,iVBORw0KGgoAAHwAAAABJRU5ErkJggg==';
+    expect(isImageRemote(localUrl)).to.be.false;
+  });
+
+  it('should retun false for same host, port and protocol', () => {
+    const windowOrigin = 'https://www.atlassian.com:1234';
+    const url = 'https://www.atlassian.com:1234/assets/images/image.png';
+    expect(isImageRemote(url, windowOrigin)).to.be.false;
+  });
+
+  it('should return true for the same host, port, but different protocols', () => {
+    const windowOrigin = 'https://www.atlassian.com:1234';
+    const url = 'http://www.atlassian.com:1234/assets/images/image.png';
+    expect(isImageRemote(url, windowOrigin)).to.be.true;
+  });
+
+  it('should return true for the same host, protocol, but different ports', () => {
+    const windowOrigin = 'https://www.atlassian.com:1234';
+    const url = 'https://www.atlassian.com:1254/assets/images/image.png';
+    expect(isImageRemote(url, windowOrigin)).to.be.true;
+  });
+
+  it('should return true for the same port, protocol, but different hosts', () => {
+    const windowOrigin = 'https://www.atlassian.com:1234';
+    const url = 'https://www.atlassian.io:1234/assets/images/image.png';
+    expect(isImageRemote(url, windowOrigin)).to.be.true;
+  });
+});

9cd2b346175cefcaeadda09a62fdeda9034929b9
James Newell <jnewell@atlassian.com>
2017-04-19 10:28:19 +1000

    fix(component): fix example media item names and add collectionName (otherwise they don't work)
    affects: @atlaskit/media-test-helpers

--- packages/media-test-helpers/src/exampleMediaItems.ts
+++ packages/media-test-helpers/src/exampleMediaItems.ts
@@ -1,5 +1,5 @@
 import {MediaItemType} from '@atlaskit/media-core';
-import {defaultCollectionName} from './collectionNames';
+import {defaultCollectionName as collectionName} from './collectionNames';
 
 const fileType: MediaItemType = 'file';
 const linkType: MediaItemType = 'link';
@@ -38,34 +38,39 @@ export const unknownUrlPreviewId = {
 
 // === LINK ===
 
-export const genericLinkIdentifier = {
+export const genericLinkId = {
   id: '517dade5-5489-4e8c-a113-42319878c4a5',
-  mediaItemType: linkType
+  mediaItemType: linkType,
+  collectionName
 };
 
-export const spotifyLinkIdentifier = {
+export const spotifyLinkId = {
   id: 'b3205cf6-7c26-4208-a1a2-50488bffa973',
-  mediaItemType: linkType
+  mediaItemType: linkType,
+  collectionName
 };
 
-export const youtubeLinkIdentifier = {
+export const youtubeLinkId = {
   id: '5c024c78-8dee-4a6c-9967-6276abd80286',
-  mediaItemType: linkType
+  mediaItemType: linkType,
+  collectionName
 };
 
-export const trelloLinkIdentifier = {
+export const trelloLinkId = {
   id: '70997118-3b5a-49f0-8608-43f6e0b9796e',
-  mediaItemType: linkType
+  mediaItemType: linkType,
+  collectionName
 };
 
-export const twitterLinkIdentifier = {
+export const twitterLinkId = {
   id: 'c3e22950-99b2-4f6e-9696-26117310741f',
   mediaItemType: linkType
 };
 
-export const linkPlayerIdentifier = {
+export const linkPlayerId = {
   id: 'f0e80555-cf97-44ae-afef-9cbfae8c73c7',
-  mediaItemType: linkType
+  mediaItemType: linkType,
+  collectionName
 };
 
 // === FILE ===
@@ -73,29 +78,29 @@ export const linkPlayerIdentifier = {
 export const genericFileId = {
   id: '2dfcc12d-04d7-46e7-9fdf-3715ff00ba40',
   mediaItemType: fileType,
-  collectionName: defaultCollectionName
+  collectionName
 };
 
 export const videoFileId = {
   id: '1b01a476-83b4-4f44-8192-f83b2d00913a', // mp4 video
   mediaItemType: fileType,
-  collectionName: defaultCollectionName
+  collectionName
 };
 
 export const imageFileId = {
   id: '5556346b-b081-482b-bc4a-4faca8ecd2de', // jpg image
   mediaItemType: fileType,
-  collectionName: defaultCollectionName
+  collectionName
 };
 
 export const docFileId = {
   id: '71cd7e7d-4e86-4b89-a0b4-7f6ffe013c94',
   mediaItemType: fileType,
-  collectionName: defaultCollectionName
+  collectionName
 };
 
 export const unknownFileId = {
   id: 'e0652e68-c596-4800-8a91-1920e6b8a585',
   mediaItemType: fileType,
-  collectionName: defaultCollectionName
+  collectionName
 };

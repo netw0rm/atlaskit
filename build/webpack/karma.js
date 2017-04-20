853ef46702bd8bc25b9cd843c72d5c85390aadd0
ckrishnakumar <ckrishnakumar@atlassian.com>
2017-04-20 15:05:40 +1000

    fix(build): regex - avoid test files in coverage

--- build/webpack/karma.js
+++ build/webpack/karma.js
@@ -5,7 +5,7 @@ delete standardConfig.entry;
 
 standardConfig.module.postLoaders = [
   {
-    test: /^*.ts$/,
+    test: /^((?!\.spec\.ts).)*.ts$/,
     exclude: /(node_modules|tests)/,
     loader: 'istanbul-instrumenter-loader', // used to to get the code coverage for TypeScript
   },

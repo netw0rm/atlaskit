6444c8b6a191d35a02c74c74b09d8ed0a2291c17
Luke Batchelor <lbatchelor@atlassian.com>
2017-04-26 11:33:03 +1000

    test(component): moves lozenge tests to mocha unit testing
    affects: @atlaskit/lozenge

--- packages/lozenge/test/unit/index.js
+++ packages/lozenge/test/unit/index.js
@@ -1,29 +1,28 @@
 import { mount } from 'enzyme';
 import React from 'react';
 
-import Lozenge, { APPEARANCE_ENUM } from '../src/Lozenge';
+import Lozenge, { APPEARANCE_ENUM } from '../../src/Lozenge';
 
 describe('Lozenge', () => {
   describe('isBold property', () => {
     it('should not be the default', () => {
-      mount(<Lozenge />).prop('isBold').should.equal(false);
+      expect(mount(<Lozenge />).prop('isBold')).to.equal(false);
     });
     it('should change when toggled', () => {
-      mount(<Lozenge isBold />).prop('isBold').should.equal(true);
+      expect(mount(<Lozenge isBold />).prop('isBold')).to.equal(true);
     });
   });
   describe('appearance property', () => {
     it('should be "default" when not set', () => {
-      mount(<Lozenge />).prop('appearance').should.equal('default');
+      expect(mount(<Lozenge />).prop('appearance')).to.equal('default');
     });
     it('should change when set to an approved value', () => {
       APPEARANCE_ENUM.values.forEach((value) => {
-        mount(<Lozenge appearance={value} />).prop('appearance').should.equal(value);
+        expect(mount(<Lozenge appearance={value} />).prop('appearance')).to.equal(value);
       });
     });
     it('should revert to "default" when set to an invalid value', () => {
-      mount(<Lozenge appearance="foo" />).getNode().validAppearance()
-        .should.equal('default');
+      expect(mount(<Lozenge appearance="foo" />).getNode().validAppearance()).to.equal('default');
     });
   });
 });

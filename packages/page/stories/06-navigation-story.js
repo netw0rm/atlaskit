8c945555cb3d82cb593c6b57eda4def30506f7c3
Sean Curtis <scurtis@atlassian.com>
2017-04-27 16:52:29 +1000

    fix(component): page no longer renders badly if there is not much content and a banner specified
    affects: @atlaskit/page

--- packages/page/stories/06-navigation-story.js
+++ packages/page/stories/06-navigation-story.js
@@ -3,15 +3,9 @@ import React from 'react';
 import Navigation from '@atlaskit/navigation';
 import Banner from '@atlaskit/banner';
 import AkToggle from '@atlaskit/toggle';
-import styled from 'styled-components';
-import Lorem from 'react-lorem-component';
 
 import Page, { Grid, GridColumn } from '../src';
 import { name } from '../package.json';
-
-const Dummy = styled.div`
-  background: #fea;
-`;
 
 class ToggleBannerPage extends React.PureComponent {
   state = {
@@ -60,34 +54,6 @@ class ToggleBannerPage extends React.PureComponent {
               }}
             >Toggle banner</AkToggle>
           </GridColumn>
-        </Grid>
-        <Grid>
-          <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
-          <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
-          <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
-          <GridColumn medium={3}><Dummy>3 col</Dummy></GridColumn>
-        </Grid>
-        <Grid>
-          <GridColumn medium={4}><Dummy>4 col</Dummy></GridColumn>
-          <GridColumn medium={4}><Dummy>4 col</Dummy></GridColumn>
-          <GridColumn medium={4}><Dummy>4 col</Dummy></GridColumn>
-        </Grid>
-        <Grid>
-          <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
-          <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
-          <GridColumn medium={2}><Dummy>2 col</Dummy></GridColumn>
-        </Grid>
-        <Grid layout="fluid">
-          <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
-          <GridColumn medium={5}><Dummy>5 col</Dummy></GridColumn>
-          <GridColumn medium={2}><Dummy>2 col</Dummy></GridColumn>
-        </Grid>
-        <Grid>
-          <GridColumn medium={2}><Dummy>LHS</Dummy></GridColumn>
-          <GridColumn medium={8}>
-            <Dummy><Lorem count={30} /></Dummy>
-          </GridColumn>
-          <GridColumn medium={2}><Dummy>RHS</Dummy></GridColumn>
         </Grid>
       </Page>
     );

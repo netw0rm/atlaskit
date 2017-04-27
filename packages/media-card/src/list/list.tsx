ccd348dc86e421156975f8961cc2d07f4c6a829f
James Newell <jnewell@atlassian.com>
2017-04-28 09:28:29 +1000

    feat(component): added an animation for items added to CardList. Removed load more button.
    affects: @atlaskit/media-card
    
    BREAKING CHANGE:
    
    Removed load more button.
    
    ISSUES CLOSED: FIL-3998

--- packages/media-card/src/list/list.tsx
+++ packages/media-card/src/list/list.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Subscription } from 'rxjs/Subscription';
 import { AxiosError } from 'axios';
-import Button from '@atlaskit/button';
 import { MediaItem, MediaCollection, MediaCollectionItem, Context, CollectionAction, DataUriService } from '@atlaskit/media-core';
+import {CSSTransitionGroup} from 'react-transition-group';
 import { DEFAULT_CARD_DIMENSIONS } from '../files';
 import { CardDimensions } from '../index';
 import { Provider } from '../card';
 import { MediaCard } from '../mediaCard';
 import { InfiniteScroll } from './infiniteScroll';
-import { CardListWrapper, Spinner, LoadMoreButtonContainer } from './styled';
+import { ListWrapper, ListItemWrapper, Spinner } from './styled';
 
 export interface CardListProps {
   context: Context;
@@ -72,6 +72,13 @@ export class CardList extends Component<CardListProps, CardListState> {
   providersByMediaItemId: {[id: string]: Provider} = {};
   private dataURIService: DataUriService;
 
+  private ubsubscribe() {
+    const {subscription} = this.state;
+    if (subscription) {
+      subscription.unsubscribe();
+    }
+  }
+
   private shouldUpdateState(nextProps: CardListProps): boolean {
     return (nextProps.collectionName !== this.props.collectionName)
       || (nextProps.context !== this.props.context)
@@ -83,9 +90,7 @@ export class CardList extends Component<CardListProps, CardListState> {
     const pageSize = this.props.pageSize || CardList.defaultPageSize;
     const provider = context.getMediaCollectionProvider(collectionName, pageSize);
 
-    if (this.state && this.state.subscription) {
-      this.state.subscription.unsubscribe();
-    }
+    this.ubsubscribe();
 
     this.dataURIService = context.getDataUriService(collectionName);
 
@@ -107,12 +112,8 @@ export class CardList extends Component<CardListProps, CardListState> {
         this.setState({
           ...this.state,
           collection,
-          hasNextPage: true,
           loading: false
         });
-      },
-      complete: (): void => {
-        this.setState({ ...this.state, hasNextPage: false, loading: false });
       },
       error: (error: AxiosError): void => {
         this.setState({ ...this.state, error, loading: false });
@@ -121,7 +122,6 @@ export class CardList extends Component<CardListProps, CardListState> {
 
     this.setState({
       loadNextPage: () => provider.controller().loadNextPage(),
-      hasNextPage: false,
       collection: undefined,
       error: undefined,
       subscription
@@ -132,60 +132,51 @@ export class CardList extends Component<CardListProps, CardListState> {
     this.updateState(this.props);
   }
 
-  componentWillReceiveProps(nextProps: CardListProps, nextContext: any): void {
+  componentWillReceiveProps(nextProps: CardListProps): void {
     if (this.shouldUpdateState(nextProps)) {
       this.updateState(nextProps);
     }
   }
 
   componentWillUnmount() {
-    if (this.state.subscription) {
-      this.state.subscription.unsubscribe();
-    }
+    this.ubsubscribe();
   }
 
   render(): JSX.Element {
+    const {height} = this.props;
+    const {loading, error, collection} = this.state;
     const emptyComponent = this.props.emptyComponent || EmptyComponent;
     const loadingComponent = this.props.loadingComponent || LoadingComponent;
     const errorComponent = this.props.errorComponent || ErrorComponent;
 
-    if (!this.state.loading) {
-      if (this.state.error) {
-        if (this.state.error.response && this.state.error.response.status === 404) {
-          return emptyComponent;
-        }
-
-        return errorComponent;
-      } else if (!this.state.collection) {
-        return loadingComponent;
+    if (error) {
+      if (error.response && error.response.status === 404) {
+        return emptyComponent;
       } else {
-        if (this.useInfiniteScroll) {
-          return (
-            <InfiniteScroll
-              height={this.props.height}
-              onThresholdReached={() => this.loadNextPage()}
-            >
-              <CardListWrapper className="card-list">
-                {this.renderCardList()}
-                {this.renderLoadMoreButton()}
-              </CardListWrapper>
-            </InfiniteScroll>
-          );
-        } else {
-          return (
-            <CardListWrapper className="card-list">
-              {this.renderCardList()}
-              {this.renderLoadMoreButton()}
-            </CardListWrapper>
-          );
-        }
+        return errorComponent;
       }
-    } else {
+    }
+
+    if (loading || !collection) {
       return loadingComponent;
     }
+
+    if (this.useInfiniteScroll) {
+      return (
+        <InfiniteScroll
+          height={height}
+          onThresholdReached={() => this.loadNextPage()}
+        >
+          {this.renderList()}
+        </InfiniteScroll>
+      );
+    } else {
+      return this.renderList();
+    }
+
   }
 
-  private renderCardList(): JSX.Element {
+  private renderList(): JSX.Element {
     const { collection } = this.state;
 
     const actions = this.props.actions || [];
@@ -209,7 +200,7 @@ export class CardList extends Component<CardListProps, CardListState> {
         if (!mediaItem.details.id) { return null; }
 
         return (
-          <li key={`${index}-${mediaItem.details.id}`}>
+          <ListItemWrapper key={`${mediaItem.details.id}-${mediaItem.details.occurrenceKey}`}>
             <MediaCard
               provider={this.providersByMediaItemId[mediaItem.details.id]}
               dataURIService={this.dataURIService}
@@ -222,17 +213,22 @@ export class CardList extends Component<CardListProps, CardListState> {
               appearance={cardAppearance}
               actions={cardActions(mediaItem)}
             />
-          </li>
+          </ListItemWrapper>
         );
       }) : null
     ;
 
     return (
-      <ul>
-        {cards}
-      </ul>
+      <ListWrapper className="card-list">
+        <CSSTransitionGroup
+          transitionName="card-list-item"
+          transitionEnterTimeout={750}
+          transitionLeave={false}
+        >
+          {cards}
+        </CSSTransitionGroup>
+      </ListWrapper>
     );
-
   }
 
   /*
@@ -260,37 +256,6 @@ export class CardList extends Component<CardListProps, CardListState> {
 
   private isNullOrUndefined(value: any): boolean {
     return (value === null) || (value === undefined);
-  }
-
-  private get showLoadMoreButton(): boolean {
-    if (!this.state.hasNextPage) { return false; }
-
-    return this.state.hasNextPage && (
-      (this.props.showLoadMoreButton === true) ||
-      (this.isNullOrUndefined(this.props.showLoadMoreButton) && !this.useInfiniteScroll)
-    );
-  }
-
-  private renderLoadMoreButton(): JSX.Element | null {
-    if (this.showLoadMoreButton && this.state.hasNextPage) {
-      const title = this.state.loading ? 'Loading...' : 'Load more';
-      return (
-        <LoadMoreButtonContainer style={{ width: this.cardWidth }}>
-          <Button
-            className="load-more-button"
-            onClick={this.onLoadMoreButtonClick}
-          >
-            {title}
-          </Button>
-        </LoadMoreButtonContainer>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  private onLoadMoreButtonClick = () => {
-    this.loadNextPage();
   }
 
   loadNextPage = (): void => {

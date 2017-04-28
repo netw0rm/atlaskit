1239b33d33a818f1298cb2f74e11137c6fd51b23
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-04-28 13:46:01 +1000

    feat(component): add Webp support to requests
    affects: @atlaskit/media-core

--- packages/media-core/src/services/util/createRequest.ts
+++ packages/media-core/src/services/util/createRequest.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {MediaApiConfig} from '../../config';
+import {checkWebpSupport} from '../../utils';
 
 export interface CreateRequestFunc {
   (requestOptions: RequestOptions): Promise<any>;
@@ -22,24 +23,31 @@ export interface RequestOptions {
 
 export default (requesterOptions: RequesterOptions) => (requestOptions: RequestOptions) : any => {
   return requesterOptions.config.tokenProvider(requesterOptions.collectionName).then(token => {
-    return axios({
-      method: requestOptions.method || 'get',
-      url: requestOptions.url,
-      baseURL: requesterOptions.config.serviceHost,
-      headers: {
+    return checkWebpSupport().then(isWebpSupported => {
+      const headers = {
         ...requestOptions.headers,
         'X-Client-Id': requesterOptions.clientId,
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
-      },
-      params: {
-        collection: requesterOptions.collectionName,
-        ...requestOptions.params
-      },
-      data: requestOptions.data,
-      responseType: requestOptions.responseType
-    })
-      .then(response => response.data)
-    ;
+      } as any;
+      const accept = isWebpSupported ? 'image/webp,image/*,*/*;q=0.8' : null;
+
+      // Having an undefined value for the 'accept' property will add {accept: 'undefined'} to the headers
+      // which will cause the request to always fail, thats why we need to be more explicit
+      if (accept) { headers.accept = accept; }
+
+      return axios({
+        method: requestOptions.method || 'get',
+        url: requestOptions.url,
+        baseURL: requesterOptions.config.serviceHost,
+        headers,
+        params: {
+          collection: requesterOptions.collectionName,
+          ...requestOptions.params
+        },
+        data: requestOptions.data,
+        responseType: requestOptions.responseType
+      }).then(response => response.data);
+    });
   });
 };

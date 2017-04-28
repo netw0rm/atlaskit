2e91f44301cd9b0ef3dbfdb3dab6790a43668144
Jed Watson <jed.watson@me.com>
2017-04-28 15:48:47 +1000

    docs(docs): add avatar usage and examples to docs
    affects: @atlaskit/avatar

--- packages/avatar/docs/index.js
+++ packages/avatar/docs/index.js
@@ -1,18 +1,59 @@
-import React, { PureComponent } from 'react';
-import Avatar from '../src';
+import React from 'react';
+import styled from 'styled-components';
 
-export default class Docs extends PureComponent {
-  render() {
-    return (
-      <div>
-        <div>
-          Avatars represent users, and may also include a presence (online /
-          offline / etc) indicator.
-        </div>
-        <div>
-          <Avatar />
-        </div>
-      </div>
-    );
-  }
-}
+/* eslint-disable import/no-duplicates, import/first */
+import AvatarExample from './AvatarExample';
+import avatarExampleSrc from '!raw-loader!./AvatarExample';
+import PresenceExample from './PresenceExample';
+import presenceExampleSrc from '!raw-loader!./PresenceExample';
+
+import avatarSrc from '!raw-loader!../src/components/Avatar';
+import presenceSrc from '!raw-loader!../src/components/Presence';
+/* eslint-enable import/no-duplicates, import/first */
+
+export const componentSource = [
+  { name: 'Avatar', src: avatarSrc },
+  { name: 'Presence', src: presenceSrc },
+];
+
+const Usage = styled.pre`
+  background-color: #F4F5F7;
+  border-radius: 5px;
+  margin: 14px 0;
+  padding: 8px;
+`;
+
+export const description = (
+  <div>
+    <h3>Usage</h3>
+    <p>
+      This package exports an <code>Avatar</code> component and
+      a <code>Presence</code> component:
+    </p>
+    <Usage>
+      {"import Avatar, { Presence } from '@atlaskit/avatar';"}
+    </Usage>
+    <p>
+      Use the <code>Avatar</code> component to represent users with their
+      profile picture. Optionally, a presence to indicate online status can also
+      be displayed.
+    </p>
+    <p>
+      You can use the <code>Presence</code> component independently for contexts
+      where the profile picture is not required (e.g. next to a username)
+    </p>
+  </div>
+);
+
+export const examples = [
+  {
+    title: 'Avatar',
+    Component: AvatarExample,
+    src: avatarExampleSrc,
+  },
+  {
+    title: 'Presence',
+    Component: PresenceExample,
+    src: presenceExampleSrc,
+  },
+];

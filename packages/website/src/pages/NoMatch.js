b0cde47bdcc40a53be060187d462f5dbb83f8b9c
Ben Conolly <bconolly@cli-24476.office.atlassian.com>
2017-04-28 18:07:49 +1000

    WIP(docs): resolve minor issues with previous commit
    affects: @atlaskit/website

--- packages/website/src/pages/NoMatch.js
+++ packages/website/src/pages/NoMatch.js
@@ -1,8 +1,8 @@
 import React, { PropTypes, PureComponent } from 'react';
 import Helmet from 'react-helmet';
-import { Heading, Intro, Section } from '../components/Type';
-import lostImage from '../images/404-image.png';
 import styled from 'styled-components';
+import { Heading, Intro } from '../components/Type';
+import lostImage from '../images/404-image.png';
 
 const Img = styled.img`
   height: auto;
@@ -13,26 +13,24 @@ const Img = styled.img`
 
 const Center = styled.div`
   text-align: center;
-`
+`;
 
 export default class WelcomePage extends PureComponent {
   static propTypes = {
     location: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   }
   render() {
-    const { location } = this.props;
-
     return (
       <Center>
-          <Helmet title="Page not found (404)" />
-          <Img
-            alt="Adventurers"
-            src={lostImage}
-          />
-          <Heading>Looks like we&#39;re off the beaten track</Heading>
-          <Intro>
-            Let the nav to your left guide you back
-          </Intro>
+        <Helmet title="Page not found (404)" />
+        <Img
+          alt="Adventurers"
+          src={lostImage}
+        />
+        <Heading>Looks like we&#39;re off the beaten track</Heading>
+        <Intro>
+          Let the nav to your left guide you back
+        </Intro>
       </Center>
     );
   }

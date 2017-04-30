6a1df6d5ebb76221ea9a215491fdf19517899227
Jonathan Yeo <jyeo@atlassian.com>
2017-05-01 09:46:53 +1000

    chore(component): update stories
    affects: @atlaskit/quick-search

--- packages/quick-search/stories/10-quick-search-story.jsx
+++ packages/quick-search/stories/10-quick-search-story.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-// import { AkSearch } from '@atlaskit/navigation';
 
 import { name } from '../package.json';
-import QuickSearch, { AkQuickSearchResultsList } from '../src/';
+import QuickSearch from '../src/';
 import { ParsingSearchResource } from '../src/api/SearchResource';
 // import MockSearchResource from '../test/helpers/MockSearchResource';
-import { ResultParser } from '../src/api/JsonToResultParser';
 import BasicNav from '../../navigation/stories/components/BasicNavigation';
 
 const searchResource = new ParsingSearchResource({
@@ -16,27 +14,15 @@ const searchResource = new ParsingSearchResource({
 // const searchResource = new MockSearchResource(200);
 
 storiesOf(name, module)
-  .add('Ungrouped Results', () => {
-    const resultCallbacks = {
-      HipChatConversation: () => console.log('*click*'),
-    };
-    const resultParser = new ResultParser(() => {}, resultCallbacks);
-    return (
-      <BasicNav
-        searchDrawerContent={(
-          <QuickSearch
-            searchResource={searchResource}
-            jsonToResultParser={resultParser}
-            resultCallbacks={resultCallbacks}
-            onSearchTerminate={() => {}}
-          />
-        )}
-      >
-        Hi
-      </BasicNav>
-    );
-  })
-  .add('Grouped Results', () => (
+  .add('Quick-search Component', () => (
+    <QuickSearch
+      searchResource={searchResource}
+      resultCallbacks={{
+        HipChatConversation: () => console.log('*click*'),
+      }}
+    />
+  ))
+  .add('In a search drawer', () => (
     <BasicNav
       searchDrawerContent={(
         <QuickSearch
@@ -49,38 +35,4 @@ storiesOf(name, module)
     >
       Hi
     </BasicNav>
-  ))
-  .addCodeExampleStory('Stateful QuickSearch', () => {
-    class TestDataSearchResults extends React.PureComponent {
-      constructor() {
-        super();
-        this.handleDataChange = this.handleDataChange.bind(this);
-        this.state = {
-          data: [],
-        };
-      }
-
-      componentWillUpdate() {
-        console.log(this.state);
-      }
-
-      handleDataChange = (e) => {
-        const data = JSON.parse(e.target.value);
-        console.log(data);
-        this.setState({
-          data,
-        });
-      }
-
-      render() {
-        return (
-          <div>
-            <AkQuickSearchResultsList items={this.state.data} />
-            <input type="text" onChange={this.handleDataChange} />
-          </div>
-        );
-      }
-    }
-
-    return <TestDataSearchResults />;
-  });
+  ));

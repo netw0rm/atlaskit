d825def93caa292ed984051cd1751af2004fe4e1
Bradley Ayers <bayers@atlassian.com>
2017-05-01 14:55:24 +1000

    chore(build): use lerna for running tslint.
    affects: @atlaskit/code, @atlaskit/editor-bitbucket, @atlaskit/editor-core, @atlaskit/editor-cq,
    
    @atlaskit/editor-hipchat, @atlaskit/editor-jira, @atlaskit/emoji, @atlaskit/icon,
    
    @atlaskit/media-avatar-picker, @atlaskit/media-card, @atlaskit/media-core, @atlaskit/media-editor,
    
    @atlaskit/media-filmstrip, @atlaskit/media-test-helpers, @atlaskit/media-viewer, @atlaskit/mention,
    
    @atlaskit/reactions, @atlaskit/renderer, @atlaskit/util-component-ts-template,
    
    @atlaskit/util-data-test

--- build/bin/lint.tslint.sh
+++ build/bin/lint.tslint.sh
@@ -4,7 +4,7 @@ export PATH="`yarn bin`:$PATH"
 ROOT=$(dirname $0)/../..
 
 chalk --no-stdin -t "{blue TSLinting files…}"
-tslint -c $ROOT/tslint.json $@ \
+tslint -c $ROOT/build/tslint/base.json $@ \
   $ROOT'/packages/*/src/**/*.{ts,tsx,d.ts}' \
   $ROOT'/packages/*/stories/**/*.{ts,tsx,d.ts}' \
   $ROOT'/packages/*/*.{ts,tsx,d.ts}'

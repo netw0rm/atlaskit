a903452be7034eb4e43216aa5766ec4b76359b4f
Luke Batchelor <lbatchelor@atlassian.com>
2017-05-02 16:02:52 +1000

    chore(docs): adds docs and updates story for onRadioChange prop
    affects: @atlaskit/field-radio-group

--- packages/field-radio-group/stories/readme/RadioGroupOverviewSmart.jsx
+++ packages/field-radio-group/stories/readme/RadioGroupOverviewSmart.jsx
@@ -5,11 +5,17 @@ const items = [
   { name: 'color', value: 'red', label: 'Red' },
   { name: 'color', value: 'blue', label: 'Blue', defaultSelected: true },
   { name: 'color', value: 'yellow', label: 'Yellow' },
+  { name: 'color', value: 'green', label: 'Green' },
 ];
 
 export default (
   <FieldRadioGroup
     items={items}
     label="Pick your favourite color:"
+    onRadioChange={(e) => {
+      if (e.target.value === 'yellow') {
+        e.preventDefault();
+      }
+    }}
   />
 );

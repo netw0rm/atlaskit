73356fce04f197f8146776281f19daf01e88afd9
Craig Petchell <cpetchell@atlassian.com>
2017-05-05 11:52:41 +1000

    feat(component): upgrade emoji. Remove need for style overrides.
    affects: @atlaskit/renderer
    
    ISSUES CLOSED: FS-904

--- packages/renderer/stories/renderer-story.tsx
+++ packages/renderer/stories/renderer-story.tsx
@@ -5,6 +5,7 @@ import { emoji as emojiData } from '@atlaskit/util-data-test';
 import * as React from 'react';
 
 import Renderer from '../src';
+import Emoji from '../src/nodes/emoji';
 import Paragraph from '../src/nodes/paragraph';
 import HardBreak from '../src/nodes/hardBreak';
 import Em from '../src/marks/em';
@@ -16,7 +17,7 @@ import Subsup from '../src/marks/subsup';
 import Underline from '../src/marks/underline';
 import Code from '../src/marks/code';
 import { name } from '../package.json';
-import { document } from './story-data';
+import { document, evilburnsEmojiId, grinEmojiId, lorem } from './story-data';
 
 const tokenProvider = StoryBookTokenProvider.tokenProvider;
 
@@ -93,6 +94,68 @@ storiesOf(name, module)
   .add('nodes/mention', () => (
     <Mention id="abcd-abcd-abcd" text="@Oscar Wallhult" />
   ))
+  .add('nodes/emoji', () => {
+    // tslint:disable-next-line:variable-name
+    const Sample = (props: any) => (
+      <span>
+        Example emoji:&nbsp;
+        <Emoji emojiId={evilburnsEmojiId} emojiProvider={props.emojiProvider} />
+        <Emoji emojiId={grinEmojiId} emojiProvider={props.emojiProvider} />
+        <Emoji emojiId={{ shortName: ':nope:' }} emojiProvider={props.emojiProvider} />
+      </span>
+    );
+    const sampleStyle = {
+      display: 'inline-block',
+      verticalAlign: 'top',
+      paddingRight: '10px',
+      width: '45%',
+    };
+    return (
+      <div>
+        <p>
+          This story shows emoji in various contexts, the line height between the left and
+          right columns should be consistent if the emoji do not impact the line height.
+        </p>
+        <hr/>
+        <div style={sampleStyle}>
+          <Paragraph><Sample emojiProvider={emojiProvider} /></Paragraph>
+          <hr/>
+          <Paragraph>{lorem} <Sample emojiProvider={emojiProvider} /> {lorem}</Paragraph>
+          <hr/>
+          <h1><Sample emojiProvider={emojiProvider} /></h1>
+          <hr/>
+          <h2><Sample emojiProvider={emojiProvider} /></h2>
+          <hr/>
+          <h3><Sample emojiProvider={emojiProvider} /></h3>
+          <hr/>
+          <h4><Sample emojiProvider={emojiProvider} /></h4>
+          <hr/>
+          <h5><Sample emojiProvider={emojiProvider} /></h5>
+          <hr/>
+          <h6><Sample emojiProvider={emojiProvider} /></h6>
+          <hr/>
+        </div>
+        <div style={sampleStyle}>
+          <Paragraph><Sample/></Paragraph>
+          <hr/>
+          <Paragraph>{lorem} <Sample/> {lorem}</Paragraph>
+          <hr/>
+          <h1><Sample/></h1>
+          <hr/>
+          <h2><Sample/></h2>
+          <hr/>
+          <h3><Sample/></h3>
+          <hr/>
+          <h4><Sample/></h4>
+          <hr/>
+          <h5><Sample/></h5>
+          <hr/>
+          <h6><Sample/></h6>
+          <hr/>
+        </div>
+      </div>
+    );
+  })
   .add('nodes/paragraph', () => (
     <Paragraph>This is a paragraph</Paragraph>
   ))

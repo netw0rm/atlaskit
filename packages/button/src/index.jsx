0a0b72a00c22bf3991b2a5931d3cedb32fd7c4a8
Ben Conolly <bconolly@atlassian.com>
2017-05-05 16:20:24 +1000

    WIP(docs): add button docs to website
    affects: @atlaskit/button
    
    ISSUES CLOSED: #2262

--- packages/button/src/index.jsx
+++ packages/button/src/index.jsx
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 import styles from './less/styles.less';
 
-import { appearance, type, spacing, theme } from './internal/enumerated-properties';
 import getClasses from './internal/get-button-classes';
 import Content from './Content';
 import Icon from './Icon';
@@ -10,35 +9,67 @@ import Span from './Span';
 import Link from './Link';
 import Button from './Button';
 
+/*
+==========================================
+NOTE: appearance, type, spacing and theme have been moved in-line
+to the props and default props. The canonical list can be found at
+./internal/enumerated-properties.jsx
+==========================================
+*/
+
 export default class AkButton extends Component {
   static propTypes = {
-    appearance: PropTypes.oneOf(appearance.values),
-    type: PropTypes.oneOf(type.values),
+    /** The base styling to apply to the button. */
+    appearance: PropTypes.oneOf([
+      'primary',
+      'default',
+      'subtle',
+      'link',
+      'subtle-link',
+    ]),
+    /** Set whether it is a button or a form submission. */
+    type: PropTypes.oneOf(['button', 'submit']),
+    /** Provides a url for buttons being used as a link. */
     href: PropTypes.string,
+    /** Pass target down to a link within the button component, if a href is provided. */
     target: PropTypes.string,
+    /** Name property of a linked form that the button submits when clicked. */
     form: PropTypes.string,
+    /** Set if the button is disabled. */
     isDisabled: PropTypes.bool,
-    spacing: PropTypes.oneOf(spacing.values),
+    /** Set the amount of padding in the button. */
+    spacing: PropTypes.oneOf(['default', 'compact', 'none']),
+    /** Change the style to indicate the button is selected. */
     isSelected: PropTypes.bool,
-    theme: PropTypes.oneOf(theme.values),
+    /** Change the default styling. */
+    theme: PropTypes.oneOf(['default', 'dark']),
+    /** Places an icon within the button, before the button's text. */
     iconBefore: PropTypes.element,
+    /** Places an icon within the button, after the button's text. */
     iconAfter: PropTypes.element,
+    /** Add a classname to the button. */
     className: PropTypes.string,
+    /** Handler to be called on click. */
     onClick: PropTypes.func,
+    /** Assign specific tabIndex order to the underlying html button. */
     tabIndex: PropTypes.number,
+    /** Pass aria-haspopup to underlying html button. */
     ariaHaspopup: PropTypes.bool,
+    /** Pass aria-expanded to underlying html button. */
     ariaExpanded: PropTypes.bool,
+    /** Pass aria-controls to underlying html button. */
     ariaControls: PropTypes.string,
+    /** Provide a unique id to the button. */
     id: PropTypes.string,
   }
 
   static defaultProps = {
-    appearance: appearance.default,
-    type: type.default,
+    appearance: 'default',
+    type: 'button',
     isDisabled: false,
-    spacing: spacing.default,
+    spacing: 'default',
     isSelected: false,
-    theme: theme.default,
+    theme: 'default',
     tabIndex: null,
   }
 

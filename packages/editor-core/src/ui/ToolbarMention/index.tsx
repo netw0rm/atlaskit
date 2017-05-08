c72c45ef274a20949018a6e9a73d0faa6238cd64
Eduard Shvedai <eshvedai@atlassian.com>
2017-05-08 16:44:38 +1000

    feat(component): added creation of mentions by clicking on mentions toolbar icon
    affects: @atlaskit/editor-core

---
+++ packages/editor-core/src/ui/ToolbarMention/index.tsx
@@ -0,0 +1,59 @@
+import MentionIcon from '@atlaskit/icon/glyph/editor/mention';
+import * as React from 'react';
+import { PureComponent } from 'react';
+import { analyticsDecorator as analytics } from '../../analytics';
+import { EditorView, Fragment, Slice } from '../../prosemirror';
+import { MentionsState } from '../../plugins/mentions';
+import ToolbarButton from '../ToolbarButton';
+import * as styles from './styles';
+
+export interface Props {
+  editorView: EditorView;
+  pluginState: MentionsState;
+}
+
+export interface State {
+  disabled: boolean;
+}
+
+export default class ToolbarImage extends PureComponent<Props, State> {
+  state: State = { disabled: false };
+
+  componentDidMount() {
+    this.props.pluginState.subscribe(this.handlePluginStateChange);
+  }
+
+  componentWillUmount() {
+    this.props.pluginState.unsubscribe(this.handlePluginStateChange);
+  }
+
+  render() {
+    const { disabled } = this.state;
+
+    return (
+      <ToolbarButton
+        wrapperClassName={styles.button}
+        onClick={this.handleInsertMention}
+        disabled={disabled}
+        title="mention a person (@)"
+        iconBefore={<MentionIcon label="Mention" />}
+      />
+    );
+  }
+
+  private handlePluginStateChange = (pluginState: MentionsState) => {
+    this.setState({
+      disabled: pluginState.mentionDisabled()
+    });
+  }
+
+  @analytics('atlassian.editor.mention.button')
+  private handleInsertMention = () => {
+    const { editorView } = this.props;
+    const { state } = editorView;
+    const node = state.schema.text('@', [ state.schema.mark('mentionQuery') ]);
+    editorView.dispatch(
+      state.tr.replaceSelection(new Slice(Fragment.from(node), 0, 0))
+    );
+  }
+}

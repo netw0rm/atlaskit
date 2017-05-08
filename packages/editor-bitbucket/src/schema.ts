2e4029ce2f2b0622d915bf91d68558ede20907fb
Stanislav Sysoev <ssysoev@atlassian.com>
2017-05-08 12:51:56 +1000

    feat(component): use createSchema helper in editor-bitbucket
    affects: @atlaskit/editor-bitbucket
    
    ISSUES CLOSED: ED-1525

--- packages/editor-bitbucket/src/schema.ts
+++ packages/editor-bitbucket/src/schema.ts
@@ -1,61 +1,42 @@
 import {
-  Schema,
-  doc,
-  paragraph,
-  text,
-  em,
-  strong,
   code as coreCode,
-  strike,
-  link,
-  bulletList,
-  orderedList,
-  listItem,
-  heading,
-  blockquote,
-  codeBlock,
-  hardBreak,
-  rule,
-  image,
-  emoji,
-  emojiQuery,
-  mention,
-  mentionQuery,
   MarkSpec,
-  NodeSpec
+  NodeSpec,
+  createSchema
 } from '@atlaskit/editor-core';
 
-const nodes = {
-  doc,
-  paragraph,
-  text,
-  bulletList,
-  orderedList,
-  listItem,
-  heading,
-  blockquote,
-  codeBlock,
-  hardBreak,
-  rule,
-  image,
-  mention,
-  emoji,
-};
+const nodes = [
+  'doc',
+  'paragraph',
+  'text',
+  'bulletList',
+  'orderedList',
+  'listItem',
+  'heading',
+  'blockquote',
+  'codeBlock',
+  'hardBreak',
+  'rule',
+  'image',
+  'mention',
+  'emoji'
+];
 
 const code = {
   ...coreCode,
   excludes: 'em strike strong mentionQuery emojiQuery'
 } as MarkSpec;
 
-const marks = {
-  em,
-  strong,
-  code,
-  strike,
-  link,
-  mentionQuery,
-  emojiQuery
-};
+const marks = [
+  'em',
+  'strong',
+  'strike',
+  'link',
+  'mentionQuery',
+  'emojiQuery'
+];
+
+const customMarkSpecs = { code };
 
 export { MarkSpec, NodeSpec };
-export default new Schema<typeof nodes, typeof marks>({ nodes, marks });
+export default createSchema({ nodes, marks, customMarkSpecs});

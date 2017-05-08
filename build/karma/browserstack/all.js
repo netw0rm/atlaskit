ae2f59c0be8dd9f371b40b897077cfe2164d36b0
Luke Batchelor <lbatchelor@atlassian.com>
2017-05-08 11:56:31 +1000

    chore(build): fixes missing polyfills in browserstack tests

--- build/karma/browserstack/all.js
+++ build/karma/browserstack/all.js
@@ -1,19 +1,17 @@
 const path = require('path');
 
+// This is not the entrypoint for browserstack tests, see browserstack.js and browserstackAll.js
+
 const karmaConf = require('../base');
-const assignPattern = require('../assignPattern');
+const addPolyFills = require('../addPolyFills');
 const setUpEnzyme = require('../setUpEnzyme');
+const assignPattern = require('../assignPattern');
 const removeSourcemaps = require('../removeSourceMaps');
 
-module.exports = (config, { keepSourceMaps = false } = {}) => {
+module.exports = (config) => {
   karmaConf(config);
-  setUpEnzyme(config);
-  if (!keepSourceMaps) {
-    removeSourcemaps(config);
-  }
   assignPattern(config, path.join(__dirname, 'all.entry.js'));
-
-  config.set({
-    browsers: ['jsdom'],
-  });
+  addPolyFills(config);
+  setUpEnzyme(config);
+  removeSourcemaps(config);
 };

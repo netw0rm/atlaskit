b465d5c1fc4bbe08b1bbf20aab7e9fb81a74b9b1
Eduard Shvedai <eshvedai@atlassian.com>
2017-05-09 17:23:35 +1000

    fix(component): fixed toolbarMention test
    affects: @atlaskit/editor-core

--- packages/editor-core/test/browser/ui/ToolbarMention.tsx
+++ packages/editor-core/test/browser/ui/ToolbarMention.tsx
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 import * as React from 'react';
-import mentionsPlugins from '../../src/plugins/mentions';
-import ToolbarMention from '../../src/ui/ToolbarMention';
-import { doc, p, makeEditor, fixtures } from '../../src/test-helper';
-import defaultSchema from '../../src/test-helper/schema';
+import mentionsPlugins from '../../../src/plugins/mentions';
+import ToolbarMention from '../../../src/ui/ToolbarMention';
+import { doc, p, makeEditor, fixtures } from '../../../src/test-helper';
+import defaultSchema from '../../../src/test-helper/schema';
 import MentionIcon from '@atlaskit/icon/glyph/editor/mention';
 
 describe('ToolbarMention', () => {
@@ -21,7 +21,7 @@ describe('ToolbarMention', () => {
     const toolbarMention = mount(<ToolbarMention pluginState={pluginState} editorView={editorView} />);
     toolbarMention.find(MentionIcon).simulate('click');
     const { state } = editorView;
-    expect(state.doc.rangeHasMark(0, 2, state.schema.marks.mentionQuery)).to.be.true;
+    expect(state.doc.rangeHasMark(0, 2, state.schema.marks.mentionQuery)).to.equal(true);
   });
 
 });

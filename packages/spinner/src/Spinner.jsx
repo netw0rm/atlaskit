d6971ee54cb1e55e10bbae619c6482228a71fdac
Ben Conolly <bconolly@atlassian.com>
2017-05-11 14:49:12 +1000

    docs(docs): add website documentation for spinner
    affects: @atlaskit/spinner

--- packages/spinner/src/Spinner.jsx
+++ packages/spinner/src/Spinner.jsx
@@ -18,10 +18,16 @@ const NOOP = () => {};
 
 export default class Spinner extends PureComponent {
   static propTypes = {
+    /** Time in milliseconds after component mount before spinner is visible. */
     delay: PropTypes.number,
+    /** Set the spinner color to white, for use in dark-themed UIs. */
     invertColor: PropTypes.bool,
+    /** Setting this to true causes the spinner to animate its disappearance.
+    Setting it to false again will cause the spinner to animate back in. */
     isCompleting: PropTypes.bool,
+    /** Handler for once the spinner has completed its outro animation */
     onComplete: PropTypes.func,
+    /** Size of the spinner. */
     size: PropTypes.oneOfType([
       PropTypes.number,
       PropTypes.oneOf(SIZES),

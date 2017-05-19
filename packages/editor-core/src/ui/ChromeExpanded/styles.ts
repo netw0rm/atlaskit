5018087ab8997fd66c5bffdf7592d648992bb8ec
Eduard Shvedai <eshvedai@atlassian.com>
2017-05-19 11:07:23 +1000

    feat(component): added styled components to Chrome expanded and Chrome collapsed components
    affects: @atlaskit/editor-core

--- packages/editor-core/src/ui/ChromeExpanded/styles.ts
+++ packages/editor-core/src/ui/ChromeExpanded/styles.ts
@@ -1,6 +1,6 @@
 import { akEditorSubtleAccent, akEditorMentionSelected } from '../../styles';
 import { akBorderRadius, akGridSize } from '@atlaskit/util-shared-styles';
-import { style } from 'typestyle';
+import styled from 'styled-components';
 
 export const createNestedListStyles = (): any => {
   const styles = {};
@@ -15,163 +15,152 @@ export const createNestedListStyles = (): any => {
   return styles;
 };
 
-export const container = style({
-  backgroundColor: 'white',
-  border: `1px solid ${akEditorSubtleAccent}`,
-  boxSizing: 'border-box',
-  borderRadius: akBorderRadius,
+export const Container = styled.div`
+  background-color: white;
+  border: 1px solid ${akEditorSubtleAccent};
+  box-sizing: border-box;
+  border-radius: ${akBorderRadius};
 
-  // Create a stacking context, so that the toolbar can be placed above the content.
-  position: 'relative',
+  /* Create a stacking context, so that the toolbar can be placed above the content */
+  position: relative;
 
-  $nest: {
-    '&:focus': {
-      outline: 'none'
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Content = styled.div`
+  position: relative;
+
+  & .ie11 {
+    overflow: visible;
+    word-wrap: break-word;
+  }
+
+  .ProseMirror {
+    position: relative;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+    outline: none;
+    padding: 12px 20px;
+  }
+
+  .ProseMirror ul, .ProseMirror ol {
+    padding-left: 30px;
+    cursor: default;
+  }
+
+  .ProseMirror blockquote {
+    padding-left: 1em;
+    border-left: 3px solid #eee;
+    margin-left: 0;
+    margin-right: 0;
+  }
+
+  .ProseMirror pre {
+    white-space: pre-wrap;
+  }
+
+  .ProseMirror li {
+    position: relative;
+    /* Dont do weird stuff with marker clicks */
+    pointer-events: none;
+
+    > p:not(:first-child) {
+      margin: 4px 0 0 0;
     }
   }
-});
+  .ProseMirror ol {
+    $nest: createNestedListStyles(),
+  }
 
-export const content = style({
-  position: 'relative',
+  .ProseMirror li > * {
+    pointer-events: auto
+  }
 
-  $nest: {
-    '& .ie11': {
-      overflow: 'visible',
-      wordWrap: 'break-word'
-    },
+  .ProseMirror-hideselection *::selection {
+    $unique: true;
+    background: transparent;
+  }
 
-    '.ProseMirror': {
-      position: 'relative',
-      wordWrap: 'break-word',
-      whiteSpace: 'pre-wrap',
-      outline: 'none',
-      padding: '12px 20px'
-    },
+  .ProseMirror-hideselection *::-moz-selection {
+    $unique: true;
+    background: transparent;
+  }
 
-    '.ProseMirror ul, .ProseMirror ol': {
-      paddingLeft: '30px',
-      cursor: 'default'
-    },
+  .ProseMirror-selectednode {
+    outline: none;
+  }
 
-    '.ProseMirror blockquote': {
-      paddingLeft: '1em',
-      borderLeft: '3px solid #eee',
-      marginLeft: '0',
-      marginRight: '0'
-    },
+  .ProseMirror-selectednode:empty {
+    outline: 2px solid #8cf;
+  }
 
-    '.ProseMirror pre': {
-      whiteSpace: 'pre-wrap'
-    },
+  .ProseMirror-selectednode .ak-mention {
+    background: ${akEditorMentionSelected};
+  }
 
-    '.ProseMirror li': {
-      position: 'relative',
-      /* Don't do weird stuff with marker clicks */
-      pointerEvents: 'none',
+  /* Make sure li selections wrap around markers */
+  li.ProseMirror-selectednode {
+    outline: none;
+  },
 
-      $nest: {
-        '> p:not(:first-child)': {
-          margin: '4px 0 0 0'
-        }
-      }
-    },
+  li.ProseMirror-selectednode:after {
+    content: '';
+    position: absolute;
+    left: -32px;
+    right: -2px;
+    top: -2px;
+    bottom: -2px;
+    border: 2px solid #8cf;
+    pointer-events: none;
+  }
+`;
 
-    '.ProseMirror ol': {
-      $nest: createNestedListStyles(),
-    },
+export const Footer = styled.div`
+  font-size: 14px;
+  padding: 20px;
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+`;
 
-    '.ProseMirror li > *': {
-      pointerEvents: 'auto'
-    },
+export const FooterActions = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
 
-    '.ProseMirror-hideselection *::selection': {
-      $unique: true,
-      background: 'transparent'
-    },
+export const IconButton = styled.div`
+  cursor: pointer;
+  font-size: inherit;
+  background: none;
+  border: none;
+  padding: 0;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
 
-    '.ProseMirror-hideselection *::-moz-selection': {
-      $unique: true,
-      background: 'transparent'
-    },
+export const Toolbar = styled.div`
+  align-items: center;
+  display: flex;
+  height: 40px;
+  padding-left: ${akGridSize};
+  padding-right: ${akGridSize};
+  position: relative;
 
-    '.ProseMirror-selectednode': {
-      outline: 'none',
-    },
+  & > * {
+    align-items: center;
+    display: flex;
+    margin-left: 10px;
 
-    '.ProseMirror-selectednode:empty': {
-      outline: '2px solid #8cf',
-    },
-
-    '.ProseMirror-selectednode .ak-mention': {
-      background: akEditorMentionSelected
-    },
-
-    /* Make sure li selections wrap around markers */
-    'li.ProseMirror-selectednode': {
-      outline: 'none'
-    },
-
-    'li.ProseMirror-selectednode:after': {
-      content: '',
-      position: 'absolute',
-      left: '-32px',
-      right: '-2px',
-      top: '-2px',
-      bottom: '-2px',
-      border: '2px solid #8cf',
-      pointerEvents: 'none'
+    &:first-child {
+      margin-left: 0;
     }
   }
-});
+`;
 
-export const footer = style({
-  fontSize: '14px',
-  padding: '20px',
-  paddingTop: '10px',
-  display: 'flex',
-  alignItems: 'center',
-  flexGrow: 1,
-});
-
-export const footerActions = style({
-  display: 'flex',
-  flexGrow: 1,
-});
-
-export const iconButton = style({
-  cursor: 'pointer',
-  fontSize: 'inherit',
-  background: 'none',
-  border: 'none',
-  padding: 0,
-  marginLeft: '5px',
-  marginRight: '5px',
-});
-
-export const toolbar = style({
-  alignItems: 'center',
-  display: 'flex',
-  height: '40px',
-  paddingLeft: akGridSize,
-  paddingRight: akGridSize,
-  position: 'relative',
-
-  $nest: {
-    '& > *': {
-      alignItems: 'center',
-      display: 'flex',
-      marginLeft: '10px',
-
-      $nest: {
-        '&:first-child': {
-          marginLeft: 0,
-        }
-      }
-    }
-  }
-});
-
-export const secondaryToolbar = style({
-  alignItems: 'center',
-  display: 'flex'
-});
+export const SecondaryToolbar = styled.div`
+  align-items: center;
+  display: flex;
+`;

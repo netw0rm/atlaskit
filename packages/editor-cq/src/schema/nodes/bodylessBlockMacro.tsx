796abd4f2e5e8c6dd2980bd9d5fc9d29ef960ab9
Huanhuan Huang <hhuang@atlassian.com>
2017-05-25 06:00:06 +1000

    fix(component): fixed indentation; Make sure inlineMacro match tag span[data-macro-id] not all span
    affects: @atlaskit/editor-cq

--- packages/editor-cq/src/schema/nodes/bodylessBlockMacro.tsx
+++ packages/editor-cq/src/schema/nodes/bodylessBlockMacro.tsx
@@ -1,7 +1,7 @@
 import {
-    akBorderRadius,
-    akColorN30,
-    akColorN50,
+  akBorderRadius,
+  akColorN30,
+  akColorN50,
 } from '@atlaskit/util-shared-styles';
 import { NodeSpec, NodeView } from '@atlaskit/editor-core';
 import * as React from 'react';
@@ -9,84 +9,84 @@ import * as ReactDOM from 'react-dom';
 import { style } from 'typestyle';
 
 const nodeClassName = style({
-    alignItems: 'center',
-    background: akColorN30,
-    border: `1px solid ${akColorN50}`,
-    borderRadius: akBorderRadius,
-    boxSizing: 'border-box',
-    cursor: 'default',
-    display: 'block',
-    fontSize: '13px',
-    margin: '0 2px',
-    minHeight: 24,
-    padding: '0 4px',
-    userSelect: 'none',
-    verticalAlign: 'middle',
-    whiteSpace: 'nowrap',
+  alignItems: 'center',
+  background: akColorN30,
+  border: `1px solid ${akColorN50}`,
+  borderRadius: akBorderRadius,
+  boxSizing: 'border-box',
+  cursor: 'default',
+  display: 'block',
+  fontSize: '13px',
+  margin: '0 2px',
+  minHeight: 24,
+  padding: '0 4px',
+  userSelect: 'none',
+  verticalAlign: 'middle',
+  whiteSpace: 'nowrap',
 
-    $nest: {
-        '&.ProseMirror-selectednode': {
-            background: akColorN50,
-            outline: 'none'
-        }
+  $nest: {
+    '&.ProseMirror-selectednode': {
+      background: akColorN50,
+      outline: 'none'
     }
+  }
 });
 
 export default {
-    group: 'block',
-    atom: true,
-    attrs: {
-        macroId: { default: null },
-        placeholderUrl: {default: null}
-    },
-    parseDOM: [{
-        tag: 'div',
-        getAttrs: (dom: HTMLElement) => ({
-            macroId: dom.dataset.macroId,
-            placeholderUrl: dom.dataset.placeholderUrl
-        })
-    }],
-    toDOM(node: any) {
-        const attrs = {
-            'data-macro-id': node.attrs.macroId
-        };
+  group: 'block',
+  atom: true,
+  attrs: {
+    macroId: { default: null },
+    placeholderUrl: { default: null }
+  },
+  parseDOM: [{
+    tag: 'div',
+    getAttrs: (dom: HTMLElement) => ({
+      macroId: dom.dataset.macroId,
+      placeholderUrl: dom.dataset.placeholderUrl
+    })
+  }],
+  toDOM(node: any) {
+    const attrs = {
+      'data-macro-id': node.attrs.macroId
+    };
 
-        return ['div', attrs, node.attrs.macroId];
-    }
+    return ['div', attrs, node.attrs.macroId];
+  }
 } as NodeSpec;
 
 export const bodylessBlockMacroNodeView = (node: any, view: any, getPos: () => number): NodeView => {
-    const { macroId, placeholderUrl } = node.attrs;
+  const { macroId, placeholderUrl } = node.attrs;
 
-    let dom: HTMLElement | undefined = document.createElement('div');
-    dom.setAttribute('class', nodeClassName);
-    dom.dataset.macroId = macroId;
-    dom.dataset.placeholderUrl = placeholderUrl;
+  let dom: HTMLElement | undefined = document.createElement('div');
+  dom.setAttribute('class', nodeClassName);
+  dom.dataset.macroId = macroId;
+  dom.dataset.placeholderUrl = placeholderUrl;
 
-    dom.setAttribute('spellcheck', 'false');
+  dom.setAttribute('spellcheck', 'false');
 
-    let macroNodeClassName = style({
-        display: 'block',
-        color: '#707070',
-        lineHeight: '24px',
-        verticalAlign: 'top',
-        backgroundImage: 'url(' + placeholderUrl +')',
-        backgroundRepeat: 'none'
-    });
+  let macroNodeClassName = style({
+    display: 'block',
+    color: '#707070',
+    lineHeight: '24px',
+    verticalAlign: 'top',
+    backgroundImage: 'url(' + placeholderUrl + ')',
+    backgroundRepeat: 'none'
+  });
 
-    ReactDOM.render(
-        <div className={macroNodeClassName} />,
-        dom
-    );
+  ReactDOM.render(
+    <div className={macroNodeClassName} />,
+    dom
+  );
 
-    return {
-        get dom() {
-            return dom;
-        },
+  return {
+    get dom() {
+      return dom;
+    },
 
-        destroy() {
-            ReactDOM.unmountComponentAtNode(dom!);
-            dom = undefined;
-        }
-    };
+    destroy() {
+      ReactDOM.unmountComponentAtNode(dom!);
+      dom = undefined;
+    }
+  };
 };
--- packages/editor-cq/src/schema/nodes/inlineMacro.tsx
+++ packages/editor-cq/src/schema/nodes/inlineMacro.tsx
@@ -1,7 +1,7 @@
 import {
-    akBorderRadius,
-    akColorN30,
-    akColorN50,
+  akBorderRadius,
+  akColorN30,
+  akColorN50,
 } from '@atlaskit/util-shared-styles';
 import { NodeSpec, NodeView } from '@atlaskit/editor-core';
 import * as React from 'react';
@@ -9,76 +9,73 @@ import * as ReactDOM from 'react-dom';
 import { style } from 'typestyle';
 
 const nodeClassName = style({
-    alignItems: 'center',
-    background: akColorN30,
-    border: `1px solid ${akColorN50}`,
-    borderRadius: akBorderRadius,
-    boxSizing: 'border-box',
-    cursor: 'default',
-    display: 'inline-flex',
-    fontSize: '13px',
-    margin: '0 2px',
-    minHeight: 24,
-    padding: '0 4px',
-    userSelect: 'none',
-    verticalAlign: 'middle',
-    whiteSpace: 'nowrap',
+  alignItems: 'center',
+  background: akColorN30,
+  border: `1px solid ${akColorN50}`,
+  borderRadius: akBorderRadius,
+  boxSizing: 'border-box',
+  cursor: 'default',
+  display: 'inline-flex',
+  fontSize: '13px',
+  margin: '0 2px',
+  minHeight: 24,
+  padding: '0 4px',
+  userSelect: 'none',
+  verticalAlign: 'middle',
+  whiteSpace: 'nowrap',
 
-    $nest: {
-        '&.ProseMirror-selectednode': {
-            background: akColorN50,
-            outline: 'none'
-        }
+  $nest: {
+    '&.ProseMirror-selectednode': {
+      background: akColorN50,
+      outline: 'none'
     }
+  }
 });
 export default {
-    group: 'inline',
-    inline: true,
-    atom: true,
-    attrs: {
-        macroId: { default: null },
-        macroName: { default: null },
-        placeholderUrl: {default: null},
-        params: { default: null }
-    },
-    parseDOM: [{
-        tag: 'span',
-        getAttrs: (dom: HTMLElement) => ({
-            macroId: dom.dataset.macroId,
-            placeholderUrl: dom.dataset.placeholderUrl,
-            params: dom.dataset.params
-        })
-    }],
-    toDOM(node: any) {
-        return ['span', node.attrs, node.attrs.macroId];
-    }
+  group: 'inline',
+  inline: true,
+  atom: true,
+  attrs: {
+    macroId: { default: null },
+    macroName: { default: null },
+    placeholderUrl: { default: null },
+    params: { default: null }
+  },
+  parseDOM: [{
+    tag: 'span[data-macro-id]',
+    getAttrs: (dom: HTMLElement) => ({
+      macroId: dom.dataset.macroId,
+      placeholderUrl: dom.dataset.placeholderUrl,
+      params: dom.dataset.params
+    })
+  }]
 } as NodeSpec;
 
 export const inlineMacroNodeView = (node: any, view: any, getPos: () => number): NodeView => {
-    const { macroId, macroName, placeholderUrl, params } = node.attrs;
+  const { macroId, macroName, placeholderUrl, params } = node.attrs;
 
-    let dom: HTMLElement | undefined = document.createElement('span');
-    dom.setAttribute('class', nodeClassName);
-    dom.dataset.macroId = macroId;
-    dom.dataset.placeholderUrl = placeholderUrl;
-    dom.dataset.params = params;
-    dom.dataset.macroName = macroName;
+  let dom: HTMLElement | undefined = document.createElement('span');
+  dom.setAttribute('class', nodeClassName);
+  dom.dataset.macroId = macroId;
+  dom.dataset.placeholderUrl = placeholderUrl;
+  dom.dataset.params = params;
+  dom.dataset.macroName = macroName;
 
-    dom.setAttribute('spellcheck', 'false');
+  dom.setAttribute('spellcheck', 'false');
 
-    ReactDOM.render(
-        <img src={placeholderUrl} />,
-        dom
-    );
+  ReactDOM.render(
+    <img src={placeholderUrl} />,
+    dom
+  );
 
-    return {
-        get dom() {
-            return dom;
-        },
+  return {
+    get dom() {
+      return dom;
+    },
 
-        destroy() {
-            ReactDOM.unmountComponentAtNode(dom!);
-            dom = undefined;
-        }
-    };
+    destroy() {
+      ReactDOM.unmountComponentAtNode(dom!);
+      dom = undefined;
+    }
+  };
 };

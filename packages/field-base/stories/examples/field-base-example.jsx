0440540515abc26508013c76615904e829d7a4df
Ben Conolly <bconolly@atlassian.com>
2017-05-24 14:08:57 +1000

    fix(refactor): add onBlur and onStateless no-ops to example FieldStateless for stories
    affects: @atlaskit/field-base

--- packages/field-base/stories/examples/field-base-example.jsx
+++ packages/field-base/stories/examples/field-base-example.jsx
@@ -4,6 +4,8 @@ import { FieldBaseStateless } from '@atlaskit/field-base';
 export default (
   <FieldBaseStateless
     appearance="subtle"
+    onBlur={() => {}}
+    onFocus={() => {}}
   >
     hover over me to
   </FieldBaseStateless>

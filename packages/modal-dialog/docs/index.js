d3f55a1569718e40ace180edf2e360b3b102c5cf
Ben Conolly <bconolly@atlassian.com>
2017-05-24 14:41:32 +1000

    docs(docs): update website docs for new paths. Remove examples.
    affects: @atlaskit/modal-dialog

--- packages/modal-dialog/docs/index.js
+++ packages/modal-dialog/docs/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { version } from '../package.json';
+/*
+NOTE: Examples have been disabled as they don't play nicely with the website layout
+and are hidden behind the navigation panel.
+*/
 
 /* eslint-disable import/no-duplicates, import/first */
-import Examples from './Examples';
-import examplesSrc from '!raw-loader!./Examples';
-import WidthExamples from './WidthExamples';
-import widthExamplesSrc from '!raw-loader!./WidthExamples';
+// import Examples from './Examples';
+// import examplesSrc from '!raw-loader!./Examples';
+// import WidthExamples from './WidthExamples';
+// import widthExamplesSrc from '!raw-loader!./WidthExamples';
 /* eslint-enable import/no-duplicates, import/first */
 
 export const description = (
@@ -18,21 +23,21 @@ export const description = (
       To ensure you get the exit animation when dismissing a modal dialog, first
       set <code>isOpen</code> to <code>false</code>. Once the animation is finished
       the <code>onDialogDismissed</code> function prop will be called, at which
-      point you can stop rendering the modal dialog.
+      point you can stop rendering the modal dialog. For examples see <a href={`https://aui-cdn.atlassian.com/atlaskit/stories/@atlaskit/modal-dialog/${version}/`}>The Storybook</a>
     </p>
   </div>
 );
 
-export const examples = [
-
-  {
-    title: 'Basic Usage',
-    Component: Examples,
-    src: examplesSrc,
-  },
-  {
-    title: 'Example Width Options',
-    Component: WidthExamples,
-    src: widthExamplesSrc,
-  },
-];
+export const examples = [];
+// export const examples = [
+//   {
+//     title: 'Basic Usage',
+//     Component: Examples,
+//     src: examplesSrc,
+//   },
+//   {
+//     title: 'Example Width Options',
+//     Component: WidthExamples,
+//     src: widthExamplesSrc,
+//   },
+// ];

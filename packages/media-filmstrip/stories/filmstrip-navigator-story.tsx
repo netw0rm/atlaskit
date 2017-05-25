6700435355b484493d0e6ed6693f9257aeca3c94
Michael James <mjames@atlassian.com>
2017-05-25 11:38:07 +1000

    WIP(component): identified issue with lazyloading filmstrip
    affects: @atlaskit/media-filmstrip
    
    implementing stories to surface the issue

--- packages/media-filmstrip/stories/filmstrip-navigator-story.tsx
+++ packages/media-filmstrip/stories/filmstrip-navigator-story.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
 import {Card, CardView, CardEvent} from '@atlaskit/media-card';
 import {createStorybookContext, genericUrlPreviewId, genericLinkId, genericFileId} from '@atlaskit/media-test-helpers';
+
 import {FilmStripNavigator} from '../src';
+import {
+  OverflowContainer,
+  Spacer
+} from './styled';
 
 const context = createStorybookContext();
 
@@ -10,13 +15,33 @@ const clickAction = (event: CardEvent) => {
   action('click')(event.mediaItemDetails);
 };
 
-storiesOf('FilmStripNavigator', {})
-  .add('With a Card and CardView', () => (
-    <FilmStripNavigator>
+const getDefaultNavigator = (key?) => {
+  return (
+    <FilmStripNavigator key={key}>
       <CardView onClick={clickAction} status="loading"/>
       <CardView onClick={clickAction} status="complete" metadata={{mediaType: 'doc', name: 'foobar.docx', size: 1000}}/>
       <Card onClick={clickAction} appearance="image" context={context} identifier={genericUrlPreviewId}/>
       <Card onClick={clickAction} appearance="image" context={context} identifier={genericLinkId}/>
       <Card onClick={clickAction} context={context} identifier={genericFileId}/>
     </FilmStripNavigator>
-  ));
+  );
+};
+
+
+storiesOf('FilmStripNavigator', {})
+  .add('With a Card and CardView', () => getDefaultNavigator())
+  .add('Lazy loading - Overflow', () => {
+    return (
+      <div style={{margin: '40px'}}>
+        <h2>Scroll down to test lazyload in an overflow container</h2>
+        <OverflowContainer>
+          {
+            [1, 1, 1, 1, 1].map((_, i) => [
+              <Spacer key={i}><h2>Keep scrolling!</h2></Spacer>,
+              getDefaultNavigator(i + 100)
+            ])
+          }
+        </OverflowContainer>
+      </div>
+    );
+  });

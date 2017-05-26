b687d680bf45a6b24fc0b71955b841eab29c16fc
Dmitrii Sorin <dsorin@atlassian.com>
2017-05-26 11:17:08 +1000

    fix(component): move react components into nodeviews
    affects: @atlaskit/editor-cq

--- packages/editor-cq/src/schema/nodes/jiraIssue.tsx
+++ packages/editor-cq/src/schema/nodes/jiraIssue.tsx
@@ -1,62 +1,4 @@
-import {
-  akBorderRadius,
-  akColorN30,
-  akColorN50,
-} from '@atlaskit/util-shared-styles';
-import { NodeSpec, NodeView } from '@atlaskit/editor-core';
-import { JiraLogo } from '@atlaskit/logo';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import styled from 'styled-components';
-
-// tslint:disable-next-line:variable-name
-const WrapperNode = styled.span`
-  align-items: center;
-  background: ${akColorN30};
-  border: 1px solid ${akColorN50};
-  border-radius: ${akBorderRadius};
-  box-sizing: border-box;
-  cursor: default;
-  display: inline-flex;
-  font-size: 13px;
-  margin: 0 2px;
-  min-height: 24px;
-  padding: 0 4px;
-  user-select: none;
-  vertical-align: middle;
-  white-space: nowrap;
-
-  &.ProseMirror-selectednode {
-    background: ${akColorN50};
-    outline: none;
-  }
-`;
-
-// tslint:disable-next-line:variable-name
-const JiraChildNode = styled.span`
-  display: inline-block;
-  color: #707070;
-  line-height: 24px;
-  vertical-align: top;
-
-  &:before {
-    color: black;
-    content: "JIRA | ";
-  }
-`;
-
-// tslint:disable-next-line:variable-name
-const SvgChildNode = styled.span`
-  display: inline-block;
-  height: 24px;
-  vertical-align: top;
-  width: 24px;
-
-  & > div {
-    height: 24px;
-    width: 24px;
-  }
-`;
+import { NodeSpec } from '@atlaskit/editor-core';
 
 export default {
   group: 'inline',
@@ -70,7 +12,7 @@ export default {
     serverId: { default: null },
   },
   parseDOM: [{
-    tag: 'span[jira-issue]',
+    tag: 'span[data-node-type="jiraIssue"]',
     getAttrs: (dom: HTMLElement) => ({
       issueKey: dom.textContent,
       macroId: dom.dataset.macroId,
@@ -81,45 +23,14 @@ export default {
   }],
   toDOM(node: any) {
     const attrs = {
+      'data-node-type': 'jiraIssue',
       'data-macro-id': node.attrs.macroId,
       'data-schema-version': node.attrs.schemaVersion,
       'data-server': node.attrs.server,
       'data-server-id': node.attrs.serverId,
-      'jira-issue': node.attrs.issueKey,
+      'data-jira-issue': node.attrs.issueKey,
     };
 
     return ['span', attrs, node.attrs.issueKey];
   }
 } as NodeSpec;
-
-export const jiraIssueNodeView = (node: any): NodeView => {
-  const { issueKey, macroId, schemaVersion, server, serverId } = node.attrs;
-
-  let dom: HTMLElement | undefined = document.createElement('span');
-  dom.dataset.macroId = macroId;
-  dom.dataset.schemaVersion = schemaVersion;
-  dom.dataset.server = server;
-  dom.dataset.serverId = serverId;
-  dom.setAttribute('spellcheck', 'false');
-
-  ReactDOM.render(
-    <WrapperNode>
-      <SvgChildNode>
-        <JiraLogo size="small" collapseTo="icon"/>
-      </SvgChildNode>
-      <JiraChildNode>{issueKey}</JiraChildNode>
-    </WrapperNode>,
-    dom
-  );
-
-  return {
-    get dom() {
-      return dom;
-    },
-
-    destroy() {
-      ReactDOM.unmountComponentAtNode(dom!);
-      dom = undefined;
-    }
-  };
-};
--- packages/editor-cq/test/browser/unsupportedBlock.tsx
+++ packages/editor-cq/test/browser/unsupportedBlock.tsx
@@ -1,32 +1,16 @@
+import * as React from 'react';
 import { expect } from 'chai';
-import { unsupportedBlockNodeView } from '../../src/schema/nodes/unsupportedBlock';
-import 'react';
+import { mount } from 'enzyme';
+import ReactUnsupportedBlockNode from '../../src/nodeviews/ui/unsupportedBlock';
 
-describe.skip('unsupportedBlock', () => {
-  it('should have a not null html dom', () => {
-    const dom = unsupportedBlockNodeView({ attrs: {} }).dom;
-    expect(dom).to.not.equal(undefined);
-  });
-
-  it('should return a node of type span', () => {
-    const dom = unsupportedBlockNodeView({ attrs: {} }).dom;
-    expect(dom!.nodeName).to.equal('SPAN');
-  });
-
-  it('should have well defined class attribute on first child span', () => {
-    const dom = unsupportedBlockNodeView({ attrs: {} }).dom;
-    expect(dom!.firstChild!.attributes.getNamedItem('class')).to.not.equal(undefined);
+describe('unsupportedBlock - React component', () => {
+  it('should return a node of type div', () => {
+    const wrapper = mount(<ReactUnsupportedBlockNode/>);
+    expect(wrapper.getDOMNode().tagName).to.equal('DIV');
   });
 
   it('should have text content as string "Unsupported content"', () => {
-    const dom = unsupportedBlockNodeView({ attrs: {} }).dom;
-    expect(dom!.firstChild!.textContent).to.equal('Unsupported content');
-  });
-
-  it('should have dom set to undefined once destroy is called', () => {
-    const nodeView = unsupportedBlockNodeView({ attrs: {} });
-    expect(nodeView.dom).to.not.equal(undefined);
-    nodeView!.destroy!();
-    expect(nodeView.dom).to.equal(undefined);
+    const wrapper = mount(<ReactUnsupportedBlockNode/>);
+    expect(wrapper.text()).to.equal('Unsupported content');
   });
 });

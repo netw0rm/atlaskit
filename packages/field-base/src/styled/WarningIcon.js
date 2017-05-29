3ea648fe4de5a8c3ec71381cef09552f768797b8
Boris Gvozdev <bgvozdev@atlassian.com>
2017-05-29 13:28:05 +1000

    fix(component): field-base: fix alignment of warning icon
    affects: @atlaskit/field-base
    
    ISSUES CLOSED: AK-2500

--- packages/field-base/src/styled/WarningIcon.js
+++ packages/field-base/src/styled/WarningIcon.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
-import { colorInvalid, innerHeight } from './constants';
+import { colorInvalid } from './constants';
 
 const WarningIcon = styled.div`
   align-items: center;
   color: ${colorInvalid};
   display: flex;
-  height: ${innerHeight}px;
 `;
 
 export default WarningIcon;

b78dca782baa4deb04b343cdc0db0e89a4db8233
Ben Gummer <bgummer@atlassian.com>
2017-06-02 12:58:25 -0700

    feat(component): added new optional bold flags, controlled by the Flag.appearance prop
    affects: @atlaskit/flag
    
    ISSUES CLOSED: AK-2452

--- packages/flag/stories/components/YellowWarningIcon.jsx
+++ packages/flag/stories/components/YellowWarningIcon.jsx
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
-import SuccessIcon from '@atlaskit/icon/glyph/check-circle';
+import WarningIcon from '@atlaskit/icon/glyph/warning';
 import { akColorY300 } from '@atlaskit/util-shared-styles';
 
 export default class YellowWarningIcon extends PureComponent {
   render() {
     return (
       <div style={{ color: akColorY300 }}>
-        <SuccessIcon label="Info" />
+        <WarningIcon label="Info" />
       </div>
     );
   }

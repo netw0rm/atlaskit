b0fb3e468a4b4950f2abbd91cde6126a706bef98
Trey Shugart <treshugart@gmail.com>
2017-06-06 15:33:57 -0700

    chore(build): remove old lint scripts and unused bins.

--- build/bin/lint.eslint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-export PATH="`yarn bin`:$PATH"
-NODE_MODULES=`npm root`
-
-chalk --no-stdin -t "{blue ESLinting files…}"
-eslint --color --format "$NODE_MODULES/eslint-friendly-formatter" . --ext .js --ext .jsx $@

f48a361258f9f2416e0f82a629f3ba9d4ae788af
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-06-06 14:09:48 +1000

    chore(component): add test for preventPreflight
    affects: @atlaskit/media-core

--- packages/media-core/test/createRequest.spec.ts
+++ packages/media-core/test/createRequest.spec.ts
@@ -7,6 +7,9 @@ let fakeXhr;
 let myFakeResponses;
 
 describe('createRequest()', () => {
+  const token = 'ABC';
+  const clientId = '1234';
+  const serviceHost = 'http://example.com';
 
   beforeEach(() => {
     myFakeResponses = [];
@@ -19,13 +22,13 @@ describe('createRequest()', () => {
   });
 
   it('should send the client ID and auth token', () => {
-    const tokenProvider = sinon.stub().returns(Promise.resolve('ABCD'));
+    const tokenProvider = sinon.stub().returns(Promise.resolve(token));
 
     const request = createRequest({
-      clientId: '1234',
+      clientId,
       config: {
         tokenProvider,
-        serviceHost: 'http://example.com'
+        serviceHost
       }
     });
 
@@ -34,10 +37,30 @@ describe('createRequest()', () => {
     return request({url: '/some-api/links'}).then(json => {
       expect(tokenProvider.calledOnce).to.be.true;
       expect(myFakeResponses[0].url).to.equal('http://example.com/some-api/links');
-      expect(myFakeResponses[0].requestHeaders['X-Client-Id']).to.equal('1234');
-      expect(myFakeResponses[0].requestHeaders['Authorization']).to.equal('Bearer ABCD');
+      expect(myFakeResponses[0].requestHeaders['X-Client-Id']).to.equal(clientId);
+      expect(myFakeResponses[0].requestHeaders['Authorization']).to.equal(`Bearer ${token}`);
     });
-
   });
 
+  it('should send auth arguments using queryParams when preventPreflight is true', () => {
+    const tokenProvider = sinon.stub().returns(Promise.resolve(token));
+
+    const request = createRequest({
+      clientId,
+      config: {
+        tokenProvider,
+        serviceHost
+      },
+      preventPreflight: true
+    });
+
+    setTimeout(() => myFakeResponses[0].respond(200), 0);
+
+    return request({url: '/some-api/links'}).then(json => {
+      expect(tokenProvider.calledOnce).to.be.true;
+      expect(myFakeResponses[0].url).to.equal(`http://example.com/some-api/links?token=${token}&client=${clientId}`);
+      expect(myFakeResponses[0].requestHeaders['X-Client-Id']).to.be.undefined;
+      expect(myFakeResponses[0].requestHeaders['Authorization']).to.be.undefined;
+    });
+  });
 });

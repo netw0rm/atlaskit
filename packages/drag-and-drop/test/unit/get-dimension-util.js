94beb1d408cab56f31e00feafb46b6a1b7ab871f
Alex Reardon <areardon@atlassian.com>
2017-06-06 17:10:51 +1000

    testing inside droppable

--- packages/drag-and-drop/test/unit/get-dimension-util.js
+++ packages/drag-and-drop/test/unit/get-dimension-util.js
@@ -1,31 +1,31 @@
 // @flow
 import type { Dimension } from '../../src/types';
 
+let count = 0;
+
 export default ({
-  id = 'drag-1',
+  id = `item-${++count}`,
   top = 0,
   bottom = 100,
   left = 0,
   right = 100,
   margin = 0,
 }: Object = {}): Dimension => {
-  const dimension: Dimension = (() => {
-    const height = (top + margin) + (bottom + margin);
-    const width = (left + margin) + (right + margin);
-    return {
-      id,
-      top: top + margin,
-      bottom: bottom + margin,
-      left: left + margin,
-      right: right + margin,
-      center: {
-        x: width / 2,
-        y: height / 2,
-      },
-      height,
-      width,
-    };
-  })();
+  const height = (top + margin) + (bottom + margin);
+  const width = (left + margin) + (right + margin);
+  const dimension: Dimension = {
+    id,
+    top: top + margin,
+    bottom: bottom + margin,
+    left: left + margin,
+    right: right + margin,
+    center: {
+      x: width / 2,
+      y: height / 2,
+    },
+    height,
+    width,
+  };
 
   return dimension;
 };
--- packages/drag-and-drop/test/unit/state/get-draggables-inside-droppable.spec.js
+++ packages/drag-and-drop/test/unit/state/get-draggables-inside-droppable.spec.js
@@ -1,16 +1,52 @@
 // @flow
 import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import getDraggablesInsideDroppable from '../../../src/state/get-draggables-inside-droppable';
+import getDimension from '../get-dimension-util';
+import type { Dimension, DimensionMap } from '../../../src/types';
 
 describe('get draggables inside a droppable', () => {
-  const droppable: Dimension = {
-    id: 'drop-1',
-  };
+  const droppable: Dimension = getDimension({
+    top: 0,
+    left: 0,
+    right: 100,
+    bottom: 100,
+  });
 
   const inside: Dimension[] = [
-
+    getDimension({
+      top: 20,
+      left: 20,
+      right: 80,
+      bottom: 80,
+    }),
+    getDimension({
+      top: 30,
+      left: 30,
+      right: 70,
+      bottom: 70,
+    }),
+  ];
+  const outside: Dimension[] = [
+    getDimension({
+      top: 200,
+      left: 200,
+      right: 300,
+      bottom: 400,
+    }),
   ];
 
-  it('should only return dimensions that are inside a droppable', () => {
+  const draggables: DimensionMap = inside
+    .concat(outside)
+    .reduce((previous: DimensionMap, current: Dimension): DimensionMap => {
+      previous[current.id] = current;
+      return previous;
+    }, {});
 
+  it('should only return dimensions that are inside a droppable', () => {
+    const result: Dimension[] = getDraggablesInsideDroppable(droppable, draggables);
+    expect(result).to.deep.equal(inside);
   });
+
+  // other edge cases tested in get-inside-dimension
 });

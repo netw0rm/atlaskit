bc8f348642e2c9e37f8fe43dde47a4da81b2d317
Alex Reardon <areardon@atlassian.com>
2017-06-07 15:07:06 +1000

    tests for get-droppable-over

--- packages/drag-and-drop/src/state/get-droppable-over.js
+++ packages/drag-and-drop/src/state/get-droppable-over.js
@@ -2,10 +2,12 @@
 import type { DroppableId, Position, DimensionMap } from '../types';
 import isInsideDimension from './is-inside-dimension';
 
-export default (target: Position,
-droppableDimensions: DimensionMap): ?DroppableId => {
+export default (
+  target: Position,
+  droppableDimensions: DimensionMap
+): ?DroppableId => {
   const maybeId: ?DroppableId = Object.keys(droppableDimensions)
     .find(key => isInsideDimension(target, droppableDimensions[key]));
 
-  return maybeId;
+  return maybeId || null;
 };

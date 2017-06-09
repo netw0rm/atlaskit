1f0b8a0023a932cebdc47ee61387d7a560ed8df6
Paul Curren <pcurren@atlassian.com>
2017-06-09 11:55:29 +1000

    Searching for emoji and tokenising the emoji

--- packages/emoji/src/search/AsciiEmojiRecognisingTokenizerAndSanitizer.ts
+++ packages/emoji/src/search/AsciiEmojiRecognisingTokenizerAndSanitizer.ts
@@ -1,9 +1,10 @@
 import { ISanitizer, ITokenizer } from 'js-search';
 
+// TODO PAC write a more complete set of tests for this
+
 // See https://github.com/emojione/emojione/blob/master/lib/js/emojione.js
 // This should be included as a dependency, but it's massive, containing all the images, etc. So copied for now.
 export const asciiEmojiRegexp = '(\\*\\\\0\\/\\*|\\*\\\\O\\/\\*|\\-___\\-|\\:\'\\-\\)|\'\\:\\-\\)|\'\\:\\-D|\\>\\:\\-\\)|>\\:\\-\\)|\'\\:\\-\\(|\\>\\:\\-\\(|>\\:\\-\\(|\\:\'\\-\\(|O\\:\\-\\)|0\\:\\-3|0\\:\\-\\)|0;\\^\\)|O;\\-\\)|0;\\-\\)|O\\:\\-3|\\-__\\-|\\:\\-Þ|\\:\\-Þ|\\<\\/3|<\\/3|\\:\'\\)|\\:\\-D|\'\\:\\)|\'\\=\\)|\'\\:D|\'\\=D|\\>\\:\\)|>\\:\\)|\\>;\\)|>;\\)|\\>\\=\\)|>\\=\\)|;\\-\\)|\\*\\-\\)|;\\-\\]|;\\^\\)|\'\\:\\(|\'\\=\\(|\\:\\-\\*|\\:\\^\\*|\\>\\:P|>\\:P|X\\-P|\\>\\:\\[|>\\:\\[|\\:\\-\\(|\\:\\-\\[|\\>\\:\\(|>\\:\\(|\\:\'\\(|;\\-\\(|\\>\\.\\<|>\\.<|#\\-\\)|%\\-\\)|X\\-\\)|\\\\0\\/|\\\\O\\/|0\\:3|0\\:\\)|O\\:\\)|O\\=\\)|O\\:3|B\\-\\)|8\\-\\)|B\\-D|8\\-D|\\-_\\-|\\>\\:\\\\|>\\:\\\\|\\>\\:\\/|>\\:\\/|\\:\\-\\/|\\:\\-\\.|\\:\\-P|\\:Þ|\\:Þ|\\:\\-b|\\:\\-O|O_O|\\>\\:O|>\\:O|\\:\\-X|\\:\\-#|\\:\\-\\)|\\(y\\)|\\<3|<3|\\:D|\\=D|;\\)|\\*\\)|;\\]|;D|\\:\\*|\\=\\*|\\:\\(|\\:\\[|\\=\\(|\\:@|;\\(|D\\:|\\:\\$|\\=\\$|#\\)|%\\)|X\\)|B\\)|8\\)|\\:\\/|\\:\\\\|\\=\\/|\\=\\\\|\\:L|\\=L|\\:P|\\=P|\\:b|\\:O|\\:X|\\:#|\\=X|\\=#|\\:\\)|\\=\\]|\\=\\)|\\:\\])';
-// Go on Sylvain, put it in your InputRule! ^^^^
 
 const asciiRegexp = '^' + asciiEmojiRegexp + '$';
 
@@ -21,12 +22,19 @@ export class AsciiEmojiRecognisingTokenizerAndSanitizer implements ITokenizer  {
     this.delegateSanitizer = sanitizer;
   }
 
-  tokenize(text : string) : Array<string> {
+  /**
+   * If text is recognised as an ASCII emoji then do not tokenize it. Otherwise
+   * use the delegateTokenizer to tokenize. This is tailored for index time and therefore
+   * also understands arrays of ascii emoji, where they appear comma separated.
+   *
+   * @param text the text to be tokenized
+   */
+  tokenize(text: string): Array<string> {
     const parts = text.split(',');
     for (let index = 0; index < parts.length; index++) {
-      if (!this.isAsciiEmoji(parts[0])) {
+      if (!isAsciiEmoji(parts[0])) {
         // if any part is not an emoji then delegate
-        return this.delegateTokenizer.tokenize(this.delegateSanitizer.sanitize(text));
+        return this.tokenizeNonEmojiText(text);
       }
     }
 
@@ -34,9 +42,26 @@ export class AsciiEmojiRecognisingTokenizerAndSanitizer implements ITokenizer  {
     return parts;
   }
 
+  /**
+   * If an ascii representation of an emoji is recognised then return it unprocessed, but also include
+   * any additional tokens extracted from the string by the delegateTokenizer and delegateSanitizer.
+   * Unlike the tokenize method this method does not give any special recognition to arrays.
+   *
+   * The first token in a returned array will be the ascii emoji if one was recognised.
+   *
+   * @param text the text to be tokenized
+   */
+  tokenizeAndPreserveEmoji(text: string): Array<string> {
+    const parts: string[] = [];
+    if (isAsciiEmoji(text)) {
+      parts.push(text);
+    }
 
-  private isAsciiEmoji(text: string): boolean {
-    return text.match(asciiRegexp) != null;
+    return parts.concat(this.tokenizeNonEmojiText(text));
+  }
+
+  private tokenizeNonEmojiText(text: string): Array<string> {
+    return this.delegateTokenizer.tokenize(this.delegateSanitizer.sanitize(text));
   }
 }
 
@@ -45,3 +70,7 @@ export class NoOpSanitizer implements ISanitizer {
     return text;
   }
 }
+
+export function isAsciiEmoji(text: string): boolean {
+    return text.match(asciiRegexp) != null;
+};

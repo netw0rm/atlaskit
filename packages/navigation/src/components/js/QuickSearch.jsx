6ea5bda56562946432d49110008ab69d1f661154
Jonathan Yeo <jyeo@atlassian.com>
2017-06-13 16:49:36 +1000

    feat(component): create basic quick search component
    affects: @atlaskit/navigation
    
    Create basic quick search component.  At this stage it is essentially just a plain composition of
    Search and SearchResults

---
+++ packages/navigation/src/components/js/QuickSearch.jsx
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import { AkSearch, AkSearchResults } from '../../../src';
+
+export default class QuickSearch extends PureComponent {
+  static propTypes = {
+    /* Search pass-through props */
+    isLoading: PropTypes.bool,
+    onChange: PropTypes.func.isRequired,
+    onSearchClear: PropTypes.func,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+
+    /* SearchResults pass-through props */
+    results: AkSearchResults.propTypes.results,
+  }
+
+  render() {
+    return (
+      <AkSearch
+        isLoading={this.props.isLoading}
+        onChange={this.props.onChange}
+        onSearchClear={this.props.onSearchClear}
+        placeholder={this.props.placeholder}
+        value={this.props.value}
+      >
+        <AkSearchResults results={this.props.results} />
+      </AkSearch>
+    );
+  }
+}

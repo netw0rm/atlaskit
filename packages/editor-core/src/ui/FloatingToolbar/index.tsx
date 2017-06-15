0e738d9a20cc4d0f39d92dfb9d80477e08e16159
Stanislav Sysoev <ssysoev@atlassian.com>
2017-06-15 10:54:09 +1000

    feat(component): new popups and dropdowns for editor-core
    affects: @atlaskit/editor-core
    
    BREAKING CHANGE:
    Might introduce some regressions, releasing new major version as a precaution
    
    ISSUES CLOSED: ED-1468 ED-1440

--- packages/editor-core/src/ui/FloatingToolbar/index.tsx
+++ packages/editor-core/src/ui/FloatingToolbar/index.tsx
@@ -1,139 +1,51 @@
 import * as React from 'react';
 import { PureComponent } from 'react';
-import * as ReactDOM from 'react-dom';
-import OutsideClickable from '../OutsideClickable';
-import Popper, { IPopper } from './../../popper';
+import Popup from '../Popup';
 import { Container } from './styles';
-import { akEditorFloatingPanelZIndex } from '../../styles';
 export type Coordniates = { left: number, right: number, top: number, bottom: number };
 
 export interface Props {
-  autoPosition?: boolean;
-  align?: 'left' | 'center' | 'right';
   onOutsideClick?: () => void;
-  target?: HTMLElement;
-  spacing?: 'none';
   className?: string | undefined;
-  onExtractStyle?: (state: any) => Coordniates | undefined;
+  children?: any;
+  target?: HTMLElement;
+  offset?: number[];
+  fitWidth?: number;
+  fitHeight?: number;
+  onPositionCalculated?: (position: any) => any;
+  popupsMountPoint?: HTMLElement;
+  popupsBoundariesElement?: HTMLElement;
 }
 
-export interface State {
-  position?: string;
-  transform?: string;
-}
-
-export default class FloatingToolbar extends PureComponent<Props, State> {
-  state: State = {};
-  popper?: IPopper;
-  content?: HTMLElement;
-
-  componentDidMount() {
-    this.applyPopper(this.props);
-  }
-
-  componentWillUnmount() {
-    if (this.popper) {
-      this.popper.destroy();
-      this.popper = undefined;
-    }
-  }
-
-  componentWillReceiveProps(nextProps: Props) {
-    this.applyPopper(nextProps);
-  }
-
-  extractStyles = (state: any) => {
-    if (state) {
-      const { onExtractStyle } = this.props;
-      const { left, top } = (onExtractStyle && onExtractStyle(state)) || state.offsets.popper;
-      this.setState({
-        position: state.offsets.popper.position,
-        transform: `translate3d(${Math.round(left)}px, ${Math.round(top)}px, 0px)`,
-      });
-    }
-  }
-
-  private applyPopper(props: Props): void {
-    const target = props.target || ReactDOM.findDOMNode(this).parentElement!;
-    const boundary = this.findBoundary(target);
-
-    if (target && boundary && this.content instanceof HTMLElement) {
-      if (this.popper) {
-        this.popper.destroy();
-      }
-
-      this.popper = new Popper(target, this.content, {
-        onCreate: this.extractStyles,
-        onUpdate: this.extractStyles,
-        placement: this.popperPlacement(),
-        boundariesElement: boundary,
-        modifiers: {
-          applyStyle: {
-            enabled: false,
-          },
-          offset: {
-            enabled: true,
-            offset: '0 3px',
-          },
-          hide: {
-            enabled: false
-          },
-          flip: {
-            enabled: this.props.autoPosition,
-            flipVariations: true,
-          },
-          preventOverflow: {
-            enabled: this.props.autoPosition,
-            moveWithTarget: true,
-          },
-        },
-      });
-    }
-  }
-
+export default class FloatingToolbar extends PureComponent<Props, any> {
   render() {
-    const { position, transform } = this.state;
-    const { className } = this.props;
-    const padding = this.props.spacing === 'none'
-      ? '0'
-      : undefined;
+    const {
+      children,
+      className,
+      target, offset, fitWidth, fitHeight, popupsMountPoint, popupsBoundariesElement,
+      onPositionCalculated
+    } = this.props;
+
+    if (!target) {
+      return null;
+    }
 
     return (
-      <OutsideClickable onClick={this.props.onOutsideClick}>
-        <Container
-          innerRef={this.handleOutsideClickableRef}
-          style={{ top: 0, left: 0, position, transform, padding, zIndex: akEditorFloatingPanelZIndex }}
-          className={`${className || ''}`}
+
+        <Popup
+          target={target}
+          offset={offset}
+          fitWidth={fitWidth}
+          fitHeight={fitHeight}
+          mountTo={popupsMountPoint}
+          boundariesElement={popupsBoundariesElement}
+          onPositionCalculated={onPositionCalculated}
         >
-          {this.props.children}
-        </Container>
-      </OutsideClickable>
+          <Container className={`${className || ''}`} >
+            {children}
+          </Container>
+        </Popup>
+
     );
-  }
-
-  private handleOutsideClickableRef = (ref) => {
-    this.content = ref;
-  }
-
-  private findBoundary(elem: HTMLElement): HTMLElement {
-    while (elem.parentElement) {
-      elem = elem.parentElement;
-      if (elem.dataset['editorChrome']) {
-        return elem;
-      }
-    }
-    return elem;
-  }
-
-  private popperPlacement() {
-    const align = this.props.align || 'left';
-    switch (align) {
-      case 'left':
-        return 'bottom-start';
-      case 'center':
-        return 'bottom';
-      case 'right':
-        return 'bottom-end';
-    }
   }
 }

8ad83d90696f4d46a9b2bc07cc3dccffc09d7fae
Lachlan Hunt <lhunt@atlassian.com>
2017-06-16 15:26:23 +1000

    WIP(component): request trial dialog, generic and confluence
    affects: @atlaskit/cross-acquisition

--- packages/cross-acquisition/src/components/ConfluenceRequestTrial.jsx
+++ packages/cross-acquisition/src/components/ConfluenceRequestTrial.jsx
@@ -1,4 +1,13 @@
+import React, { Component } from 'react';
+import { ConfluenceLogo } from '@atlaskit/logo';
 import RequestTrial from './RequestTrial';
 
-// TODO: Determine whether to use composition of inheritance.
-export default RequestTrial;
+export default class ConfluenceRequestTrial extends Component {
+  render() {
+    return (
+      <RequestTrial productLogo={<ConfluenceLogo />} header="Request access to confluence">
+        <p>Send a request for your admin to activate confluence</p>
+      </RequestTrial>
+    );
+  }
+}
--- packages/cross-acquisition/src/components/RequestTrial.jsx
+++ packages/cross-acquisition/src/components/RequestTrial.jsx
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AtlassianLogo } from '@atlaskit/logo';
+import ModalDialog from '@atlaskit/modal-dialog';
+import Button from '@atlaskit/button';
+import styled from 'styled-components';
+
+export const RequestTrialHeader = styled.h3`
+  margin-top: 0px;
+`;
 
 export default class RequestTrial extends Component {
+  static propTypes = {
+    productLogo: PropTypes.element,
+    header: PropTypes.string.isRequired,
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    productLogo: <AtlassianLogo />,
+  }
+
   render() {
     return (
-      <div />
+      <div>
+        <ModalDialog
+          isOpen
+          header={this.props.productLogo}
+          footer={
+            <p>
+              <Button appearance="primary">Request access</Button>
+              <Button appearance="subtle-link">Cancel</Button>
+            </p>
+          }
+        >
+          <RequestTrialHeader>{this.props.header}</RequestTrialHeader>
+          {this.props.children}
+        </ModalDialog>
+      </div>
     );
   }
 }

596c17f6044cc22df37f60daf774875920474cb3
Paul Curren <pcurren@atlassian.com>
2017-06-22 07:50:30 +1000

    test(component): added missing tests for when MentionItem should render a numeric avatar.
    affects: @atlaskit/mention

--- packages/mention/test/components/MentionItemSpec.tsx
+++ packages/mention/test/components/MentionItemSpec.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import { expect } from 'chai';
 import LockCircleIcon from '@atlaskit/icon/glyph/lock-circle';
-
 import { MentionDescription } from '../../src/types';
 import { Props, State } from '../../src/components/MentionList';
 import MentionItem from '../../src/components/MentionItem';
+import NumericAvatar from '../../src/components/NumericAvatar';
+import Avatar from '@atlaskit/avatar';
 
 const mentionWithNickname = {
     id: '0',
@@ -72,5 +73,44 @@ describe('MentionItem', () => {
       avatarUrl: '',
     });
     expect(component.find(LockCircleIcon).length).to.equal(0);
+  });
+
+  it('should display number on avatar for specific avatar URLs', () => {
+    const component = setupMentionItem({
+      id: '1',
+      name: 'Monkey Trousers',
+      mentionName: 'monkeytrousers',
+      avatarUrl: 'ak-numvatar:32',
+    });
+
+    const avatar = component.find(NumericAvatar);
+    expect(avatar.length).to.equal(1);
+    expect(avatar.prop('num')).to.equal(32);
+  });
+
+  it('should display default avator for numeric URL containing non-number', () => {
+    const component = setupMentionItem({
+      id: '1',
+      name: 'Monkey Trousers',
+      mentionName: 'monkeytrousers',
+      avatarUrl: 'ak-numvatar:32d',
+    });
+
+    expect(component.find(NumericAvatar).length).to.equal(0);
+    expect(component.find(Avatar).length).to.equal(1);
+  });
+
+  it('should display standard avatar for HTTP urls', () => {
+    const expectedUrl = 'http://www.example.org/monkeytrousers.png';
+    const component = setupMentionItem({
+      id: '1',
+      name: 'Monkey Trousers',
+      mentionName: 'monkeytrousers',
+      avatarUrl: expectedUrl,
+    });
+
+    const avatar = component.find(Avatar);
+    expect(avatar.length).to.equal(1);
+    expect(avatar.prop('src')).to.equal(expectedUrl);
   });
 });

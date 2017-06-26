258f72d82df8b33ca1ef07399e112cf320531326
James Newell <jnewell@atlassian.com>
2017-06-26 14:43:40 +1000

    WIP: added app types to media-core

--- packages/media-core/src/item.ts
+++ packages/media-core/src/item.ts
@@ -54,7 +54,7 @@ export interface Resources {
   image?: Resource;
   file?: Resource;
   player?: Resource;
-  app?: any; // FIXME
+  app?: App;
 }
 
 export interface Resource {
@@ -66,3 +66,58 @@ export interface Resource {
   html?: string;
 }
 
+export interface App {
+  title: AppTitle;
+  description?: AppDescription;
+  details?: AppDetail[];
+  context?: AppContext;
+  preview?: {url: string}; // an image URL
+  background?: {url: string}; // an image URL
+}
+
+export interface AppTitle {
+  text: string;
+  user?: AppUser;
+  lozenge?: AppLozenge;
+}
+
+export interface AppDescription {
+  title?: string; // the bolded bit
+  text: string;
+}
+
+export interface AppIcon {
+  url: string;    // an image URL
+  label: string;  // accessibility text e.g. tooltip or voiceover
+}
+
+export interface AppUser {
+  icon: AppIcon;
+}
+
+export interface AppBadge {
+  value: number;
+  max?: number;
+  theme?: 'default' | 'dark';
+  appearance?: 'default' | 'primary' | 'important' | 'added' | 'removed';
+}
+
+export interface AppLozenge {
+  text: string;
+  bold?: boolean;
+  appearance?: 'default' | 'success' | 'removed' | 'inprogress' | 'new' | 'moved';
+}
+
+export interface AppDetail {
+  title?: string;
+  icon?: AppIcon;
+  badge?: AppBadge;
+  lozenge?: AppLozenge;
+  users?: AppUser[];
+  text?: string;
+}
+
+export interface AppContext {
+  text: string;
+  icon?: AppIcon;
+}

4e6233f1850c087ebd858f174c30e9946b469d99
Jonathan Yeo <jyeo@atlassian.com>
2017-06-26 16:33:37 +1000

    *WIP(refactor): wip refactor
    refactor(refactor): refactor selectItem state to selectItemId
    
    affects: @atlaskit/navigation

--- packages/navigation/src/components/js/QuickSearchWithKeyboardControls.jsx
+++ packages/navigation/src/components/js/QuickSearchWithKeyboardControls.jsx
@@ -9,6 +9,11 @@ const flattenResults = results => (
   ), [])
 );
 
+/**
+ * The value, null, is used to represent 'no selection'.  Please remember to
+ * appropriately check for null when using the selectedItemId state
+ */
+
 export const withKeyboardControls = QuickSearchComp => (
   class WithKeyboardControls extends Component {
     static propTypes = {
@@ -16,41 +21,82 @@ export const withKeyboardControls = QuickSearchComp => (
       onResultClick: PropTypes.func.isRequired,
     }
 
+    static defaultProps = {
+      results: [],
+    }
+
     flatResults = flattenResults(this.props.results);
 
+    /**
+     * Get the ID of an item by its index in the flatResults array
+     * Returns null for a failed index or if id is empty|undefined
+     */
+    getItemIdByIndex = index => (this.flatResults[index] && this.flatResults[index].id) || null;
+
+    /**
+     * Get an item's index in the flatResults array by its ID
+     * Returns a numberic index or null
+     */
+    getItemIndexById = (id) => {
+      const item = this.getItemById(id);
+      const index = this.flatResults.indexOf(item);
+      return index >= 0 ? index : null;
+    }
+
+    /**
+     * Find an item in the flatResults array by its ID
+     * Returns the item object or null
+     */
+    getItemById = id => this.flatResults.find(item => item.id === id) || null;
+
     state = {
-      selectedItem: this.flatResults[0] || null,
+      selectedItemId: this.getItemIdByIndex(0),
     }
 
     componentWillReceiveProps(nextProps) {
       if (nextProps.results) {
         this.flatResults = flattenResults(nextProps.results);
         this.setState({
-          selectedItem: this.flatResults[0],
+          selectedItemId: this.getItemIdByIndex(0),
         });
       }
     }
 
-    selectedItemIndex = () => {
-      const currendIdx = this.flatResults.indexOf(this.state.selectedItem);
-      return currendIdx >= 0 ? currendIdx : null;
-    }
-
     selectNext = () => {
-      const currentIdx = this.selectedItemIndex();
-      const newIdx = (currentIdx + 1) < this.flatResults.length ? currentIdx + 1 : 0;
+      // Don't bother if results is empty
+      if (this.props.results.length === 0) return;
+
+      let newIdx;
+      const currentIdx = this.getItemIndexById(this.state.selectedItemId);
+      // If nothing is selected, select the first element
+      // Otherwise, select the next element, wrapping around to the top if necessary
+      if (this.state.selectedItemId === null || currentIdx === null) {
+        newIdx = 0;
+      } else {
+        newIdx = (currentIdx + 1 < this.flatResults.length) ? currentIdx + 1 : 0;
+      }
       this.setState({
-        selectedItem: this.flatResults[newIdx],
+        selectedItemId: this.getItemIdByIndex(newIdx),
       });
     };
 
     selectPrevious = () => {
-      const currentIdx = this.selectedItemIndex();
-      const newIdx = currentIdx > 0 ? currentIdx - 1 : this.flatResults.length - 1;
+      // Don't bother if results is empty
+      if (this.props.results.length === 0) return;
+
+      let newIdx;
+      const currentIdx = this.getItemIndexById(this.state.selectedItemId);
+      // If nothing is selected, select the last element
+      // Otherwise, select the previous element, wrapping around to the bottom if necessary
+      if (this.state.selectedItemId === null || currentIdx === null) {
+        newIdx = this.flatResults.length - 1;
+      } else {
+        newIdx = (currentIdx > 0) ? currentIdx - 1 : this.flatResults.length - 1;
+      }
       this.setState({
-        selectedItem: this.flatResults[newIdx],
+        selectedItemId: this.getItemIdByIndex(newIdx),
       });
-    }
+    };
 
     handleSearchKeyDown = (event) => {
       if (event.key === 'ArrowUp') {
@@ -59,8 +105,9 @@ export const withKeyboardControls = QuickSearchComp => (
       } else if (event.key === 'ArrowDown') {
         event.preventDefault(); // Don't move cursor around in search input field
         this.selectNext();
-      } else if (event.key === 'Enter') {
-        this.props.onResultClick(this.state.selectedItem);
+      } else if (event.key === 'Enter' && this.state.selectedItemId) {
+        event.preventDefault(); // Don't fire submit event from input
+        this.props.onResultClick(this.getItemById(this.state.selectedItemId));
       }
     };
 
@@ -69,7 +116,7 @@ export const withKeyboardControls = QuickSearchComp => (
         <QuickSearchComp
           {...this.props}
           onSearchKeyDown={this.handleSearchKeyDown}
-          selectedItemId={this.state.selectedItem && this.state.selectedItem.id}
+          selectedItemId={this.state.selectedItemId}
         />
       );
     }

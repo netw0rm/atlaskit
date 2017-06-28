7b47bdf5bf60dca96e6407ca8086e94aade43ea4
Dmitrii Sorin <dsorin@atlassian.com>
2017-06-28 15:00:33 +1000

    fix(component): copy hipchat schema into the editor-core
    affects: @atlaskit/editor-core

--- packages/editor-core/src/schema/index.ts
+++ packages/editor-core/src/schema/index.ts
@@ -39,3 +39,4 @@ export { createSchema } from './create-schema';
 export { bitbucketSchema } from './bitbucket-schema';
 export { confluenceSchema } from './confluence-schema';
 export { defaultSchema } from './default-schema';
+export { hipchatSchema } from './hipchat-schema';

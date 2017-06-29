87a52d9d4e5325df37bdbfb16f5ffbff32364580
Jacob Bass <jbass@atlassian.com>
2017-06-29 15:11:52 +1000

    dont bootstrap in postinstall

--- build/bin/postinstall.sh
+++ build/bin/postinstall.sh
@@ -18,31 +18,31 @@ if [[ -z "$BITBUCKET_COMMIT" ]]; then
 fi
 
 # check if we are running in CI (BITBUCKET_COMMIT will exist in CI)
-if [[ "$BITBUCKET_COMMIT" != "" ]]; then
-  # piping to cat is used to put stdout in a non-TTY mode (hides the progress bar)
-  $LERNA_LOC bootstrap | cat
-  # we test PIPESTATUS here to know if they previous command failed
-  test ${PIPESTATUS[0]} -eq 0
-else
-  # Need to bootstrap @atlaskit/util-cz-atlaskit-changelog so that we can run commitizen (hide the output as it's pretty noisy)
-  $CHALK --no-stdin -t "{blue Bootstrapping @atlaskit/util-cz-lerna-changelog for commitizen}"
-  yarn run bootstrap/single @atlaskit/util-cz-atlaskit-changelog > /dev/null
-
-  # Also need to bootstrap icons because bootstrap/single/with-deps wont pick it up
-  $CHALK --no-stdin -t "{blue Bootstrapping @atlaskit/icon...}"
-  # $LERNA_LOC bootstrap --scope=@atlaskit/icon > /dev/null
-  yarn run bootstrap/single @atlaskit/icon > /dev/null
-
-
-  $CHALK --no-stdin -t "{green ~~~~~~~~~~~~~~~~~~~~~~~~~~~~}"
-  $CHALK --no-stdin -t "{green ~~ SUCCESSFULLY INSTALLED ~~}"
-  $CHALK --no-stdin -t "{green ~~~~~~~~~~~~~~~~~~~~~~~~~~~~}"
-  echo ""
-  $CHALK --no-stdin -t "{green You can now bootstrap the packages using one of the following commands}"
-  echo ""
-  $CHALK --no-stdin -t "{green \`yarn run bootstrap\` - Installs and links all packages}"
-  $CHALK --no-stdin -t "{green \`yarn run bootstrap/single @atlaskit/packageName\` - Installs and links a single package}"
-  $CHALK --no-stdin -t "{green \`yarn run bootstrap/single/with-deps @atlaskit/packageName\` - Installs and links a single package and all depdendencies of that package}"
-
-  $PSA
-fi
+# if [[ "$BITBUCKET_COMMIT" != "" ]]; then
+#   # piping to cat is used to put stdout in a non-TTY mode (hides the progress bar)
+#   $LERNA_LOC bootstrap | cat
+#   # we test PIPESTATUS here to know if they previous command failed
+#   test ${PIPESTATUS[0]} -eq 0
+# else
+#   # Need to bootstrap @atlaskit/util-cz-atlaskit-changelog so that we can run commitizen (hide the output as it's pretty noisy)
+#   $CHALK --no-stdin -t "{blue Bootstrapping @atlaskit/util-cz-lerna-changelog for commitizen}"
+#   yarn run bootstrap/single @atlaskit/util-cz-atlaskit-changelog > /dev/null
+#
+#   # Also need to bootstrap icons because bootstrap/single/with-deps wont pick it up
+#   $CHALK --no-stdin -t "{blue Bootstrapping @atlaskit/icon...}"
+#   # $LERNA_LOC bootstrap --scope=@atlaskit/icon > /dev/null
+#   yarn run bootstrap/single @atlaskit/icon > /dev/null
+#
+#
+#   $CHALK --no-stdin -t "{green ~~~~~~~~~~~~~~~~~~~~~~~~~~~~}"
+#   $CHALK --no-stdin -t "{green ~~ SUCCESSFULLY INSTALLED ~~}"
+#   $CHALK --no-stdin -t "{green ~~~~~~~~~~~~~~~~~~~~~~~~~~~~}"
+#   echo ""
+#   $CHALK --no-stdin -t "{green You can now bootstrap the packages using one of the following commands}"
+#   echo ""
+#   $CHALK --no-stdin -t "{green \`yarn run bootstrap\` - Installs and links all packages}"
+#   $CHALK --no-stdin -t "{green \`yarn run bootstrap/single @atlaskit/packageName\` - Installs and links a single package}"
+#   $CHALK --no-stdin -t "{green \`yarn run bootstrap/single/with-deps @atlaskit/packageName\` - Installs and links a single package and all depdendencies of that package}"
+#
+#   $PSA
+# fi

8d53fb8f3dae05b852476d3467f8d73856fdb9fc
Luke Batchelor <lbatchelor@atlassian.com>
2017-06-30 16:33:33 +1000

    chore(build): adds lerna semantic release as a root dep and uses its version for release
    This is to test if the globally installed lsr is not releasing certain packages

--- build/bin/release.process.bash
+++ build/bin/release.process.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-LSR="lerna-semantic-release"
+LSR="./node_modules/.bin/lerna-semantic-release"
 
 $LSR pre && yarn run docs && $LSR post && $LSR perform && echo "Released packages:" && touch .released-packages && cat .released-packages || exit 1

c91286dc80e3db2a67f6de26ddcdc23276beb078
Andrew Hammond <ahammond@atlassian.com>
2017-07-04 16:33:49 +1000

    WIP(component): add spinner for intializing screen
    affects: @atlaskit/cross-acquisition

--- packages/cross-acquisition/src/common/components/InitializingScreen.jsx
+++ packages/cross-acquisition/src/common/components/InitializingScreen.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import ModalDialog from '@atlaskit/modal-dialog';
+import Spinner from '@atlaskit/spinner';
+import InitializingSpinnerDiv from '../styled/InitializingSpinnerDiv';
 
 class InitializingScreen extends Component {
   render() {
-    return <div>Initializing</div>;
+    return (
+      <ModalDialog isOpen width="small" header={<div />} footer={<div />}>
+        <InitializingSpinnerDiv>
+          <Spinner size="large" isCompleting={false} />
+        </InitializingSpinnerDiv>
+      </ModalDialog>
+    );
   }
 }
 

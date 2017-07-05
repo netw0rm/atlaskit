3d6d3ea69b188e83bb3b5257ed09ce20048721a7
Jacob Bass <jbass@atlassian.com>
2017-07-05 15:38:32 +1000

    test(component): field-base should use local paths rather than import from @atlaskit/field-base to a
    affects: @atlaskit/field-base

--- packages/field-base/test/unit/Label.js
+++ packages/field-base/test/unit/Label.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
 
-import { Label } from '@atlaskit/field-base';
+import { Label } from '../../src';
 import {
   RequiredIndicator,
   LabelInner,

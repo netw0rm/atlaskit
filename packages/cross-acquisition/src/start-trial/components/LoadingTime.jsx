e8a7a37e8e115052271fb515f9766e96a0c998ed
George Burrows <gburrows@atlassian.com>
2017-07-05 15:54:31 +1000

    WIP(component): add progress to loading
    affects: @atlaskit/cross-acquisition

--- packages/cross-acquisition/src/start-trial/components/LoadingTime.jsx
+++ packages/cross-acquisition/src/start-trial/components/LoadingTime.jsx
@@ -14,28 +14,29 @@ import { crossSellShape } from '../../common/components/CrossSellProvider';
 export default class LoadingTime extends Component {
   static propTypes = {
     onComplete: PropTypes.func.isRequired,
+    progress: PropTypes.number,
   };
 
   static contextTypes = crossSellShape;
 
   render() {
     return (
-    <ModalDialog
-      isOpen
-      width="small"
-      header={this.context.crossSell.productLogo}
-      footer={
-        <StartTrialFooter>
-          <Button isDisabled={this.props.progress === undefined || this.props.progress < 100} onClick={this.props.onComplete} appearance="primary">Go to Confluence</Button>
-          <Button onClick={this.props.onComplete} appearance="subtle-link" >Close</Button>
-        </StartTrialFooter>
-      }
-    >
-      <StartTrialDialog>
-        <StartTrialHeader>You're almost there...</StartTrialHeader>
-        <ProgressBar progress={this.props.progress} />
-      </StartTrialDialog>
-    </ModalDialog>
+      <ModalDialog
+        isOpen
+        width="small"
+        header={this.context.crossSell.productLogo}
+        footer={
+          <StartTrialFooter>
+            <Button isDisabled={this.props.progress === undefined || this.props.progress < 100} onClick={this.props.onComplete} appearance="primary">Go to Confluence</Button>
+            <Button onClick={this.props.onComplete} appearance="subtle-link" >Close</Button>
+          </StartTrialFooter>
+        }
+      >
+        <StartTrialDialog>
+          <StartTrialHeader>You are almost there...</StartTrialHeader>
+          <ProgressBar progress={this.props.progress} />
+        </StartTrialDialog>
+      </ModalDialog>
     );
   }
 }

9befc791217e0fe50a409039761443ec659ecdef
Pete Gleeson <pgleeson@atlassian.com>
2017-07-06 18:10:42 +1000

    feat(component): adds rendering of ellipsis in pagination component when there are lots of pages
    affects: @atlaskit/pagination

--- packages/pagination/src/styled/index.jsx
+++ packages/pagination/src/styled/index.jsx
@@ -10,3 +10,10 @@ export const ButtonActive = styled(Button)`
   color: #000 !important; /* @todo: until Button supports not faded disabled state */
   font-weight: bold;
 `;
+
+export const Ellipsis = styled.span`
+  display: flex;
+  padding: 0 10px;
+  text-align: center;
+  align-items: center;
+`;

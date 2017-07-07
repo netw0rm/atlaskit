152ba311958f8f5d1fe1f5b3ab17dd7807638ac6
Luke Batchelor <lbatchelor@atlassian.com>
2017-07-07 16:20:42 +1000

    test(package): moves logo tests to jest
    affects: @atlaskit/logo

--- packages/logo/jest/unit/Logo.js
+++ packages/logo/jest/unit/Logo.js
@@ -1,27 +1,27 @@
 import { shallow, mount } from 'enzyme';
 import React from 'react';
-import Logo from '../src/components/LogoBase';
+import Logo from '../../src/components/LogoBase';
 
 describe('<Logo />', () => {
   describe('props', () => {
     it('should render an svg', () => {
       const wrapper = mount(<Logo><svg /></Logo>);
       // throw shallow(<svg />);
-      expect(wrapper.find('svg')).to.have.length(1);
+      expect(wrapper.find('svg')).toHaveLength(1);
     });
     describe('collapseTo prop', () => {
       it('should not collapse by default', () => {
         const wrapper = shallow(<Logo size="small" />);
-        expect(wrapper.props().collapseTo).to.equal(undefined);
+        expect(wrapper.props().collapseTo).toBe(undefined);
       });
 
       // Note: other values can be supplied, but will cause a PropType warning
       it('should accept "type" or "icon"', () => {
         const typeWrapper = shallow(<Logo collapseTo="type" size="small" />);
-        expect(typeWrapper.props().collapseTo).to.equal('type');
+        expect(typeWrapper.props().collapseTo).toBe('type');
 
         const iconWrapper = shallow(<Logo collapseTo="icon" size="small" />);
-        expect(iconWrapper.props().collapseTo).to.equal('icon');
+        expect(iconWrapper.props().collapseTo).toBe('icon');
       });
     });
   });

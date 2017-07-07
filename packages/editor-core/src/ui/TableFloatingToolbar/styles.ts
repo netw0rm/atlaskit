dda933cd00649af8b238c8eab096509dbdbd0b72
Eduard Shvedai <eshvedai@atlassian.com>
2017-07-07 14:36:42 +1000

    refactor(typings): refactor typings
    affects: @atlaskit/editor-core

--- packages/editor-core/src/ui/TableFloatingToolbar/styles.ts
+++ packages/editor-core/src/ui/TableFloatingToolbar/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { akColorN80 } from '@atlaskit/util-shared-styles';
 
 // tslint:disable-next-line:variable-name
-export const Toolbar: any = styled.div`
+export const Toolbar = styled.div`
   background-color: white;
   border-radius: 3px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
@@ -14,14 +14,14 @@ export const Toolbar: any = styled.div`
   }
 `;
 // tslint:disable-next-line:variable-name
-export const AdvanceMenuItemWrap: any = styled.span`
+export const AdvanceMenuItemWrap = styled.span`
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 110px;
 `;
 // tslint:disable-next-line:variable-name
-export const AdvanceMenuItemAfter: any = styled.span`
+export const AdvanceMenuItemAfter = styled.span`
   color: ${akColorN80};
   font-size: 80%;
 `;

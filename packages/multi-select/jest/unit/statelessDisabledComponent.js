878e42c4d73fc76344858c30bb2d3288ac71fef2
Jacob Bass <jbass@atlassian.com>
2017-07-10 12:45:07 +1000

    chore(build): update multi-select to use jest
    affects: @atlaskit/multi-select

--- packages/multi-select/jest/unit/statelessDisabledComponent.js
+++ packages/multi-select/jest/unit/statelessDisabledComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { FieldBaseStateless } from '@atlaskit/field-base';
 import { MultiSelectStateless } from '../../src';
+import Trigger from '../../src/components/Trigger';
 
 import { name } from '../../package.json';
 
@@ -15,7 +16,7 @@ describe(`${name} - stateless`, () => {
     window.cancelAnimationFrame = animStub;
   });
 
-  describe('invalid component', () => {
+  describe('disabled component', () => {
     let wrapper;
     const selectItems = [
       {
@@ -32,24 +33,30 @@ describe(`${name} - stateless`, () => {
     beforeEach(() => {
       wrapper = mount(<MultiSelectStateless
         appearance="subtle"
-        isInvalid
-        isOpen
-        invalidMessage="invalid message"
+        isDisabled
         items={selectItems}
         selectedItems={selectedItems}
       />);
     });
 
-    it('should pass isInvalid property to field base', () => {
-      expect(wrapper.find(FieldBaseStateless).prop('isInvalid')).to.equal(true);
+    it('native select should be "disabled"', () => {
+      expect(wrapper.find('select[disabled]').length).toBe(1);
     });
 
-    it('should pass invalidMessage property to field base', () => {
-      expect(wrapper.find(FieldBaseStateless).prop('invalidMessage')).to.equal('invalid message');
+    it('should pass appearance property to field base', () => {
+      expect(wrapper.find(FieldBaseStateless).prop('appearance')).toBe('subtle');
     });
 
-    it('should set isDialogOpen property on field base', () => {
-      expect(wrapper.find(FieldBaseStateless).prop('isDialogOpen')).to.equal(true);
+    it('should pass isDisabled property to field base', () => {
+      expect(wrapper.find(FieldBaseStateless).prop('isDisabled')).toBe(true);
+    });
+
+    it('should pass isDisabled property to Trigger sub-component', () => {
+      expect(wrapper.find(Trigger).prop('isDisabled')).toBe(true);
+    });
+
+    it('should not render input if disabled', () => {
+      expect(wrapper.find('input[disabled]').length).toBe(0);
     });
   });
 });

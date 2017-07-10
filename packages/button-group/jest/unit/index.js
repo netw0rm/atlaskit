95cfb17ea7ae77e3ae6a3366a3490ae8617d453c
Jacob Bass <jbass@atlassian.com>
2017-07-10 10:23:33 +1000

    chore(build): configure jest to run from project root as well as package root, to speed up running t
    affects: @atlaskit/button-group

--- packages/button-group/jest/unit/index.js
+++ packages/button-group/jest/unit/index.js
@@ -9,15 +9,15 @@ describe(name, () => {
   describe('basic behavior', () => {
     it('should render all the children', () => {
       const group = shallow(<ButtonGroup><div>1</div><div>2</div><div>3</div></ButtonGroup>);
-      expect(group.contains(<div>1</div>)).to.equal(true);
-      expect(group.contains(<div>2</div>)).to.equal(true);
-      expect(group.contains(<div>3</div>)).to.equal(true);
-      expect(group.children().length).to.equal(3);
+      expect(group.contains(<div>1</div>)).toBe(true);
+      expect(group.contains(<div>2</div>)).toBe(true);
+      expect(group.contains(<div>3</div>)).toBe(true);
+      expect(group.children().length).toBe(3);
     });
 
     it(`should have className ${styles.wrapper}`, () => {
       const group = shallow(<ButtonGroup />);
-      expect((group).hasClass((styles.wrapper))).to.equal(true);
+      expect((group).hasClass((styles.wrapper))).toBe(true);
     });
   });
 });

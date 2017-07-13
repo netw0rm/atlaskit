9e407b5550f77e61a49e13686dfd35a7570c1a87
Ben Conolly <bconolly@atlassian.com>
2017-07-13 16:00:17 +1000

    test(docs): move tests to jest
    affects: @atlaskit/button

--- packages/button/jest/unit/buttonGroup.js
+++ packages/button/jest/unit/buttonGroup.js
@@ -3,16 +3,16 @@ import React from 'react';
 
 import { name } from '../../package.json';
 import { ButtonGroup } from '../../src';
-import { ButtonGroupDiv } from '../../src/styled/ButtonGroup';
+import ButtonGroupDiv from '../../src/styled/ButtonGroup';
 
-describe(`${name} - button group`, () => {
+describe(name, () => {
   describe('basic behavior', () => {
     it('should render all the children', () => {
       const group = shallow(<ButtonGroup><div>1</div><div>2</div><div>3</div></ButtonGroup>);
-      expect(group.contains(<div>1</div>)).to.equal(true);
-      expect(group.contains(<div>2</div>)).to.equal(true);
-      expect(group.contains(<div>3</div>)).to.equal(true);
-      expect(group.children().length).to.equal(3);
+      expect(group.contains(<div>1</div>)).toBe(true);
+      expect(group.contains(<div>2</div>)).toBe(true);
+      expect(group.contains(<div>3</div>)).toBe(true);
+      expect(group.children().length).toBe(3);
     });
 
     it('should have a ButtonGroupDiv', () => {

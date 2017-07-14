6f7b508e20689b5dc8c985bfe91a43bc2f091d74
Dmitrii Sorin <dsorin@atlassian.com>
2017-07-14 14:37:43 +1000

    feat(component): add trigger property to AkProfilecardTrigger (possible values: click, hover (defaul
    affects: @atlaskit/profilecard
    
    ISSUES CLOSED: ED-1867

--- packages/profilecard/stories/util.js
+++ packages/profilecard/stories/util.js
@@ -1,20 +1,17 @@
-export const random = int => Math.floor(Math.random() * (int + 1));
+import { profilecard as profilecardUtils } from '@atlaskit/util-data-test';
+import {
+  AkProfileClient,
+  modifyResponse,
+} from '../src';
 
-export const getWeekday = () => {
-  const array = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  const index = random(6);
+const { getMockProfileClient: getMockProfileClientUtil } = profilecardUtils;
+const MockProfileClient = getMockProfileClientUtil(AkProfileClient, modifyResponse);
 
-  return {
-    index,
-    string: array[index],
-  };
-};
+export const getMockProfileClient = (cacheSize, cacheMaxAge) => (
+  new MockProfileClient({
+    cacheSize,
+    cacheMaxAge,
+  })
+);
 
-export const getTimeString = () => {
-  const minFormat = new Intl.NumberFormat('us-EN', { minimumIntegerDigits: 2 });
-  const hours = random(23);
-  const minutes = random(59);
-  const meridiem = ['am', 'pm'][Math.floor(hours / 12)];
-
-  return `${hours === 0 ? 12 : hours % 12}:${minFormat.format(minutes)}${meridiem}`;
-};
+export default null;

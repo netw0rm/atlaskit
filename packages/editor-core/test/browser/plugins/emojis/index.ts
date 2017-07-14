netw0rm/atlaskit

commit 362d541a25274dc0b852c475fb989788900afe87

feat(component): keep the leading colon in the query being passed to the typeahead
affects: @atlaskit/editor-core

Keeping the leading colon in the query so that it can be used by the typeahead to match against
ascii representations of emojis that start with a colon. It doesn't have any effect on the beahviour
of js-search so same filtering as before will occur.

ISSUES CLOSED: FS-916

packages/editor-core/src/plugins/emojis/index.ts
@@ -73,7 +73,7 @@ export class EmojiState {
       }
 
       const { nodeBefore, /*nodeAfter*/ } = selection.$from;
-      const newQuery = (nodeBefore && nodeBefore.textContent || '').substr(1); // + (nodeAfter && nodeAfter.textContent || '');
+      const newQuery = (nodeBefore && nodeBefore.textContent || '');
 
       if (this.query !== newQuery) {
         dirty = true;

commit a1038121a8d3bfe52fb75138d255e0f21f20efe7

fix(component): dismiss and cancel emoji typeahead when query is empty
affects: @atlaskit/editor-core

Typing a space after the colon character would dismiss the typeahead but would not cancel the emoji
query so typing another character matching emojis would bring the typeahead back up. Similar to how
mention works, it now dismisses the typeahead and cancel the query. I believe this was a regression
caused by 362d541.

ISSUES CLOSED: FS-1149

packages/editor-core/src/plugins/emojis/index.ts
@@ -1,4 +1,4 @@
-import { EmojiId, EmojiProvider } from '@atlaskit/emoji';
+import { EmojiId, EmojiProvider, EmojiSearchResult, EmojiDescription } from '@atlaskit/emoji';
 import {
   EditorState,
   EditorView,
@@ -32,11 +32,11 @@ export class EmojiState {
   onSelectPrevious = (): boolean => false;
   onSelectNext = (): boolean => false;
   onSelectCurrent = (): boolean => false;
-  onTrySelectCurrent = (): boolean => false;
 
   private changeHandlers: StateChangeHandler[] = [];
   private state: EditorState<any>;
   private view: EditorView;
+  private queryResult: EmojiDescription[] = [];
 
   constructor(state: EditorState<any>, providerFactory: ProviderFactory) {
     this.changeHandlers = [];
@@ -174,6 +174,8 @@ export class EmojiState {
         state.tr.replaceWith(start, end, [node, textNode])
       );
       view.focus();
+      this.queryActive = false;
+      this.query = undefined;
     } else {
       this.dismiss();
     }
@@ -184,12 +186,46 @@ export class EmojiState {
       case 'emojiProvider':
         provider.then((emojiProvider: EmojiProvider) => {
           this.emojiProvider = emojiProvider;
+          if (this.emojiProvider) {
+            this.emojiProvider.subscribe(this.onProviderChange);
+          }
         }).catch(() => {
+          if (this.emojiProvider) {
+            this.emojiProvider.unsubscribe(this.onProviderChange);
+          }
           this.emojiProvider = undefined;
         });
         break;
     }
   }
+
+  trySelectCurrent = (): boolean => {
+    const emojisCount = this.getEmojisCount();
+    if (emojisCount === 1) {
+      this.insertEmoji(this.queryResult[0]);
+      return true;
+    } else if (emojisCount === 0 || this.isEmptyQuery()) {
+      this.dismiss();
+    }
+
+    return false;
+  }
+
+  private getEmojisCount = (): number => {
+    return (this.queryResult && this.queryResult.length) || 0;
+  }
+
+  private isEmptyQuery = (): boolean => {
+    return !this.query || this.query === ':';
+  }
+
+  onSearchResult = (searchResults: EmojiSearchResult): void => {
+    this.queryResult = searchResults.emojis;
+  }
+
+  private onProviderChange = {
+    result: this.onSearchResult,
+  };
 
   setView(view: EditorView) {
     this.view = view;

packages/editor-core/src/plugins/emojis/keymap.ts
@@ -56,7 +56,7 @@ export function keymapPlugin(schema: Schema<any, any>): Plugin {
       return false;
     }
 
-    return emojisPlugin.onTrySelectCurrent();
+    return emojisPlugin.trySelectCurrent();
   }, list);
 
   return keymap(list);

packages/editor-core/src/ui/EmojiTypeAhead/index.tsx
@@ -38,7 +38,6 @@ export default class EmojiTypeAhead extends PureComponent<Props, State> {
     pluginState.onSelectPrevious = this.handleSelectPrevious;
     pluginState.onSelectNext = this.handleSelectNext;
     pluginState.onSelectCurrent = this.handleSelectCurrent;
-    pluginState.onTrySelectCurrent = this.handleTrySelectCurrent;
   }
 
   componentWillUmount() {
@@ -109,19 +108,6 @@ export default class EmojiTypeAhead extends PureComponent<Props, State> {
     }
 
     return true;
-  }
-
-  private handleTrySelectCurrent = (): boolean => {
-    const emojisCount = this.getEmojisCount();
-    const { query } = this.state;
-    if (emojisCount === 1) {
-      (this.typeAhead as AkEmojiTypeAhead).chooseCurrentSelection();
-      return true;
-    } else if (emojisCount === 0 || !query) {
-      this.pluginState.dismiss();
-    }
-
-    return false;
   }
 
   private getEmojisCount(): number {

packages/editor-core/test/browser/plugins/emojis/index.ts
@@ -154,7 +154,7 @@ describe('emojis', () => {
     describe('Space', () => {
       it('should be ignored if there is no emojiProvider', () => {
         const { editorView, pluginState } = editor(doc(p(emojiQuery(':grin{<>}'))));
-        const spy = sinon.spy(pluginState, 'onTrySelectCurrent');
+        const spy = sinon.spy(pluginState, 'trySelectCurrent');
 
         forceUpdate(editorView); // Force update to ensure active query.
         sendKeyToPm(editorView, 'Space');
@@ -163,7 +163,7 @@ describe('emojis', () => {
 
       it('should be ignored if there is no active query', () => {
         const { editorView, pluginState } = editor(doc(p('Hello')));
-        const spy = sinon.spy(pluginState, 'onTrySelectCurrent');
+        const spy = sinon.spy(pluginState, 'trySelectCurrent');
         (pluginState as any).emojiProvider = true;
         forceUpdate(editorView); // Force update to ensure active query.
 
@@ -171,15 +171,36 @@ describe('emojis', () => {
         expect(spy.called, 'was not called').to.equal(false);
       });
 
-      it('should call "onTrySelectCurrent" which should return false by default', () => {
+      it('should call "trySelectCurrent" which should return false', () => {
         const { editorView, pluginState } = editor(doc(p(emojiQuery(':grin{<>}'))));
-        const spy = sinon.spy(pluginState, 'onTrySelectCurrent');
+        const spy = sinon.spy(pluginState, 'trySelectCurrent');
         (pluginState as any).emojiProvider = true;
         forceUpdate(editorView); // Force update to ensure active query.
 
         sendKeyToPm(editorView, 'Space');
         expect(spy.called, 'was called').to.equal(true);
         expect(spy.returned(false), 'return value').to.equal(true);
+      });
+
+      it('should call "insertEmoji" if there is only 1 result', () => {
+        const { editorView, pluginState } = editor(doc(p(emojiQuery(':grin{<>}'))));
+        const spy = sinon.spy(pluginState, 'insertEmoji');
+        (pluginState as any).emojiProvider = true;
+        forceUpdate(editorView); // Force update to ensure active query.
+        pluginState.onSearchResult({categories: {}, emojis: [grinEmoji]});
+
+        sendKeyToPm(editorView, 'Space');
+        expect(spy.calledWith(grinEmoji), 'was called').to.equal(true);
+      });
+
+      it('should call "dismiss" if the query is empty', () => {
+        const { editorView, pluginState } = editor(doc(p(emojiQuery(':{<>}'))));
+        const spy = sinon.spy(pluginState, 'dismiss');
+        (pluginState as any).emojiProvider = true;
+        forceUpdate(editorView); // Force update to ensure active query.
+
+        sendKeyToPm(editorView, 'Space');
+        expect(spy.called, 'was called').to.equal(true);
       });
     });
 
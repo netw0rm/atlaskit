c917a116ddb68e1adcb4fe046b4a6427ba56f0d9
Stanislav Sysoev <ssysoev@atlassian.com>
2017-07-17 08:28:31 +1000

    feat(component): add max content size plugin
    affects: @atlaskit/editor-core
    
    ISSUES CLOSED: ED-2077

--- packages/editor-core/src/editor/create-editor/index.ts
+++ packages/editor-core/src/editor/create-editor/index.ts
@@ -1,2 +1,3 @@
 export { default as createEditor } from './create-editor';
 export { default as getUiComponent } from './get-ui-component';
+export { default as createPluginsList } from './create-plugins-list';

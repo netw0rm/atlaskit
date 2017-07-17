6342e35faa7c0df4c62ce47392da9d6db0371b04
jossmac <joss.mackison@gmail.com>
2017-07-17 13:44:14 +1000

    refactor(component): move color declarations to a ThemeColor object in their respective files
    affects: @atlaskit/avatar
    
    This is in preparation for moving to a common \`theme\` for dark mode

--- packages/avatar/src/styled/AvatarImage.js
+++ packages/avatar/src/styled/AvatarImage.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 import { akColorN40 } from '@atlaskit/util-shared-styles';
 import { getBorderRadius } from './utils';
 
+const ThemeColor = { backgroundLoading: akColorN40 };
+
 // if image is loading, we hide the image so it doesn't obscure the gray loading
 // block until the source image is loaded.
-const getBackgroundColor = (props: { isLoading: boolean }) => (props.isLoading ? akColorN40 : 'transparent');
+const getBackgroundColor = (props: { isLoading: boolean }) => (
+  props.isLoading ? ThemeColor.backgroundLoading : 'transparent'
+);
 
 export const Span = styled.span`
   background-color: ${getBackgroundColor};

6129930ae7586c91080937b8afd895a3d94254b5
James Kyle <me@thejameskyle.com>
2017-07-19 12:10:56 +1000

    Inherit stdio to maintain logs with colors

--- scripts/utils/processes.js
+++ scripts/utils/processes.js
@@ -3,6 +3,10 @@
 import pSpawn from 'projector-spawn';
 
 export function spawn(cmd: string, args: Array<string> = [], opts?: child_process$spawnOpts = {}) { // eslint-disable-line
+  if (!opts.stdio) {
+    opts.stdio = 'inherit';
+  }
+
   return pSpawn(cmd, args, opts).catch((err) => {
     console.error(err); // eslint-disable-line no-console
     process.exit(1);

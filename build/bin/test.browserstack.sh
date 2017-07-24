20583ee3cf596e1f6278672e17d6cdebe400e7bb
Huanhuan Huang <hhuang@atlassian.com>
2017-07-24 16:06:28 +1000

    fix(build): temparary run bs test in my branch.

--- build/bin/test.browserstack.sh
+++ build/bin/test.browserstack.sh
@@ -10,9 +10,6 @@ CHALK="`yarn bin`/chalk"
 # $RUN_BROWSERSTACK_IN_BRANCH is used to disable browserstack tests in CI without having to merge
 # a build. We can set to "*" or empty to have it run always, or set to a specific branch to whitelist
 # the attempted fixing branch
-if [[ "$RUN_BROWSERSTACK_IN_BRANCH" = "*"  ||
-      "$RUN_BROWSERSTACK_IN_BRANCH" = "$CURRENT_BRANCH" ||
-      -z "$RUN_BROWSERSTACK_IN_BRANCH" ]] ; then
   # Run the Browserstack tests
   echo
   $CHALK --no-stdin -t "{blue Running browserstack test with all stages}"
@@ -24,6 +21,3 @@ if [[ "$RUN_BROWSERSTACK_IN_BRANCH" = "*"  ||
   -- \
   node --max-old-space-size=4096 node_modules/.bin/karma start \
   ./build/karma/browserstack/browserstackAll.js
-fi
-
-

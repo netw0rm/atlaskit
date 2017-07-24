7dc90d66af3fee9e1b0671339d635afb90ca573e
Craig Petchell <cpetchell@atlassian.com>
2017-07-25 08:43:55 +1000

    feat(component): remove emoji and mention test data from util-data-test.
    affects: @atlaskit/util-data-test
    
    BREAKING CHANGE:
    Emoji and mention data can now be imported directly from the component.
    
    ISSUES CLOSED: FS-1205

--- packages/util-data-test/src/mention/index.ts
+++
@@ -1,9 +0,0 @@
-import * as mentionStoryData from './story-data';
-import * as mentionTestData from './mention-data';
-import * as mockMentionResource from './mock-mention-resource';
-
-export default {
-  mentionStoryData,
-  mentionTestData,
-  mockMentionResource,
-};

53ab69df317f019fbaa02e16d14fe1afda8ed5fc
ckrishnakumar <ckrishnakumar@atlassian.com>
2017-07-25 16:12:26 +1000

    chore(component): update tests to run using jest
    affects: @atlaskit/renderer

--- packages/renderer/test/unit/nodes/panel.tsx
+++ packages/renderer/test/unit/nodes/panel.tsx
@@ -4,8 +4,8 @@ import { expect } from 'chai';
 import Panel from '../../../src/nodes/panel';
 
 describe('<Panel/>', () => {
-  context('info', () => {
-    const infoPanel = shallow(<Panel type="info">This is a info panel</Panel>);
+    let context = 'info';
+    const infoPanel = shallow(<Panel type="info">This is a info panel</Panel> , { context });
 
     it('should wrap content with <div>-tag', () => {
       expect(infoPanel.name()).to.equal('styled.div');
@@ -14,10 +14,9 @@ describe('<Panel/>', () => {
     it('should have two children', () => {
       expect(infoPanel.children()).to.have.length(2);
     });
-  });
 
-  context('note', () => {
-    const notePanel = shallow(<Panel type="note">This is a note panel</Panel>);
+    context = 'note';
+    const notePanel = shallow(<Panel type="note">This is a note panel</Panel> , { context });
 
     it('should wrap content with <div>-tag', () => {
       expect(notePanel.name()).to.equal('styled.div');
@@ -26,10 +25,9 @@ describe('<Panel/>', () => {
     it('should have two children', () => {
       expect(notePanel.children()).to.have.length(2);
     });
-  });
 
-  context('tip', () => {
-    const tipPanel = shallow(<Panel type="tip">This is a tip panel</Panel>);
+    context = 'tip';
+    const tipPanel = shallow(<Panel type="tip">This is a tip panel</Panel> , { context });
 
     it('should wrap content with <div>-tag', () => {
       expect(tipPanel.name()).to.equal('styled.div');
@@ -38,9 +36,8 @@ describe('<Panel/>', () => {
     it('should have two children', () => {
       expect(tipPanel.children()).to.have.length(2);
     });
-  });
 
-  context('warning', () => {
+    context = 'warning';
     const warningPanel = shallow(<Panel type="warning">This is a warning panel</Panel>);
 
     it('should wrap content with <div>-tag', () => {
@@ -50,5 +47,5 @@ describe('<Panel/>', () => {
     it('should have two children', () => {
       expect(warningPanel.children()).to.have.length(2);
     });
+
   });
-});

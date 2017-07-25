0768c9a6476602bcb8ece92ac916bd3ee9347017
Jed Watson <jed.watson@me.com>
2017-07-25 14:27:21 +1000

    feat(component): add reset and typography styles to theme
    affects: @atlaskit/theme

--- packages/theme/src/index.js
+++ packages/theme/src/index.js
@@ -52,9 +52,50 @@ export function themeValue(path) {
   };
 }
 
-const Wrapper = styled.div`
+const ThemeReset = styled.div`
   background-color: ${themeValue('colors.background')};
+  color: ${themeValue('colors.text')};
+
+  a {
+    color: ${themeValue('colors.link')};
+  }
+  a:hover {
+    color: ${themeValue('colors.linkHover')};
+  }
+  a:active {
+    color: ${themeValue('colors.linkActive')};
+  }
+  a:focus {
+    outline-color: ${themeValue('colors.linkOutline')};
+  }
+  h1 {
+    color: ${themeValue('colors.heading')};
+  }
+  h2 {
+    color: ${themeValue('colors.heading')};
+  }
+  h3 {
+    color: ${themeValue('colors.heading')};
+  }
+  h4 {
+    color: ${themeValue('colors.heading')};
+  }
+  h5 {
+    color: ${themeValue('colors.heading')};
+  }
+  h6 {
+    color: ${themeValue('colors.subtleHeading')};
+  }
+  small {
+    color: ${themeValue('colors.subtleText')};
+  }
 `;
+
+function getStylesheetResetCSS(currentTheme) {
+  return `
+    body { background: ${currentTheme.colors.background}; };
+  `;
+}
 
 export class AtlasKitThemeProvider extends Component {
   static propTypes = {
@@ -63,22 +104,45 @@ export class AtlasKitThemeProvider extends Component {
   static defaultProps = {
     mode: DEAULT_THEME_MODE,
   };
+  static childContextTypes = {
+    hasAtlasKitThemeProvider: PropTypes.bool,
+  };
+  static contextTypes = {
+    hasAtlasKitThemeProvider: PropTypes.bool,
+  };
   constructor(props) {
     super(props);
     this.state = { theme: { [CHANNEL]: buildTheme(this.props.mode) } };
   }
+  getChildContext() {
+    return { hasAtlasKitThemeProvider: true };
+  }
+  componentWillMount() {
+    if (!this.context.hasAtlasKitThemeProvider) {
+      const css = getStylesheetResetCSS(this.state.theme[CHANNEL]);
+      this.stylesheet = document.createElement('style');
+      this.stylesheet.type = 'text/css';
+      this.stylesheet.innerHTML = css;
+      document.head.appendChild(this.stylesheet);
+    }
+  }
   componentWillReceiveProps(newProps) {
     if (newProps.mode !== this.props.mode) {
-      this.setState({ theme: { [CHANNEL]: buildTheme(newProps.mode) } });
+      const newTheme = buildTheme(newProps.mode);
+      if (this.stylesheet) {
+        const css = getStylesheetResetCSS(newTheme);
+        this.stylesheet.innerHTML = css;
+      }
+      this.setState({ theme: { [CHANNEL]: newTheme } });
     }
   }
   render() {
     const { children } = this.props; // eslint-disable-line react/prop-types
     return (
       <ThemeProvider theme={this.state.theme}>
-        <Wrapper>
+        <ThemeReset>
           {children}
-        </Wrapper>
+        </ThemeReset>
       </ThemeProvider>
     );
   }

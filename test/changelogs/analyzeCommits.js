8a500b880f2dcaa19210c6288d981df7c86fb371
Ben Conolly <bconolly@atlassian.com>
2017-07-26 10:47:38 +1000

    feat(build): re-add changelog process under new idea
    affects: @atlaskit/util-cz-atlaskit-changelog

--- test/changelogs/analyzeCommits.js
+++
@@ -1,6 +0,0 @@
-describe('analyzeCommits()', () => {
-  it('should return a type');
-  it('should return an array of commits');
-  it('a commit should have a hash');
-  it('a commit should have a message');
-});

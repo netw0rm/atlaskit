73452490ba84bfe69458ae72095bce2f8e8f75ce
Sean Curtis <scurtis@atlassian.com>
2017-07-26 18:02:32 +1000

    feat(component): improved nav scrollbar styles in Chrome/Webkit/Edge
    affects: @atlaskit/navigation

--- packages/navigation/src/components/styled/ContainerNavigationChildren.jsx
+++ packages/navigation/src/components/styled/ContainerNavigationChildren.jsx
@@ -8,6 +8,21 @@ const ContainerNavigationChildren = styled.div`
   flex-direction: column;
   overflow-y: auto;
   padding: ${gridSize}px ${layout.padding.side}px;
+
+  &::-webkit-scrollbar {
+    height: ${gridSize}px;
+    width: ${gridSize}px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0,0,0,0.2);
+    border-radius: ${gridSize}px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(0,0,0,0.3);
+  }
+  -ms-overflow-style: -ms-autohiding-scrollbar;
 `;
 
 ContainerNavigationChildren.displayName = 'ContainerNavigationChildren';

ac4f81adc207cbe15f1da5db54219d4874af416a
Ben Conolly <bconolly@atlassian.com>
2017-07-31 13:14:30 +1000

    chore(build): turn changelogs back on

--- build/bin/release.process.bash
+++ build/bin/release.process.bash
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 LSR="./node_modules/.bin/lerna-semantic-release"
 
-# This is just disabled until monday when we regenerate all the changelogs
-# node ./build/bin/changelogs && $LSR pre && yarn run docs && $LSR post && $LSR perform && echo "Released packages:" && touch .released-packages && cat .released-packages || exit 1
-$LSR pre && yarn run docs && $LSR post && $LSR perform && echo "Released packages:" && touch .released-packages && cat .released-packages || exit 1
+node ./build/bin/changelogs && $LSR pre && yarn run docs && $LSR post && $LSR perform && echo "Released packages:" && touch .released-packages && cat .released-packages || exit 1

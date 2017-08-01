fe444938d7e4ffd578682690c75b51b39cac7db7
Paul Curren <pcurren@atlassian.com>
2017-08-01 14:46:25 +1000

    WIP(component): add a class that will help store the most frequently used emoji.
    affects: @atlaskit/emoji
    
    The first part of a longer piece of work. This class tracks the usage of emoji.
    Next steps will be
    to persist this data in local storage.

---
+++ packages/emoji/src/DuplicateLimitedQueue.ts
@@ -0,0 +1,142 @@
+/**
+ * A queue which will limit the number of duplicates that it holds. When the duplicate limit is reached
+ * the earliest inserted duplicate (the "original") is removed to make room for the new insertion.
+ */
+export default class DuplicateLimitedQueue {
+
+  private maximumSize: number;
+  private perItemSize: number;
+
+  private items: Array<string>;
+  private itemCountMap: Map<string, number>;
+
+  /**
+   * An array derived from items and itemCountMap which holds each item once and is ordered by
+   * how often an item is duplicated in the items array.
+   */
+  private itemsOrderedByFrequency: Array<string>;
+
+  /**
+   * Construct a new DuplicateLimitedQueue. The total size of the queue will be maxDuplicates * minUnique.
+   *
+   * @param maxDuplicates the maximum number of duplicates allowed per key
+   * @param minUnique the minimum number of unique items the queue should allow
+   */
+  constructor(maxDuplicates: number, minUnique: number) {
+    if (maxDuplicates < 1) {
+      throw new RangeError('The maxDuplicates parameter must be at least 1');
+    }
+
+    if (minUnique < 1) {
+      throw new RangeError('The minUnique parameter must be at least 1');
+    }
+
+    this.maximumSize = maxDuplicates * minUnique;
+    this.perItemSize = maxDuplicates;
+
+    this.items = new Array<string>();
+    this.itemCountMap = new Map<string, number>();
+    this.itemsOrderedByFrequency = new Array<string>();
+  }
+
+  /**
+   * @param item the item to add to the queue.
+   */
+  enqueue(item: string): void {
+    const count = this.itemCountMap.get(item);
+    if (count && count >= this.perItemSize) {
+      // find the first item with that key in the array and remove it
+      this.removeFirstOccurrence(item);
+    } else {
+      if (this.items.length >= this.maximumSize) {
+        this.remove();
+      }
+    }
+
+    this.add(item);
+
+    // enqueues are less time sensitive (and may happen less frequently) than queries against the queue
+    // so do the ordering work on each enqueue.
+    this.itemsOrderedByFrequency = this.orderItemsByFrequency();
+  }
+
+  /**
+   * Return the items in the queue, ordered by how often they are duplicated. The items with the
+   * most duplicates come first in the returned Array.
+   *
+   * If there are no items in the queue then an empty Array will be returned.
+   */
+  getItemsOrderedByDuplicateCount(): Array<string> {
+    return this.itemsOrderedByFrequency;
+  }
+
+  /**
+   * Get an array of items from the queue ordered by how often they are duplicated in the queue.
+   */
+  private orderItemsByFrequency(): Array<string> {
+    const orderedEntries = Array.from(this.itemCountMap.entries())
+      .sort((a: [string, number], b: [string, number]) => {
+        return b[1] - a[1];
+    });
+
+    const orderedKeys = new Array<string>();
+    // convert to an array of id's only
+    orderedEntries.map((value: [string, number]) => {
+      orderedKeys.push(value[0]);
+    });
+
+    return orderedKeys;
+  }
+
+  private decrementCount(item: string): void {
+      let count = this.itemCountMap.get(item);
+      if (count) {
+        count--;
+        if (count > 0) {
+          this.itemCountMap.set(item, count);
+        } else {
+          this.itemCountMap.delete(item);
+        }
+      }
+  }
+
+  /**
+   * Walk the list of items and remove the first occurrence of the matching item.
+   *
+   * @param item the item to be removed.
+   */
+  private removeFirstOccurrence(item: string) {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+      this.decrementCount(item);
+    }
+  }
+
+  /**
+   * Remove the first item from the queue and update the itemCountMap accordingly.
+   * @return the item add the front of the queue or undefined if the queue is empty
+   */
+  private remove(): string | undefined {
+    const removed = this.items.shift();
+    if (removed !== undefined) {
+      this.decrementCount(removed);
+    }
+
+    return removed;
+  }
+
+  /**
+   * Add the supplied item to the end of the queue and update the itemCountMap accordingly.
+   * @param item the item to be added to the queue
+   */
+  private add(item: string): void {
+    this.items.push(item);
+    const count = this.itemCountMap.get(item);
+    if (count) {
+        this.itemCountMap.set(item, count + 1);
+    } else {
+      this.itemCountMap.set(item, 1);
+    }
+  }
+}

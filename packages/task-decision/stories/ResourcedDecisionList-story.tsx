2ff20ff73b6b10fbe176bded290f5932f9d167e2
Craig Petchell <cpetchell@atlassian.com>
2017-08-02 18:39:46 +1000

    fix(component): fix renderer dependency
    affects: @atlaskit/task-decision

--- packages/task-decision/stories/ResourcedDecisionList-story.tsx
+++ packages/task-decision/stories/ResourcedDecisionList-story.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from '@kadira/storybook';
 import * as React from 'react';
-import Renderer from '@atlaskit/renderer';
+import { ReactRenderer as Renderer } from '@atlaskit/editor-core/dist/es2015/renderer';
 
 import { DecisionQuery } from '../src/types';
 import ResourcedDecisionList from '../src/components/ResourcedDecisionList';

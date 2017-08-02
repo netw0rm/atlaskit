5c51622de2e35c4e5a87545913b87f8dd6a4700a
ttjandra <ttjandra@atlassian.com>
2017-08-02 11:14:12 +1000

    refactor(component): refactored filtering of duplicate emoji logic
    affects: @atlaskit/emoji

--- packages/emoji/src/api/AtlassianEmojiMigrationResource.ts
+++ packages/emoji/src/api/AtlassianEmojiMigrationResource.ts
@@ -3,40 +3,38 @@ import { customType } from '../constants';
 import EmojiRepository from './EmojiRepository';
 import EmojiResource from './EmojiResource';
 
+/**
+ * EmojiResource that removes an Atlassian emoji if there is a corresponding site emoji duplicate
+ * This will temporarily be exported to editor-core -> editor-hipchat -> * during the Atlassian
+ * emoji migration process and used in lieu of the base EmojiResource.
+ * After successful migration, this will be removed and replaced by EmojiResource.
+ */
 export default class AtlassianEmojiMigrationResource extends EmojiResource {
 
   protected initEmojiRepository(emojiResponses: EmojiResponse[]): void {
     let emojis: EmojiDescription[] = [];
-    let siteEmojis: EmojiDescription[] = [];
-    let atlassianShortNameMap: Map<string, boolean> = new Map();
+    const siteEmojis: Set<string> = new Set();
+    const atlassianEmojis: Set<string> = new Set();
 
     emojiResponses.forEach(emojiResponse => {
       if (emojiResponse.emojis[0].type.toUpperCase() === customType) {
-        siteEmojis = emojiResponse.emojis;
+       emojiResponse.emojis.forEach(emoji => siteEmojis.add(emoji.shortName));
       }
       if (emojiResponse.emojis[0].type.toUpperCase() === 'ATLASSIAN') {
-        this.mapShortNameToIndex(emojiResponse.emojis, emojis.length, atlassianShortNameMap);
+        emojiResponse.emojis.forEach(emoji => atlassianEmojis.add(emoji.shortName));
       }
       emojis = emojis.concat(emojiResponse.emojis);
     });
-    emojis = this.removeDuplicateAtlassianEmoji(emojis, siteEmojis, atlassianShortNameMap);
+
+    const duplicateEmojis: Set<string> = new Set(Array.from(atlassianEmojis).filter(name => siteEmojis.has(name)));
+    emojis = this.removeDuplicateAtlassianEmoji(emojis, duplicateEmojis);
     this.emojiRepository = new EmojiRepository(emojis);
   }
 
-  private mapShortNameToIndex(emojis: EmojiDescription[], listLength: number, shortNameToIndexMap: Map<string, boolean>): void {
-    // Only store boolean since Atlassian emojis have unique shortNames
-    emojis.forEach(emoji => shortNameToIndexMap.set(emoji.shortName, true));
-  }
-
-  private removeDuplicateAtlassianEmoji(emojis: EmojiDescription[], siteEmojis: EmojiDescription[], atlassianShortNameMap: Map<string, boolean>): EmojiDescription[] {
-    if (!atlassianShortNameMap.size) {
+  private removeDuplicateAtlassianEmoji(emojis: EmojiDescription[], duplicateEmojis: Set<string>): EmojiDescription[] {
+    if (!duplicateEmojis.size) {
       return emojis;
     }
-    siteEmojis.forEach(siteEmoji => {
-      if (atlassianShortNameMap.has(siteEmoji.shortName)) {
-        emojis = emojis.filter(emoji => emoji.shortName !== siteEmoji.shortName || emoji.type.toUpperCase() !== 'ATLASSIAN');
-      }
-    });
-    return emojis;
+    return emojis.filter(emoji => !(duplicateEmojis.has(emoji.shortName) && emoji.type.toUpperCase() === 'ATLASSIAN'));
   }
 }

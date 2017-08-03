3eb753119b2e51a245bb94efc6a3415e823cccc7
Joss Mackison <jmackison@atlassian.com>
2017-08-03 18:05:18 +1000

    feat(component): dark mode for avatar
    affects: @atlaskit/avatar

--- packages/avatar/src/styled/Icon.js
+++ packages/avatar/src/styled/Icon.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { BORDER_WIDTH } from './constants';
+import { colors } from '../../../theme/src';
 
 // set fallbacks for border color/width to protect consumers from invalid values
 export const Outer = styled.span`
   align-content: center;
   align-items: center;
-  background-color: ${props => props.bgColor};
+  background-color: ${props => (props.bgColor || colors.background)};
   border-radius: 50%;
   box-sizing: border-box;
   display: flex;

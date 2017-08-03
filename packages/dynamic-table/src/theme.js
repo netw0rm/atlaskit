567056a22ff3d6a63a8ba4436019a6995c89218f
Joss Mackison <jmackison@atlassian.com>
2017-08-03 10:08:17 +1000

    style(docs): remove unused vars
    affects: @atlaskit/dynamic-table

--- packages/dynamic-table/src/theme.js
+++ packages/dynamic-table/src/theme.js
@@ -1,8 +1,5 @@
 import { colors, themed } from '../../theme/src';
 
-// eslint-disable-next-line import/prefer-default-export
-export const PKG_NM = 'dynamic-table';
-
 export const arrow = {
   defaultColor: themed({ light: colors.N40, dark: colors.DN40 }),
   selectedColor: themed({ light: colors.N300, dark: colors.DN300 }),

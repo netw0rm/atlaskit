1a3090940ec5054263c1c6a713ba4b052203e37f
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-08-03 17:52:46 +1000

    Use opener element to calculate transform origin

---
+++ packages/media-viewer/stories/styled.ts
@@ -0,0 +1,15 @@
+/* tslint:disable:variable-name */
+import styled from 'styled-components';
+
+export const OpenerButton = styled.button`
+  margin-top: 200px;
+  margin-left: 500px;
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  max-height: 150px;
+  border: 1px solid #ccc;
+  padding: 5px;
+  bacground: white;
+`;

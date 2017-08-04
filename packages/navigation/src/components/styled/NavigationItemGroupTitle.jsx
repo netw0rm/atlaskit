9bf4b2adcd5a8f2f63a1b0d6e929cbb83964f12c
Ben Gummer <bgummer@atlassian.com>
2017-08-04 08:22:56 -0700

    chore(PR): use grid unit for line height calculation

--- packages/navigation/src/components/styled/NavigationItemGroupTitle.jsx
+++ packages/navigation/src/components/styled/NavigationItemGroupTitle.jsx
@@ -2,12 +2,13 @@
 import styled from 'styled-components';
 import { whenCollapsed } from '../../theme/util';
 import { truncate } from '../../utils/mixins';
+import { gridSize } from '../../shared-variables';
 
 const groupTitleFontSize = 11;
 
 const NavigationItemGroupTitle = styled.div`
   font-size: ${groupTitleFontSize}px;
-  line-height: ${16 / groupTitleFontSize};
+  line-height: ${(gridSize * 2) / groupTitleFontSize};
   font-weight: 600;
   ${truncate()}
 

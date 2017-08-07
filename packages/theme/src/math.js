b219b0dd4a96d95c8f69d21ee628282442255a54
Jed Watson <jed.watson@me.com>
2017-08-07 15:22:58 +1000

    fix(component): fix math functions
    affects: @atlaskit/theme

--- packages/theme/src/math.js
+++ packages/theme/src/math.js
@@ -1,9 +1,11 @@
+// @flow
+
 export function add(fn: (props: Object) => Number, addend: Number): Number {
-  return props => fn(props) / addend;
+  return props => fn(props) + addend;
 }
 
 export function subtract(fn: (props: Object) => Number, subtrahend: Number): Number {
-  return props => fn(props) / subtrahend;
+  return props => fn(props) - subtrahend;
 }
 
 export function multiply(fn: (props: Object) => Number, factor: Number): Number {

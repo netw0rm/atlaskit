8e31acc6c696fba121b27a0f62d55c4c8ac9aa08
Ben Conolly <bconolly@atlassian.com>
2017-08-08 15:54:48 +1000

    feat(component): add topOffset to navigation
    affects: @atlaskit/navigation
    
    Top Offset is added to navigation to allow navigation and banner to play nicely with each other. As
    part of this goal, the change in height is animated in time with the banner expansion.
    
    ISSUES CLOSED: AK-1728

--- packages/navigation/stories/11-global-secondary-actions-story.jsx
+++ packages/navigation/stories/11-global-secondary-actions-story.jsx
@@ -12,6 +12,77 @@ import HtmlPage from './components/HtmlPage';
 import BasicNavigation from './components/BasicNavigation';
 import emmaAvatar from './emma.png';
 
+import Navigation from '@atlaskit/navigation';
+import Banner from '@atlaskit/banner';
+import Button from '@atlaskit/button';
+
+import Page, { Grid, GridColumn } from '@atlaskit/page';
+
+class ToggleBannerPage extends React.PureComponent {
+  state = {
+    isBannerOpen: false,
+  }
+
+  render() {
+    return (
+      <Page
+        isBannerOpen={this.state.isBannerOpen}
+        banner={
+          <Banner
+            appearance="error"
+            isOpen={this.state.isBannerOpen}
+          >
+            Your JIRA OnDemand license is about to expire.
+            There are two days left to renew your license
+          </Banner>
+        }
+        navigation={
+          <Navigation
+            topOffset={this.state.isBannerOpen ? 52 : 0}
+            width={this.state.navigationWidth}
+            isOpen={this.state.isNavigationOpen}
+            globalSecondaryActions={[
+              <AkGlobalItem>
+                <SettingsIcon label="Settings" secondaryColor="inherit" />
+              </AkGlobalItem>,
+              <AkGlobalItem>
+                <ListIcon label="Some super cool list" secondaryColor="inherit" />
+              </AkGlobalItem>,
+              <AkGlobalItem>
+                <QuestionCircleIcon label="Help icon" secondaryColor="inherit" />
+              </AkGlobalItem>,
+              <AkGlobalItem>
+                <AkAvatar size="small" src={emmaAvatar} />
+              </AkGlobalItem>,
+            ]}
+            onResize={({ width, isOpen }) => {
+              this.setState({
+                navigationWidth: width,
+                isNavigationOpen: isOpen,
+              });
+            }}
+          >
+            Hello world
+          </Navigation>
+        }
+      >
+        <Grid>
+          <GridColumn>
+            <p>Toggle banner</p>
+            <Button
+              onClick={() => {
+                this.setState({
+                  isBannerOpen: !this.state.isBannerOpen,
+                });
+              }}
+            >Banner is: {this.state.isBannerOpen ? 'open' : 'closed'}</Button>
+          </GridColumn>
+        </Grid>
+      </Page>
+    );
+  }
+}
+
 storiesOf(name, module)
   .add('with no secondary actions', () => (
     <HtmlPage>
@@ -53,4 +124,7 @@ storiesOf(name, module)
         ]}
       />
     </HtmlPage>
+  ))
+  .add('with a banner at the top of the page', () => (
+    <ToggleBannerPage />
   ));

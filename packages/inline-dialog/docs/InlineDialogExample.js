49d8c5d779b1dfeb444ee956c8c195f7287a8a71
Joss Mackison <jmackison@atlassian.com>
2017-08-10 10:54:44 +1000

    fix(component): inline-dialog: fix vertical padding
    affects: @atlaskit/inline-dialog

--- packages/inline-dialog/docs/InlineDialogExample.js
+++ packages/inline-dialog/docs/InlineDialogExample.js
@@ -2,6 +2,14 @@ import React, { PureComponent } from 'react';
 import InlineDialog from '@atlaskit/inline-dialog';
 import Button from '@atlaskit/button';
 
+const content = (
+  <div style={{ maxWidth: 320 }}>
+    <h5>Title</h5>
+    <p>Cheesecake gingerbread cupcake soufflé.</p>
+    <p>Macaroon cupcake powder dragée liquorice fruitcake cookie sesame snaps cake.</p>
+  </div>
+);
+
 export default class InlineDialogExample extends PureComponent {
   state = {
     dialogOpen: true,
@@ -13,10 +21,10 @@ export default class InlineDialogExample extends PureComponent {
     return (
       <div style={{ height: '120px' }}>
         <InlineDialog
-          content="The content of the dialog"
+          content={content}
           isOpen={this.state.dialogOpen}
         >
-          <Button onClick={this.toggleDialog}>Toggle Inline Dialog</Button>
+          <Button onClick={this.toggleDialog}>Toggle Dialog</Button>
         </InlineDialog>
       </div>
     );

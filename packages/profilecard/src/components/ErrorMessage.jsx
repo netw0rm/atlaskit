802136821c8654eca299b029259200f0f354d198
Thorsten Basse <tbasse@atlassian.com>
2017-08-11 13:20:21 +1000

    feat(component): add not-found error state to profilecard
    affects: @atlaskit/profilecard
    
    add a second error state to the profilecard when the server responds with NotFound
    
    ISSUES CLOSED: DIR-340

--- packages/profilecard/src/components/ErrorMessage.jsx
+++ packages/profilecard/src/components/ErrorMessage.jsx
@@ -8,23 +8,50 @@ import styles from '../styles/profilecard.less';
 export default class ErrorMessage extends PureComponent {
   static propTypes = {
     reload: PropTypes.func,
+    errorType: PropTypes.shape({
+      reason: PropTypes.string,
+    }),
   };
-  static defaultProps = {};
+
+  static defaultProps = {
+    errorType: {
+      reason: 'default',
+    },
+  };
+
+  renderNotFound = () => (
+    <p>The user is no longer available for the site</p>
+  )
+
+  renderDefault = () => (
+    <div>
+      <p>
+        Oops, looks like we’re having issues
+        <br />
+        {this.props.reload ? <span>Try again and we’ll give it another shot</span> : null}
+      </p>
+      {this.props.reload ? <AkButton
+        appearance="link"
+        compact
+        onClick={this.props.reload}
+      >Try again</AkButton> : null}
+    </div>
+  );
 
   render() {
+    const { reason } = this.props.errorType;
+    let content = null;
+
+    if (reason === 'NotFound') {
+      content = this.renderNotFound();
+    } else {
+      content = this.renderDefault();
+    }
+
     return (
       <div className={styles.errorMessage}>
         <CrossCircleIcon label="icon error" size="large" />
-        <p>
-          Oops, looks like we’re having issues
-          <br />
-          {this.props.reload ? <span>Try again and we’ll give it another shot</span> : null}
-        </p>
-        {this.props.reload ? <AkButton
-          appearance="link"
-          compact
-          onClick={this.props.reload}
-        >Try again</AkButton> : null}
+        {content}
       </div>
     );
   }

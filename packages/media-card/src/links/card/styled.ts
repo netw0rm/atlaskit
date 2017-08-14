d065ad860541d7ae35e4795a488ff5612e822c26
Ivan Loire <iloire@atlassian.com>
2017-08-14 17:29:35 +1000

    fix(component): fix sizing regression in small cards
    affects: @atlaskit/media-card
    
    ISSUES CLOSED: MSW-146

--- packages/media-card/src/links/card/styled.ts
+++ packages/media-card/src/links/card/styled.ts
@@ -4,8 +4,6 @@ import { akColorB400 } from '@atlaskit/util-shared-styles';
 import { Href, HrefProps } from '../../utils/href';
 
 export const A = styled(Href)`
-  // Explicitly setting display here to avoid links getting all available width
-  display: inline-block;
   color: initial;
   // We need to do this to make TS happy
   ${(props: HrefProps) => ''}

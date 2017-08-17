e43126c6f286719bbf6ff89677a013ca7418ee05
Craig Petchell <cpetchell@atlassian.com>
2017-08-17 16:36:20 -0500

    fix(component): add missing dependency from InfiniteScroll
    affects: @atlaskit/task-decision

--- packages/task-decision/src/api/TaskDecisionLoader.ts
+++ packages/task-decision/src/api/TaskDecisionLoader.ts
@@ -13,7 +13,7 @@ import {
 export const loadLatestItems = (query: Query, existing: Item[], provider: TaskDecisionProvider, recentUpdateContext: RecentUpdateContext): Promise<Item[]> => {
   if (recentUpdateContext.localId) {
     // Retry until localId is found
-    return loadWithRetry(() => provider.getItems(query).then(r => r.items), recentUpdateContext)
+    return retryIteration(() => provider.getItems(query).then(r => r.items), recentUpdateContext)
       .then(items => mergeItems(existing, items));
   }
   // Just load
@@ -38,19 +38,19 @@ export interface ItemLoader<T> {
   (): Promise<T[]>;
 }
 
-const retryDelays = [ 500, 1000, 1500, 2500, 4000, 6000, 8000, 10000 ];
+const retryDelaysInMilliseconds = [ 500, 1000, 1500, 2500, 4000, 6000, 8000, 10000 ];
 
-export const loadWithRetry = <T extends Item>(loader: ItemLoader<T>, recentUpdateContext: RecentUpdateContext, retry: number = 0): Promise<T[]> => {
-  return loadWithDelay(loader, retryDelays[retry]).then(items => {
+export const retryIteration = <T extends Item>(loader: ItemLoader<T>, recentUpdateContext: RecentUpdateContext, retry: number = 0): Promise<T[]> => {
+  return loadWithDelay(loader, retryDelaysInMilliseconds[retry]).then(items => {
     if (items.filter(item => item.localId === recentUpdateContext.localId).length > 0) {
       return items;
     }
-    const delay = retryDelays[retry || 0];
+    const delay = retryDelaysInMilliseconds[retry || 0];
     if (!delay) {
       // Give up - just retry what we've got.
       return items;
     }
-    return loadWithRetry(loader, recentUpdateContext, retry + 1);
+    return retryIteration(loader, recentUpdateContext, retry + 1);
   });
 };
 

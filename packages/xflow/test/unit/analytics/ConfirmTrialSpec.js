e32d208aa3a03f72af0e2ffc4e43198334b52ab9
Lachlan Hunt <lhunt@atlassian.com>
2017-08-17 11:55:04 +1000

    WIP(component): fix confirm trial unit tests that broke after deactivated state changes
    affects: @atlaskit/xflow

--- packages/xflow/test/unit/analytics/ConfirmTrialSpec.js
+++ packages/xflow/test/unit/analytics/ConfirmTrialSpec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { ConfirmTrialBase } from '../../../src/start-trial/components/ConfirmTrial';
 import { withAnalyticsSpy, waitFor } from '../../util';
+import { INACTIVE } from '../../../src/common/productProvisioningStates';
 
 const noop = () => {};
 
@@ -9,42 +10,41 @@ const defaultProps = {
   productLogo: <div />,
   onComplete: noop,
   onCancel: noop,
-  heading: '',
-  message: '',
+  status: INACTIVE,
+  trialHeading: '',
+  trialMessage: '',
+  reactivateHeading: '',
+  reactivateMessage: '',
 };
 
 test('ConfirmTrial should fire an appropriate analytics event when it is mounted', () => {
   const spy = jest.fn();
-  mount(
-      withAnalyticsSpy(
-      spy,
-        <ConfirmTrialBase {...defaultProps} />
-      )
-    );
-  return waitFor(() => expect(spy).toHaveBeenCalledWith('xflow.confirm-trial.displayed', expect.any(Object)));
+  mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
+  return waitFor(() =>
+    expect(spy).toHaveBeenCalledWith('xflow.confirm-trial.displayed', expect.any(Object))
+  );
 });
 
 test('ConfirmTrial should fire an appropriate analytics event when the confirm button is clicked', () => {
   const spy = jest.fn();
-  const mountWrapper = mount(
-    withAnalyticsSpy(
-      spy,
-      <ConfirmTrialBase {...defaultProps} />
+  const mountWrapper = mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
+  mountWrapper.find('#xflow-confirm-trial-confirm-button').simulate('click');
+  return waitFor(() =>
+    expect(spy).toHaveBeenCalledWith(
+      'xflow.confirm-trial.confirm-button.clicked',
+      expect.any(Object)
     )
   );
-  mountWrapper.find('#xflow-confirm-trial-confirm-button').simulate('click');
-  return waitFor(() => expect(spy).toHaveBeenCalledWith('xflow.confirm-trial.confirm-button.clicked', expect.any(Object)));
 });
 
 test('ConfirmTrial should fire an appropriate analytics event when the cancel button is clicked', () => {
   const spy = jest.fn();
-  const mountWrapper = mount(
-    withAnalyticsSpy(
-      spy,
-      <ConfirmTrialBase {...defaultProps} />
+  const mountWrapper = mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
+  mountWrapper.find('#xflow-confirm-trial-cancel-button').simulate('click');
+  return waitFor(() =>
+    expect(spy).toHaveBeenCalledWith(
+      'xflow.confirm-trial.cancel-button.clicked',
+      expect.any(Object)
     )
   );
-  mountWrapper.find('#xflow-confirm-trial-cancel-button').simulate('click');
-  return waitFor(() => expect(spy).toHaveBeenCalledWith('xflow.confirm-trial.cancel-button.clicked', expect.any(Object)));
 });
-

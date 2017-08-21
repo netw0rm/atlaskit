2b55b8929756375afd6c86cd04239e5eaf40496e
Ben Conolly <bconolly@atlassian.com>
2017-08-21 14:11:30 +1000

    docs(stories): refactor nav with banner story to be neater
    affects: @atlaskit/navigation

--- packages/navigation/stories/11-global-secondary-actions-story.jsx
+++ packages/navigation/stories/11-global-secondary-actions-story.jsx
@@ -10,78 +10,8 @@ import { name } from '../package.json';
 import { AkGlobalItem, presetThemes } from '../src/';
 import HtmlPage from './components/HtmlPage';
 import BasicNavigation from './components/BasicNavigation';
+import ToggleBannerPage from './components/withBannerAndTopOffset';
 import emmaAvatar from './emma.png';
-
-import Navigation from '@atlaskit/navigation';
-import Banner from '@atlaskit/banner';
-import Button from '@atlaskit/button';
-
-import Page, { Grid, GridColumn } from '@atlaskit/page';
-
-class ToggleBannerPage extends React.PureComponent {
-  state = {
-    isBannerOpen: false,
-  }
-
-  render() {
-    return (
-      <Page
-        isBannerOpen={this.state.isBannerOpen}
-        banner={
-          <Banner
-            appearance="error"
-            isOpen={this.state.isBannerOpen}
-          >
-            Your JIRA OnDemand license is about to expire.
-            There are two days left to renew your license
-          </Banner>
-        }
-        navigation={
-          <Navigation
-            topOffset={this.state.isBannerOpen ? 52 : 0}
-            width={this.state.navigationWidth}
-            isOpen={this.state.isNavigationOpen}
-            globalSecondaryActions={[
-              <AkGlobalItem>
-                <SettingsIcon label="Settings" secondaryColor="inherit" />
-              </AkGlobalItem>,
-              <AkGlobalItem>
-                <ListIcon label="Some super cool list" secondaryColor="inherit" />
-              </AkGlobalItem>,
-              <AkGlobalItem>
-                <QuestionCircleIcon label="Help icon" secondaryColor="inherit" />
-              </AkGlobalItem>,
-              <AkGlobalItem>
-                <AkAvatar size="small" src={emmaAvatar} />
-              </AkGlobalItem>,
-            ]}
-            onResize={({ width, isOpen }) => {
-              this.setState({
-                navigationWidth: width,
-                isNavigationOpen: isOpen,
-              });
-            }}
-          >
-            Hello world
-          </Navigation>
-        }
-      >
-        <Grid>
-          <GridColumn>
-            <p>Toggle banner</p>
-            <Button
-              onClick={() => {
-                this.setState({
-                  isBannerOpen: !this.state.isBannerOpen,
-                });
-              }}
-            >Banner is: {this.state.isBannerOpen ? 'open' : 'closed'}</Button>
-          </GridColumn>
-        </Grid>
-      </Page>
-    );
-  }
-}
 
 storiesOf(name, module)
   .add('with no secondary actions', () => (

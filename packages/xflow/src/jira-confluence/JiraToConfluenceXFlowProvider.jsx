ed64b4feab365cb7125d1cfb8f54494b5359cd49
Andrew Hammond <ahammond@atlassian.com>
2017-08-22 16:52:51 +1000

    fix(component): minor copy fix for ConfirmTrial
    affects: @atlaskit/xflow

--- packages/xflow/src/jira-confluence/JiraToConfluenceXFlowProvider.jsx
+++ packages/xflow/src/jira-confluence/JiraToConfluenceXFlowProvider.jsx
@@ -143,7 +143,7 @@ export const defaultProps = intl => ({
         <FormattedMessage
           id="xflow.j2c.start-trial.confirm-trial.message"
           tagName="p"
-          defaultMessage="Once your trial finishes, billing will start.{br}Easily cancel at any time in Manage application.{br}We'll email your billing contact 3 days in advance."
+          defaultMessage="Once your trial finishes, billing will start.{br}Easily cancel at any time in Manage applications.{br}We'll email your billing contact 3 days in advance."
           values={{ br: <br /> }}
         />
       ),

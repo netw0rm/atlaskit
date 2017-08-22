8d2f2e366a242c2bd00428fa9f84ad32006bba35
Ben Conolly <bconolly@atlassian.com>
2017-08-22 17:06:51 +1000

    refactor(component): dark mode done
    affects: @atlaskit/tag

--- packages/tag/src/styled/Chrome.jsx
+++ packages/tag/src/styled/Chrome.jsx
@@ -1,29 +1,31 @@
 import styled from 'styled-components';
-import {
-  akGridSizeUnitless,
-  akHelperMixins,
-} from '@atlaskit/util-shared-styles';
-import { colors, themed } from '@atlaskit/theme';
-import { buttonWidthUnitless, borderRadius, tagHeight } from './constants';
+import { colors, themed, gridSize, borderRadius } from '@atlaskit/theme';
+import { buttonWidthUnitless, tagHeight, focusRingColor } from './constants';
 import { backgroundColor, backgroundColorHover, textColor, textColorHover } from './theme';
+
+const gridSizeUnitless = gridSize();
 
 const colorRemoval = themed({ light: colors.R500, dark: colors.DN30 });
 const colorRemovalHover = themed({ light: colors.N700, dark: colors.DN30 });
 const backgroundColorRemoval = themed({ light: colors.R50, dark: colors.R100 });
+
 export default styled.span`
-  ${akHelperMixins.focusRing.default};
+&:focus {
+  box-shadow: 0 0 0 2px ${focusRingColor};
+  outline: none;
+}
   background-color: ${p => (p.markedForRemoval ? backgroundColorRemoval(p) : backgroundColor(p))};
   color: ${p => (p.markedForRemoval ? colorRemoval(p) : textColor(p))};
-  border-radius: ${({ isRounded }) => (isRounded ? `${buttonWidthUnitless / 2}px` : borderRadius)};
+  border-radius: ${({ isRounded }) => (isRounded ? `${buttonWidthUnitless / 2}px` : `${borderRadius()}px`)};
   cursor: default;
   display: flex;
   height: ${tagHeight};
   line-height: 1;
-  margin: ${akGridSizeUnitless / 2}px;
+  margin: ${gridSizeUnitless / 2}px;
   padding: 0;
   overflow: ${({ isRemoved, isRemoving }) => ((isRemoved || isRemoving) ? 'hidden' : 'initial')};
   &:hover {
-    ${akHelperMixins.focusRing.none};
+    box-shadow: none;
     background-color: ${p => (p.markedForRemoval ? backgroundColorRemoval(p) : backgroundColorHover(p))};
     color: ${p => (p.markedForRemoval ? colorRemovalHover(p) : textColorHover(p))};
   }

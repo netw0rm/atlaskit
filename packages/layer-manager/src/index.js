767659ca58e766758a54260d2ad68d3e1ecbfc1d
Joss Mackison <jmackison@atlassian.com>
2017-08-24 11:47:50 +1000

    docs(component): prepare docs for layer-manager and cleanup unused files
    affects: @atlaskit/layer-manager

--- packages/layer-manager/src/index.js
+++ packages/layer-manager/src/index.js
@@ -1,2 +1,2 @@
-export { default } from './components/layer-manager';
+export { default } from './components/LayerManager';
 export { Gateway, GatewayDest, GatewayProvider, GatewayRegistry } from './components/gateway';

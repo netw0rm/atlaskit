f4e1c65847480986a6652a91aed724f0c23fb3d4
Ben Conolly <bconolly@atlassian.com>
2017-08-29 09:06:36 +1000

    feat(component): droplist has darkmode style options
    affects: @atlaskit/droplist
    
    ISSUES CLOSED: AK-3399

--- packages/droplist/src/utils/index.js
+++ packages/droplist/src/utils/index.js
@@ -1,25 +1,17 @@
-import {
-akColorB400,
-akColorN0,
-akColorN20,
-akColorB200,
-akColorN40,
-akColorN50,
-akColorN600,
-} from '@atlaskit/util-shared-styles';
+import { colors } from '@atlaskit/theme';
 
 export function getInputBackground({ isChecked, isDisabled, isHovered, isPressed }) {
-  let background = akColorN40;
+  let background = colors.N40;
 
-  if (isHovered) background = akColorN50;
-  if (isPressed) background = akColorB200;
-  if (isChecked) background = akColorB400;
-  if (isDisabled) background = akColorN20;
-  if (isChecked && isDisabled) background = akColorN600;
+  if (isHovered) background = colors.N50;
+  if (isPressed) background = colors.B200;
+  if (isChecked) background = colors.B400;
+  if (isDisabled) background = colors.N20;
+  if (isChecked && isDisabled) background = colors.N600;
 
   return background;
 }
 
 export function getInputFill({ isChecked }) {
-  return isChecked ? akColorN0 : 'transparent';
+  return isChecked ? colors.N0 : 'transparent';
 }

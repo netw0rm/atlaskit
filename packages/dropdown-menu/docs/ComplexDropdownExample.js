fa6acb1aa2b8d66d60f02a7e029fdc4b4456ccd9
Ben Gummer <bgummer@atlassian.com>
2017-08-29 01:52:48 +0000

    chore(docs): fix dropdown-menu website example
    affects: @atlaskit/dropdown-menu

--- packages/dropdown-menu/docs/ComplexDropdownExample.js
+++ packages/dropdown-menu/docs/ComplexDropdownExample.js
@@ -12,19 +12,17 @@ const ComplexMenuExample = () => (
     position="right middle"
     onOpenChange={e => console.log('dropdown opened', e)}
   >
-    <DropdownItemGroupRadio id="cities">
-      <DropdownItemGroupRadio title="Australia">
-        <DropdownItemRadio id="sydney">Sydney</DropdownItemRadio>
-      </DropdownItemGroupRadio>
-      <DropdownItemGroupRadio title="United States">
-        <DropdownItemRadio id="san-francisco">San Francisco</DropdownItemRadio>
-        <DropdownItemRadio id="austin">Austin</DropdownItemRadio>
-      </DropdownItemGroupRadio>
-      <DropdownItemGroupRadio title="Elsewhere">
-        <DropdownItemRadio id="amsterdam">Amsterdam</DropdownItemRadio>
-        <DropdownItemRadio id="yokohama">Yokohama</DropdownItemRadio>
-        <DropdownItemRadio id="manila">Manila</DropdownItemRadio>
-      </DropdownItemGroupRadio>
+    <DropdownItemGroupRadio id="cities-aus" title="Australia">
+      <DropdownItemRadio id="sydney">Sydney</DropdownItemRadio>
+    </DropdownItemGroupRadio>
+    <DropdownItemGroupRadio id="cities-usa" title="United States">
+      <DropdownItemRadio id="san-francisco">San Francisco</DropdownItemRadio>
+      <DropdownItemRadio id="austin">Austin</DropdownItemRadio>
+    </DropdownItemGroupRadio>
+    <DropdownItemGroupRadio id="cities-elsewhere" title="Elsewhere">
+      <DropdownItemRadio id="amsterdam">Amsterdam</DropdownItemRadio>
+      <DropdownItemRadio id="yokohama">Yokohama</DropdownItemRadio>
+      <DropdownItemRadio id="manila">Manila</DropdownItemRadio>
     </DropdownItemGroupRadio>
   </DropdownMenu>
 );

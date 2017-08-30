b344810ff9957a9e50214abb21aab7b1c1c0c60f
Joss Mackison <jmackison@atlassian.com>
2017-08-30 15:22:31 +1000

    feat(component): removed z-index for integration with @atlaskit/layer-manager
    affects: @atlaskit/blanket
    
    BREAKING CHANGE:
    removed z-index

--- packages/blanket/src/styled.js
+++ packages/blanket/src/styled.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { colors, themed } from '@atlaskit/theme';
-import { akZIndexBlanket } from '@atlaskit/util-shared-styles';
 
 const backgroundColor = themed({ light: colors.N100A, dark: colors.DN90A });
 export const opacity = p => (p.isTinted ? 1 : 0);
@@ -16,5 +15,4 @@ export default styled.div`
   right: 0;
   top: 0;
   transition: opacity 220ms;
-  z-index: ${akZIndexBlanket};
 `;

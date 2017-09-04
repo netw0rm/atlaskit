0632c02daeb4a256e3de59744dcb1483f540fa87
Ben Conolly <bconolly@atlassian.com>
2017-09-04 17:03:49 +1000

    fix(component): add placeholder color for light and dark mode
    affects: @atlaskit/field-text

--- packages/field-text/src/styled/Input.jsx
+++ packages/field-text/src/styled/Input.jsx
@@ -1,4 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { colors, themed } from '@atlaskit/theme';
+
+const getPlaceholderStyle = style => css`
+&::-webkit-input-placeholder { /* WebKit, Blink, Edge */
+  ${style}
+}
+&:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+   ${style}
+   opacity:  1;
+}
+&::-moz-placeholder { /* Mozilla Firefox 19+ */
+   ${style}
+   opacity:  1;
+}
+&:-ms-input-placeholder { /* Internet Explorer 10-11 */
+   ${style}
+}
+&::-ms-input-placeholder { /* Microsoft Edge */
+   ${style}
+}
+`;
+const subtleText = themed({ light: colors.N70, dark: colors.DN90 });
+const getPlaceholderColor = css`color: ${subtleText};`;
 
 const InputElement = styled.input`
   background: transparent;
@@ -17,6 +40,7 @@ const InputElement = styled.input`
   &:invalid {
     box-shadow: none;
   }
+  ${getPlaceholderStyle(getPlaceholderColor)}
 `;
 
 export default InputElement;

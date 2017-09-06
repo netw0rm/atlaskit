f348ccb4ddcf7528ce6ede7e115c6a501052f3a5
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2017-09-06 20:21:09 +1000

    feat(component): bump media-core dep and introduce asap issuer way of auth
    affects: @atlaskit/media-test-helpers

--- packages/media-test-helpers/src/contextProvider.ts
+++ packages/media-test-helpers/src/contextProvider.ts
@@ -1,11 +1,25 @@
-import { StoryBookTokenProvider } from './tokenProvider';
-import { collectionNames } from './collectionNames';
-import { ContextFactory, Context } from '@atlaskit/media-core';
+import {StoryBookAuthProvider} from './authProvider';
+import {collectionNames} from './collectionNames';
+import {ContextFactory, Context} from '@atlaskit/media-core';
 
-export const defaultClientId = '5a9812fc-d029-4a39-8a46-d3cc36eed7ab';
-export const defaultServiceHost = 'https://dt-api-filestore.internal.app.dev.atlassian.io';
+export const defaultParams = {
+  clientId: '5a9812fc-d029-4a39-8a46-d3cc36eed7ab',
+  asapIssuer: 'micros/media-playground',
+  serviceHost: 'https://dt-api-filestore.internal.app.dev.atlassian.io'
+};
 
-export const createStorybookContext = (clientId = defaultClientId, serviceHost = defaultServiceHost): Context => {
+export interface AuthParameter {
+  serviceHost: string;
+  authType: 'client' | 'asap';
+}
+
+/**
+ * Creates and returns `Context` (from `media-core`) based on the data provided in parameter object.
+ *
+ * @param {AuthParameter} authParameter specifies serviceName and whatever auth should be done with clientId or asapIssuer
+ * @returns {Context}
+ */
+export const createStorybookContext = (authParameter: AuthParameter): Context => {
   const scopes = {
     'urn:filestore:file:*': ['read'],
     'urn:filestore:chunk:*': ['read']
@@ -13,6 +27,9 @@ export const createStorybookContext = (clientId = defaultClientId, serviceHost =
   collectionNames.forEach(c => {
     scopes[`urn:filestore:collection:${c}`] = ['read', 'update'];
   });
-  const tokenProvider = StoryBookTokenProvider.withAccess(scopes);
-  return ContextFactory.create({ clientId, serviceHost, tokenProvider });
+
+  const isAsapEnvironment = authParameter.authType === 'asap';
+  const authProvider = StoryBookAuthProvider.create(isAsapEnvironment, scopes);
+
+  return ContextFactory.create({serviceHost: authParameter.serviceHost, authProvider});
 };

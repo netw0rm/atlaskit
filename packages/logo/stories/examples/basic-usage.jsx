c4db7fc81eb68fc4c654c241d398a47e977b3d7f
Sean Curtis <scurtis@atlassian.com>
2017-09-11 12:52:33 +1000

    feat(component): new and updated company and product logos
    affects: @atlaskit/logo
    
    BREAKING CHANGE:
    All logos have been updated with new assets, please test these inside your application to make sure
    they look good.

--- packages/logo/stories/examples/basic-usage.jsx
+++ packages/logo/stories/examples/basic-usage.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
+import {
+  AtlassianLogo,
+  BitbucketLogo,
+  ConfluenceLogo,
+  HipchatLogo,
+  JiraLogo,
+  JiraCoreLogo,
+  JiraServiceDeskLogo,
+  JiraSoftwareLogo,
+  StatuspageLogo,
+  StrideLogo,
+} from '@atlaskit/logo';
+import {
+  akGridSizeUnitless,
+  akColorN700,
+  akColorB200,
+  akColorB400,
+} from '@atlaskit/util-shared-styles';
+import styled from 'styled-components';
 
-import { AtlassianLogo, BitbucketLogo, ConfluenceLogo, HipchatLogo, JiraLogo } from '@atlaskit/logo';
+const LogoSpacer = styled.div`
+  margin-right: ${akGridSizeUnitless}px;
+  color: akColorP300,
+`;
+
+const logos = [
+  AtlassianLogo,
+  BitbucketLogo,
+  ConfluenceLogo,
+  HipchatLogo,
+  JiraLogo,
+  JiraCoreLogo,
+  JiraServiceDeskLogo,
+  JiraSoftwareLogo,
+  StatuspageLogo,
+  StrideLogo,
+];
 
 export default (
-  <span>
-    <AtlassianLogo />
-    <BitbucketLogo />
-    <ConfluenceLogo />
-    <HipchatLogo />
-    <JiraLogo />
-  </span>
+  logos.map((Logo, logoIndex) => (
+    <LogoSpacer key={logoIndex}>
+      <Logo
+        textColor={akColorN700}
+        iconColor={akColorB200}
+        iconGradientStart={akColorB400}
+        iconGradientStop={akColorB200}
+      />
+    </LogoSpacer>
+  ))
 );
--- packages/logo/test/unit/Index.js
+++ packages/logo/test/unit/Index.js
@@ -6,14 +6,19 @@ import { name } from '../../package.json';
 describe(name, () => {
   describe('Product logos', () => {
     it('are properly defined', () => {
-      expect(Object.keys(logos)).toEqual(expect.arrayContaining([
+      expect(Object.keys(logos)).toEqual([
         'AtlassianLogo',
         'BitbucketLogo',
         'ConfluenceLogo',
         'HipchatLogo',
         'JiraLogo',
+        'JiraCoreLogo',
+        'JiraServiceDeskLogo',
+        'JiraSoftwareLogo',
+        'StatuspageLogo',
+        'StrideLogo',
         'default',
-      ]));
+      ]);
     });
     it('has the Atlassian logo exported as default', () => {
       expect(DefaultLogo).toBe(logos.AtlassianLogo);

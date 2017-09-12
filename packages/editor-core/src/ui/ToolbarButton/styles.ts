b67568aa1648dc32754029f7481c19107c1a8efa
Eduard Shvedai <eshvedai@atlassian.com>
2017-09-12 11:35:26 +1000

    fix(component): fix blocktype toolbar dropdown button for BB
    affects: @atlaskit/editor-core

--- packages/editor-core/src/ui/ToolbarButton/styles.ts
+++ packages/editor-core/src/ui/ToolbarButton/styles.ts
@@ -3,7 +3,6 @@ import AkButtonDefault from '@atlaskit/button';
 
 // tslint:disable-next-line:variable-name
 export const AkButton: any = styled(AkButtonDefault)`
-  line-height: 0;
 
   & + & {
     margin-left: 4px;

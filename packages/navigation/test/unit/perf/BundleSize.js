a4bdb91684715bd80570058dc8cbce66ad86d992
Raja Bellebon <rbellebon@atlassian.com>
2017-09-13 14:48:15 +1000

    test(build): add a bundle size test for navigation
    affects: @atlaskit/navigation

--- packages/navigation/test/unit/perf/BundleSize.js
+++ packages/navigation/test/unit/perf/BundleSize.js
@@ -1,30 +1,15 @@
 const bytes = require('bytes');
 const fs = require('fs');
-const cmd = require('node-cmd');
+const sourceTrace = require('source-trace');
 // This threshold corresponds to 5% of the latest bundle value
 const thresholdBundle = 2000;
 
 let threshold;
 
-function cmdGet(name) {
-  return new Promise((resolve, reject) => {
-    cmd.get(name, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-}
-
 describe('Bundle', () => {
   beforeAll(async () => {
-    // This has been added to avoid any async timeout issue
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
-    const data = await cmdGet('yar');
-    const deps = data.split(/\r?\n/).filter(Boolean);
-    const stats = deps.map(d => fs.statSync(d).size);
+    const data = sourceTrace('./packages/navigation/src');
+    const stats = data.map(d => fs.statSync(d).size);
     const sum = stats.reduce((prev, curr) => prev + curr, 0);
     threshold = bytes(sum).includes('MB') ? Number(bytes(sum).replace('MB', '')) * 1000 : Number(bytes(sum).replace('kB', '').trim());
   });

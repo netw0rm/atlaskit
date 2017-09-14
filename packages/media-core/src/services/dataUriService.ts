0aaf89edcbb729d011b16a32a80efad60d017df5
Jimmy Luong <jluong@jimmys-mbp.office.atlassian.com>
2017-09-14 11:55:13 -0500

    feat(media-core): add fetch thumbnail to data uri service
    affects: @atlaskit/media-core
    
    ISSUES CLOSED: MSW-233

--- packages/media-core/src/services/dataUriService.ts
+++ packages/media-core/src/services/dataUriService.ts
@@ -1,7 +1,7 @@
-import { readBlob } from './util/blobReader';
-import createRequest, { CreateRequestFunc } from './util/createRequest';
-import { MediaItem } from '../';
-import { AuthProvider } from '../auth';
+import {readBlob} from './util/blobReader';
+import createRequest, {CreateRequestFunc} from './util/createRequest';
+import {ArtifactName, FileItem, isFileItem, MediaItem} from '../';
+import {AuthProvider} from '../auth';
 
 export type DataUri = string;
 export type ImageResizeMode = 'crop' | 'fit' | 'full-fit';
@@ -9,11 +9,17 @@ export type ImageResizeMode = 'crop' | 'fit' | 'full-fit';
 export interface DataUriService {
   fetchOriginalDataUri(mediaItem: MediaItem): Promise<DataUri>;
   fetchImageDataUri(mediaItem: MediaItem, width: number, height: number, mode?: ImageResizeMode): Promise<DataUri>;
+  fetchThumbnailDataUri(mediaItem: MediaItem, options?: FetchThumbnailOptions): Promise<DataUri>;
+}
+
+export type ThumbnailSize = 'normal' | 'large';
+export interface FetchThumbnailOptions {
+  readonly size?: ThumbnailSize;
 }
 
 export class MediaDataUriService implements DataUriService {
-
-  private request: CreateRequestFunc;
+  private readonly maxAge = 3600;
+  private readonly request: CreateRequestFunc;
 
   constructor(
     private readonly authProvider: AuthProvider,
@@ -31,7 +37,7 @@ export class MediaDataUriService implements DataUriService {
   fetchOriginalDataUri(mediaItem: MediaItem): Promise<DataUri> {
     return this.fetchSomeDataUri(
       `/file/${mediaItem.details.id}/binary`, {
-        'max-age': 3600,
+        'max-age': this.maxAge,
         collection: this.collectionName
       });
   }
@@ -42,17 +48,48 @@ export class MediaDataUriService implements DataUriService {
         width,
         height,
         mode,
-        'max-age': 3600,
+        'max-age': this.maxAge,
         collection: this.collectionName
       });
   }
 
-  fetchSomeDataUri(url: string, params: Object): Promise<DataUri> {
+  fetchThumbnailDataUri(mediaItem: MediaItem, options?: FetchThumbnailOptions): Promise<DataUri> {
+    if (isFileItem(mediaItem)) {
+      return this.fetchFileThumbnailHref(mediaItem, options)
+        .then((href) => this.fetchSomeDataUri(href, {
+          'max-age': this.maxAge,
+          collection: this.collectionName
+        }));
+    } else {
+      return Promise.reject(new Error('not yet implemented'));
+    }
+  }
+
+  private fetchSomeDataUri(url: string, params: Object): Promise<DataUri> {
     return this.request({
       url,
       params,
       responseType: 'image'
     })
       .then(readBlob);
+  }
+
+  private fetchFileThumbnailHref({details}: FileItem, options: FetchThumbnailOptions = {}): Promise<string> {
+    const artifactName = this.mapFileThumbnailSizeToArtifactName(options.size);
+    const artifact = details.artifacts && details.artifacts[artifactName];
+    if (artifact) {
+      return Promise.resolve(artifact.href);
+    } else {
+      return Promise.reject(new Error('no thumbnail is available for this file'));
+    }
+  }
+
+  private mapFileThumbnailSizeToArtifactName(size?: ThumbnailSize): ArtifactName {
+    switch (size) {
+      case 'large':
+        return 'thumb_large.jpg';
+      default:
+        return 'thumb.jpg';
+    }
   }
 }
--- packages/media-core/test/unit/services/dataUriService.spec.ts
+++ packages/media-core/test/unit/services/dataUriService.spec.ts
@@ -1,35 +1,146 @@
-import { MediaDataUriService } from '../../../src/services/dataUriService';
-import {AuthProvider} from '../../../src/auth';
+jest.mock('../../../src/services/util/createRequest');
 
-const clientId = 'some-client-id';
-const collectionName = 'some-collection-name';
-const token = 'some-token';
-const serviceHost = 'some-service-host';
+import {FileItem} from '../../../src';
+import {MediaDataUriService} from '../../../src/services/dataUriService';
+import {CreateRequestFunc, default as createRequest} from '../../../src/services/util/createRequest';
 
 describe('MediaDataUriService', () => {
-  const authProvider: AuthProvider = () => Promise.resolve({
-    token: token,
-    clientId: clientId
+  const fileId = 'some-file-id';
+  const clientId = 'some-client-id';
+  const collectionName = 'some-collection-name';
+  const token = 'some-token';
+  const serviceHost = 'some-service-host';
+  const authProvider = () => Promise.resolve({token, clientId});
+  const fileItemWithNoThumbnails: FileItem = {
+    type: 'file',
+    details: {
+      id: fileId,
+      artifacts: {
+      }
+    }
+  };
+  const fileItemWithThumbnails: FileItem = {
+    type: 'file',
+    details: {
+      id: fileId,
+      artifacts: {
+        'thumb.jpg': {
+          href: '/file/some-file-id/artifact/thumb_120.jpg/binary',
+          processingStatus: 'succeeded'
+        },
+        'thumb_large.jpg': {
+          href: '/file/some-file-id/artifact/thumb_320.jpg/binary',
+          processingStatus: 'succeeded'
+        }
+      }
+    }
+  };
+
+  const setup = () => {
+    const request = jest.fn().mockReturnValue(Promise.resolve(new Blob()));
+
+    (createRequest as jest.Mock<CreateRequestFunc>).mockReturnValue(request);
+
+    const dataUriService = new MediaDataUriService(authProvider, serviceHost, collectionName);
+
+    return {
+      request,
+      dataUriService
+    };
+  };
+
+  afterEach(function () {
+    jest.resetAllMocks();
   });
-  const service = new MediaDataUriService(authProvider, serviceHost, collectionName);
 
   describe('fetchImageDataUri()', () => {
-    it('should use "crop" resize mode as default', () => {
-      const fetchSomeDataUriSpy = jest.fn();
-      service.fetchSomeDataUri = fetchSomeDataUriSpy;
+    it('should create request function', async () => {
+      setup();
 
-      service.fetchImageDataUri({type: 'file', details: {}}, 100, 100);
+      expect(createRequest).toBeCalledWith({
+        collectionName,
+        config: {
+          authProvider,
+          serviceHost
+        }
+      });
+    });
 
-      expect(fetchSomeDataUriSpy.mock.calls[0][1].mode).toBe('crop');
+    it('should use "crop" resize mode as default', async () => {
+      const {dataUriService, request} = setup();
+
+      await dataUriService.fetchImageDataUri(fileItemWithThumbnails, 100, 100);
+
+      expect(request).toBeCalledWith({
+        url: '/file/some-file-id/image',
+        params: {
+          mode: 'crop',
+          width: 100,
+          height: 100,
+          collection: 'some-collection-name',
+          'max-age': 3600
+        },
+        responseType: 'image'
+      });
     });
 
     it('should allow consumers to specify a resize mode', () => {
-      const fetchSomeDataUriSpy = jest.fn();
-      service.fetchSomeDataUri = fetchSomeDataUriSpy;
+      const {dataUriService, request} = setup();
 
-      service.fetchImageDataUri({type: 'file', details: {}}, 100, 100, 'full-fit');
+      dataUriService.fetchImageDataUri(fileItemWithThumbnails, 200, 200, 'full-fit');
 
-      expect(fetchSomeDataUriSpy.mock.calls[0][1].mode).toBe('full-fit');
+      expect(request).toBeCalledWith({
+        url: '/file/some-file-id/image',
+        params: {
+          mode: 'full-fit',
+          width: 200,
+          height: 200,
+          collection: 'some-collection-name',
+          'max-age': 3600
+        },
+        responseType: 'image'
+      });
+    });
+
+    it('should request normal thumbnail given no size option', async () => {
+      const {dataUriService, request} = setup();
+
+      await dataUriService.fetchThumbnailDataUri(fileItemWithThumbnails);
+
+      expect(request).toBeCalledWith({
+        url: '/file/some-file-id/artifact/thumb_120.jpg/binary',
+        params: {
+          collection: 'some-collection-name',
+          'max-age': 3600
+        },
+        responseType: 'image'
+      });
+    });
+
+    it('should request large thumbnail given large size option', async () => {
+      const {dataUriService, request} = setup();
+
+      await dataUriService.fetchThumbnailDataUri(fileItemWithThumbnails, {size: 'large'});
+
+      expect(request).toBeCalledWith({
+        url: '/file/some-file-id/artifact/thumb_320.jpg/binary',
+        params: {
+          collection: 'some-collection-name',
+          'max-age': 3600
+        },
+        responseType: 'image'
+      });
+    });
+
+    it('should reject and not make any request given file item with no thumbnail artifacts', async () => {
+      const {dataUriService, request} = setup();
+
+      await expect(dataUriService.fetchThumbnailDataUri(fileItemWithNoThumbnails))
+        .rejects.toMatchObject({
+          message: 'no thumbnail is available for this file'
+        });
+
+      expect(request).not.toBeCalled();
     });
   });
 });

68958a6157143d5c437f3582efe085618b3922cd
Ben Gummer <bgummer@atlassian.com>
2017-09-14 15:06:47 -0500

    fix(component): global primary icon in drawer is now slightly lighter (N300)
    affects: @atlaskit/navigation
    
    ISSUES CLOSED: AK-3529

--- packages/navigation/src/components/styled/DrawerPrimaryIcon.jsx
+++ packages/navigation/src/components/styled/DrawerPrimaryIcon.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { akColorN300 } from '@atlaskit/util-shared-styles';
 import { drawerBackIconSize } from '../../utils/drawer-style-variables';
 
 const DrawerPrimaryIcon = styled.div`
@@ -7,6 +8,8 @@ const DrawerPrimaryIcon = styled.div`
   height: ${drawerBackIconSize}px;
   justify-content: center;
   width: ${drawerBackIconSize}px;
+  // Drawer colour is always white (not themed) so fixed colour here is fine.
+  color: ${akColorN300};
 `;
 
 DrawerPrimaryIcon.displayName = 'DrawerPrimaryIcon';

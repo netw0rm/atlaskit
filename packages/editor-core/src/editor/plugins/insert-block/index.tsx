cf7a7dac5007c70b96c8ed10dfca615ecf321871
Scott Sidwell <ssidwell@atlassian.com>
2017-09-20 09:59:46 +1000

    feat(component): remove bottom toolbar from full-page editor
    affects: @atlaskit/editor-core
    
    ISSUES CLOSED: ED-2783

--- packages/editor-core/src/editor/plugins/insert-block/index.tsx
+++ packages/editor-core/src/editor/plugins/insert-block/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { EditorPlugin } from '../../types';
 import { stateKey as blockTypeStateKey } from '../../../plugins/block-type';
+import { stateKey as mediaStateKey } from '../../../plugins/media';
 import { stateKey as tablesStateKey } from '../../../plugins/table';
 import ToolbarInsertBlock from '../../../ui/ToolbarInsertBlock';
 
@@ -8,10 +9,12 @@ const insertBlockPlugin: EditorPlugin = {
   primaryToolbarComponent(editorView) {
     const pluginStateBlockType = blockTypeStateKey.getState(editorView.state);
     const pluginStateTable = tablesStateKey.getState(editorView.state);
+    const pluginStateMedia = mediaStateKey.getState(editorView.state);
     return <ToolbarInsertBlock
       editorView={editorView}
       pluginStateBlockType={pluginStateBlockType}
       pluginStateTable={pluginStateTable}
+      pluginStateMedia={pluginStateMedia}
     />;
   }
 };

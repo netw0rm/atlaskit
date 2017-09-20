6d410e89e022d8687448247dd79f333c7c89de3e
Kamil Kaczmarczyk <kkaczmarczyk@atlassian.com>
2017-09-20 10:43:33 +0200

    WIP(component): adding a page header component
    affects: @atlaskit/page-header

---
+++ packages/page-header/src/components/PageHeader.jsx
@@ -0,0 +1,42 @@
+// @flow
+
+import React, { PureComponent, Element } from 'react';
+import type { PropType } from 'babel-plugin-react-flow-props-to-prop-types'; // eslint-disable-line import/no-extraneous-dependencies
+
+import {
+  Outer,
+  BreadcrumbsContainer,
+  TitleWrapper,
+  Title,
+  ActionsWrapper,
+  BottomBarWrapper,
+} from '../styled/PageHeader';
+
+type DefaultProps = {
+  breadcrumbs?: PropType<Element<any>, any>,
+  actions?: PropType<Element<any>, any>,
+  bottomBar?: PropType<Element<any>, any>,
+};
+
+type Props = DefaultProps & {
+  children: PropType<Array<Element<any>> | Element<any>, any>,
+};
+
+export default class PageHeader extends PureComponent {
+  defaultProps: DefaultProps = { breadcrumbs: undefined };
+  props: Props;
+  render() {
+    const { breadcrumbs, actions, bottomBar, children } = this.props;
+
+    return (
+      <Outer>
+        {breadcrumbs && <BreadcrumbsContainer> {breadcrumbs} </BreadcrumbsContainer>}
+        <TitleWrapper>
+          <Title>{children}</Title>
+          <ActionsWrapper>{actions}</ActionsWrapper>
+        </TitleWrapper>
+        {bottomBar && <BottomBarWrapper> {bottomBar} </BottomBarWrapper>}
+      </Outer>
+    );
+  }
+}

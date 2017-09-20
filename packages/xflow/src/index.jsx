977a9b24c41fdcdc8eecb1a6b3267a9e577ed77d
Andrew Hammond <ahammond@atlassian.com>
2017-09-20 11:06:48 +1000

    refactor(component): change to export default as
    affects: @atlaskit/xflow

--- packages/xflow/src/index.jsx
+++ packages/xflow/src/index.jsx
@@ -1,6 +1,6 @@
 export { withXFlowProvider, XFlowProvider, xFlowShape } from './common/components/XFlowProvider';
 export { default as RequestOrStartTrial } from './common/components/RequestOrStartTrial';
-export { RequestProductTrialOptOut } from './request-trial-opt-out/components/OptOut';
+export { default as RequestProductTrialOptOut } from './request-trial-opt-out/components/OptOut';
 export {
   default as JiraToConfluenceXFlowProvider,
 } from './jira-confluence/JiraToConfluenceXFlowProvider';

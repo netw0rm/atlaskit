2f2a2dc9dbbdda7a493c600c8863f74944839748
Herman Chow <hchow@atlassian.com>
2017-09-25 11:38:09 +1000

    feat(component): update user groups and wording related to JSD.
    affects: @atlaskit/xflow

--- packages/xflow/src/jira-jsd/retrieveJiraUsers.js
+++ packages/xflow/src/jira-jsd/retrieveJiraUsers.js
@@ -1,14 +1,10 @@
 import retrieveUserManagementUsers, {
   JIRA_SOFTWARE_GROUP,
-  JIRA_CORE_GROUP,
-  JIRA_SERVICE_DESK_GROUP,
   SITE_ADMINS_GROUP,
 } from '../common/retrieveUserManagementUsers';
 
 const VALID_GROUPS = [
   JIRA_SOFTWARE_GROUP,
-  JIRA_CORE_GROUP,
-  JIRA_SERVICE_DESK_GROUP,
   SITE_ADMINS_GROUP,
 ];
 

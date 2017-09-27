1b554a1af46355756aaf111ee44b3b9345f9ac20
Ben Conolly <bconolly@atlassian.com>
2017-09-27 10:49:31 +1000

    fix(component): use withTheme to get theme for icon color
    affects: @atlaskit/toggle

--- packages/toggle/src/styled/Handle.jsx
+++ packages/toggle/src/styled/Handle.jsx
@@ -1,18 +1,24 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { colors, themed } from '@atlaskit/theme';
 import { getHeight, paddingUnitless, transition } from './constants';
 
 const backgroundColor = themed({ light: colors.N0, dark: colors.DN600 });
 const backgroundColorChecked = themed({ light: colors.N0, dark: colors.DN0 });
+const backgroundColorDisabled = themed({ light: colors.N0, dark: colors.DN0 });
 
 const getTransform = ({ isChecked, size }) => (isChecked
   ? `translateX(${getHeight({ size })}px)`
   : 'initial'
 );
 
+const getBackgroundColor = ({ isChecked, isDisabled, ...rest }) => {
+  if (isDisabled) return backgroundColorDisabled(rest);
+  if (isChecked) return backgroundColorChecked(rest);
+  return backgroundColor(rest);
+};
+
 export default styled.span`
-  ${({ isDisabled }) => (isDisabled ? css`opacity: 0.5` : '')}
-  background-color: ${({ isChecked }) => (isChecked ? backgroundColorChecked : backgroundColor)};
+  background-color: ${getBackgroundColor};
   border-radius: 50%;
   bottom: ${2 * paddingUnitless}px;
   content: "";

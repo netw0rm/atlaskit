80b24f8a2814c474c1691e00a3e7bed37f0209b9
Sean Curtis <scurtis@atlassian.com>
2017-09-28 17:09:14 +1000

    feat(component): icon component performance improved and the glyph can now be passed as a string.
    affects: @atlaskit/icon

--- packages/icon/src/components/Icon.jsx
+++ packages/icon/src/components/Icon.jsx
@@ -14,21 +14,34 @@ const sizes = {
 // Once styled-components is bumped > 2.X.X we can enjoy `toHaveStyleRule` from
 // https://github.com/styled-components/jest-styled-components#tohavestylerule
 
+const getSize = props => {
+  if (props.size) {
+    return `height: ${sizes[props.size]}; width: ${sizes[props.size]};`;
+  }
+  return null;
+};
+
 export const spanStyles = css`
+  ${getSize}
   color: ${p => p.primaryColor || 'currentColor'};
   display: inline-block;
   fill: ${p => p.secondaryColor || colors.background};
-  height: ${p => p.size};
   line-height: 1;
-  width: ${p => p.size};
+  > svg {
+    ${getSize}
+    max-height: 100%;
+    max-width: 100%;
+    overflow: hidden;
+    vertical-align: bottom;
+  }
 `;
 
-export const Span = styled.span`${spanStyles}`;
+export const IconWrapper = styled.span`${spanStyles}`;
 
 class Icon extends PureComponent {
   static propTypes = {
     /** Glyph to show by Icon component (not required when you import a glyph directly) */
-    glyph: PropTypes.func.isRequired,
+    glyph: PropTypes.oneOfType([PropTypes.string, PropTypes.func]).isRequired,
     /** String to apply as the SVG title element */
     label: PropTypes.string.isRequired,
     /** onClick handler for the icon element */
@@ -47,38 +60,37 @@ class Icon extends PureComponent {
 
   render() {
     const {
-      glyph: Glyph,
+      glyph,
       onClick,
       primaryColor,
       secondaryColor,
       size,
-      ...svgProps
     } = this.props;
 
-    const dimensions = sizes[size];
-    const svgStyles = {
-      height: dimensions,
-      maxHeight: '100%',
-      maxWidth: '100%',
-      overflow: 'hidden',
-      verticalAlign: 'bottom',
-      width: dimensions,
-    };
-
+    // handling the glyphs as strings
+    if (typeof glyph === 'string') {
+      return (
+        <IconWrapper
+          onClick={onClick}
+          primaryColor={primaryColor}
+          secondaryColor={secondaryColor}
+          size={size}
+          aria-label={this.props.label}
+          dangerouslySetInnerHTML={{ __html: glyph }}
+        />
+      );
+    }
+    // handling the glyphs when passed through as functions
     return (
-      <Span
+      <IconWrapper
         onClick={onClick}
         primaryColor={primaryColor}
         secondaryColor={secondaryColor}
-        size={dimensions}
+        size={size}
+        aria-label={this.props.label}
       >
-        <Glyph
-          role="img"
-          style={svgStyles}
-          title={this.props.label}
-          {...svgProps}
-        />
-      </Span>
+        {glyph}
+      </IconWrapper>
     );
   }
 }

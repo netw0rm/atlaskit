aa0cd1a82091a0755260917827181e017034d12d
Jonathan Yeo <jyeo@atlassian.com>
2017-10-03 18:15:06 +1100

    refactor(refactor): drawer tests: unmount drawer after-each to remove window event listener
    affects: @atlaskit/navigation

--- packages/navigation/test/unit/DrawerSpec.jsx
+++ packages/navigation/test/unit/DrawerSpec.jsx
@@ -21,51 +21,62 @@ describe('<Drawer />', () => {
     window.dispatchEvent(event);
   };
 
+  let drawerWrapper;
+  const onBackButtonStub = jest.fn();
+  const onKeyDownStub = jest.fn();
+  beforeEach(() => {
+    drawerWrapper = mount(
+      <Drawer
+        isOpen
+        onBackButton={onBackButtonStub}
+        onKeyDown={onKeyDownStub}
+      />
+    );
+  });
+
+  afterEach(() => {
+    // Remove keydown event listener on `window`
+    drawerWrapper.unmount();
+    onBackButtonStub.mockReset();
+    onKeyDownStub.mockReset();
+  });
+
   describe('onKeyDown func is NOT provided', () => {
     it('should do the default behaviours if the key is Escape', () => {
-      const onBackButtonStub = jest.fn();
-      mount(<Drawer onBackButton={onBackButtonStub} />);
       keyDown(escKeyCode);
       expect(onBackButtonStub).toHaveBeenCalledWith(event);
     });
     it('should NOT do the default behaviours if the key is NOT Escape', () => {
-      const onBackButtonStub = jest.fn();
-      mount(<Drawer onBackButton={onBackButtonStub} />);
       keyDown(delKeyCode);
-      expect(onBackButtonStub).not.toHaveBeenCalledWith(event);
+      expect(onBackButtonStub).not.toHaveBeenCalled();
+    });
+    it('should NOT do the default behaviours if the drawer is closed', () => {
+      drawerWrapper.setProps({ isOpen: false });
+      keyDown(escKeyCode);
+      expect(onBackButtonStub).not.toHaveBeenCalled();
     });
   });
 
   describe('onKeyDown func is provided', () => {
     it('should call the provided function', () => {
-      const onKeyDownStub = jest.fn();
-      mount(<Drawer onKeyDown={onKeyDownStub} />);
       keyDown(escKeyCode);
       expect(onKeyDownStub).toHaveBeenCalledWith(event);
     });
     describe('and default is not prevented', () => {
-      let onKeyDownStub;
-      let onBackButtonStub;
-      beforeEach(() => {
-        onKeyDownStub = jest.fn();
-        onBackButtonStub = jest.fn();
-        mount(<Drawer onKeyDown={onKeyDownStub} onBackButton={onBackButtonStub} />);
-      });
       it('should also do the default behaviours if the key is Escape', () => {
         keyDown(escKeyCode);
         expect(onBackButtonStub).toHaveBeenCalledWith(event);
       });
       it('should NOT do the default behaviours if the key is NOT Escape', () => {
         keyDown(delKeyCode);
-        expect(onBackButtonStub).not.toHaveBeenCalledWith(event);
+        expect(onBackButtonStub).not.toHaveBeenCalled();
       });
     });
 
     describe('and default is prevented', () => {
       it('should NOT do the default behaviours', () => {
-        const onKeyDownStub = (e) => { e.preventDefault(); };
-        const onBackButtonStub = jest.fn();
-        mount(<Drawer onKeyDown={onKeyDownStub} onBackButton={onBackButtonStub} />);
+        const onKeyDownStubDefaultPrevented = (e) => { e.preventDefault(); };
+        drawerWrapper.setProps({ onKeyDown: onKeyDownStubDefaultPrevented });
         keyDown(escKeyCode);
         expect(onBackButtonStub).not.toHaveBeenCalledWith(event);
       });
@@ -76,30 +87,28 @@ describe('<Drawer />', () => {
     describe('is open', () => {
       it('render the header', () => {
         const header = <div />;
-        const wrapper = mount(<Drawer isOpen header={header} />);
-
-        expect(wrapper.find(ContainerHeader).contains(header)).toBe(true);
+        drawerWrapper.setProps({ header });
+        expect(drawerWrapper.find(ContainerHeader).contains(header)).toBe(true);
       });
 
       it('should render its children', () => {
         const content = <div className="foo" />;
-        expect(mount(<Drawer isOpen>{content}</Drawer>)
-          .contains(content)).toBe(true);
+        drawerWrapper.setProps({ children: content });
+        expect(drawerWrapper.contains(content)).toBe(true);
       });
     });
 
     describe('is closed', () => {
       it('should not render its children', () => {
         const content = <div className="foo" />;
-        expect(mount(<Drawer isOpen={false}>{content}</Drawer>)
-          .contains(content)).toBe(false);
+        drawerWrapper.setProps({ isOpen: false, children: content });
+        expect(drawerWrapper.contains(content)).toBe(false);
       });
 
       it('should not render a provided header', () => {
         const header = <div />;
-        const wrapper = mount(<Drawer isOpen={false} header={header} />);
-
-        expect(wrapper.find(ContainerHeader).contains(header)).toBe(false);
+        drawerWrapper.setProps({ isOpen: false, header });
+        expect(drawerWrapper.find(ContainerHeader).contains(header)).toBe(false);
       });
     });
   });
@@ -108,9 +117,9 @@ describe('<Drawer />', () => {
     describe('is open', () => {
       it('should render the primaryIcon', () => {
         const icon = <img alt="foo" />;
-        const wrapper = mount(<Drawer isOpen primaryIcon={icon} />);
+        drawerWrapper.setProps({ primaryIcon: icon });
 
-        expect(wrapper
+        expect(drawerWrapper
           .find(DrawerSide)
           .find(DrawerPrimaryIcon)
           .props().children).toBe(icon);
@@ -118,36 +127,33 @@ describe('<Drawer />', () => {
 
       it('should render backIcon inside a <DrawerBackIcon />', () => {
         const icon = <img alt="foo" />;
-        const wrapper = mount(<Drawer isOpen backIcon={icon} />);
+        drawerWrapper.setProps({ backIcon: icon });
 
-        expect(wrapper
+        expect(drawerWrapper
           .find(DrawerSide)
           .find(DrawerBackIcon)
           .props().children).toBe(icon);
       });
 
       it('should trigger onBackButton when the DrawerTrigger is clicked', () => {
-        const spy = jest.fn();
-        mount(<Drawer isOpen onBackButton={spy} />)
-          .find(DrawerTrigger).find(GlobalItem).simulate('click');
-        expect(spy).toHaveBeenCalled();
+        drawerWrapper.find(DrawerTrigger).find(GlobalItem).simulate('click');
+        expect(onBackButtonStub).toHaveBeenCalled();
       });
 
       it('should default the back icon position at 0px', () => {
-        expect(mount(
-          <Drawer isOpen />
-        ).find(DrawerBackIconWrapper).props().iconOffset).toBe(0);
+        expect(drawerWrapper
+          .find(DrawerBackIconWrapper).props().iconOffset).toBe(0);
       });
 
       it('should update the backIconOffset based on where the backIcon is rendered', () => {
-        expect(mount(
-          <Drawer isOpen iconOffset={123} />
-        ).find(DrawerBackIconWrapper).props().iconOffset).toBe(123);
+        drawerWrapper.setProps({ iconOffset: 123 });
+        expect(drawerWrapper
+          .find(DrawerBackIconWrapper).props().iconOffset).toBe(123);
       });
 
       it('should wrap a DrawerBackIcon in a DrawerTrigger', () => {
         expect(
-          mount(<Drawer isOpen />)
+          drawerWrapper
             .find(DrawerTrigger)
             .find(DrawerBackIcon)
             .exists()
@@ -162,31 +168,29 @@ describe('<Drawer />', () => {
 
   describe('blanket', () => {
     it('should render a blanket when the drawer is open and closed', () => {
-      expect(mount(<Drawer isOpen />).find(Blanket).length).toBe(1);
-      expect(mount(<Drawer isOpen={false} />).find(Blanket).length).toBe(1);
+      expect(drawerWrapper.find(Blanket).length).toBe(1);
+      drawerWrapper.setProps({ isOpen: false });
+      expect(drawerWrapper.find(Blanket).length).toBe(1);
     });
 
     it('should render a blocking blanket when the drawer is open', () => {
-      const blanket = mount(<Drawer isOpen />).find(Blanket);
+      const blanket = drawerWrapper.find(Blanket);
       expect(blanket.props().isTinted).toBe(true);
       expect(blanket.props().canClickThrough).toBe(false);
     });
 
     it('should hide the blanket when the drawer is closed', () => {
-      const blanket = mount(<Drawer isOpen={false} />).find(Blanket);
+      drawerWrapper.setProps({ isOpen: false });
+      const blanket = drawerWrapper.find(Blanket);
       expect(blanket.props().isTinted).toBe(false);
       expect(blanket.props().canClickThrough).toBe(true);
     });
 
     it('should treat clicking an open blanket as if it where a back button', () => {
-      const onBackButton = jest.fn();
-      const blanket = mount(<Drawer isOpen onBackButton={onBackButton} />).find(Blanket);
-
-      expect(blanket.props().onBlanketClicked).toBe(onBackButton);
-
-      blanket.simulate('click');
-
-      expect(onBackButton).toHaveBeenCalled();
+      drawerWrapper
+        .find(Blanket)
+        .simulate('click');
+      expect(onBackButtonStub).toHaveBeenCalled();
     });
   });
 });

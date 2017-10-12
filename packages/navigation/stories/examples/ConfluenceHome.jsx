07e985a44ccad78bd9e876477b36e05e07dea7d9
Raja Bellebon <rbellebon@atlassian.com>
2017-10-12 20:00:21 +1100

    docs(stories): remove spaces and fix indentations
    affects: @atlaskit/navigation

--- packages/navigation/stories/examples/ConfluenceHome.jsx
+++ packages/navigation/stories/examples/ConfluenceHome.jsx
@@ -170,7 +170,6 @@ export default class ConfluenceHome extends PureComponent {
         ...this.state.stack,
         [
           <AkNavigationItem icon={<CalendarIcon label="Calendar" />} text="Calendars" />,
-
           <AkNavigationItem icon={<img alt="questions" src={QuestionConfluence} />} text="Questions" />,
         ],
       ],

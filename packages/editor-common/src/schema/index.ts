2d4274213d70c76d76500d3fcc1e8f0baf797ba9
Oscar Wallhult <owallhult@atlassian.com>
2017-10-16 13:30:39 +1100

    feat(component): fixing exports
    affects: @atlaskit/editor-common

--- packages/editor-common/src/schema/index.ts
+++ packages/editor-common/src/schema/index.ts
@@ -1,5 +1,6 @@
 export * from './marks';
 export * from './nodes';
+export * from './unsupported';
 
 export { createSchema } from './create-schema';
 export { bitbucketSchema } from './bitbucket-schema';
@@ -7,4 +8,16 @@ export { confluenceSchema } from './confluence-schema';
 export { defaultSchema } from './default-schema';
 export { hipchatSchema } from './hipchat-schema';
 
-export { default as createJIRASchema } from './jira-schema';
+export {
+  default as createJIRASchema,
+  isSchemaWithLists,
+  isSchemaWithMentions,
+  isSchemaWithLinks,
+  isSchemaWithAdvancedTextFormattingMarks,
+  isSchemaWithCodeBlock,
+  isSchemaWithBlockQuotes,
+  isSchemaWithMedia,
+  isSchemaWithSubSupMark,
+  isSchemaWithTextColor,
+  isSchemaWithTables
+} from './jira-schema';

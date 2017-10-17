195d17f9893c13f1f3e22740f14c1c687bb09409
Dmitrii Sorin <dsorin@atlassian.com>
2017-10-18 10:22:05 +1100

    refactor(component): clean all renderer files
    affects: @atlaskit/renderer
    
    BREAKING CHANGE:
    Preparation work for splitting editor-core into editor and renderer. Renderer is now an empty
    package

--- packages/renderer/src/marks/mono.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { PureComponent } from 'react';
-
-export default class Mono extends PureComponent<{}, {}> {
-  render() {
-    return <span style={{fontFamily: 'monospace', whiteSpace: 'pre-wrap'}}>{this.props.children}</span>;
-  }
-}

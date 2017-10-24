82059544224e9767b4faf041679b7410b0007366
Ben Conolly <bconolly@atlassian.com>
2017-10-24 14:51:14 +1100

    feat(component): website redirects to new ak site if the component vanishes.
    affects: @atlaskit/website

--- packages/website/build/data.template.js
+++ packages/website/build/data.template.js
@@ -9,20 +9,22 @@ module.exports = ({ components }) => `/* eslint-disable global-require, quote-pr
  */
 const components = {${components.map(component => `
   '${component.key}': {
-    description: '${component.pkg.description}',
+    description: '${component.pkg.description || component.description}',
     docs: ${component.docs ? `require('../../${component.key}/docs')` : 'false'},
     changelog: ${JSON.stringify(component.changelog)},
     props: ${JSON.stringify(component.props)},
     key: '${component.key}',
     name: '${component.name}',
     packageName: '${component.pkg.name}',
-    maintainers: ${JSON.stringify(component.pkg.maintainers || [])},
+    maintainers: ${component.pkg.maintainers
+      ? JSON.stringify(component.pkg.maintainers || [])
+      : JSON.stringify(component.maintainers)
+    },
     isPublished: '${component.isPublished}',
     publishedDate: '${component.lastPublishedOn}',
-    version: '${component.pkg.version}',
+    version: '${component.pkg.version || component.currentVersion}',
     versions: [${component.versions.map(v => `'${v}'`).join(', ')}],
     storybooks: [${component.storybooks.map(v => `'${v}'`).join(', ')}],
-    isPattern: ${!!component.isPattern},
     supportsDarkMode: ${!!component.supportsDarkMode},
     ${component.nestedDocs
       ? `components: {
@@ -30,6 +32,7 @@ const components = {${components.map(component => `
       },`
       : ''
     }
+    packageHasBeenMoved: ${!!component.packageHasBeenMoved},
   },
 `).join('')}};
 

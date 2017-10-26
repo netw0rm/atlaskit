9f823c8121beffce5579234e6d4e5db8709267ca
Jarred Mack <jmack2@.atlassian.com>
2017-10-26 16:41:08 +1100

    feat(component): migrate shared components to editor common
    affects: @atlaskit/editor-common

--- packages/editor-common/src/index.ts
+++ packages/editor-common/src/index.ts
@@ -1,9 +1,10 @@
+export * from './schema';
+export * from './utils';
+
 import ProviderFactory, { WithProviders } from './providerFactory';
 export { ProviderFactory, WithProviders };
 
 // Namespace the exports for other packages
-import * as schema from './schema';
 import * as styles from './styles';
 import * as ui from './ui';
-import * as utils from './utils';
-export { schema, styles, ui, utils };
+export { styles, ui };
--- packages/editor-common/src/ui/index.tsx
+++ packages/editor-common/src/ui/index.tsx
@@ -1,5 +1,5 @@
 export { default as Emoji } from './Emoji';
-// export { default as Media } from './Media'; // @TODO Import after merge
+export { default as MediaItem } from './MediaItem';
 export { default as Mention } from './Mention';
 export { default as Popup } from './Popup';
 export { default as Portal } from './Portal';

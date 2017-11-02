c2d0a3c1777f12f47de92bf6fe5c4ca77879370f
ckrishnakumar <ckrishnakumar@atlassian.com>
2017-11-02 14:08:22 +1100

    WIP(component): call emoji suggestion service in mock
    affects: @atlaskit/editor-core

--- packages/editor-core/stories/mock-emoji-suggestions-provider.ts
+++ packages/editor-core/stories/mock-emoji-suggestions-provider.ts
@@ -1,10 +1,24 @@
+import axios, { AxiosRequestConfig } from "axios";
+
 export default class EmojiSuggestionsProviderMock {
-  search(query: string): Promise<any> {
-    return Promise.resolve({
-      emojis: [ '🚘', '🚖', '🏎', '🚓' ]
+  search(query: string): Promise<AxiosRequestConfig> {
+    const config = {
+      headers: {},
+      baseURL: "https://pf-nlp-service.us-west-2.staging.atl-paas.net/deepmoji"
+    };
+
+    const url = `/${query}`;
+    return new Promise((resolve, reject) => {
+      axios.get(url, config)
+        .then(response => {
+          resolve(response.data);
+        })
+        .catch(reject);
     });
   }
 }
 
 export const emojiSuggestionsProvider = new EmojiSuggestionsProviderMock();
-export const emojiSuggestionsProviderPromise = Promise.resolve(emojiSuggestionsProvider);
+export const emojiSuggestionsProviderPromise = Promise.resolve(
+  emojiSuggestionsProvider
+);

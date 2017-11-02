ea4404c96d36e67665daf1155c0cbe050bf35fa0
Jared Crowe <jcrowe@atlassian.com>
2017-11-03 09:37:48 +1100

    docs(stories): fix router story
    affects: @atlaskit/breadcrumbs

--- packages/breadcrumbs/stories/20-breadcrumbs-react-router-story.jsx
+++ packages/breadcrumbs/stories/20-breadcrumbs-react-router-story.jsx
@@ -1,12 +1,12 @@
 import { storiesOf } from '@kadira/storybook';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { name } from '../package.json';
 import TitledPage from './component/TitledPage';
 
 const imports: Array<Array<string>> = [
   ['React', 'react'],
-  ['Breadcrumbs, { BreadcrumbsItem }', 'ak-breadcrumbs'],
+  ['Breadcrumbs, { BreadcrumbsItem }', '@atlaskit/breadcrumbs'],
 ];
 
 function makePage(title) {
@@ -16,13 +16,13 @@ function makePage(title) {
 storiesOf(name, module)
   .addCodeExampleStory('with react-router', () => (
     <Router>
-      <div>
-        <Route component={makePage('Container home')} />
+      <Switch>
         <Route component={makePage('Page 1')} path="/page1" />
         <Route component={makePage('Page 2')} path="/page2" />
         <Route component={makePage('Page 3')} path="/page3" />
         <Route component={makePage('Page 4')} path="/page4" />
-      </div>
+        <Route component={makePage('Container home')} />
+      </Switch>
     </Router>
     ),
     { imports }

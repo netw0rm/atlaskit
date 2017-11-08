cfc4c11c98476ae053f375199481fe714152a95a
yjutard <yjutard@atlassian.com>
2017-11-08 16:06:42 +1100

    test(stories): xflow - new story for internal component SuccessFlag
    affects: @atlaskit/xflow

---
+++ packages/xflow/stories/110-SuccessFlag-story.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import SuccessFlag from '../src/common/components/SuccessFlag';
+
+import setupStorybookAnalytics from './util/setupStorybookAnalytics';
+
+storiesOf('common/SuccessFlag')
+  .add('basic', () => setupStorybookAnalytics(<SuccessFlag
+    showFlag
+    title="(Title) all done!"
+    description="(description) That went pretty smoothly."
+    source="(source)"
+  />))
+  .add('advanced', () => setupStorybookAnalytics(<SuccessFlag
+    showFlag
+    title="(Title) all done!"
+    description="(description) That went pretty smoothly."
+    flagActions={[
+      { content: 'shut up and take my money', onClick: action('Flag action') },
+    ]}
+    source="(source)"
+    onDismissed={action('Flag dismissed.')}
+  />));

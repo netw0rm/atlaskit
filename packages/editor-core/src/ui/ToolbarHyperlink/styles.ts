2507d1f3b7f87c2e5f8c22440bf3c69c266c97b5
Luke Batchelor <lbatchelor@atlassian.com>
2017-11-09 15:42:34 +1100

    chore(build): Removes the editor packages

--- packages/editor-core/src/ui/ToolbarHyperlink/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-// tslint:disable-next-line:variable-name
-export const OuterContainer = styled.span`
-  position: relative;
-`;
--- packages/renderer/package.json.d.ts
+++
@@ -1,3 +0,0 @@
-export const description: string;
-export const name: string;
-export const version: string;

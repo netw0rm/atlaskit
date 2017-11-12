ace01ad3c5a023fd7a4c078b0ca8fe21c181ae37
Max Puckeridge <mpuckeridge@atlassian.com>
2017-11-13 10:42:49 +1100

    feat(component): export two new providers
    affects: @atlaskit/xflow

--- packages/xflow/src/index.jsx
+++ packages/xflow/src/index.jsx
@@ -1,9 +1,7 @@
 export { withXFlowProvider, XFlowProvider, xFlowShape } from './common/components/XFlowProvider';
 export { default as RequestOrStartTrial } from './common/components/RequestOrStartTrial';
 export { default as RequestProductTrialOptOut } from './request-trial-opt-out/components/OptOut';
-export {
-  default as JiraToJSDXFlowProvider,
-} from './product-xflow-providers/JiraToJSDXFlowProvider';
-export {
-  default as JiraToConfluenceXFlowProvider,
-} from './product-xflow-providers/JiraToConfluenceXFlowProvider';
+export { default as JiraToJSDXFlowProvider } from './product-xflow-providers/JiraToJSDXFlowProvider';
+export { default as JiraToConfluenceXFlowProvider } from './product-xflow-providers/JiraToConfluenceXFlowProvider';
+export { default as JiraToJCXFlowProvider } from './product-xflow-providers/JiraToJCXFlowProvider';
+export { default as JiraToJSWXFlowProvider } from './product-xflow-providers/JiraToJSWXFlowProvider';

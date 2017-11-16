c6f1f3309373c632bbc97442f9438dbc48f99d69
yjutard <yjutard@atlassian.com>
2017-11-16 14:55:29 +1100

    feat(component): xflow - now able to retrieve current user and groups in both confluence and jira
    affects: @atlaskit/xflow

--- packages/xflow/src/common/services/tenantContext.js
+++ packages/xflow/src/common/services/tenantContext.js
@@ -3,17 +3,45 @@
 
 import 'es6-promise/auto';
 import 'whatwg-fetch';
+import pAny from 'p-any';
 
 const SITE_ADMINS_GROUP_NAME = 'site-admins';
 
 export const JIRA_CLOUD_ID_URL = '/rest/product-fabric/1.0/cloud/id';
 export const CONFLUENCE_CLOUD_ID_URL = '/wiki/rest/product-fabric/1.0/cloud/id';
 export const JIRA_CURRENT_USER_AND_GROUPS_URL = '/rest/api/2/myself?expand=groups';
+export const CONFLUENCE_CURRENT_USER_URL = '/wiki/rest/api/user/current';
+export const CONFLUENCE_USER_GROUPS_URL = accountId => `/wiki/rest/api/user/memberof?accountId=${accountId}`;
+
 const DEFAULT_AVATAR_URL = 'https://i2.wp.com/avatar-cdn.atlassian.com/default/96?ssl=1';
 const AVATAR_REGEXP = /^https:\/\/avatar-cdn.atlassian.com\/[A-Za-z0-9]+/;
 
-function fetchSameOrigin(url) {
-  return fetch(url, { credentials: 'same-origin' });
+function fetchSameOrigin(url, prefix = '') {
+  return fetch(url, { credentials: 'same-origin' })
+    .then(response => {
+      if (response.ok && response.status === 200) return response.text();
+
+      let msg = `${prefix} fetch error`;
+      if (response.status !== 200) {
+        msg += `, status = ${response.status}`;
+      } else if (!response.ok) {
+        msg += ', !ok';
+      }
+      msg += '!';
+
+      const err = new Error(msg);
+      err.status = response.status;
+
+      throw err;
+    })
+    .then(responseText => {
+      try {
+        return JSON.parse(responseText);
+      } catch (err) {
+        err.message = `${prefix}JSON parse error!`;
+        throw err;
+      }
+    });
 }
 
 /**
@@ -37,17 +65,26 @@ export const getAvatarUrl = ({ avatarUrls }) => {
 let currentUserPromiseCached = null;
 export function fetchCurrentUser() {
   // WIP only works in JIRA context (not confluence)
-  currentUserPromiseCached = currentUserPromiseCached
-    || fetchSameOrigin(JIRA_CURRENT_USER_AND_GROUPS_URL)
-    .then((response) => {
-      if (response.status !== 200) {
-        throw new Error(
-          `Unable to retrieve information about current user. Status: ${response.status}`
-        );
-      }
+  currentUserPromiseCached = currentUserPromiseCached || (() => pAny([
+    // Jira allows fetching user + group in one go
+    fetchSameOrigin(JIRA_CURRENT_USER_AND_GROUPS_URL, 'Jira endpoint:'),
+    // Confluence needs 2 calls:
+    fetchSameOrigin(CONFLUENCE_CURRENT_USER_URL, 'Confluence endpoint #1:')
+      .then(user => fetchSameOrigin(CONFLUENCE_USER_GROUPS_URL(user.accountId), 'Confluence endpoint #2:')
+      // fuse user and her/his groups into the Jira format
+        .then(groups => {
+          user.groups = {};
+          user.groups.items = groups.results;
+          return user;
+        })),
+  ])
+  .catch(aggregateErr => {
+    aggregateErr.message = 'Unable to retrieve information about current user: ';
+    aggregateErr.message += [...aggregateErr].map(err => err.message).join(', ');
 
-      return response.json();
-    });
+    throw aggregateErr;
+  })
+  )();
 
   return currentUserPromiseCached;
 }
@@ -79,14 +116,16 @@ export const getInstanceName = () => window.location.hostname;
  * Attempt to fetch cloud id from JIRA, then Confluence, otherwise throw an error
  */
 export const fetchCloudId = async () => {
-  let response = await fetchSameOrigin(JIRA_CLOUD_ID_URL);
-  if (!response.ok) {
-    response = await fetchSameOrigin(CONFLUENCE_CLOUD_ID_URL);
-  }
-  if (!response.ok) {
-    throw new Error(`Unable to retrieve cloud id. Status: ${response.status}`);
-  }
+  const response = await pAny([
+    fetchSameOrigin(JIRA_CLOUD_ID_URL, 'Jira endpoint:'),
+    fetchSameOrigin(CONFLUENCE_CLOUD_ID_URL, 'Confluence endpoint:'),
+  ])
+  .catch(aggregateErr => {
+    aggregateErr.message = 'Unable to retrieve cloud id: ';
+    aggregateErr.message += [...aggregateErr].map(err => err.message).join(', ');
 
-  const { cloudId } = await response.json();
-  return cloudId;
+    throw aggregateErr;
+  });
+
+  return response.cloudId;
 };
--- packages/xflow/test/unit/common/services/tenantContextSpec.js
+++ packages/xflow/test/unit/common/services/tenantContextSpec.js
@@ -8,95 +8,191 @@ import {
   fetchCurrentUser,
   JIRA_CLOUD_ID_URL,
   CONFLUENCE_CLOUD_ID_URL,
+  JIRA_CURRENT_USER_AND_GROUPS_URL,
+  CONFLUENCE_CURRENT_USER_URL,
+  CONFLUENCE_USER_GROUPS_URL,
 } from '../../../../src/common/services/tenantContext';
 import jiraAdminResponse from '../mock-data/fetchUserAndGroupsJiraAdmin.json';
 import nonAdminResponse from '../mock-data/fetchUserAndGroupsNonAdmin.json';
 import siteAdminResponse from '../mock-data/fetchUserAndGroupsSiteAdmin.json';
 
-const mockEndpointWithResponse = (response) => {
-  const url = '/rest/api/2/myself?expand=groups';
-  fetchMock.mock(url, response, { method: 'GET' });
-};
-
-const mockEndpointWithFailureStatus = (status) => {
-  const url = '/rest/api/2/myself?expand=groups';
-  fetchMock.mock(url, status);
-};
-
 describe('tenantContext', () => {
+  const DUMMY_HTML_SINCE_NO_ENDPOINT = '<html>some html...'; // real case when a url is not recognized -> returns home page
+
   afterEach(fetchMock.restore);
   afterEach(() => fetchCurrentUser.resetCache());
 
-  describe('fetchCloudId()', () => {
-    it('should return the expected cloud id from Jira', async () => {
-      const EXPECTED_CLOUD_ID = 'not-an-instance-name';
-      fetchMock.mock(JIRA_CLOUD_ID_URL, { cloudId: EXPECTED_CLOUD_ID }, { method: 'GET' });
-      const result = await fetchCloudId();
-      return expect(result).toBe(EXPECTED_CLOUD_ID);
+  describe('fetchCurrentUser()', () => {
+    const EXPECTED_USER = {
+      displayName: 'foo',
+      accountId: '12345',
+      groups: {
+        items: [
+          {
+            name: 'site-admins',
+            self: 'https://foo',
+          },
+        ],
+      },
+    };
+
+    describe('when in a JIRA-only instance', () => {
+      it('should return the current user', async () => {
+        fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, EXPECTED_USER);
+        fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+        const result = await fetchCurrentUser();
+        return expect(result).toEqual(EXPECTED_USER);
+      });
     });
 
-    it('should return the expected cloud id from Confluence if Jira fails', async () => {
-      const EXPECTED_CLOUD_ID = 'instance-without-jira';
-      fetchMock.mock(JIRA_CLOUD_ID_URL, 500);
-      fetchMock.mock(CONFLUENCE_CLOUD_ID_URL, { cloudId: EXPECTED_CLOUD_ID }, { method: 'GET' });
-      const result = await fetchCloudId();
-      return expect(result).toBe(EXPECTED_CLOUD_ID);
+    describe('when in a Confluence-only instance', () => {
+      it('should return the current user', async () => {
+        fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+        fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, {
+          displayName: 'foo',
+          accountId: '12345',
+          // no groups
+        });
+        fetchMock.getOnce(CONFLUENCE_USER_GROUPS_URL(EXPECTED_USER.accountId), {
+          results: [
+            {
+              name: 'site-admins',
+              self: 'https://foo',
+            },
+          ],
+        });
+        const result = await fetchCurrentUser();
+        return expect(result).toEqual(EXPECTED_USER);
+      });
     });
 
-    it('will reject with an error if both endpoints return a 500', async () => {
+    describe('when in a Confluence + Jira instance', () => {
+      it('should return the current user', async () => {
+        fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, EXPECTED_USER);
+        fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, {
+          displayName: 'foo',
+          accountId: '12345',
+          // no groups
+        });
+        fetchMock.getOnce(CONFLUENCE_USER_GROUPS_URL(EXPECTED_USER.accountId), {
+          results: [
+            {
+              name: 'site-admins',
+              self: 'https://foo',
+            },
+          ],
+        });
+        const result = await fetchCurrentUser();
+        return expect(result).toEqual(EXPECTED_USER);
+      });
+    });
+
+    it('should reject with an error if both endpoints return a 500', async () => {
       expect.assertions(1);
-      fetchMock.mock(JIRA_CLOUD_ID_URL, 500);
-      fetchMock.mock(CONFLUENCE_CLOUD_ID_URL, 500);
+      fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, 500);
+      fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, 500);
+      try {
+        await fetchCurrentUser();
+      } catch (err) {
+        expect(err.message.startsWith('Unable to retrieve information about current user:')).toBe(true);
+      }
+    });
+
+    it('should reject with an error if both endpoints are not recognized', async () => {
+      expect.assertions(1);
+      fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+      fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+      try {
+        await fetchCurrentUser();
+      } catch (err) {
+        expect(err.message.startsWith('Unable to retrieve information about current user:')).toBe(true);
+      }
+    });
+  });
+
+  describe('fetchCloudId()', () => {
+    const EXPECTED_CLOUD_ID = 'I-m-a-cloud-id';
+
+    describe('when in a JIRA-only instance', () => {
+      it('should return the expected cloud id', async () => {
+        fetchMock.getOnce(JIRA_CLOUD_ID_URL, { cloudId: EXPECTED_CLOUD_ID });
+        fetchMock.getOnce(CONFLUENCE_CLOUD_ID_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+        const result = await fetchCloudId();
+        return expect(result).toBe(EXPECTED_CLOUD_ID);
+      });
+    });
+
+    describe('when in a Confluence-only instance', () => {
+      it('should return the expected cloud id', async () => {
+        fetchMock.getOnce(JIRA_CLOUD_ID_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+        fetchMock.getOnce(CONFLUENCE_CLOUD_ID_URL, { cloudId: EXPECTED_CLOUD_ID });
+        const result = await fetchCloudId();
+        return expect(result).toBe(EXPECTED_CLOUD_ID);
+      });
+    });
+
+    describe('when in a Confluence + Jira instance', () => {
+      it('should return the expected cloud id', async () => {
+        fetchMock.getOnce(JIRA_CLOUD_ID_URL, { cloudId: EXPECTED_CLOUD_ID });
+        fetchMock.getOnce(CONFLUENCE_CLOUD_ID_URL, { cloudId: EXPECTED_CLOUD_ID });
+        const result = await fetchCloudId();
+        return expect(result).toBe(EXPECTED_CLOUD_ID);
+      });
+    });
+
+    it('should reject with an error if both endpoints return a 500', async () => {
+      expect.assertions(1);
+      fetchMock.getOnce(JIRA_CLOUD_ID_URL, 500);
+      fetchMock.getOnce(CONFLUENCE_CLOUD_ID_URL, 500);
       try {
         await fetchCloudId();
-      } catch (e) {
-        expect(e).toEqual(
-          new Error('Unable to retrieve cloud id. Status: 500')
-        );
+      } catch (err) {
+        expect(err.message.startsWith('Unable to retrieve cloud id')).toBe(true);
+      }
+    });
+
+    it('should reject with an error if both endpoints are not recognized', async () => {
+      expect.assertions(1);
+      fetchMock.getOnce(JIRA_CLOUD_ID_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+      fetchMock.getOnce(CONFLUENCE_CLOUD_ID_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
+      try {
+        await fetchCloudId();
+      } catch (err) {
+        expect(err.message.startsWith('Unable to retrieve cloud id')).toBe(true);
       }
     });
   });
 
   describe('isCurrentUserSiteAdmin', () => {
     it('will return false if he/she is only a Jira administrator', async () => {
-      mockEndpointWithResponse(jiraAdminResponse);
+      fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, jiraAdminResponse);
+      fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
       const result = await isCurrentUserSiteAdmin();
       return expect(result).toBe(false);
     });
 
     it('will return false if she/he is not an admin', async () => {
-      mockEndpointWithResponse(nonAdminResponse);
+      fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, nonAdminResponse);
+      fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
       const result = await isCurrentUserSiteAdmin();
       return expect(result).toBe(false);
     });
 
     it('will return true if she/he is a site admin', async () => {
-      mockEndpointWithResponse(siteAdminResponse);
+      fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, siteAdminResponse);
+      fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, DUMMY_HTML_SINCE_NO_ENDPOINT);
       const result = await isCurrentUserSiteAdmin();
       return expect(result).toBe(true);
     });
 
-    it('will reject with an error if the endpoint returns a 404', async () => {
+    it('will reject with an error if all endpoint have an error', async () => {
       expect.assertions(1);
-      mockEndpointWithFailureStatus(404);
+      fetchMock.getOnce(CONFLUENCE_CURRENT_USER_URL, 500);
+      fetchMock.getOnce(JIRA_CURRENT_USER_AND_GROUPS_URL, 500);
       try {
         await isCurrentUserSiteAdmin();
-      } catch (e) {
-        expect(e).toEqual(
-          new Error('Unable to retrieve information about current user. Status: 404')
-        );
-      }
-    });
-
-    it('will reject with an error if the endpoint returns a 500', async () => {
-      expect.assertions(1);
-      mockEndpointWithFailureStatus(500);
-      try {
-        await isCurrentUserSiteAdmin();
-      } catch (e) {
-        expect(e).toEqual(
-          new Error('Unable to retrieve information about current user. Status: 500')
-        );
+      } catch (err) {
+        expect(err.message.startsWith('Unable to retrieve information about current user:')).toBe(true);
       }
     });
   });

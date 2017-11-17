476c7194cec45788a4a000780bd48e28223ede3a
yjutard <yjutard@atlassian.com>
2017-11-17 14:12:42 +1100

    refactor(refactor): xflow - refactor analytics tests into a describe container
    affects: @atlaskit/xflow

--- packages/xflow/test/unit/analytics/ConfirmTrialSpec.jsx
+++ packages/xflow/test/unit/analytics/ConfirmTrialSpec.jsx
@@ -5,47 +5,49 @@ import { withAnalyticsSpy, waitFor } from '../../util';
 
 import { INACTIVE } from '../../../src/common/productProvisioningStates';
 
-const noop = () => {};
+describe('<ConfirmTrial> analytics', () => {
+  const noop = () => {};
 
-const defaultProps = {
-  productLogo: <div />,
-  onComplete: noop,
-  onCancel: noop,
-  status: INACTIVE,
-  trialHeading: '',
-  trialMessage: '',
-  reactivateHeading: '',
-  reactivateMessage: '',
-};
+  const defaultProps = {
+    productLogo: <div />,
+    onComplete: noop,
+    onCancel: noop,
+    status: INACTIVE,
+    trialHeading: '',
+    trialMessage: '',
+    reactivateHeading: '',
+    reactivateMessage: '',
+  };
 
-test('ConfirmTrial should fire an appropriate analytics event when it is mounted', () => {
-  const spy = jest.fn();
-  mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith('xflow.confirm-trial.displayed', expect.any(Object))
-  );
-});
+  it('should fire an appropriate analytics event when it is mounted', () => {
+    const spy = jest.fn();
+    mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith('xflow.confirm-trial.displayed', expect.any(Object))
+    );
+  });
 
-test('ConfirmTrial should fire an appropriate analytics event when the confirm button is clicked', () => {
-  const spy = jest.fn();
-  const mountWrapper = mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
-  mountWrapper.find('#xflow-confirm-trial-confirm-button').simulate('click');
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith(
-      'xflow.confirm-trial.confirm-button.clicked',
-      expect.any(Object)
-    )
-  );
-});
+  it('should fire an appropriate analytics event when the confirm button is clicked', () => {
+    const spy = jest.fn();
+    const mountWrapper = mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
+    mountWrapper.find('#xflow-confirm-trial-confirm-button').simulate('click');
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith(
+        'xflow.confirm-trial.confirm-button.clicked',
+        expect.any(Object)
+      )
+    );
+  });
 
-test('ConfirmTrial should fire an appropriate analytics event when the cancel button is clicked', () => {
-  const spy = jest.fn();
-  const mountWrapper = mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
-  mountWrapper.find('#xflow-confirm-trial-cancel-button').simulate('click');
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith(
-      'xflow.confirm-trial.cancel-button.clicked',
-      expect.any(Object)
-    )
-  );
+  it('should fire an appropriate analytics event when the cancel button is clicked', () => {
+    const spy = jest.fn();
+    const mountWrapper = mount(withAnalyticsSpy(spy, <ConfirmTrialBase {...defaultProps} />));
+    mountWrapper.find('#xflow-confirm-trial-cancel-button').simulate('click');
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith(
+        'xflow.confirm-trial.cancel-button.clicked',
+        expect.any(Object)
+      )
+    );
+  });
 });
--- packages/xflow/test/unit/analytics/LoadingTimeSpec.jsx
+++ packages/xflow/test/unit/analytics/LoadingTimeSpec.jsx
@@ -4,65 +4,68 @@ import { LoadingTimeBase } from '../../../src/request-or-start-trial/components/
 import { withAnalyticsSpy, waitFor } from '../../util';
 import { ACTIVATING, ACTIVE } from '../../../src/common/productProvisioningStates';
 
-const noop = () => {};
+describe('<LoadingTime> analytics', () => {
+  const noop = () => {
+  };
 
-const defaultProps = {
-  productLogo: <div />,
-  onComplete: noop,
-  goToProduct: noop,
-  progress: 0,
-  status: ACTIVATING,
-  heading: '',
-};
+  const defaultProps = {
+    productLogo: <div />,
+    onComplete: noop,
+    goToProduct: noop,
+    progress: 0,
+    status: ACTIVATING,
+    heading: '',
+  };
 
-test('LoadingTime should fire an appropriate analytics event when it is mounted', () => {
-  const spy = jest.fn();
-  mount(withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} />));
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith('xflow.loading-product-trial.displayed', expect.any(Object))
-  );
-});
+  it('should fire an appropriate analytics event when it is mounted', () => {
+    const spy = jest.fn();
+    mount(withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} />));
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith('xflow.loading-product-trial.displayed', expect.any(Object))
+    );
+  });
 
-test('LoadingTime should fire an appropriate analytics event when loading timed out', () => {
-  const spy = jest.fn();
-  mount(withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} progress={100} />));
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith('xflow.loading-product-trial.timed.out', expect.any(Object))
-  );
-});
+  it('should fire an appropriate analytics event when loading timed out', () => {
+    const spy = jest.fn();
+    mount(withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} progress={100} />));
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith('xflow.loading-product-trial.timed.out', expect.any(Object))
+    );
+  });
 
-test('LoadingTime should fire an appropriate analytics event when loading finished', () => {
-  const spy = jest.fn();
-  mount(
-    withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} progress={100} status={ACTIVE} />)
-  );
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith(
-      'xflow.loading-product-trial.loading.finished',
-      expect.any(Object)
-    )
-  );
-});
+  it('should fire an appropriate analytics event when loading finished', () => {
+    const spy = jest.fn();
+    mount(
+      withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} progress={100} status={ACTIVE} />)
+    );
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith(
+        'xflow.loading-product-trial.loading.finished',
+        expect.any(Object)
+      )
+    );
+  });
 
-test('LoadingTime should fire an appropriate analytics event the close button is clicked', () => {
-  const spy = jest.fn();
-  const mountWrapper = mount(withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} />));
-  mountWrapper.find('#xflow-loading-close-button').simulate('click');
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith('xflow.loading-product-trial.close', expect.any(Object))
-  );
-});
+  it('should fire an appropriate analytics event the close button is clicked', () => {
+    const spy = jest.fn();
+    const mountWrapper = mount(withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} />));
+    mountWrapper.find('#xflow-loading-close-button').simulate('click');
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith('xflow.loading-product-trial.close', expect.any(Object))
+    );
+  });
 
-test('LoadingTime should fire an appropriate analytics event the go to product button is clicked', () => {
-  const spy = jest.fn();
-  const mountWrapper = mount(
-    withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} progress={100} status={ACTIVE} />)
-  );
-  mountWrapper.find('#xflow-loading-go-to-product-button').simulate('click');
-  return waitFor(() =>
-    expect(spy).toHaveBeenCalledWith(
-      'xflow.loading-product-trial.go.to.product',
-      expect.any(Object)
-    )
-  );
+  it('should fire an appropriate analytics event the go to product button is clicked', () => {
+    const spy = jest.fn();
+    const mountWrapper = mount(
+      withAnalyticsSpy(spy, <LoadingTimeBase {...defaultProps} progress={100} status={ACTIVE} />)
+    );
+    mountWrapper.find('#xflow-loading-go-to-product-button').simulate('click');
+    return waitFor(() =>
+      expect(spy).toHaveBeenCalledWith(
+        'xflow.loading-product-trial.go.to.product',
+        expect.any(Object)
+      )
+    );
+  });
 });

091388a2847f2960d416d0a5d89ac167c3d650e4
awakeling <awakeling@atlassian.com>
2017-11-21 16:16:08 +1100

    fix(component): issue/MEGATRON-391: Conditionally use staging xflow url, if url is "localhost" or en
    affects: @atlaskit/xflow

--- packages/xflow/src/common/services/xflowService.js
+++ packages/xflow/src/common/services/xflowService.js
@@ -1,6 +1,15 @@
-function getXFlowEndPoint() {
-  return process.env.NODE_ENV !== 'development' ? 'https://api-private.atlassian.com/xflow' : 'https://api-private.stg.atlassian.com/xflow';
-}
+export const XFLOW_PROD_URL = 'https://api-private.atlassian.com/xflow';
+export const XFLOW_STAG_URL = 'https://api-private.stg.atlassian.com/xflow';
+
+export const getXFlowEndPoint = (w = window) => {
+  let isProd = true;
+  try {
+    isProd = !(w.location.hostname.endsWith('jira-dev.com') || w.location.hostname === 'localhost');
+  } catch (e) {
+    // silence
+  }
+  return isProd ? XFLOW_PROD_URL : XFLOW_STAG_URL;
+};
 
 export const notifyAccessEndpoint = () =>
   `${getXFlowEndPoint()}/notify-users-of-product-access`;

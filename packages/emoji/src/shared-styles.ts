45d7bb9d3111959c7b5e6a13e00cfb78c49c7f8a
Sylvain Guillope <sguillope@atlassian.com>
2017-11-28 10:41:34 +1100

    fix(package): use theme package instead of util-shared-styles
    affects: @atlaskit/emoji
    
    ISSUES CLOSED: FS-1526

--- packages/emoji/src/shared-styles.ts
+++ packages/emoji/src/shared-styles.ts
@@ -1,24 +1,23 @@
 import {
-  akBorderRadius,
-  akColorN30,
-  akColorN40,
-} from '@atlaskit/util-shared-styles';
+  borderRadius,
+  colors,
+} from '@atlaskit/theme';
 
 import { emojiPickerWidth } from './constants';
 
 export const emojiPickerListWidth = emojiPickerWidth;
 export const emojiPickerListHeight = 205;
 
-export const emojiPickerBorderColor = akColorN40; // This has not been confirmed by the ADG yet
+export const emojiPickerBorderColor = colors.N40; // This has not been confirmed by the ADG yet
 export const emojiPickerBoxShadow = '0 3px 6px rgba(0, 0, 0, 0.2)';
 export const emojiFooterBoxShadow = '0px -1px 1px 0px rgba(0, 0, 0, 0.1)';
 
-export const noDialogContainerBorderColor = akColorN40; // This has not been confirmed by the ADG yet
-export const noDialogContainerBorderRadius = akBorderRadius;
+export const noDialogContainerBorderColor = colors.N40; // This has not been confirmed by the ADG yet
+export const noDialogContainerBorderRadius = borderRadius();
 export const noDialogContainerBoxShadow = '0 3px 6px rgba(0, 0, 0, 0.2)';
 
-export const akEmojiSelectedBackgroundColor = akColorN30;
-export const emojiPreviewSelectedColor = akColorN30;
+export const akEmojiSelectedBackgroundColor = colors.N30;
+export const emojiPreviewSelectedColor = colors.N30;
 
 export const emojiTypeAheadMaxHeight = 350;
 export const emojiTypeAheadWidth = 350;

326b65d71a447cc84f47982bc86fd1756ccfaa8b
Ben Conolly <ben@thinkmill.com.au>
2017-11-27 17:11:43 +1100

    refactor(component): breadcrumbs deleted, recreated in new repo

--- packages/breadcrumbs/docs/components.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = [
-  { name: 'Breadcrumbs', src: path.join(__dirname, '../src/components/BreadcrumbsStateless.jsx') },
-  { name: 'BreadcrumbsItem', src: path.join(__dirname, '../src/components/BreadcrumbsItem.jsx') },
-];

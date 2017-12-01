4ece99b2d6df81b586553c6ebc20ecc047d08d52
Jack Field <jfield@atlassian.com>
2017-12-01 13:40:21 +1100

    test(stories): adding story for initialization dialog with header
    affects: @atlaskit/xflow

--- packages/xflow/stories/120-InitializingScreen-story.jsx
+++ packages/xflow/stories/120-InitializingScreen-story.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { storiesOf } from '@kadira/storybook';
 import InitializingScreen from '../src/common/components/InitializingScreen';
+import mockXFlowProviderFactory from './helpers/mockXFlowProviderFactory';
+import JiraToJSWXFlowProvider from '../src/product-xflow-providers/JiraToJSWXFlowProvider';
+
+const MockXFlowProvider = mockXFlowProviderFactory(JiraToJSWXFlowProvider);
 
 storiesOf('common/InitializingScreen')
   .add('raw', () => <InitializingScreen isOpen />)
+  .add('with header', () => (
+    <MockXFlowProvider>
+      <InitializingScreen isOpen />
+    </MockXFlowProvider>
+  ))
   .add('real case (2s loading)', () => <TestComponent />);
 
 class TestComponent extends Component {

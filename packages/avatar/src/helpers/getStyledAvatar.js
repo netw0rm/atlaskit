502103e442d2a2944f26e628d7cbc7f08a3b87a9
Alex Reardon <areardon@atlassian.com>
2017-12-05 11:07:01 +1100

    chore(component): removing avatar from old repo so it can be moved into the new repo

--- packages/avatar/src/helpers/getStyledAvatar.js
+++
@@ -1,4 +0,0 @@
-import styledCache from './styledCache';
-import { getInnerStyles } from '../styled/utils';
-
-export default styledCache(getInnerStyles);
--- packages/avatar/src/helpers/getStyledAvatarItem.js
+++
@@ -1,4 +0,0 @@
-import styledCache from './styledCache';
-import { getStyles } from '../styled/AvatarItem';
-
-export default styledCache(getStyles);

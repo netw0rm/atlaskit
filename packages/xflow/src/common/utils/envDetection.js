23328bcc143ecff1a002088a8665c28584a93b7a
Max Puckeridge <mpuckeridge@atlassian.com>
2017-12-07 11:48:40 +1100

    fix(component): fix environment detection in IE11
    affects: @atlaskit/xflow

--- packages/xflow/src/common/utils/envDetection.js
+++ packages/xflow/src/common/utils/envDetection.js
@@ -1,10 +1,12 @@
 export const API_PROD_URL = 'https://api-private.atlassian.com';
 export const API_STAG_URL = 'https://api-private.stg.atlassian.com';
 
+export const JIRA_DEV = '.jira-dev.com';
+
 export function getEnvAPIUrl(w = window) {
   let isProd = true;
   try {
-    isProd = !(w.location.hostname.endsWith('jira-dev.com') || w.location.hostname === 'localhost');
+    isProd = !(w.location.hostname.slice(-JIRA_DEV.length) === JIRA_DEV || w.location.hostname === 'localhost');
   } catch (e) {
     // silence
   }

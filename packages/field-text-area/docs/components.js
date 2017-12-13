319482834f31daca063d5677cee27990e5a852c3
Mike Gardiner <mgardiner@atlassian.com>
2017-12-13 14:56:13 +1100

    chore(component): Migrated Field Text Area to new repo. remove from here

--- packages/field-text-area/docs/components.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = [
-  { name: 'Text Field Area', src: path.join(__dirname, '../src/components/FieldTextArea.jsx') },
-  { name: 'FieldTextAreaStateless', src: path.join(__dirname, '../src/components/FieldTextAreaStateless.jsx') },
-];
--- packages/field-text-area/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import FieldTextArea from './components/FieldTextArea';
-import FieldTextAreaStateless from './components/FieldTextAreaStateless';
-
-export default FieldTextArea;
-export { FieldTextAreaStateless };

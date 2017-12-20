996ca77b1df5f56de27401546c0367cc59621534
Ben Conolly <ben@thinkmill.com.au>
2017-12-20 12:47:14 +1100

    refactor(component): remove icon and reduced ui pack

--- packages/icon/build/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Relative to this directory
-  srcDir: '../icons',
-  processedDir: '../icons/processed',
-  destDir: '../glyph',
-  maxWidth: 24,
-  maxHeight: 24,
-};
--- packages/icon/src/index.jsx
+++
@@ -1,1 +0,0 @@
-export { default, size } from './components/Icon';

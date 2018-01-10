81fbe8fc9b513859edb0d835aaa83abe32169eb8
= <clee@atlassian.com>
2018-01-11 09:43:53 +1100

    chore(dummy): remove from ak repo
    Remove multi-select from ak repo in prep for migration to ak mk 2 repo

--- packages/multi-select/docs/USAGE.md
+++
@@ -1,5 +0,0 @@
-# MultiSelect
-
-## Try it out
-
-Detailed docs and example usage can be found [here](https://aui-cdn.atlassian.com/atlaskit/stories/@atlaskit/multi-select/@VERSION@/).
--- packages/multi-select/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import MultiSelect from './components/Stateful';
-import MultiSelectStateless from './components/Stateless';
-
-export default MultiSelect;
-export { MultiSelectStateless };

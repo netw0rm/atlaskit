abb3c33dd8dcbe163b7a0b53012b72d7e1a59ec4
Sean Curtis <scurtis@atlassian.com>
2018-01-22 11:24:02 +1100

    refactor(component): removing packages that have been migrated already.

--- packages/spotlight/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export Spotlight from './Spotlight';
-export SpotlightManager from './SpotlightManager';
-export SpotlightRegistry from './SpotlightRegistry';
-export SpotlightTarget from './SpotlightTarget';

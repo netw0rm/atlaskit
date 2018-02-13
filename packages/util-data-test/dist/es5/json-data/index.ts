d03b1609d80a36c0a662ad90c9ac3bebc3031336
ttjandra <ttjandra@atlassian.com>
2018-02-14 09:22:25 +1100

    feat(component): migrated json data back to util-data-test
    affects: @atlaskit/util-data-test

---
+++ packages/util-data-test/dist/es5/json-data/index.ts
@@ -0,0 +1,4 @@
+/**
+ * This file is used in development with lerna
+ */
+export * from '../../../src';
--- packages/util-data-test/src/index.ts
+++ packages/util-data-test/src/index.ts
@@ -1,7 +1,13 @@
 import profilecard from './profilecard';
+import mention from './mention';
+import emoji from './emoji';
+import taskDecision from './task-decision';
 
 export {
   profilecard,
+  mention,
+  emoji,
+  taskDecision
 };
 
 export default {};

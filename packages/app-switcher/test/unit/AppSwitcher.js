8e280b55e128a57fa5caa4e1a6f122b5ba950958
Jack Field <jfield@atlassian.com>
2018-02-14 11:42:37 +1100

    test(component): add test for linked application labels
    affects: @atlaskit/app-switcher

--- packages/app-switcher/test/unit/AppSwitcher.js
+++ packages/app-switcher/test/unit/AppSwitcher.js
@@ -3,6 +3,7 @@ import { shallow, mount } from 'enzyme';
 import React from 'react';
 import sinon from 'sinon';
 import { DropdownMenuStateless } from '@atlaskit/dropdown-menu';
+import Lozenge from '@atlaskit/lozenge';
 import AppSwitcher from '../../src';
 import { name } from '../../package.json';
 
@@ -142,5 +143,33 @@ describe(name, () => {
       />
     );
     expect(wrapper.find('.app-switcher-suggested-application')).toHaveLength(0);
+  });
+
+  it('should show linked application labels', () => {
+    const label = '7 Days Left';
+    const linkedApplications = {
+      ...data.linkedApplications,
+      apps: [
+        {
+          name: 'JIRA',
+          url: 'https://instance.atlassian.net/',
+          product: 'jira',
+        },
+        {
+          name: 'Confluence',
+          url: 'https://instance.atlassian.net/wiki',
+          product: 'confluence',
+          label,
+        },
+      ],
+    };
+    const wrapper = mount(
+      <AppSwitcher
+        {...data}
+        isDropdownOpenInitially
+        linkedApplications={linkedApplications}
+      />
+    );
+    expect(wrapper.find(Lozenge).text()).toBe(label);
   });
 });

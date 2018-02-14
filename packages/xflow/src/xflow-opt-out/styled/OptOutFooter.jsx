c32b29a73db1a6d6a8214478ae0a47c8cc402eaa
Lachlan Hunt <lhunt@atlassian.com>
2018-02-14 16:56:55 +1100

    fix(component): fix opt-out dialog to work with new @atlaskit/modal-dialog
    affects: @atlaskit/xflow

--- packages/xflow/src/xflow-opt-out/styled/OptOutFooter.jsx
+++ packages/xflow/src/xflow-opt-out/styled/OptOutFooter.jsx
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
-import { gridSize } from '@atlaskit/theme';
-
 const OptOutFooter = styled.div`
-  margin-bottom: ${gridSize}px;
-  margin-right: ${gridSize}px;
   text-align: right;
+  padding: 14px 20px 20px;
 `;
 
 OptOutFooter.displayName = 'OptOutFooter';

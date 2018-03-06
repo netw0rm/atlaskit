a193187827dd97bc082bf302b2ff0ee9eb373ba1
Max Puckeridge <mpuckeridge@atlassian.com>
2018-03-06 13:38:16 +1100

    fix(component): use /gateway/api/ even on localhost. Use storage to override if you must.
    affects: @atlaskit/xflow

--- packages/xflow/src/common/utils/envDetection.js
+++ packages/xflow/src/common/utils/envDetection.js
@@ -1,18 +1,9 @@
 export const API_URL = '/gateway/api';
-export const API_DEV_URL = 'https://api-private.stg.atlassian.com';
 
 export function getEnvAPIUrl(w = window, s = window.sessionStorage) {
   // Necessary for facilitating mocked servers and testing
   if (s && s.getItem('xflowEnvAPIUrlOverride')) {
     return s.getItem('xflowEnvAPIUrlOverride');
   }
-
-  let isLocalhost = false;
-  try {
-    isLocalhost = w.location.hostname === 'localhost';
-  } catch (e) {
-    // silence
-  }
-
-  return isLocalhost ? API_DEV_URL : API_URL;
+  return API_URL;
 }
--- packages/xflow/test/unit/common/utils/envDetectionSpec.js
+++ packages/xflow/test/unit/common/utils/envDetectionSpec.js
@@ -1,32 +1,9 @@
-import { API_URL, API_DEV_URL, getEnvAPIUrl } from '../../../../src/common/utils/envDetection';
+import { API_URL, getEnvAPIUrl } from '../../../../src/common/utils/envDetection';
 
 describe('envDetection', () => {
   describe('getEnvAPIUrl', () => {
-    it('should return the staging API URL if the hostname is localhost', () => {
-      const mockWindow = {
-        location: {
-          hostname: 'localhost',
-        },
-      };
-      expect(getEnvAPIUrl(mockWindow)).toEqual(API_DEV_URL);
-    });
-
-    it('should return the production API URL if the hostname ends with jira-dev.com', () => {
-      const mockWindow = {
-        location: {
-          hostname: 'xflow.jira-dev.com',
-        },
-      };
-      expect(getEnvAPIUrl(mockWindow)).toEqual(API_URL);
-    });
-
-    it('should return the production API URL if the hostname ends with atlassian.net', () => {
-      const mockWindow = {
-        location: {
-          hostname: 'xflow.atlassian.net',
-        },
-      };
-      expect(getEnvAPIUrl(mockWindow)).toEqual(API_URL);
+    it('should return the API URL if no session storage URL override exists', () => {
+      expect(getEnvAPIUrl({})).toEqual(API_URL);
     });
 
     it('should return the session storage URL override if present', () => {

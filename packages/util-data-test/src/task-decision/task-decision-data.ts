f6fa165c44ad847b21b465fa3da11f9ebb3e6822
ttjandra <ttjandra@atlassian.com>
2018-03-27 09:44:38 +1100

    refactor(component): fS-1697 fix builds
    affects: @atlaskit/util-data-test

--- packages/util-data-test/src/task-decision/task-decision-data.ts
+++
@@ -1,9 +0,0 @@
-import { ServiceDecisionResponse, ServiceItemResponse, ServiceTaskResponse } from '@atlaskit/task-decision';
-
-declare var require: {
-  <T>(path: string): T;
-};
-
-export const getServiceDecisionsResponse = (): ServiceDecisionResponse => require('../json-data/sample-decisions.json') as ServiceDecisionResponse;
-export const getServiceTasksResponse = (): ServiceTaskResponse => require('../json-data/sample-tasks.json') as ServiceTaskResponse;
-export const getServiceItemsResponse = (): ServiceItemResponse => require('../json-data/sample-elements.json') as ServiceItemResponse;

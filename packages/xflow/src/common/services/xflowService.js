ab16d26af41b6fa5f819872cdac6941d03aa43f0
Mario Maradiaga <mmaradiaga@atlassian.com>
2018-04-11 14:22:07 +1000

    feat(component): updated activate product endpoint to target xflow service
    affects: @atlaskit/xflow
    
    Currently the xflow component was requesting the product activation to a
    billing API. Instead we're
    now pointing to an endpoint in the xflow service.
    
    ISSUES CLOSED: EUE-261

--- packages/xflow/src/common/services/xflowService.js
+++ packages/xflow/src/common/services/xflowService.js
@@ -2,11 +2,13 @@ import { getEnvAPIUrl } from '../utils/envDetection';
 
 export const getXFlowEndPoint = () => `${getEnvAPIUrl()}/xflow`;
 
-export const notifyAccessEndpoint = (cloudId) =>
+export const notifyAccessEndpoint = cloudId =>
   `${getXFlowEndPoint()}/${cloudId}/notify-users-of-product-access`;
 
 export const productRequestEndpoint = cloudId =>
   `${getXFlowEndPoint()}/${cloudId}/request-admins-for-product-trial`;
 
-export const licenseInformationEndpoint = (cloudId) =>
+export const licenseInformationEndpoint = cloudId =>
   `${getXFlowEndPoint()}/${cloudId}/license-information`;
+
+export const startTrialEndpoint = cloudId => `${getXFlowEndPoint()}/${cloudId}/activate-product`;

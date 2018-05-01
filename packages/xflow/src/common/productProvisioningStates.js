4bc0e4173b3d6cf32e9bb8af0ccd4bb74180ffd6
Lachlan Hunt <lhunt@atlassian.com>
2018-05-01 16:53:18 +1000

    feat(component): remove @atlaskit/xflow, superseded by @atlassiansox/xflow-ui

--- packages/xflow/src/common/productProvisioningStates.js
+++
@@ -1,5 +0,0 @@
-export const ACTIVE = Symbol('ACTIVE');
-export const ACTIVATING = Symbol('ACTIVATING');
-export const INACTIVE = Symbol('INACTIVE');
-export const DEACTIVATED = Symbol('DEACTIVATED');
-export const UNKNOWN = Symbol('UNKNOWN');
--- packages/xflow/src/common/utils/envDetection.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = '/gateway/api';
-
-export function getEnvAPIUrl(w = window, s = window.sessionStorage) {
-  // Necessary for facilitating mocked servers and testing
-  if (s && s.getItem('xflowEnvAPIUrlOverride')) {
-    return s.getItem('xflowEnvAPIUrlOverride');
-  }
-  return API_URL;
-}
